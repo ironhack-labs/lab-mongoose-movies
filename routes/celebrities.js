24d6c1c65c73dd684b897d30f80eca03d34da552
Anthony Bruscantini <ajbruscantini@gmail.com>
2017-02-10 11:43:23 +0100

    iteration 4 done.

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -6,6 +6,10 @@ const Celebrity = require('../models/celebrity');
 
 const router = express.Router();
 
+router.get('/new', (req, res, next) => {
+  res.render('celebrities/new');
+});
+
 router.get('/:id', (req, res, next) => {
   celebrityId = req.params.id;
   Celebrity.findById(celebrityId, (err, celebrity) =>{
@@ -18,6 +22,21 @@ router.get('/', (req, res, next) => {
   Celebrity.find({}, (err, docs) => {
     if (err) return next(err);
     res.render('celebrities/index', {celebs: docs});
+  });
+});
+
+router.post('/', (req, res, next) => {
+  const body = req.body;
+  const celebToCreate = {
+    name : body.name,
+    occupation : body.occupation,
+    catchPhrase : body.catchPhrase
+  };
+
+  const newCeleb = new Celebrity(celebToCreate);
+  newCeleb.save((err) =>{
+    if (err) res.render('celebrities/new');
+    res.redirect('/');
   });
 });
 
