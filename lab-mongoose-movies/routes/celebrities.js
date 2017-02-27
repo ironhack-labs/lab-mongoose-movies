4a074036d5efde7539c6f75fbfedd6a32494b7dd
Jeremy Beagle <beaglej2012@fau.edu>
2017-02-27 14:44:39 -0500

    Iteration 4 completed

--- lab-mongoose-movies/routes/celebrities.js
+++ lab-mongoose-movies/routes/celebrities.js
@@ -20,6 +20,30 @@ router.get('/celebrities', (req, res, next) => {
 
 });
 
+router.get('/celebrities/new', (req, res, next) => {
+  res.render('celebrities/new');
+});
+
+router.post('/celebrities', (req, res, next) => {
+  console.log('In the post');
+  const celebrityInfo = {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase
+  };
+
+  const newCelebrity = new CelebrityModel(celebrityInfo);
+
+  newCelebrity.save((err) => {
+    console.log('in the save cb');
+    if (err) {
+      res.render('/celebrities/new');
+    }
+
+    res.redirect('/celebrities');
+  });
+});
+
 router.get('/celebrities/:id', (req, res, next) => {
   const celebrityId = req.params.id;
 
@@ -36,5 +60,7 @@ router.get('/celebrities/:id', (req, res, next) => {
     });
   });
 });
+
+
 
 module.exports = router;
