0d9c1ad06ecb4d4f2a6a8d8c2f3a3fffc4e529b5
Andreas Karpasitis <karpas@Andreass-MacBook-Pro.local>
2017-04-27 21:59:57 +0200

    done

--- moviesproject/app.js
+++ moviesproject/app.js
@@ -15,6 +15,7 @@ mongoose.connect('mongodb://localhost/celebrities');
 var index = require('./routes/index');
 var users = require('./routes/users');
 const celebrities = require('./routes/celebrities');
+const movies = require('./routes/movies');
 
 var app = express();
 
@@ -33,6 +34,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/celebrities', celebrities);
+app.use('/movies', movies);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- moviesproject/bin/seeds.js
+++ moviesproject/bin/seeds.js
@@ -1,33 +1,60 @@
 /*jshint esversion: 6*/
 const mongoose = require('mongoose');
 
-const Celebrity = require('../models/celebrities');
+const Movie = require('../models/movies');
 
 mongoose.connect('mongodb://localhost:27017/celebrities');
 
-const Celebrities = [
+// const Celebrities = [
+//   {
+//     name: 'Christopher Walken',
+//     occupation: 'Actor',
+//     catchPhrase: 'Just listen to this'
+//   },
+//   {
+//     name: 'Mathew McConnahey',
+//     occupation: 'Actor',
+//     catchPhrase: 'Alright, alright, alright'
+//   },
+//   {
+//     name: 'Tom Cruise',
+//     occupation: 'Actor',
+//     catchPhrase: 'Awesome'
+//   }];
+//
+// Celebrity.create(Celebrities, (err, docs)=> {
+//   if(err) {
+//     throw err;
+//   }
+//   docs.forEach((celebrity)=>{
+//     console.log(celebrity.name);
+//   });
+//   mongoose.connection.close();
+// });
+
+const Movies = [
   {
-    name: 'Christopher Walken',
-    occupation: 'Actor',
-    catchPhrase: 'Just listen to this'
+    title: 'The Attack of the Sheep',
+    genre: 'Thriller',
+    plot: 'A herd of sheep attack a lonely bicycle rider on the way to his AA meeting.'
   },
   {
-    name: 'Mathew McConnahey',
-    occupation: 'Actor',
-    catchPhrase: 'Alright, alright, alright'
+    title: 'Blue Sun',
+    genre: 'Sci-Fi',
+    plot: 'One quiet day, the sun suddenly turns blue while a couple is vacationing in Phuket Thailand.'
   },
   {
-    name: 'Tom Cruise',
-    occupation: 'Actor',
-    catchPhrase: 'Awesome'
+    title: 'Goodbye Yesterday',
+    genre: 'Drama',
+    plot: 'A woman remembers that a man did not say goodbye to her, the previous day. She is on a mission to find him and kill him.'
   }];
 
-Celebrity.create(Celebrities, (err, docs)=> {
+Movie.create(Movies, (err, docs)=> {
   if(err) {
     throw err;
   }
-  docs.forEach((celebrity)=>{
-    console.log(celebrity.name);
+  docs.forEach((movie)=>{
+    console.log(movie.title);
   });
   mongoose.connection.close();
 });
