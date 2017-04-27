39425f4531c7738d4e267a4a5214273dd3fae156
Isak La Fleur <isak.engdahl@gmail.com>
2017-04-27 17:51:04 +0200

    fixed the edit link

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
 mongoose.connect('mongodb://localhost:27017/mongoose-movies');
 
 const index = require('./routes/index');
@@ -16,12 +15,9 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
--- routes/celebrities/index.js
+++ routes/celebrities/index.js
@@ -6,7 +6,30 @@ const router = express.Router();
 
 /* Display the new celebrity form */
 router.get('/new', (req, res) => {
-  res.render('./celebrities/new');
+  res.render('celebrities/new');
+});
+
+
+
+/* Show all celebrities */
+router.get('/', (req, res, next) => {
+  Celebrity.find({}, (err, celebrities) => {
+    if (err) {
+      next(err);
+    }
+    res.render('celebrities', { celebrities });
+  });
+});
+
+/* Show a specific celebrity */
+router.get('/:celebrityId', (req, res, next) => {
+  const celebrityId = req.params.celebrityId;
+  Celebrity.findById(celebrityId, (err, celebrity) => {
+    if (err) {
+      next(err);
+    }
+    res.render('celebrities/show', { celebrity });
+  });
 });
 
 /* Add a new celebrity to the database */
@@ -20,32 +43,41 @@ router.post('/', (req, res, next) => {
   const newCelebrity = new Celebrity(celebrityInfo);
   newCelebrity.save((err) => {
     if (err) {
-      return res.render('./celebrities/new', { errors: newCelebrity.errors });
+      return res.render('celebrities/new', { errors: newCelebrity.errors });
     }
-    return res.redirect('./celebrities');
+    return res.redirect('celebrities');
   });
 });
 
-/* Show all celebrities */
-router.get('/', (req, res, next) => {
-  Celebrity.find({}, (err, celebrities) => {
-    if (err) {
-      next(err);
-    }
-    res.render('./celebrities/', { celebrities });
+/* Delete Route: */
+router.get('/:id/delete', (req, res, next) => {
+  const celebrityId = req.params.id;
+  Celebrity.findByIdAndRemove(celebrityId, (err) => {
+    if (err) { next(err); }
+    return res.redirect('/celebrities');
   });
 });
 
-/* Show a specific celebrity */
-router.get('/:celebrityId', (req, res, next) => {
-  const celebrityID = req.params.celebrityId;
-  Celebrity.findById(celebrityID, (err, celebrity) => {
-    if (err) {
-      next(err);
-    }
-    res.render('./celebrities/show', { celebrity });
+/* Editing Celebrities */
+router.get('/:id/edit', (req, res, next) => {
+  const celebrityId = req.params.id;
+  Celebrity.findById(celebrityId, (err, celebrity) => {
+    if (err) { next(err); }
+    res.render('celebrities/edit', { celebrity });
   });
 });
 
+router.post('/:id', (req, res, next) => {
+  const productId = req.params.id;
+  const updates = {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase,
+  };
+  Celebrity.findByIdAndUpdate(productId, updates, (err, product) => {
+    if (err) { return next(err); }
+    return res.redirect('celebrities');
+  });
+});
 
 module.exports = router;
