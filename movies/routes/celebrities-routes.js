fa6ebdb5dba42fb38f2bc2e598928d5b1c69d075
Adel Acebal <adelacebal@yahoo.com>
2017-04-29 23:14:23 -0400

    4 iterations done

--- movies/routes/celebrities-routes.js
+++ movies/routes/celebrities-routes.js
@@ -16,6 +16,34 @@ celebritiesRoutes.get('/celebrities', (req, res, next) => {
   });
 });
 
+celebritiesRoutes.get('/celebrities/new', (req, res, next) => {
+  res.render('celebrities/celebrity-new-view.ejs');
+});
+
+celebritiesRoutes.post('/celebrities/new', (req, res, next) => {
+  const theCelebrity = new Celebrity({
+    name: req.body.celebrityName,
+    occupation: req.body.celebrityOccupation,
+    catchPhrase: req.body.celebrityCatchPhrase,
+  });
+  theCelebrity.save((err) => {
+    if(err){
+      res.render('products/celebrity-new-view.ejs', {
+        errors: theCelebrity.errors
+      });
+      return;
+    }
+
+    res.redirect('/celebrities');
+  });
+});
+
+
+
+
+
+
+
 celebritiesRoutes.get('/celebrities/:id', (req, res, next) => {
   const celebrityId = req.params.id;
 
