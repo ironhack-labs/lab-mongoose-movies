1ffd8ff95c54700ade286a413f8283b802e765a5
Alejandro <ajimenez1724@gmail.com>
2017-05-02 15:16:22 -0400

    iteration #6 ready

--- movies/routes/celebrities.js
+++ movies/routes/celebrities.js
@@ -56,6 +56,64 @@ celebrityModel.findById( myCelebrityId, (err, thatCelebrity) => {
   });
 });
 
+//UPDATE
+
+celebrityRoutes.get('/celebrities/:id/edit', (req, res, next) => {
+    //                         |
+  const celebrityId = req.params.id;
+
+  celebrityModel.findById(celebrityId, (err, thatCelebrity) => {
+    if (err) {
+      next(err);
+      return;
+    }
+
+    res.render('celebrities/edit', {
+      celebrity: thatCelebrity
+    });
+  });
+});
+
+celebrityRoutes.post('/celebrities/:id', (req, res, next) => {
+    //                          |
+  const celebrityId = req.params.id;
+
+    // gather the form values into an object
+  const celebrityChanges = {
+    name: req.body.celebrityName,
+    ocupation: req.body.celebrityOcupation,
+    catchPhrase: req.body.celebritycatchPhrase,
+    profileImage: req.body.celebrityImageUrl
+  };
+
+  celebrityModel.findByIdAndUpdate(
+      // 1st arg -> which document (id of the document)
+    celebrityId,
+      // 2nd arg -> which changes to save (from the form)
+    celebrityChanges,
+      // 3rd arg -> CALLBACK!
+    (err, thatCelebrity) => {
+      if (err) {
+        res.render('celebrities/edit', {
+          celebrity: thatCelebrity,
+          validationErrors: thatCelebrity.errors
+        });
+        return;
+      }
+
+      // this is how you would redirect to product details page
+      // res.redirect(`/products/${celebrityId}`);
+
+      res.redirect('/celebrities');
+    }
+  );
+});
+
+
+
+
+// DESTROY
+
 celebrityRoutes.post('/celebrities/:id/delete', (req,res, next) => {
   const celebrityId= req.params.id;
 
