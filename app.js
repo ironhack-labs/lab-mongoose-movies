cbd9748a20c99d1890f97bfaeee7ae82556426fd
artseven <yabkevich.artsiom.09@gmail.com>
2017-05-04 17:16:28 -0400

    Finished

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Express app';
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/movies.js
+++ routes/movies.js
@@ -16,6 +16,29 @@ movieRoutes.get('/movies', (req, res, next) => {
   });
 });
 
+
+
+movieRoutes.get('/movies/new', (req, res, next) => {
+  res.render('movies/new-movie.ejs');
+});
+
+movieRoutes.post('/movies/new', (req, res, next) => {
+  const theMovie = new Movie({
+    title: req.body.movieTitle,
+    genre: req.body.movieGenre,
+    plot: req.body.moviePlot
+  });
+  theMovie.save((err) => {
+    if (err) {
+      res.render('movies/new-movie.ejs', {
+        errors: theMovie.errors
+      });
+      return;
+    }
+    res.redirect('/movies');
+  });
+});
+
 movieRoutes.get('/movies/:id', (req, res, next) => {
   const movieId = req.params.id;
 
@@ -35,5 +58,61 @@ movieRoutes.get('/movies/:id', (req, res, next) => {
     });
   });
 });
+
+movieRoutes.get('/movies/:id/edit', (req, res, next) => {
+  const movieId = req.params.id;
+
+  Movie.findById(movieId, (err, theMovie) => {
+    if (err) {
+      next(err);
+      return;
+    }
+
+    res.render('movies/movie-edit.ejs', {
+      movie: theMovie
+    });
+  });
+});
+
+movieRoutes.post('/movies/:id', (req, res, next) => {
+  const movieId = req.params.id;
+
+  const movieChanges = {
+    title: req.body.movieTitle,
+    genre: req.body.movieGenre,
+    plot: req.body.moviePlot
+  };
+
+  Movie.findByIdAndUpdate(
+    movieId,
+    movieChanges,
+    (err, theMovie) => {
+      if (err) {
+        res.render('movies/movie-edit.ejs', {
+          movie: theMovie
+        });
+        next(err);
+        return;
+      }
+
+      res.redirect('/movies');
+    }
+  );
+});
+
+movieRoutes.post('/movies/:id/delete', (req, res, next) => {
+  const movieId = req.params.id;
+
+  Movie.findByIdAndRemove(movieId, (err, theMovie) => {
+    if(err) {
+      next(err);
+      return;
+    }
+    res.redirect('/movies');
+  });
+});
+
+
+
 
 module.exports = movieRoutes;
