d05b3006d679b975b66183587ce12f2cd1511a3c
colbyallenc <colbyallenc@gmail.com>
2017-05-04 15:24:19 -0400

    Iteration 5,6

--- mongoose-movies/routes/celebrities-route.js
+++ mongoose-movies/routes/celebrities-route.js
@@ -44,21 +44,88 @@ router.get('/celebrities/:id', (req, res, next) => {
       if (err) {
       next(err);
       return;
-      }
-      //404 if no product was found
+        }
       if (!theCelebrity) {
         next();
         return;
-      }
+        }
     res.render('celebrities/celebrities-details-view.ejs', {
       celebrities: theCelebrity
       });
     });
   });
 
+// celebrities/edit get
+router.get('/celebrities/:id/edit', (req, res, next) => {
+        const celebritiesId = req.params.id;
+        Celebrity.findById(celebritiesId, (err, theCelebrity) => {
+            if (err) {
+            next(err);
+            return;
+              }
+        res.render('celebrities/edit-celebrities-view.ejs', {
+          celebrities: theCelebrity
+          });
+        });
+      });
 
+// celebrity/edit post
+router.post('/celebrities/:id', (req, res, next) => {
+    const celebrityId = req.params.id;
+    const celebrityChanges = {
+      name: req.body.celebrityName,
+      occupation: req.body.celebrityOccupation,
+      catchPhrase: req.body.celebrityCatchPhrase
+        };
+  Celebrity.findByIdAndUpdate(
+      celebrityId,
+      celebrityChanges,
+      (err, theCelebrity) => {
+        if (err) {
+          res.render('celebrities/edit-celebrities-view.ejs', {
+            celebrities: theCelebrity,
+            validationErrors: theCelebrity.errors
+          });
+        return;
+          }
+      res.redirect('/celebrities');
+      }
+    );
+  });
 
+// celebrity/delete post
+router.post('/celebrities/:id/delete', (req, res, next)=>{
+    const celebrityId = req.params.id;
+    Celebrity.findByIdAndRemove(celebrityId, (err, theCelebrity) =>{
+      if (err){
+        next(err);
+        return;
+        }
+      res.redirect('/celebrities');
+    });
+  });
 
+///search
+router.get('/search', (req, res, next)=> {
+    const searchTerm = req.query.celebritySearchTerm;
+    if (!searchTerm){
+        res.render('celebrities/search-view.ejs');
+        return;
+        }
+    const searchRegex = new RegExp(searchTerm);
+    Celebrity.find(
+      {name: searchRegex},
+      (err, searchResults) => {
+        if(err) {
+          next(err);
+          return;
+          }
+      res.render('celebrities/search-view.ejs', {
+        celebrities: searchResults
+        });
+      }
+    );
+  });
 
 
 module.exports = router;
