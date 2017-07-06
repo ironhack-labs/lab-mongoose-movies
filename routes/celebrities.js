8ba0e164404ee3b934dafcd02720c7e00cde044a
Monica Calderaro <monicacalderaro@Monicas-MacBook-Air.local>
2017-07-06 20:51:16 +0200

    iteration 5

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -13,11 +13,21 @@ router.get('/', (req, res, next) => {
   });
 });
 
+router.get('/:id', function(req, res, next) {
+  Celebrity.findById(req.params.id, (err, celeb) => {
+    if(err){
+      next();
+      return err;
+    }
+    res.render('celebrities/show', {celebrity: celeb});
+  });
+});
+
 router.get('/new', (req, res, next) => {
     res.render('celebrities/new');
 });
 
-router.post('/new', (req, res, next) => {
+router.post('/', (req, res, next) => {
     let celebrityNew = new Celebrity({
       name: req.body.name,
       occupation: req.body.occupation,
@@ -31,6 +41,14 @@ router.post('/new', (req, res, next) => {
         res.redirect('/celebrities');
       }
     });
+});
+
+router.get('/:id/delete', function(req, res, next) {
+  let id = req.params.id;
+  Celebrity.findByIdAndRemove(id, (err, obj) => {
+    if (err){ return next(err); }
+    res.redirect("/celebrities");
+  });
 });
 
 module.exports = router;
