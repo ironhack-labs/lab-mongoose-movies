41465745ce3cb4d7a760a478074d511a8229b4ec
mikelRumayor <mikel.rumayor@gmail.com>
2017-07-06 21:59:16 +0200

    iteration 6 done

--- starter-code/movies/routes/celebrity.js
+++ starter-code/movies/routes/celebrity.js
@@ -3,14 +3,43 @@ const router  = express.Router()
 
 const Celebrity = require('../models/Celebrity')
 
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+
+  Celebrity.find({}, (err, celebrities) => {
+    res.render('celebrity/index', {
+      title: 'CELEBRITIES',
+      celebrities: celebrities
+    })
+  })
+})
+
+/* POST home page. */
+router.post('/', (req, res, next) => {
+  let {name, occupation, catchPhrase} = req.body;
+
+  let celebrity = new Celebrity({
+    name,
+    occupation,
+    catchPhrase
+  });
+  celebrity.save((err, obj) => {
+    if(err){
+      res.redirect('/celebrities/new');
+    }
+   res.redirect('/celebrities');
+  });
+
+})
+
 /* GET home page. */
 router.get('/new', (req, res, next) => {
-  Celebrity.find({}, (err, celebrity) => {
-
-    res.render('celebrity/new', {
-      title: 'CELEBRITIES',
-      celebrity: celebrity
-    })
+  res.render('celebrity/new', {
+    title: 'CELEBRITIES',
+    celebrity: {},
+    action: '/celebrities',
+    method: 'post'
   })
 })
 
@@ -28,35 +57,45 @@ router.get('/:id', (req, res, next) => {
   })
 })
 
-
-
-/* GET home page. */
-router.get('/', (req, res, next) => {
-
-  Celebrity.find({}, (err, celebrities) => {
-    res.render('celebrity/index', {
-      title: 'CELEBRITIES',
-      celebrities: celebrities
-    })
-  })
-})
-
-/* GET home page. */
-router.post('/', (req, res, next) => {
+router.post('/:id', (req, res, next) => {
+  let id = req.params.id
   let {name, occupation, catchPhrase} = req.body;
 
-  let celebrity = new Celebrity({
+  let celebrity = {
     name,
     occupation,
     catchPhrase
-  });
-  celebrity.save((err, obj) => {
-    if(err){
-      res.redirect('/celebrities/new');
-    }
-   res.redirect('/celebrities');
-  });
+  }
 
+  console.log(id)
+  Celebrity.findByIdAndUpdate(id, celebrity, (err, obj) => {
+    if (err){ next(err); }
+    res.redirect("/celebrities")
+
+  })
+})
+
+router.get('/:id/edit', (req, res, next) => {
+  let id = req.params.id
+  console.log(id)
+  Celebrity.findById(id, (err, obj) => {
+    if (err){ next(err) }
+      res.render('celebrity/new', {
+        title: 'CELEBRITIES',
+        celebrity: obj,
+        action: `/celebrities/${id}` ,
+        method: 'post'
+      })
+  })
+})
+
+router.post('/:id/delete', (req, res, next) => {
+  let id = req.params.id
+  console.log(id)
+  Celebrity.findByIdAndRemove(id, (err, obj) => {
+    if (err){ next(err); }
+    res.redirect("/celebrities")
+  });
 })
 
 module.exports = router
