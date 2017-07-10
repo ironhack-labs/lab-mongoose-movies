bede116f53f63e95642f7f0ba9324015ccf0555c
Manuel Valle <vallerem17@gmail.com>
2017-07-10 21:00:27 +0200

    Go go go

--- Mongoose_Movies/app.js
+++ Mongoose_Movies/app.js
@@ -1,33 +1,41 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+let express = require('express');
+let path = require('path');
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/MongoMovies');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+// let users = require('./routes/users');    /// ** No se requiere users **
+let index = require('./routes/index');
+let celebrities = require('./routes/celebrities');
+let movies = require('./routes/movies');
 
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// app.use('/users', users);      /// ** No se requiere users **
 app.use('/', index);
-app.use('/users', users);
+app.use('/celebrities', celebrities);
+app.use('/movies', movies);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  let err = new Error('Not Found 😞');
   err.status = 404;
   next(err);
 });
---
+++ Mongoose_Movies/routes/movies.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+
+
+
+
+
+
+module.exports = router;
