edd08ec5887653c0dec2e1f76476495eb41dc67a
Joao Campos <j.campos4g@gmail.com>
2017-09-02 18:21:13 -0400

    iteration 5 done

--- starter-code/mongoose-movies/routes/celebrities.js
+++ starter-code/mongoose-movies/routes/celebrities.js
@@ -20,6 +20,42 @@ router.get('/celebrities',(req,res,next) => {
 
 });
 
+// step one of adding new celebrities
+router.get('/celebrities/new', (req,res,next) => {
+
+    res.render('celebrities/new.ejs');
+
+});
+
+//step two of adding new celebrities
+router.post('/celebrities', (req,res,next) => {
+
+  // create new celebrity with the celebrity schema
+  const celebrity = new CelebrityModel({
+      name: req.body.celebName,
+      occupation: req.body.celebOccupation,
+      catchPhrase: req.body.celebPhrase
+  });
+
+  celebrity.save((err) => {
+
+        if (err) {
+
+            next(err);
+
+            return;
+
+        }
+
+        // STEP #3 redirect
+        // ALWAYS redirect after a successful to POST to avoid resubmitting
+        res.redirect('/celebrities');
+          // You can only redirect to a URL
+    });
+
+
+});
+
 router.get('/celebrities/:id', (req,res,next) => {
 
     const id = req.params.id;
@@ -41,5 +77,20 @@ router.get('/celebrities/:id', (req,res,next) => {
 
 });
 
+router.post('/celebrities/:id2/delete', (req,res,next) => {
+
+    const id = req.params.id2;
+
+    CelebrityModel.findByIdAndRemove(id, (err,todo) => {
+
+      if (err) {
+        next(err);
+      }
+
+      res.redirect('/celebrities');
+
+    });
+
+});
 
 module.exports = router;
