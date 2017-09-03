f771a9f9c28aaa8be060ca5d3cdb08e0ad197383
Joao Campos <j.campos4g@gmail.com>
2017-09-03 09:39:39 -0400

    done with interation 10

--- starter-code/mongoose-movies/routes/movies.js
+++ starter-code/mongoose-movies/routes/movies.js
@@ -20,5 +20,60 @@ router.get('/movies', (req,res,next) => {
 
 });
 
+router.get('/movies/new', (req,res,next) => {
+
+    res.render('movies/new.ejs');
+
+});
+
+router.post('/movies', (req,res,next) => {
+
+  // create new celebrity with the celebrity schema
+  const movie = new MovieModel({
+      title: req.body.movieTitle,
+      genre: req.body.movieGenre,
+      plot: req.body.moviePlot
+  });
+
+  movie.save((err) => {
+
+        if (err) {
+
+            next(err);
+
+            return;
+
+        }
+
+        // STEP #3 redirect
+        // ALWAYS redirect after a successful to POST to avoid resubmitting
+        res.redirect('/movies');
+          // You can only redirect to a URL
+    });
+
+});
+
+
+router.get('/movies/:id', (req,res,next) => {
+
+    const id = req.params.id;
+
+    MovieModel.findById(id, (err, info) => {
+
+        if (err) {
+          console.log("Error!");
+          next(err);
+
+        }
+        console.log(info);
+
+        res.locals.movieInfo = info;
+
+        res.render('movies/show.ejs');
+
+    });
+
+});
+
 
 module.exports = router;
