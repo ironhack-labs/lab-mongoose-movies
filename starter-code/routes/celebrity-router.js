cad617da3d0f24f43525079a8e4fa887adeca870
Ryan Wang <qwang1993@gmail.com>
2017-09-04 20:37:48 -0400

    celebrities done

--- starter-code/routes/celebrity-router.js
+++ starter-code/routes/celebrity-router.js
@@ -60,4 +60,58 @@ router.get('/celebrities/:celebId', (req, res, next) => {
   );
 });
 
+router.post('/celebrities/:celebId/delete', (req, res, next) => {
+  CelebrityModel.findByIdAndRemove(
+    req.params.celebId,
+
+    (err, celebInfo) => {
+      if(err) {
+        next(err);
+        return;
+      }
+      res.redirect('/celebrities');
+    }
+  );
+});
+
+router.get('/celebrities/:celebId/edit', (req, res, next) => {
+  CelebrityModel.findById(
+    req.params.celebId,
+
+    (err, celebFromDb) => {
+      if (err) {
+        next(err);
+        return;
+      }
+
+      res.locals.celebInfo = celebFromDb;
+
+      res.render('celebrities/edit-celebrity.ejs');
+    }
+  );
+});
+
+router.post('/celebrities/:celebId', (req, res, next) => {
+  CelebrityModel.findById(
+    req.params.celebId,
+    (err, celebFromDb) => {
+      if(err) {
+        next(err);
+        return;
+      }
+      celebFromDb.name = req.body.celebName;
+      celebFromDb.occupation = req.body.occupation;
+      celebFromDb.catchPhrase = req.body.catchPhrase;
+
+      celebFromDb.save((err) => {
+        if(err) {
+          next(err);
+          return;
+        }
+        res.redirect('/celebrities');
+      });
+    }
+  );
+});
+
 module.exports = router;
