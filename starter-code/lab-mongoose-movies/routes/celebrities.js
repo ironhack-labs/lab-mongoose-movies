49c7d62bd7a2a8407808d438b139510b05b2d337
y4izus <yaiza.garcia.mm@gmail.com>
2017-09-07 22:52:48 +0200

    Iteration #6 (Bonus): Editing Celebrities done

--- starter-code/lab-mongoose-movies/routes/celebrities.js
+++ starter-code/lab-mongoose-movies/routes/celebrities.js
@@ -13,6 +13,23 @@ router.get('/celebrities', (req, res, next) => {
   })
 })
 
+router.post('/celebrities/:id', (req, res, next) => {
+  const celebrityId = req.params.id
+
+  const celebrityUpdate = {
+      name: req.body.name,
+      occupation: req.body.occupation,
+      catchPhrase: req.body.catchPhrase
+  }
+
+  const newCelebrity = new Celebrity(celebrityUpdate)
+
+  Celebrity.findByIdAndUpdate(celebrityId, celebrityUpdate, (err, celebrity) => {
+    if (err){ return next(err); }
+    return res.redirect(`/celebrities/${celebrityId}`);
+  })
+})
+
 router.post('/celebrities', (req, res, next) => {
   const celebrityInfo = {
       name: req.body.name,
@@ -37,6 +54,15 @@ router.post('/celebrities/:id/delete', (req, res, next) => {
   Celebrity.findByIdAndRemove(celebrityId, (err, celebrity) => {
     if (err){ return next(err) }
     return res.redirect('/celebrities')
+  })
+})
+
+router.get('/celebrities/:id/edit', (req, res, next) => {
+  const celebrityId = req.params.id
+
+  Celebrity.findById(celebrityId, (err, celebrity) => {
+    if (err){ return next(err) }
+    res.render('celebrities/edit', { title:'Edit celebrity', celebrity: celebrity })
   })
 })
 
