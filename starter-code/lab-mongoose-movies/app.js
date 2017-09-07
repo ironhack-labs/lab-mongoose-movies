18b3c825c9e9ff96aa8f0a65f2d54e68c2aaaa00
y4izus <yaiza.garcia.mm@gmail.com>
2017-09-07 21:55:18 +0200

    Iteration #3: The Celebrity Details Page done

--- starter-code/lab-mongoose-movies/app.js
+++ starter-code/lab-mongoose-movies/app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/celebrities', celebritiesRouter)
+app.use('/', celebritiesRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- starter-code/lab-mongoose-movies/routes/celebrities.js
+++ starter-code/lab-mongoose-movies/routes/celebrities.js
@@ -3,13 +3,26 @@ var router = express.Router()
 const Celebrity = require('../models/Celebrity')
 
 // RETRIEVE: Celebrity list
-router.get('/', (req, res, next) => {
+router.get('/celebrities', (req, res, next) => {
   Celebrity.find({}, (err, celebrities) => {
     if (err) { return next(err) }
 
     res.render('celebrities/index', {
       title:'Celebrities',
       celebrities: celebrities
+    })
+  })
+})
+
+router.get('/celebrities/:id', (req, res, next) => {
+  const celebrityId = req.params.id
+
+  Celebrity.findById(celebrityId, (err, celebrity) => {
+    if (err) { return next(err) }
+
+    res.render('celebrities/show', {
+      title:'Celebrity Detail',
+      celebrity: celebrity
     })
   })
 })
