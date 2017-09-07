48627675657b38ec226050eecfc412c558b9021a
willjthorp <willthorp99@gmail.com>
2017-09-07 19:02:02 +0200

    Created celebrity model

---
+++ starter-code/mongoose-movies/models/celebrity.js
@@ -0,0 +1,19 @@
+/*jshint esversion: 6 */
+
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const CelebritySchema = new Schema({
+  name        : {
+    String,
+  },
+  occupation  : {
+    String,
+  },
+  catchPhrase : {
+    String,
+  }
+});
+
+const Celebrity = mongoose.model('Celebrity', CelebritySchema);
+module.exports = Celebrity;
