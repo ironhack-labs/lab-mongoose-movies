b74d81d8c9a8f8686aae9347074e3eafad6f8c64
Jad ch <jad.chalita@gmail.com>
2017-09-29 10:05:38 +0200

    Adding new movies

--- starter-code/mongovie/routes/movies.js
+++ starter-code/mongovie/routes/movies.js
@@ -10,6 +10,23 @@ router.get('/', (req, res, next) => {
   })
 })
 
+router.get('/new', (req, res, next) => {
+  res.render('movies/new')
+})
+
+router.post('/', (req, res, next) => {
+  let newMovie=  new Movie ({
+    title: req.body.title,
+    genre: req.body.genre,
+    plot: req.body.plot
+  })
+  newMovie.save( (error, newMovie) => {
+    if (error) return next(error)
+    else res.redirect('/movies')
+  })
+})
+
+
 router.get('/:id', (req, res, next) => {
   let id = req.params.id
   Movie.findOne({_id: id}, (error, movie) => {
