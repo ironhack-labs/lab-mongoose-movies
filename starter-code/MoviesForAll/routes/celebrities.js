9cb41573d9685b884da0831dc21c4fbc5ba2ca48
Reshma Patel <reshma1529@gmail.com>
2017-10-01 19:17:07 +0200

    try

--- starter-code/MoviesForAll/routes/celebrities.js
+++ starter-code/MoviesForAll/routes/celebrities.js
@@ -15,15 +15,24 @@ router.get('/', (req, res, next) => {
   });
 
 });
+//
+// router.get('/:id', (req, res, next) => {
+//   Celebrity.find({}, (err, celebrities) =>{
+//     if(err) {return next(err)}
+//
+//     res.render('celebrities/index',{
+//       celebrities: celebrities
+//     });
+//   });
+// });
 
-router.get('/:id', (req, res, next) => {
-  Celebrity.find({}, (err, celebrities) =>{
-    if(err) {return next(err)}
-
-    res.render('celebrities/index',{
-      celebrities: celebrities
-    });
-  });
-});
+// router.get('/:id', (req, res, next) => {
+//   Celebrity.findById({}, (err,req.params.id) =>{
+//     if(err) {return next(err)}
+//     res.render('celebrities/:id',{
+//       celebrities:celebrities
+//     });
+//   });
+// });
 
 module.exports = router;
