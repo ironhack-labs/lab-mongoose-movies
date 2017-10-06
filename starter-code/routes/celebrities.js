25d484cf9004527b2a488eaf6bad631fe8b38fc1
Nick Navarro <jacinto802@gmail.com>
2017-10-05 21:00:22 -0400

    almost

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -43,12 +43,10 @@ celebrityRoutes.post('/', (req, res, next) => {
         }    // redirect to the list of products if it saves
     return res.redirect('/celebrities');
   });
-
 });
 
 celebrityRoutes.get('/:id', (req, res, next) => {
    const celebrityId = req.params.id;
-
 
   Celebrity.findById(celebrityId, (err, celebrity) => {
      if (err) { return next(err); }
@@ -63,8 +61,36 @@ celebrityRoutes.get('/:id', (req, res, next) => {
      if (err){ return next(err); }
      return res.redirect('/celebrities');
    });
-
  });
+
+ celebrityRoutes.get('/:id/edit', (req, res, next) => {
+   const celebrityId = req.params.id;
+
+   Celebrity.findById(celebrityId, (err, celebrity) => {
+     if (err) {return next(err); }
+     res.render('celebrities/edit', {celebrity: celebrity});
+   })
+ })
+
+ celebrityRoutes.post('/:id', (req, res, next) => {
+   const celebrityId = req.params.id;
+
+   /*
+    * Create a new object with all of the information from the request body.
+    * This correlates directly with the schema of Product
+    */
+   const updates = {
+       name: req.body.name,
+       occupation: req.body.occupation,
+       catchPhrase: req.body.catchPhrase,
+   };
+
+   Celebrity.findByIdAndUpdate(celebrityId, updates, (err, product) => {
+     if (err){ return next(err); }
+     return res.redirect('/celebrities');
+   });
+ });
+
 
 
 module.exports = celebrityRoutes;
--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -14,6 +14,75 @@ movieRoutes.get('/', (req, res, next) => {
   });
 });
 
+movieRoutes.get('/new', (req, res, next) => {
+  res.render('movies/new');
+});
+
+movieRoutes.post('/', (req, res, next) => {
+  const movieInfo = {
+      title: req.body.title,
+      plot: req.body.plot,
+      genre: req.body.genre
+      };
+  const newMovie = new Movie(movieInfo);
+
+  newMovie.save( (err) => {
+    if (err) {
+          return res.render('movies/new', { errors: newMovie.errors });
+        }    // redirect to the list of products if it saves
+    return res.redirect('/movies');
+  });
+});
+
+
+movieRoutes.get('/:id', (req, res, next) => {
+   const movieId = req.params.id;
+
+  Movie.findById(movieId, (err, movie) => {
+     if (err) { return next(err); }
+     res.render('movies/show', {movie: movie});
+   });
+ });
+
+movieRoutes.post('/:id/delete', (req, res, next) => {
+  const movieId = req.params.id;
+
+  Movie.findByIdAndRemove(movieId, (err, movie) => {
+    if (err){ return next(err); }
+    return res.redirect('/movies');
+  });
+});
+
+
+movieRoutes.get('/:id/edit', (req, res, next) => {
+  const movieId = req.params.id;
+
+  Movie.findById(movieId, (err, movie) => {
+    if (err) {return next(err); }
+    res.render('movies/edit', {movie: movie});
+  })
+})
+
+movieRoutes.post('/:id', (req, res, next) => {
+  const movieId = req.params.id;
+
+  /*
+   * Create a new object with all of the information from the request body.
+   * This correlates directly with the schema of Product
+   */
+  const updates = {
+      title: req.body.title,
+      plot: req.body.plot,
+      genre: req.body.genre,
+  };
+
+  Movie.findByIdAndUpdate(movieId, updates, (err, product) => {
+    if (err){ return next(err); }
+    return res.redirect('/movies');
+  });
+});
+
+
 
 
 module.exports = movieRoutes;
