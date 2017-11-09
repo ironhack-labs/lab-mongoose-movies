0deb89303b7217ad368cffb73dffdfb731caea3e
Abraham Castro <cucharadepalo@gmail.com>
2017-11-09 17:34:14 +0100

    Project structure with routes, models and layout

--- starter-code/express-mongoose-movies/app.js
+++ starter-code/express-mongoose-movies/app.js
@@ -1,19 +1,23 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var sassMiddleware = require('node-sass-middleware');
+const express = require('express');
+const path = require('path');
+//var favicon = require('serve-favicon');
+const logger = require('morgan');
+//const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const sassMiddleware = require('node-sass-middleware');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/express-mongoose-movies');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const index = require('./routes/index');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+// express layouts
+app.use(expressLayouts);
+app.set('layout', __dirname + '/views/layout/layout');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -30,7 +34,6 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
