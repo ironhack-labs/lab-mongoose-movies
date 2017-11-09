839227e6a494aa8d6d9839e139de5f046569461d
nadiacarreiranunez <nadiacarreiranunez@gmail.com>
2017-11-09 19:02:22 +0100

    third itineration

--- starter-code/movies/app.js
+++ starter-code/movies/app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/', celebrity);
+app.use('/celebrity', celebrity);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- starter-code/movies/routes/celebrity.js
+++ starter-code/movies/routes/celebrity.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Celebrity = require('../model/celebrity');
 const router = express.Router();
 
-router.get('/celebrity', (req, res, next) => {
+router.get('/', (req, res, next) => {
   Celebrity.find({}, (error, celebrityList) =>{
     if(error) return next(error);
 
@@ -12,8 +12,24 @@ router.get('/celebrity', (req, res, next) => {
   });
 });
 
-router.get('/celebrity', (req, res, next) => {
-  res.render('/celebrity');
+// router.get('/celebrity', (req, res, next) => {
+//   res.render('/celebrity');
+// });
+// router.get('/bio', (req, res, next) => {
+//   res.render('celebrity/bio');
+// });
+
+
+router.get('/:id', (req, res, next) => {
+  let id = req.params.id
+
+  Celebrity.findById(id, (err, detail) => {
+    res.render('celebrity/bio', {
+      detail: detail
+    })
+  })
 });
+
+
 
   module.exports = router;
