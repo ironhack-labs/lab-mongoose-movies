2b8ba1f741eaa9d2da748f197af55d5ad9c24e92
sdediego <sergio.dediego@outlook.com>
2017-11-09 18:26:34 +0100

    Iteracion 4

--- mongoose-movies/routes/celebrities.js
+++ mongoose-movies/routes/celebrities.js
@@ -10,12 +10,31 @@ router.get('/', (req, res, next) => {
   });
 });
 
+router.get('/new', (req, res, next) => {
+  res.render('celebrities/new', {celebrity: new Celebrity()});
+});
+
 router.get('/:id', (req, res, next) => {
   let celebrityId = req.params.id;
   Celebrity.findById(celebrityId, (err, celebrity) => {
     if (err) { return next(err); }
     console.log(celebrity);
     res.render('celebrities/show', celebrity);
+  });
+});
+
+router.post('/', (req, res, next) => {
+  let celebrityInto = {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase
+  };
+
+  const newCelebrity = new Celebrity(celebrityInto);
+
+  newCelebrity.save(err => {
+    if (err) { return res.render('celebrities/new'); }
+    return res.redirect('/celebrities');
   });
 });
 
