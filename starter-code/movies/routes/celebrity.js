b4fc69cf7c1c8f7c412b22948e72f9db592600b8
nadiacarreiranunez <nadiacarreiranunez@gmail.com>
2017-11-09 19:56:17 +0100

    fourd itineration

--- starter-code/movies/routes/celebrity.js
+++ starter-code/movies/routes/celebrity.js
@@ -20,16 +20,38 @@ router.get('/', (req, res, next) => {
 // });
 
 
-router.get('/:id', (req, res, next) => {
-  let id = req.params.id
 
-  Celebrity.findById(id, (err, detail) => {
+
+router.get('/new', (req, res, next) => {
+  res.render('celebrity/new');
+});
+
+router.post('/', (req, res, next) => {
+  const celebrityInfo = {
+    celebrityName: req.body.celebrityName,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase,
+  };
+
+  // Create a new Product with the params
+  const newCelebrity = new Celebrity(celebrityInfo);
+
+  newCelebrity.save((err) => {
+    if (err) {
+      return res.render('celebrity/new', {
+        celebrity: newCelebrity
+      })
+    }
+
+    return res.redirect('/celebrity');
+  });
+});
+router.get('/:id', (req, res, next) => {
+  Celebrity.findById(req.params.id, (err, detail) => {
     res.render('celebrity/bio', {
       detail: detail
     })
   })
 });
-
-
 
   module.exports = router;
