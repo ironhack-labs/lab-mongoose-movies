70993f4618d4500720b8bb449ac2f3d2b016ab35
Maria Ruiz <mariaruizestevez@gmail.com>
2017-11-09 22:27:39 +0100

    Fourth and fifth iteration done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -13,6 +13,19 @@ router.get('/', (req, res, next) => {
   });
 });
 
+router.get('/new', (req, res, next) => {
+  res.render('celebrities/new', {});
+});
+
+router.post('/:id/delete', (req, res, next) => {
+  const id = req.params.id;
+
+ Celebrity.findByIdAndRemove(id, (err, product) => {
+   if (err){ return next(err); }
+   return res.redirect('/celebrities');
+ });
+});
+
 router.get('/:id', (req, res, next) => {
   Celebrity.findById(req.params.id, (err, celebrity) => {
     if (err) { return next(err); }
@@ -23,24 +36,22 @@ router.get('/:id', (req, res, next) => {
   });
 });
 
+router.post('/', (req, res, next) => {
+  const celebrityInfo = {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase
+  };
 
-//
-// router.post('/', (req, res, next) => {
-//   const productInfo = {
-//     name: req.body.myName,
-//     price: req.body.price,
-//     imageUrl: req.body.imageUrl,
-//     description: req.body.description
-//   };
-//
-//   // Create a new Product with the params
-//   const newProduct = new Product(productInfo);
-//
-//   newProduct.save((err) => {
-//     if (err) { return next(err) }
-//
-//     return res.redirect('/products');
-//   });
-// });
+  const newCelebrity = new Celebrity(celebrityInfo);
+
+  newCelebrity.save((err) => {
+    if (err) {
+      return res.redirect('/new');
+     }
+
+    return res.redirect('/celebrities');
+  });
+});
 
 module.exports = router;
