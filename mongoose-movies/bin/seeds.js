f60e0f3c10795d9e616e34a7ce094a7a630952fa
sdediego <sergio.dediego@outlook.com>
2017-11-10 00:08:13 +0100

    Iteracion 7

--- mongoose-movies/bin/seeds.js
+++ mongoose-movies/bin/seeds.js
@@ -1,31 +1,60 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/mongoose-movies', {useMongoClient: true});
 const Celebrity = require('../models/celebrities');
+const Movie = require('../models/movies');
 
-const celebrities = [
+//const celebrities = [
+//  {
+//    name: 'Mariano Rajoy',
+//    occupation: 'President of the Government of Spain',
+//    catchPhrase: '¿Y la europea?'
+//  },
+//  {
+//    name: 'Julio Iglesias',
+//    occupation: 'Singer',
+//    catchPhrase: 'Soy tu padre y lo sabes'
+//  },
+//  {
+//    name: 'Eduardo Inda',
+//    occupation: 'Journalist',
+//    catchPhrase: 'Tracatra'
+//  }
+//];
+
+//Celebrity.collection.drop();
+
+//Celebrity.create(celebrities, (err, docs) => {
+//  if (err) { throw err; }
+//  docs.forEach(celebrity => {
+//    console.log(celebrity.name);
+//  });
+//  mongoose.connection.close();
+//});
+
+const movies = [
   {
-    name: 'Mariano Rajoy',
-    occupation: 'President of the Government of Spain',
-    catchPhrase: '¿Y la europea?'
+    title: '50 shades of Grey',
+    genre: 'Fantasy - All publics',
+    plot: 'Piddling girl fall in love with fantasy boy'
   },
   {
-    name: 'Julio Iglesias',
-    occupation: 'Singer',
-    catchPhrase: 'Soy tu padre y lo sabes'
+    title: 'Tadeo Jones',
+    genre: 'Historical - Adults Only ',
+    plot: 'Super researcher solves ancient mistery'
   },
   {
-    name: 'Eduardo Inda',
-    occupation: 'Journalist',
-    catchPhrase: 'Tracatra'
+    title: 'Terminator',
+    genre: 'Cartoon - Parental Guidance Suggested',
+    plot: 'Kind killer machine from the future bossing around'
   }
 ];
 
-Celebrity.collection.drop();
+Movie.collection.drop();
 
-Celebrity.create(celebrities, (err, docs) => {
+Movie.create(movies, (err, docs) => {
   if (err) { throw err; }
-  docs.forEach(celebrity => {
-    console.log(celebrity.name);
+  docs.forEach(movie => {
+    console.log(movie.title);
   });
   mongoose.connection.close();
 });
