c3fa7cec0582a5ba65c6d0ef91c30c6ec2d2d3b1
Alber Pedraza <alberpedrazaes@gmail.com>
2017-11-09 20:41:57 +0100

    Iteration 4

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -12,6 +12,34 @@ const router  = express.Router();
       });
     });
   });
+
+  router.get('/new', (req, res, next) => {
+    res.render('celebrities/new', {
+      celebrities: new Celebrities()
+    });
+  });
+
+  router.post('/', (req, res, next) => {
+    const celebrityInfo = {
+      name: req.body.name,
+      occupation: req.body.occupation,
+      catchPhrase: req.body.catchPhrase
+    };
+
+  // Crea instancia
+  const newCelebrity = new Celebrities(celebrityInfo);
+
+//método
+  newCelebrity.save((err) => {
+    if (err) {
+      return res.render('celebrities/new', {
+        celebrities: newCelebrity
+      });
+    }
+    return res.redirect('/celebrities');
+  });
+  });
+
   router.get('/:id', (req, res, next) => {
     let id = req.params.id;
 
@@ -21,5 +49,7 @@ const router  = express.Router();
       });
     });
   });
+
+
 
 module.exports = router;
