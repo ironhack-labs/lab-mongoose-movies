18df32e28b357e57f6577632190d11a57456d7c1
Ernesmh <ernesmh@gmail.com>
2017-11-09 23:33:36 +0100

    iteration nine finished without the bonus --sixth--

--- starter-code/ironmovies/routes/movies.js
+++ starter-code/ironmovies/routes/movies.js
@@ -16,7 +16,17 @@ router.get('/', (req, res, next) => {
 
 
 
+router.get('/:id', (req, res, next) => {
+  let id = req.params.id
+  Movie.findById(id, (err, details) => {
+    console.log(details)
+    res.render('movies/show', {
 
+      movies: details
+
+    })
+  })
+});
 
 
 
