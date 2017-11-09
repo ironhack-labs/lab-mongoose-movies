8ffd45e8fceb5d9f5b851b7877646991bfca3d7b
Ernesmh <ernesmh@gmail.com>
2017-11-09 20:41:57 +0100

    fourth finished

--- starter-code/ironmovies/routes/celebrities.js
+++ starter-code/ironmovies/routes/celebrities.js
@@ -14,6 +14,9 @@ router.get('/', (req, res, next) => {
   });
 });
 
+router.get('/new', (req, res, next) => {
+  res.render('celebrities/new');
+});
 
 router.get('/:id', (req, res, next) => {
   let id = req.params.id
@@ -26,6 +29,33 @@ router.get('/:id', (req, res, next) => {
     })
   })
 });
+
+router.post('/', (req, res, next) => {
+  const celebrityCreated = {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase,
+  };
+
+  // Create a new Product with the params
+  const celebrity = new Celebrity(celebrityCreated);
+
+  celebrity.save((err) => {
+    if (err) {
+      return res.render('celebrities/new', {
+        celebrity: celebrity
+      })
+    }
+
+    return res.redirect('/celebrities');
+  });
+});
+
+
+
+
+
+
 
 
 module.exports = router;
