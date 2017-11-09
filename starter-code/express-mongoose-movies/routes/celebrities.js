1340f54357b0194f5e13423046d8ab18e5ba552e
Abraham Castro <cucharadepalo@gmail.com>
2017-11-09 19:57:31 +0100

    Iteration 3

--- starter-code/express-mongoose-movies/routes/celebrities.js
+++ starter-code/express-mongoose-movies/routes/celebrities.js
@@ -4,12 +4,21 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   Celebrities.find({}, (err, celebrities) => {
-    if (err) {
-      return next(err);
-    }
+    if (err) return next(err);
     res.render('celebrities/index', {
       celebrities: celebrities,
       title: 'Celebrities'
+    });
+  });
+});
+
+router.get('/:id', (req, res, next) => {
+  let celebrityId = req.params.id;
+  Celebrities.findById(celebrityId, (err, celebrity) => {
+    if (err) return next(err);
+    console.log('celebrity', celebrity);
+    res.render('celebrities/show', {
+      celebrity : celebrity
     });
   });
 });
