ec15f6a7f56350f532067f4ca24fb65fdf3088cd
Camila Maya <camilamaya21@gmail.com>
2017-11-09 18:50:04 +0100

    Iteration #3: The Celebrity Details Page

--- starter-code/mongoose-movies/routes/celebrities.js
+++ starter-code/mongoose-movies/routes/celebrities.js
@@ -8,9 +8,20 @@ router.get('/', (req, res, next) => {
       if (err) { return next(err) }
 
       res.render('celebrities/index', {
-        celebrities: celebrities
+        celebrities: celebrities,
       });
     });
 });
+
+  router.get('/:id', (req, res, next) => {
+    let id = req.params.id
+
+    Celebrity.findById(id, (err, celebrities) => {
+      res.render('celebrities/bio', {
+        celebrities: celebrities
+      })
+    })
+  });
+
 
 module.exports = router;
