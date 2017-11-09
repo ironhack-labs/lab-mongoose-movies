5c9c2a02ae870be05a6f6758bedea6a3d3ded7da
gemaderus <hi@gemaderus.com>
2017-11-09 19:56:47 +0100

    Fourth iteration finished.

--- starter-code/movies/routes/celebrity.js
+++ starter-code/movies/routes/celebrity.js
@@ -26,6 +26,33 @@ router.get('/', (req, res, next) => {
 //   res.render('celebrity/bio')
 // });
 
+router.get('/new', (req, res, next) => {
+  res.render('celebrity/new', {
+    celebrity: new Celebrity()
+  });
+});
+
+router.post('/', (req, res, next) => {
+  const celebrityInfo = {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase,
+  };
+
+  // Create a new Product with the params
+  const newCelebrity = new Celebrity(celebrityInfo);
+
+  newCelebrity.save((err) => {
+    if (err) {
+      return res.render('celebrity/new', {
+        celebrity: newCelebrity
+      })
+    }
+
+    return res.redirect('/celebrity');
+  });
+});
+
 router.get('/:id', (req, res, next) => {
   let id = req.params.id
 
@@ -35,5 +62,6 @@ router.get('/:id', (req, res, next) => {
     })
   })
 });
+
 
 module.exports = router;
