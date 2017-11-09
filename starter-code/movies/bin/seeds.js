cc96d25a01bcbd20d6d9440a717670bec228a584
J. Gaspar Prieto <jgprieto00@gmail.com>
2017-11-09 18:02:54 +0100

    finished initial iteration

---
+++ starter-code/movies/bin/seeds.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/farandula", {
+  useMongoClient: true
+});
+const Celebrities = require("../model/celebrities");
+
+const celebrities = [
+  {
+    name: 'Paco Martinez Soria',
+    occupation: 'Emigrante',
+    catchPhrase: 'Yo no tengo esa marcha'
+
+  },
+  {
+    name: 'Steve Urkel',
+    occupation: 'Vecino',
+    catchPhrase: '¿He sido yo?'
+
+  },
+  {
+    name: 'el Chavo',
+    occupation: 'Estudiante',
+    catchPhrase: 'No me simpatizas'
+  },
+]
+Celebrities.collection.drop();
+
+Celebrities.create(celebrities, (err, docs) => {
+  if (err) {
+    throw err;
+  }
+  docs.forEach(celebrities => {
+    console.log(Celebrities);
+  });
+
+  mongoose.connection.close();
+});
