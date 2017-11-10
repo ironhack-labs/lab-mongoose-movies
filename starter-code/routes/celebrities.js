fba565df3a46b521bf57b36ba8c126f6efb7e410
anajules94 <32312980+anajules94@users.noreply.github.com>
2017-11-10 09:39:15 +0100

    hasta un new celebrity

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const Celebrities = require('../models/celebrity');
+const Celebrity = require('../models/celebrity');
 
 
+//todo empieza a partir de /celebrities
 
 router.get('/', (req, res) => {
-  Celebrities.find({}, (err, celebrities) => {
+  Celebrity.find({}, (err, celebrities) => {
     if (err) {
       return next(err);
     }
-
     res.render('celebrities/index', {
       celebritiesArray: celebrities
     })
@@ -19,11 +19,35 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res, next) => {
   let id = req.params.id
-  celebrities.findById (id, (err, celebrities) => {
-    res.render('celebrities/show', {
-      celebrities: celebrities
-    })
+  Celebrity.findById (id, (err, celebrity) => {
+    res.render('celebrities/show', {celebrity: celebrity})
   })
+});
+//NUNCA PONGAS DOS PETICIONES A LA MISMA RUTA, CUENTAN COMO LA MISMA RUTA TAMBIEN SI UNO ES :ID Y OTRO RUTA NORMAL
+router.get('/create/new', (req, res, next) => {
+  res.render('celebrities/form', { celebrity: new Celebrity()
+  });
+});
+
+router.post('/', (req, res, next) => {
+  const celebrityInfo = {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase
+  };
+
+  // Create a new Product with the params
+  const newCelebrity = new Celebrity(celebrityInfo);
+
+  newCelebrity.save((err) => {
+    if (err) {
+      return res.render('celebrities/form', {
+        celebrity: newCelebrity
+      })
+    }
+
+    return res.redirect('/celebrities');
+  });
 });
 
 module.exports = router;
