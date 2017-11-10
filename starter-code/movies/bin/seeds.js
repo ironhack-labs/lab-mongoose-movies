e8d068642db576495b39c0baf716b2906e5ce79b
Clementina <mataclemy@gmail.com>
2017-11-10 09:34:13 +0100

    done

--- starter-code/movies/bin/seeds.js
+++ starter-code/movies/bin/seeds.js
@@ -1,66 +1,66 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/celebrity-movie', {useMongoClient: true});
 
-// const Celebrity = require('../models/celebrity');
-//
-// const celebrityData = [
-//   {
-//     name: "Clementina",
-//     occupation: "Administradora",
-//     catchPhrase: "Nobody cares",
-//   },
-//   {
-//     name: "Clementina2",
-//     occupation: "Administradora2",
-//     catchPhrase: "Nobody cares2",
-//   },
-//   {
-//     name: "Clementina3",
-//     occupation: "Administradora3",
-//     catchPhrase: "Nobody cares3",
-//   },
-// ];
-//
-// Celebrity.create(celebrityData, (err, docs) => {
-//   if (err) {
-//     throw err;
-//   }
-//
-//   docs.forEach((celebrity) => {
-//     console.log(celebrity.name)
-//   });
-//   mongoose.connection.close();
-// });
+const Celebrity = require('../models/celebrity');
 
-
-
-const Movie = require('../models/movie');
-
-const movieData = [
+const celebrityData = [
   {
-    title: "Clementina1",
-    genre: "Mata1",
-    plot: "Palacios1",
+    name: "Clementina",
+    occupation: "Administradora",
+    catchPhrase: "Nobody cares",
   },
   {
-    title: "Clementina2",
-    genre: "Mata2",
-    plot: "Palacios2",
+    name: "Clementina2",
+    occupation: "Administradora2",
+    catchPhrase: "Nobody cares2",
   },
   {
-    title: "Clementina3",
-    genre: "Mata3",
-    plot: "Palacios3",
+    name: "Clementina3",
+    occupation: "Administradora3",
+    catchPhrase: "Nobody cares3",
   },
 ];
 
-Movie.create(movieData, (err, docs) => {
+Celebrity.create(celebrityData, (err, docs) => {
   if (err) {
     throw err;
   }
 
-  docs.forEach((movie) => {
-    console.log(movie.title)
+  docs.forEach((celebrity) => {
+    console.log(celebrity.name)
   });
   mongoose.connection.close();
 });
+
+
+
+// const Movie = require('../models/movie');
+//
+// const movieData = [
+//   {
+//     title: "Clementina1",
+//     genre: "Mata1",
+//     plot: "Palacios1",
+//   },
+//   {
+//     title: "Clementina2",
+//     genre: "Mata2",
+//     plot: "Palacios2",
+//   },
+//   {
+//     title: "Clementina3",
+//     genre: "Mata3",
+//     plot: "Palacios3",
+//   },
+// ];
+//
+// Movie.create(movieData, (err, docs) => {
+//   if (err) {
+//     throw err;
+//   }
+//
+//   docs.forEach((movie) => {
+//     console.log(movie.title)
+//   });
+//   mongoose.connection.close();
+// });
