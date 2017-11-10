359e5e5cc2411c1f6b5b51892f0537f4b30d8269
Abraham Castro <cucharadepalo@gmail.com>
2017-11-10 09:26:39 +0100

    Iteration 4

--- starter-code/express-mongoose-movies/routes/celebrities.js
+++ starter-code/express-mongoose-movies/routes/celebrities.js
@@ -12,6 +12,12 @@ router.get('/', (req, res, next) => {
   });
 });
 
+// /celebrities/new	| GET	| Show a form to create a celebrity
+router.get('/new', (req, res, next) => {
+  res.render('celebrities/new');
+});
+
+// /celebrities/:id	| GET	| Show a specific celebrity
 router.get('/:id', (req, res, next) => {
   let celebrityId = req.params.id;
   Celebrities.findById(celebrityId, (err, celebrity) => {
@@ -20,6 +26,29 @@ router.get('/:id', (req, res, next) => {
     res.render('celebrities/show', {
       celebrity : celebrity
     });
+  });
+});
+
+// /celebrities	| POST | Send the data from the form to this
+// route to create the celebrity and save to the database
+router.post('/', (req, res, next) => {
+  const celebrityInfo = {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    imgUrl: req.body.imgUrl,
+    alive: req.body.alive,
+    catchPhrase: req.body.catchPhrase
+  };
+
+  const newCelebrity = new Celebrities(celebrityInfo);
+
+  newCelebrity.save((err) => {
+    if (err) {
+      return res.render('celebrities/new', {
+        celebrity: newCelebrity
+      });
+    }
+    return res.redirect('/celebrities');
   });
 });
 
