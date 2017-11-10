14bbdcd5ee65cea0d937378daa024a0502b846fb
gemaderus <hi@gemaderus.com>
2017-11-10 09:09:24 +0100

    W/Node Modules

--- starter-code/movies/bin/seeds.js
+++ starter-code/movies/bin/seeds.js
@@ -1,74 +1,74 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/movies', {useMongoClient: true});
 
-// const Celebrity = require('../model/celebrity');
-//
-// const celebrity = [
-//  {
-//    name  : "Nicolas Cage",
-//    occupation : "Actor",
-//    catchPhrase   : "Live the moment",
-//  },
-//
-//  {
-//    name  : "Judith Mascó",
-//    occupation : "Model",
-//    catchPhrase   : "Happy month",
-//  },
-//
-//  {
-//    name  : "Alejandro Sanz",
-//    occupation : "Singer",
-//    catchPhrase   : "Don't touch the music",
-//  },
-// ];
-//
-// Celebrity.collection.drop();
-//
-// Celebrity.create(celebrity, (err, docs) => {
-//   if (err) {
-//     throw err;
-//   }
-//
-//   docs.forEach((celebrity) => {
-//     console.log(celebrity.name);
-//   });
-//
-//   mongoose.connection.close();
-// });
+const Celebrity = require('../model/celebrity');
 
-const Movies = require('../model/movies');
-
-const movies = [
+const celebrity = [
  {
-   title  : "gosth",
-   genre : "Romantic",
-   plot   : "Sam and Molly are a very happy couple and deeply in love. Walking back to their new apartment after a night out at the theatre, they encounter a thief in a dark alley, and Sam is murdered.",
+   name  : "Nicolas Cage",
+   occupation : "Actor",
+   catchPhrase   : "Live the moment",
  },
 
  {
-   title  : "Tears and Smiles",
-   genre : "Musical",
-   plot   : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.",
+   name  : "Judith Mascó",
+   occupation : "Model",
+   catchPhrase   : "Happy month",
  },
 
  {
-   title  : "Gone with the Wind",
-   genre : "Comedy",
-   plot   : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor",
+   name  : "Alejandro Sanz",
+   occupation : "Singer",
+   catchPhrase   : "Don't touch the music",
  },
 ];
 
-Movies.collection.drop();
+Celebrity.collection.drop();
 
-Movies.create(movies, (err, docs) => {
+Celebrity.create(celebrity, (err, docs) => {
   if (err) {
     throw err;
   }
 
-  docs.forEach((movie) => {
-    console.log(movie.title);
+  docs.forEach((celebrity) => {
+    console.log(celebrity.name);
   });
 
   mongoose.connection.close();
 });
+
+// const Movies = require('../model/movies');
+//
+// const movies = [
+//  {
+//    title  : "gosth",
+//    genre : "Romantic",
+//    plot   : "Sam and Molly are a very happy couple and deeply in love. Walking back to their new apartment after a night out at the theatre, they encounter a thief in a dark alley, and Sam is murdered.",
+//  },
+//
+//  {
+//    title  : "Tears and Smiles",
+//    genre : "Musical",
+//    plot   : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.",
+//  },
+//
+//  {
+//    title  : "Gone with the Wind",
+//    genre : "Comedy",
+//    plot   : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor",
+//  },
+// ];
+//
+// Movies.collection.drop();
+//
+// Movies.create(movies, (err, docs) => {
+//   if (err) {
+//     throw err;
+//   }
+//
+//   docs.forEach((movie) => {
+//     console.log(movie.title);
+//   });
+//
+//   mongoose.connection.close();
+// });
