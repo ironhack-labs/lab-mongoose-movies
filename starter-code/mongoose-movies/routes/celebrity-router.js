3ed24b2c980685cf08488c50c333c2b46540d165
Alex Del Valle <amd407@gmail.com>
2017-11-16 14:41:56 -0500

    Iteration 4 Complete

--- starter-code/mongoose-movies/routes/celebrity-router.js
+++ starter-code/mongoose-movies/routes/celebrity-router.js
@@ -18,6 +18,9 @@ router.get("/celebrities", (req, res, next) => {
   });
 });
 
+router.get("/celebrities/new", (req, res, next) => {
+  res.render("celebrity-views/celebrity-add");
+});
 
 router.get("/celebrities/:celebId", (req, res, next) => {
   CelebrityModel.findById(req.params.celebId)
@@ -32,6 +35,57 @@ router.get("/celebrities/:celebId", (req, res, next) => {
   });
 });
 
+router.post("/celebrities", (req, res, next) => {
+
+  const theCelebrity = new CelebrityModel ({
+      name: req.body.celebrityName,
+      occupation: req.body. celebrityOccupation,
+      catchPhrase: req.body.celebrityCatchPhrase,
+      imageUrl: req.body.celebrityImageUrl,
+  });
+
+  theCelebrity.save()
+    .then( () => {
+        res.redirect("/celebrities");
+    })
+    .catch( (err) => {
+      if (theCelebrity.errors) {
+          res.locals.validationErrors
+      }
+    })
+});
+
+
+
+
+
+router.post("celebrities/:celebId", (req, res, next) => {
+  CelebrityModel.findById(req.params.celebId)
+  .then((celebrityFromDb) => {
+    celebrityFromDb.set({
+      name: req.body.celebrityName,
+      occupation: req.body. celebrityOccupation,
+      catchPhrase: req.body.celebrityCatchPhrase,
+      imageUrl: req.body.celebrityImageUrl,
+  });
+
+  res.locals.celebrityDetails = celebrityFromDb;
+
+  return celebrityFromDb.save();
+  })
+  .then( () => {
+    res.redirect(`/celebrities/${req.params.celebId}`);
+  })
+  .catch( (err) => {
+    if (err.errors) {
+      res.locals.validationErrors = err.errors;
+      res.render("celebrity-views/celebrity-add");
+    }
+    else {
+      next(err);
+    }
+  });
+});
 
 
 module.exports = router;
