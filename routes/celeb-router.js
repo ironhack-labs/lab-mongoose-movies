b41dc90919b2644bd980ba2be70feab566e4aff9
jamison05 <31230497+jamison05@users.noreply.github.com>
2017-11-17 00:38:44 -0500

    Add files via upload

--- routes/celeb-router.js
+++ routes/celeb-router.js
@@ -3,6 +3,7 @@
       console.log("Initialized the call-back2");
       const MovieModel = require("../models/movie-model");
       const router = express.Router();
+
       console.log("Initialized the call-back3");
 
 // router.get("/celeb", (req, res, next) => {
@@ -38,44 +39,41 @@ router.get('/celebrities', (req, res, next) => {
    });
 
 
+
+  //  // STEP #2: process the new product submission
+  //  router.get("/celebrities/show", (req, res, next) => {
+  //      const theCelebrity = new movieModel({
+   //
+  //          name:req.body.name,
+  //          occupation:req.body.occupation,
+  //          catchPhrase:req.body.catchPhrase, // |
+  //      }); // |                     // |
+   //
+   //
+  //      theCelebrity.save()
+  //        .then(() => {
+   //
+  //            res.redirect("/celebrities/");
+   //
+  //        })
+  //        .catch((err) => {
+   //
+  //            if (err.errors) {
+  //                res.locals.validationErrors = err.errors;
+  //                //res.render("celebrities-views/celebrity-form");
+  //            }
+   //
+  //            else {
+  //                next(err);
+  //            }
+  //        });
+  //  }); // POST /products
+
    //
    router.get("/celebrities/new", (req, res, next) => {
        res.render("celebrities-views/new");
    }); // GET /products/new
    //
-   // STEP #2: process the new product submission
-   router.get("/celebrities/show", (req, res, next) => {
-       const thecelebrity = new movieModel({
-
-           name:req.body.name,
-           occupation:req.body.occupation,
-           catchPhrase:req.body.catchPhrase, // |
-       }); // |                     // |
-           // fields from         names of the
-           // model's schema      input tags
-
-       theProduct.save()
-         .then(() => {
-             // STEP #3: redirect after a SUCCESSFUL save
-             // redirect to the list of products page
-             res.redirect("/celebrities/");
-               // you CAN'T redirect to an EJS file
-               // you can ONLY redirect to a URL
-         })
-         .catch((err) => {
-             // is this a validation error?
-             // if it is then display the form with the error messages
-             if (err.errors) {
-                 res.locals.validationErrors = err.errors;
-                 //res.render("celebrities-views/celebrity-form");
-             }
-             // if it isn't then render the error page with our error
-             else {
-                 next(err);
-             }
-         });
-   }); // POST /products
-
    router.get('/celebrities/:id', (req, res, next) => {
      MovieModel.findById(req.params.id)
      .then((celebrityFromDb) => {
@@ -88,8 +86,41 @@ router.get('/celebrities', (req, res, next) => {
          next(err);
      });
    }); // GET /products/details
+   router.post("/celebrities", (req, res, next) => {
+       const theCelebrity = new MovieModel({
+           name:req.body.celebrityName,
+           occupation:req.body.celebrityOccupation,
+           catchPhrase:req.body.celebrityCatchPhrase// |
 
+       }); // |                     // |
 
+             theCelebrity.save().then(() => {
+             res.redirect("/celebrities/show");
+
+         })
+         .catch((err) => {
+           if (err.errors){
+             res.render("celebrities-views/new");
+             console.log(err);
+          }else{
+             next(err);}
+         });
+   }); // POST /products
+
+   // use this or the POST version of deleting (not both)
+   router.get("/celebrities/:id/delete", (req, res, next) => {
+       MovieModel.findByIdAndRemove(req.params.id)
+         .then((celebFromDb) => {
+             // redirect to the list of products page
+             // (you can't see the details of a product that isn't in the DB)
+             res.redirect("/celebrities");
+               // you CAN'T redirect to an EJS file
+               // you can ONLY redirect to a URL
+         })
+         .catch((err) => {
+             next(err);
+         });
+   });
 
 
    module.exports = router;
