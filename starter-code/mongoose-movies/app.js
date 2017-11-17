9fe6f9d4bc6a7a7ce0aa3dc5b80f604796aaaba6
Alex Del Valle <amd407@gmail.com>
2017-11-16 23:59:45 -0500

    Added some basic styling to pages

--- starter-code/mongoose-movies/app.js
+++ starter-code/mongoose-movies/app.js
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'celebrity information.';
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
