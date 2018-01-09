c231ec1c91119a54bfbd9caa5ed61bd35bfffc8f
VivianSolide <vivian.sarazin@gmail.com>
2018-01-09 11:45:46 +0100

    iteration #4 done

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const Celebrity = require('../models/celebrity.js');
 
-router.get('/', (req, res, next) => {
+router.get('/celebrities/', (req, res, next) => {
   Celebrity.find({}, (err, celebrities) => {
     if (err) {
       console.error(err);
@@ -14,9 +14,12 @@ router.get('/', (req, res, next) => {
   });
 });
 
-router.get('/:id', (req, res, next) => {
-  let id = req.params.id
+router.get('/celebrities/new', (req, res, next) => {
+  res.render('celebrities/new')
+})
 
+router.get('/celebrities/:id', (req, res, next) => {
+  let id = req.params.id
   Celebrity.findById(id, (err, celeb) => {
     if (err) {
       next(err);
@@ -26,5 +29,21 @@ router.get('/:id', (req, res, next) => {
     });
   });
 });
+
+router.post('/celebrities/', (req, res, next) => {
+  const celebInfo = {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase
+  }
+  const newCelebrity = new Celebrity(celebInfo);
+  newCelebrity.save((err) => {
+    if (err) {
+      console.error(err);
+      return res.redirect('/celebrities/new');
+    }
+    return res.redirect('/celebrities');
+  })
+})
 
 module.exports = router;
