2914f59491febe7a663325c332aab7553d47b449
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-01-15 13:04:36 +0100

    Feat: Starting

---
+++ starter-code/movies/controllers/movies.controller.js
@@ -0,0 +1,10 @@
+// const Product = require('../models/product.model');
+
+module.exports.index = (req, res, next) => {
+  res.send("hhhhhh00");
+  // Product.find({}).then((products) => {
+  //   res.render('products/index', {
+  //     products: products
+  //   });
+  // });
+};
---
+++ starter-code/movies/routes/movies.routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const moviesController = require('../controllers/movies.controller');
+
+router.get('/', moviesController.index);
+router.get('/', moviesController.index);
+
+// router.get('/new', moviesController.new);
+// router.post('/', moviesController.create);
+
+// router.get('/:id/edit', moviesController.edit);
+// router.post('/:id', moviesController.update);
+
+// router.get('/:id', moviesController.show);
+
+// router.post('/:id/delete', moviesController.delete);
+
+module.exports = router;
