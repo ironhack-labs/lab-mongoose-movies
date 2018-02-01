f754b528893ed112fa405eb670c738755e9a4409
Ana Recuero <anarecsa@gmail.com>
2018-02-01 23:34:06 +0100

    trying to show form

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -5,7 +5,6 @@ const Celebrity = require('../models/Celebrity');
 // READ 
 router.get('/', (req, res, next) => {
   Celebrity.find().exec((err, celebrities) => {
-    console.log(celebrities)
     if (err) { return next(err); }
     res.render('celebrities/index', {
       celebrities: celebrities
@@ -16,8 +15,24 @@ router.get('/', (req, res, next) => {
 router.get('/:id', (req,res,next) => {
   const celebId = req.params.id;
   Celebrity.findById(celebId).exec((err,celebrity) => {
+    if (err) { return next(err); }
     res.render('celebrities/show', { celebrity: celebrity });
   })
 })
+
+// CREATE
+router.get('/new', (req, res) => {
+  console.log("new")
+  res.render('celebrities/new');
+});
+
+// router.post('/new', (req, res) => {
+//   const {name,occupation,catchPhrase} = req.body;
+//   const celebrity = new Product({name,occupation,catchPhrase});
+//   product.save( err => {
+//     if (err) { return next(err) }
+//     res.redirect('/');
+//   })
+// });
 
 module.exports = router;
