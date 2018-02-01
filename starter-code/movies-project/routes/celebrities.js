34ca39ea99edc67f6e92576a2ffbf318d83ab06a
Luis Herrera <la.herrera457@gmail.com>
2018-02-01 21:03:41 +0100

    iteration 3 working

--- starter-code/movies-project/routes/celebrities.js
+++ starter-code/movies-project/routes/celebrities.js
@@ -6,12 +6,20 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   Celebrity.find()
   .then(portfolio => {
-
     res.render('celebrities/index', {portfolio})
   })
   .catch(err => {
     return next(err);
   })
+});
+
+router.get('/show/:id', (req, res, next) => {
+  const celebId = req.params.id;
+
+  Celebrity.findById(celebId, (err, famous) => {
+    if (err) { return next(err); }
+    res.render('celebrities/show', { famous: famous });
+  });
 });
 
 module.exports = router;
