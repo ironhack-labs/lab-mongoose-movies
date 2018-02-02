ee1b59f3294929fb5fff4e4cfa940d84c73480b2
Luis Herrera <la.herrera457@gmail.com>
2018-02-02 14:55:47 +0100

    bonus iteration for celebrities done

--- starter-code/movies-project/routes/celebrities.js
+++ starter-code/movies-project/routes/celebrities.js
@@ -17,11 +17,25 @@ router.get('/new', (req, res, next) => {
   res.render('celebrities/new');
 });
 
+router.get('/:id/edit', (req, res, next) => {
+  const celebId = req.params.id;
+
+  Celebrity.findById(celebId, (err, famous) => {
+    if (err) { 
+      return next(err); 
+    }
+    res.render('celebrities/edit', { famous: famous });
+  });
+
+})
+
 router.get('/:id', (req, res, next) => {
   const celebId = req.params.id;
 
   Celebrity.findById(celebId, (err, famous) => {
-    if (err) { return next(err); }
+    if (err) { 
+      return next(err); 
+    }
     res.render('celebrities/show', { famous: famous });
   });
 });
@@ -37,7 +51,7 @@ router.post('/', (req, res, next) => {
 
   freshCelebrity.save((err) => {
     if (err){
-      res.render('celebrities/new');
+      return next(err);
     } else {
       res.redirect('/celebrities');
     }
@@ -48,9 +62,29 @@ router.post('/:id/delete', (req, res, next) => {
   const celebId = req.params.id;
 
   Celebrity.findByIdAndRemove(celebId, (err, celebrity) => {
-    if (err){ return next(err); }
+    if (err){ 
+      return next(err); 
+    }
     return res.redirect('/celebrities');
   });
 });
+
+router.post('/:id', (req, res, next) => {
+  const celebId = req.params.id;
+
+  const updateInfo = {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchphrase
+  }
+
+  Celebrity.findByIdAndUpdate(celebId, updateInfo, (err, celebrity) => {
+    if (err){ 
+      return next(err); 
+    }
+    return res.redirect('/celebrities');
+  });
+  
+})
 
 module.exports = router;
