229a9dc397c1f2b2138d51a211807aa7963195a2
Martin <mar.gardemann@gmail.com>
2018-02-02 11:53:51 +0100

    Iteration5

--- starter-code/nameOfYourProject/app.js
+++ starter-code/nameOfYourProject/app.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 const express = require('express');
 const path = require('path');
@@ -6,14 +6,13 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const mongoose       = require('mongoose'); //
+const mongoose = require('mongoose'); //
 
 const index = require('./routes/index');
 const users = require('./routes/users');
 const celebrities = require('./routes/celebrities');
 
 mongoose.connect('mongodb://localhost/celeb-movies-db'); //
-
 
 const app = express();
 
@@ -22,7 +21,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,17 +30,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/',celebrities);
+app.use('/', celebrities);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -50,7 +49,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-
 
 module.exports = app;
--- starter-code/nameOfYourProject/routes/celebrities.js
+++ starter-code/nameOfYourProject/routes/celebrities.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 const express = require('express');
 const router = express.Router();
@@ -6,31 +6,29 @@ const router = express.Router();
 // require the Celebrity model here
 const Celebrity = require('../models/celebrity');
 
-
 // router.get("/", (req, res) => {
 //     res.redirect('/celebrities');
 //   });
 
 router.get('/celebrities', (req, res, next) => {
   // Iteration #2
-  Celebrity.find({},(err, celebArray) => {
-      if (err) {
-        throw err;
-      } else {
-        let celebObject = {celebritiesList: celebArray};
-        res.render('celebrities/index', celebObject);
-      }
-    });
+  Celebrity.find({}, (err, celebArray) => {
+    if (err) {
+      throw err;
+    } else {
+      let celebObject = {celebritiesList: celebArray};
+      res.render('celebrities/index', celebObject);
+    }
+  });
 });
 
-router.get('/celebrities/new',(req,res,next) => {
-    res.render('celebrities/new');
+router.get('/celebrities/new', (req, res, next) => {
+  res.render('celebrities/new');
 });
-
 
 router.get('/celebrities/:id', (req, res, next) => {
   let parsedName = req.params.id;
-  Celebrity.findOne({name:parsedName},(err, celebMatch) => {
+  Celebrity.findOne({name: parsedName}, (err, celebMatch) => {
     if (err) {
       throw err;
     } else {
@@ -40,24 +38,33 @@ router.get('/celebrities/:id', (req, res, next) => {
   });
 });
 
+router.post('/celebrities', (req, res, next) => {
+  let inputCeleb = {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase
+  };
 
-router.post('/celebrities',(req,res,next) => {
-    let inputCeleb = {
-      name: req.body.name,
-      occupation: req.body.occupation,
-      catchPhrase: req.body.catchPhrase
-    };
-
-    const newCeleb = new Celebrity(inputCeleb);
-    newCeleb.save((err) =>{  
-      if (err) {
+  const newCeleb = new Celebrity(inputCeleb);
+  newCeleb.save((err) => {
+    if (err) {
       res.render('celebrities/new');
-    } else { 
+    } else {
       res.redirect('celebrities');
     }
-    });
-
   });
+});
 
+router.post('/celebrities/:id/delete', (req, res, next) => {
+  let parsedId = req.params.id;
+
+  Celebrity.findByIdAndRemove({_id: parsedId}, (err, celebObject) => {
+    if (err) {
+      return next(err);
+    } else {
+      res.redirect('/celebrities');
+    }
+  });
+});
 
 module.exports = router;
