190bb5d01194e7759f59a540a1998895ec726ba8
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-02-02 11:29:07 +0100

    finished all celebrity iterations

--- mongoose-movies/app.js
+++ mongoose-movies/app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+// var users = require('./routes/users');
 var celebrities = require('./routes/celebrities');
 
 mongoose.connect('mongodb://localhost/celebrities')
--- mongoose-movies/routes/celebrities.js
+++ mongoose-movies/routes/celebrities.js
@@ -13,14 +13,79 @@ router.get('/celebrities', (req, res, next) => {
       });
 });
 
+router.get('/celebrities/new', (req, res, next) => {
+      res.render('celebrities/new');
+});
+router.get('/celebrities/:id/edit', (req, res, next) => {
+  const celebrityId = req.params.id
+  Celebrity.findById(celebrityId, (err, celebrities) => {
+    if (err) { return next(err); }
+
+    res.render('celebrities/edit', {
+      celebrities: celebrities
+    });
+  });
+})
+
 router.get('/celebrities/:id', (req, res, next) => {
-  Celebrity.findOne({id: celebrity.id}, (err, celebrities) => {
+  const celebrityId = req.params.id
+
+  Celebrity.findById(celebrityId, (err, celebrities) => {
       if (err) { return next(err); }
-  
+
       res.render('celebrities/show', {
         celebrities: celebrities
       });
     });
 });
 
+
+
+
+router.post('/celebrities', (req, res, next) => {
+  let newCeleb = {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase
+  }
+
+  let celeb = new Celebrity(newCeleb)
+  celeb.save((err, docs) => {
+    if (err) {
+      console.log(err)
+    }
+    else {
+      console.log(docs)
+    }
+    res.redirect('/celebrities')
+  })
+});
+
+router.post('/celebrities/:id/delete', (req, res, next) => {
+  const celebrityId = req.params.id
+  Celebrity.findByIdAndRemove(celebrityId, (err, celebrities) => {
+    if (err) { return next(err); }
+
+    res.redirect('/celebrities');
+  });
+})
+
+
+router.post('/celebrities/:id', (req, res, next) => {
+  let updatedCeleb = {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase
+  }
+  let celebIDtoUpdate = req.params.id
+  Celebrity.findByIdAndUpdate(
+    celebIDtoUpdate,
+    updatedCeleb,
+    (err, data) => {
+    if (err) {
+      next()
+    }
+    res.redirect('/celebrities')
+  })
+})
 module.exports = router;
