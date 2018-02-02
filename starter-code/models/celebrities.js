2729e736ec0a0ffc512d5107c9dceee9656d4447
Beltran Rengifo <merloc@gmail.com>
2018-02-02 09:11:31 +0100

    some fixes

--- starter-code/models/celebrities.js
+++ starter-code/models/celebrities.js
@@ -2,10 +2,26 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CelebritySchema = new Schema({
-    name: String,
-    occupation: String,
-    catchPhrase: String
+    name: {
+        type: String,
+        min: [3, 'Hey, por un nombre de más de 3 letras'],
+        max: [20, 'Máx. 20 letras'],
+        required: [true, 'Obligatorio, tronqui monqui']
+    },
+    occupation: {
+        type: String,
+        min: [3, 'Hey, por un nombre de más de 3 letras'],
+        max: [20, 'Máx. 20 letras'],
+        required: [true, 'Obligatorio, tronqui monqui']
+    },
+    catchPhrase: {
+        type: String,
+        min: [3, 'Hey, por un nombre de más de 3 letras'],
+        max: [20, 'Máx. 20 letras'],
+        required: [true, 'Obligatorio, tronqui monqui']
+    }
 });
+
 
 const Celebrity = mongoose.model('Celebrity', CelebritySchema);
 module.exports = Celebrity;
