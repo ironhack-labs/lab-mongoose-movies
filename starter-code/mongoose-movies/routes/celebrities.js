d9f92b6598ce3d09eed331c5e36e14d7a187bb81
mosi.rivera <mosileko@yahoo.com>
2018-02-15 19:16:39 -0500

    Initial Commit

--- starter-code/mongoose-movies/routes/celebrities.js
+++ starter-code/mongoose-movies/routes/celebrities.js
@@ -45,6 +45,14 @@ router.get('/:id/delete', (req, res, next) => {
   return res.redirect('/celebrities')
 })
 
+router.get('/:id/edit', (req, res, next) => {
+
+})
+
+router.post('/:id', (req, res, next) => {
+  
+})
+
 router.get('/:id', (req, res, next) => {
   let id = req.params.id;
   console.log(id);
