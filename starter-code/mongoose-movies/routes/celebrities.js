b4dd99a31c1fa67ee026fdcec2aeaa4ef8b51871
Juan Sanchez Mesa <sanchezmesaj@gmail.com>
2018-04-14 10:39:17 +0200

    Iteration 4 - Add new celebrity

--- starter-code/mongoose-movies/routes/celebrities.js
+++ starter-code/mongoose-movies/routes/celebrities.js
@@ -2,6 +2,26 @@ const express = require('express');
 const router  = express.Router();
 const Celebrity = require("../models/Celebrity");
 
+// GET new celebrity
+router.get("/new", (req, res, next) => {
+  res.render("celebrities/new");
+});
+
+// POST new celebrity
+router.post("/", (req, res, next) => {
+  const {name, occupation, catchPhrase} = req.body;
+  const celebrity = new Celebrity({name, occupation, catchPhrase});
+
+  celebrity.save()
+    .then( celebrity => {
+      console.log(celebrity);
+      res.redirect("/celebrities");
+    })
+    .catch( () => {
+      res.render("celebrities/new");
+    })
+});
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   Celebrity.find()
@@ -18,6 +38,8 @@ router.get("/:id", (req, res, next) => {
     .then( celebrity => {
       res.render("celebrities/show", {celebrity} );
     })
-})
+});
+
+
 
 module.exports = router;
