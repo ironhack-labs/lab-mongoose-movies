7aa6b72e59f6cabea56291382b8f5bf65e6bb82e
Juan Sanchez Mesa <sanchezmesaj@gmail.com>
2018-04-14 11:21:11 +0200

    Iteration 9 - Movie detail

--- starter-code/mongoose-movies/routes/movies.js
+++ starter-code/mongoose-movies/routes/movies.js
@@ -2,6 +2,14 @@ const express = require('express');
 const router  = express.Router();
 const Movie = require("../models/Movie");
 
+// GET detail
+router.get("/:id", (req, res, next) => {
+  Movie.findById( req.params.id)
+    .then( movie => {
+      res.render("movies/show", {movie} );
+    })
+});
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   Movie.find()
