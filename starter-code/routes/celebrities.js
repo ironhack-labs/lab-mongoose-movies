d027b5d60f4589e0b338611e100e44e6f07ded0e
A-Tr <alvarotrancon@gmail.com>
2018-04-14 13:45:19 +0200

    finalizada iteracion 4

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -4,7 +4,21 @@ const Celebrity = require("../models/Celebrity");
 
 router.get("/celebrities", (req, res) => {
   Celebrity.find()
-    .then(celebrities => res.render("celebrities", {celebrities}))
+    .then(celebrities => res.render("celebrities", { celebrities }))
+    .catch(err => console.log(err));
+});
+
+// GET Create element CRUD
+router.get("/celebrities/new", (req, res) => {
+  res.render("celebrities/new");
+});
+
+// POST Create element CRUD
+router.post("/celebrities", (req, res) => {
+  const { name, occupation, catchPhrase } = req.body;
+  const celebrity = new Celebrity({ name, occupation, catchPhrase });
+  celebrity.save()
+    .then(() => res.redirect("/celebrities"))
     .catch(err => console.log(err));
 });
 
@@ -12,7 +26,7 @@ router.get("/celebrities/:id", (req, res) => {
   console.log(req.params.id);
   Celebrity.findById(req.params.id)
     .then(celebrity => res.render("celebrities/cb_details", celebrity))
-    .catch(err => console.log(err))
-})
+    .catch(err => console.log(err));
+});
 
 module.exports = router;
