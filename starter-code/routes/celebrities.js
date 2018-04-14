d28b7112daa230cb52ea2b9d5968883a42e93c4a
Alejandro <alexibape@hotmail.com>
2018-04-15 01:41:09 +0200

    Iteration3 done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -5,10 +5,25 @@ const router  = express.Router();
 
 const Celebrity = require("../models/Celebrity")
 
+
+//----------GET CELEBRITY INDEX------------
+
 router.get("/index",(req, res, next) => {
   Celebrity.find().then(celebrities => {
     
     res.render("celebrities/index",{celebrities});
+  })
+  .catch(err => {
+    console.log(err);
+  });
+});
+
+
+//-----------CELEBRITIES DETAILS---------
+
+router.get("/:id", (req, res, next) => {
+  Celebrity.findById(req.params.id).then(celebrity => {
+    res.render("celebrities/show", {celebrity});
   })
   .catch(err => {
     console.log(err);
