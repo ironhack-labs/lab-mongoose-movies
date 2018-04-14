26d1de2415da27335e53f5167ac737e125381e57
Juan Sanchez Mesa <sanchezmesaj@gmail.com>
2018-04-14 10:22:12 +0200

    Iteration 3 - Celebrity detail

--- starter-code/mongoose-movies/routes/celebrities.js
+++ starter-code/mongoose-movies/routes/celebrities.js
@@ -6,10 +6,18 @@ const Celebrity = require("../models/Celebrity");
 router.get('/', (req, res, next) => {
   Celebrity.find()
     .then( celebrities => {
-      res.render("celebrities", {celebrities});
+      res.render("celebrities/index", {celebrities});
     })
     .catch( error => {
     })
 });
+
+// GET detail
+router.get("/:id", (req, res, next) => {
+  Celebrity.findById( req.params.id)
+    .then( celebrity => {
+      res.render("celebrities/show", {celebrity} );
+    })
+})
 
 module.exports = router;
