fe74fdf8696abd3f1ae3d8233367c2e016b5a90e
luisnueda <nueda.luis@gmail.com>
2018-04-14 21:03:37 +0200

    Iteracion 1

---
+++ starter-code/lab-movies/bin/celebrity_data.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    name: "Ralph Waldo Emerson",
+    occupation: "Writer",
+    catchPhrase: "Every artist was first an amateur"
+  },
+  {
+    name: "Henri Matisse",
+    occupation: "Painter",
+    catchPhrase: "Creativity takes courage"
+  },
+  {
+    name: "Pablo Picasso",
+    occupation: "Painter",
+    catchPhrase: "Every child is an artist. The problem is how to remain an artist once we grow up."
+  }
+]
---
+++ starter-code/lab-movies/bin/celebrity_seed.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Celebrity = require("../models/Celebrity");
+const celebrity_data = require("./celebrity_data");
+
+const dbURL = "mongodb://localhost/lab-movies";
+
+mongoose.connect(dbURL).then(() =>{
+  console.log(`Conected to db ${dbURL}`);
+
+  celebrity_data.forEach( e => {
+
+    let celebrity = new Celebrity({
+      name: e.name,
+      occupation: e.occupation,
+      catchPhrase: e.catchPhrase
+    })
+    .save()
+    .then(() => {
+      console.log("Celebrity created");
+      mongoose.disconnect();
+    })
+  })
+})
