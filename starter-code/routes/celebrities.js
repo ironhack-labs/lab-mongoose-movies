6730b0aea347089457aabea472527b06deae488e
A-Tr <alvarotrancon@gmail.com>
2018-04-14 15:32:25 +0200

    casi finalizada la edicion de actores

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -13,7 +13,7 @@ router.get("/celebrities/new", (req, res) => {
   res.render("celebrities/new");
 });
 
-// POST Create element CRUD
+// POST Create Celebrity
 router.post("/celebrities", (req, res) => {
   const { name, occupation, catchPhrase } = req.body;
   const celebrity = new Celebrity({ name, occupation, catchPhrase });
@@ -22,21 +22,37 @@ router.post("/celebrities", (req, res) => {
     .catch(err => console.log(err));
 });
 
+//GET Edit celebrity
+router.get("/celebrities/:id/edit", (req, res) => {
+  Celebrity.findById(req.params.id)
+  .then(celebrity => res.render("celebrities/edit", celebrity))
+  .catch(err => console.log(err)) 
+})
+
+// POST Edit celebrity
+router.post("/celebrities/:id", (req, res) => {
+  const { name, occupation, catchPhrase } = req.body;
+  const update = { name, occupation, catchPhrase };
+  Celebrity.findByIdAndUpdate(req.params.id, update)
+  .then(celebrity => 
+    {console.log("modificacion exitosa");
+    res.render("celebrities/cb_details", celebrity)})
+})
 
 // GET Show one celebrity
 router.get("/celebrities/:id", (req, res) => {
-  console.log(req.params.id);
   Celebrity.findById(req.params.id)
-    .then(celebrity => res.render("celebrities/cb_details", celebrity))
+    .then(celebrity =>{ 
+      res.render("celebrities/cb_details", celebrity)})
     .catch(err => console.log(err));
 });
 
 // POST DELETE CELEBRITY
 router.post("/celebrities/:id/delete", (req, res) => {
-  console.log(req.params.id)
   Celebrity.findByIdAndRemove(req.params.id)
   .then(() => res.redirect("/celebrities"))
   .catch(err => console.log(err))
 })
+
 
 module.exports = router;
