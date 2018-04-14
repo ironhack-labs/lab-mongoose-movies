00694e0921d8ae624a7d7672aeedd3e8d64b15d1
Juan Sanchez Mesa <sanchezmesaj@gmail.com>
2018-04-14 11:26:47 +0200

    Iteration 10 - Add movie

--- starter-code/mongoose-movies/routes/movies.js
+++ starter-code/mongoose-movies/routes/movies.js
@@ -2,6 +2,26 @@ const express = require('express');
 const router  = express.Router();
 const Movie = require("../models/Movie");
 
+// GET new movie
+router.get("/new", (req, res, next) => {
+  res.render("movies/new");
+});
+
+// POST new movie
+router.post("/", (req, res, next) => {
+  const {title, genre, plot} = req.body;
+  const movie = new Movie({title, genre, plot});
+
+  movie.save()
+    .then( movie => {
+      console.log(movie);
+      res.redirect("/movies");
+    })
+    .catch( () => {
+      res.render("movies/new");
+    })
+});
+
 // GET detail
 router.get("/:id", (req, res, next) => {
   Movie.findById( req.params.id)
