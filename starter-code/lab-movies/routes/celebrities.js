d56c1d5fe3b6f62ecdf70a078316c23e18219160
luisnueda <nueda.luis@gmail.com>
2018-04-15 02:08:00 +0200

    Iteración 3 completada

--- starter-code/lab-movies/routes/celebrities.js
+++ starter-code/lab-movies/routes/celebrities.js
@@ -5,14 +5,25 @@ const debug = require("../log")(__filename);
 const Celebrity = require("../models/Celebrity");
 
 router.get('/', (req, res, next) => {
-
   Celebrity.find().then(celebrities =>{
     //debug(celebrities)
-    res.render('../views/celebrities/index', {celebrities});
+    res.render('celebrities/index', {celebrities});
   })
-  .catch(error => {
-    console.log(error)
-  })
+  .catch(error => console.log(error))
 });
+
+router.get("/:id", (req, res, next) => {
+
+  Celebrity.findById(req.params.id)
+  .then(celebrity_detail => {
+    debug(celebrity_detail)
+    res.render("celebrities/show", {celebrity_detail});
+  })
+  .catch(error => console.log(error));
+});
+
+
+
+
 
 module.exports = router;
