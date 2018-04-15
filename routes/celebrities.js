3cfe716b7663ecbd0d76fa6249e0f4decccc2181
Pedro Piury Blanquez <pedropiuryblanquez@MacBook-Air-de-Pedro.local>
2018-04-15 11:27:26 +0200

    Iteracion 4 a falta de detalles

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -2,11 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Celebrity = require("../models/Celebrity");
 
-/* GET home page */
-// router.get('/', (req, res, next) => {
-//   res.render('index');
-// });
-
+//Index
 router.get('/', (req, res, next) => {
   Celebrity.find().then(celebrities_data => {
     //debug(celebrities_data);
@@ -18,6 +14,33 @@ router.get('/', (req, res, next) => {
     })
 });
 
+//New celebrity GET
+router.get('/new', (req, res, next) => {
+  Celebrity.findById(req.params.id).then(celeb_data => {
+    //debug(celebrities_data);
+    console.log(celeb_data)
+    res.render("celebrities/new", { celeb_data });
+  })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
+//New celebrity POST
+router.post('/', (req, res, next) => {
+  const { name, ocupation, catchPhrase } = req.body;
+  const celeb = new Celebrity({name, ocupation, catchPhrase})
+
+  Celebrity.save().then(celeb => {
+    console.log(celeb)
+    res.render("celebrities/show", { celeb_data });
+  })
+    .catch(error => {
+      res.redirect("celebrities/new")
+    })
+});
+
+//Id to show data 
 router.get('/:id', (req, res, next) => {
   Celebrity.findById(req.params.id).then(celeb_data => {
     //debug(celebrities_data);
@@ -28,6 +51,8 @@ router.get('/:id', (req, res, next) => {
       console.log(error)
     })
 });
+
+
 
 
 
