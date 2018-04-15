a63d7949f0ff44fc14adaaca21d0b4a92b2d9f53
Javier Valiente <javara00@gmail.com>
2018-04-15 19:53:04 +0200

    i wanna cry

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,39 +1,43 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
-const Celebrity = require('../models/celebrity')
+const Celebrity = require("../models/celebrity");
 
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
-router.get('/celebrities', (req, res, next) => {
+router.get("/celebrities", (req, res, next) => {
   Celebrity.find()
-  .then(celebrities => {
-    console.log("Consulta realizada!!")
-    console.log(celebrities)
-    res.render('celebrities', {celebrities});
-  })
-  .catch(error => {
-    console.log(error)
-  })
-});
-
-router.get('/celebrity/:id', (req, res, next) => {
-  let celebrityId = req.params.id;
-  Celebrity.findOne({'_id': celebrityId})
-    .then(showCelebrities => {
-      res.render("showCelebrities", {showCelebrities})
+    .then(celebrities => {
+      console.log("Consulta realizada!!");
+      console.log(celebrities);
+      res.render("celebrities", { celebrities });
     })
     .catch(error => {
-      console.log(error)
-    })
+      console.log(error);
+    });
 });
 
-router.get("/:id/edit", (req, res) => {
-  Celebrity.findById(req.params.id).then(celebrities => {
-    res.render("newCelebrities", { celebrities });
+router.get("/celebrity/:id", (req, res, next) => {
+  let celebrityId = req.params.id;
+  Celebrity.findOne({ _id: celebrityId })
+    .then(showCelebrities => {
+      res.render("showCelebrities", { showCelebrities });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+router.get("/newCelebrities", (req, res) => {
+  res.render("newCelebrities", { newCelebrities });
+});
+router.post("/newCelebrities", (req, res) => {
+  const { name, occupation, catchPhrase } = req.body;
+  const celebrity = new Celebrity({ name, occupation, catchPhrase });
+  celebrity.save().then(celebrity => {
+    res.redirect("celebrities");
   });
 });
-
 module.exports = router;
