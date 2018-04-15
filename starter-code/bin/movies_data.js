7bd811538f24d017b0cb65b61fc0ea21f70e8ca9
Alejandro <alexibape@hotmail.com>
2018-04-15 04:10:41 +0200

    Iteration7 done

---
+++ starter-code/bin/movies_data.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    title: 'Back to the future',
+    genre: 'Science Fiction',
+    plot: 'They go to the future',
+  },
+  {
+    title: 'Men in Black',
+    genre: 'Science Fiction',
+    plot: 'Aliens',
+  }, 
+  {
+    title: 'The Wolf of Wall Street',
+    genre: 'Comedy',
+    plot: 'The wolf goes rich',
+  },
+  
+];
