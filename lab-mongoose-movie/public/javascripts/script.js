ae6242bfa49a0d8ab349529d64fe2a7f4fdb8194
muflonex <muflonex@gmail.com>
2018-04-16 03:47:18 +0200

    Me he metido en el horror de intentar que catchPhrase sea un array y no me sale nuevo objeto. Ninguna manera de meter ese array dentro del objeto no parece funcionar. No sé hacer findById con if(err) return next(err) - no me sale y la documentación no dice mucho sobre eso. No sé utilizar el debug (simplemente poner debug(algo) no funciona). Además llevo un retraso porque el ejercicio de pareja tenía que hacer en realidad you solo.

--- lab-mongoose-movie/public/javascripts/script.js
+++ lab-mongoose-movie/public/javascripts/script.js
@@ -1,5 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("IronGenerator JS imported successfully!");
+  },
+  false
+);
 
-  console.log('IronGenerator JS imported successfully!');
+const addQuote = () => {
+  // Adds an element to the document
+  let newInput = document.createElement("input"),
+    destination = document.getElementById("quotesList").lastElementChild
+  destination.insertBefore(newInput, destination.lastElementChild);
+  newInput.setAttribute("type", "text");
+  newInput.setAttribute("name", "catchPhrase");
+};
 
-}, false);
+window.onload = () =>{
+  let addLink = document.getElementById("add")
+  addLink.onclick = addQuote
+}
---
+++ lab-mongoose-movie/routes/celebrities.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const router = express.Router();
+const Celebrity = require("../models/celebrity");
+
+//  Routing to celebrities index
+router.get("/", (req, res, next) => {
+  Celebrity.find((err, celebrities) => {
+    if (err) return next(err);
+    res.render("celebrities", { celebrities });
+  });
+});
+
+//  Routing to the individual celebrity
+router.get("/:id", (req, res, next) => {
+  Celebrity.findById(req.params.id)
+    .then(celebrity => {
+      res.render("celebrities/show", { celebrity });
+    })
+    .catch(err => {
+      next();
+      return err;
+    });
+});
+
+//  Routing to the form page
+router.get("/new", (req, res, next) => {
+  res.render("celebrities/new");
+});
+
+//  Routing the form data to the celebrity route
+router.post("/", (req, res, next) => {
+  //destructuring req.body into an object
+  const catchPhrase = req.body.catchPhrase;
+  const { name, occupation } = req.body;  
+  const celebrity = new Celebrity({ name, occupation });
+
+  celebrity.catchPhrase.push(catchPhrase)
+  
+  celebrity
+    .save()
+    .then(celebrity => {
+      console.log(`${celebrity.name} added to the database.`);
+      res.redirect("/celebrities");
+    })
+    // if values missing
+    .catch(() => {
+      res.render("celebrities/new");
+    });
+});
+
+router.get("/:id/delete", (req, res, next) => {
+  Celebrity.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.redirect("/celebrities");
+    })
+    .catch(err => {
+      next();
+      return err;
+    });
+});
+
+module.exports = router;
