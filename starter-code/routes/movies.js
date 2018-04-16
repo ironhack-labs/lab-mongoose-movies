b45c0d05190728f3f418a4c439cc20dc010a13c9
aacecandev <aacecan.dev@gmail.com>
2018-04-16 09:12:33 +0200

    Finished

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -14,4 +14,75 @@ router.get('/', (req, res) => {
     .catch( err => console.log(err))
 })
 
+/* CRUD Create new element */
+
+//GET
+router.get('/new', (req,res) => {
+  res.render('movies/new')
+})
+
+//POST
+router.post('/', (req,res) => {
+
+  const {title, genre, plot} = req.body;
+  const movie = new Movies({ title, genre, plot });
+  movie.save()
+    .then( () => {
+      res.redirect('/movies')
+    })
+    .catch(err => console.log(err))
+})
+
+/* CRUD Update element */
+router.get('/:id/edit', (req,res) => {
+  Movies.findById(req.params.id)
+    .then( (movie) => {
+      console.log(movie)
+      res.render('movies/edit', movie)
+    })
+    .catch(err => console.log(err))
+})
+
+router.post('/:id', (req, res) => {
+
+  const {title, genre, plot} = req.body;
+  
+  Movies.findByIdAndUpdate(req.params.id, {
+    title,
+    genre,
+    plot
+  })
+    .then( (movie) => {
+      res.redirect('/movies')
+      console.log(`${movie.title} successfully updated`);
+    })
+    .catch( err => console.log(err))
+})
+
+/* CRUD Delete element */
+router.post('/:id/delete', (req, res) => {
+  
+  Movies.findByIdAndRemove(req.params.id)
+    .then( () => res.redirect('/movies'))
+    .catch(err => console.log(err))
+})
+
+/* CRUD Retrieve element by ID */
+
+router.get('/:id', (req,res) => {
+
+  movieId = req.params.id;
+
+  Movies.findById(movieId)
+    .then( movie => {
+
+      const data = {
+        title: movie.name,
+        movie: movie
+      };
+      res.render('movies/show', data)
+    })
+    .catch(err => console.log(err))
+})
+
 module.exports = router;
