75129bd4bbed7e38f49de8bb3b57c935aff93d17
RISTO TAPANI RAIHALA <tyokone@RISTOs-MBP.home>
2018-04-16 08:12:48 +0200

    hasta aqui

---
+++ starter-code/lab-express-movies/bin/celebrities_data.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+      name: "Meat loaf",
+      occupation: "Singer",
+      catchPhrase: "My life is far too complicated to be summed up in one song."
+    },
+    {
+      name: "Drake",
+      occupation: "Singer",
+      catchPhrase: "Yolo."
+    },
+    {
+      name: "Snoop dogg",
+      occupation: "Singer",
+      catchPhrase: "Fo' shizzle."
+    },
+  ];
