9cff94de4c5b8714fc3b678e621f3abf1b6d2e48
Tommy DiLillo <tommydilillo@gmail.com>
2018-06-24 13:16:00 -0400

    completed #5 delete

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Celebrity = require("../models/celebrity.js");
 
+//CELEBRITIES PAGE
 router.get("/", (req, res, next) => {
   Celebrity.find()
     .then(celebrities => {
@@ -15,6 +16,7 @@ router.get("/", (req, res, next) => {
     });
 });
 
+// CELEBRITY DETAIL PAGE
 router.get("/:id", (req, res, next) => {
   let celebrityId = req.params.id;
   Celebrity.findOne({ _id: celebrityId })
@@ -28,9 +30,36 @@ router.get("/:id", (req, res, next) => {
     });
 });
 
+// ADD NEW CELEBRITY
 router.get("/new", (req, res, next) => {
   res.render("celebrities/new");
 });
 
+router.post("/new", (req, res, next) => {
+  const { name, occupation, catchphrase } = req.body;
+  const newCelebrity = new Celebrity({ name, occupation, catchphrase });
+  newCelebrity
+    .save()
+    .then(celebrity => {
+      res.redirect("/celebrities/");
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+//DELETE CELEBRITY
+router.post("/:id/delete", (req, res, next) => {
+  let celebrityId = req.params.id;
+  Celebrity.findByIdAndRemove({ _id: celebrityId })
+    .then(celebrity => {
+      console.log(`celebrity deleted`);
+    })
+    .catch(error => {
+      console.log(error);
+      next();
+    })
+    .next(res.redirect("/celebrities/"));
+});
 
 module.exports = router;
