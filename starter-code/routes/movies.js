361baea0e6af52120e2d243d5be80b5379e31b4b
Nicholas Kreps <nicholas.kreps@gmail.com>
2018-06-25 00:44:27 -0400

    completed

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -27,4 +27,58 @@ router.get('/:id', (req, res, next) => {
     })
 });
 
+router.get('/new', (req, res) => {
+  res.render("movies/new");
+});
+
+router.post('/', (req, res, next) => {
+  const {title, genre, plot} = req.body;
+  const newMovie= new Movie ({title, genre, plot});
+  newMovie.save()
+  .then(movie => {
+    // console.log(celebrities);
+    res.redirect("/movies");
+  })
+  .catch(error => {
+    console.log(error);
+    next();
+  })
+})
+
+router.post('/:id/delete', (req, res, next) => {
+  let movieId = req.params.id;
+  Movie.findByIdAndRemove({'_id':movieId})
+    .then(movie => {
+      res.redirect("/movies");
+    })
+    .catch(error => {
+      console.log(error);
+      next();
+    })
+})
+
+router.get('/:id/edit', (req, res, next) => {
+  let movieId = req.params.id;
+  Movie.findOne({'_id':movieId})
+    .then(movie => {
+      // console.log(celebrities);
+      res.render("movies/edit", { movie });
+    })
+    .catch(error => {
+      console.log(error);
+      next();
+    })
+});
+
+router.post('/:id', (req, res, next) => {
+  const {title, genre, plot} = req.body;
+  Movie.update({_id: req.params.id}, { $set: {title, genre, plot}}, { new: true })
+  .then((movie) => {
+    res.redirect("/movies")
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+});
+
 module.exports = router;
