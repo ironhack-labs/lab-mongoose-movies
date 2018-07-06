744096e0a14b7bc546fc8a38df355208ae98539a
Will Schaller <willschaller@gmail.com>
2018-07-06 20:31:55 +0200

    Added celebrities route to app.js

--- starter-code/mongoose-movies/app.js
+++ starter-code/mongoose-movies/app.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const celebritiesRouter = require('./routes/celebrities');
 
 // --- Instantiations
 const app = express();
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // --- Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/celebrities', celebritiesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
