d13b24a10305cf8cb1a6e13e30c4bf6e09055836
Will Schaller <willschaller@gmail.com>
2018-07-06 20:27:07 +0200

    App.js heading comments

--- starter-code/mongoose-movies/app.js
+++ starter-code/mongoose-movies/app.js
@@ -1,24 +1,28 @@
+// --- Dependencies
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
+// --- Instantiations
 const app = express();
 
+// --- Configurations
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+// --- Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// --- Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
