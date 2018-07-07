947d89358eb4c086eb7cc7425dce81e5df3eafe7
Paula <paulagarizcun@gmail.com>
2018-07-07 12:52:41 +0200

    Iteration 3

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -9,11 +9,19 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-/* C(R)UD: Retrieve -> List all books */
+/* C(R)UD: Retrieve -> List all celebrities */
 router.get('/celebrities', (req, res, next) => {
   Celebrity.find({}).then(celebrities => {
     console.log(celebrities);
     res.render('celebrities/index', {celebrities});
+  });
+});
+/* C(R)UD: Retrieve -> List the choosen celebrity */
+router.get('/celebrities/:id', (req, res, next) => {
+  Celebrity.findById(req.params.id)
+  .then(celebrities => {
+    console.log(celebrities);
+    res.render('celebrities/show', {celebrities});
   });
 });
 
