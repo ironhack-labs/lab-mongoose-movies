b67f0290df0e3bc9d7ad96d8eb2c78d8b54521c9
Verónica Reina <veroreinah@gmail.com>
2018-07-07 11:15:30 +0200

    Iteration #2: Listing Our Celebrities

---
+++ starter-code/routes/celebrities.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const Celebrity = require("../models/Celebrity");
+
+router.get("/", (req, res, next) => {
+  Celebrity.find({})
+    .then(celebrities => {
+      res.render("celebrities/index", { celebrities });
+    })
+    .catch(err => {
+      router.next();
+    });
+});
+
+module.exports = router;
