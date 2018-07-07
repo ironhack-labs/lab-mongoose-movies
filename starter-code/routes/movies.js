5c5c83b4deee6891ef732d458278c44668cc14f4
Verónica Reina <veroreinah@gmail.com>
2018-07-07 21:34:14 +0200

    Iteration #9: The Movie Details Page

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -14,4 +14,14 @@ router.get("/movies", (req, res, next) => {
     });
 });
 
+router.get("/movies/:id", (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then(movie => {
+      res.render("movies/show", { movie });
+    })
+    .catch(err => {
+      next();
+    });
+});
+
 module.exports = router;
