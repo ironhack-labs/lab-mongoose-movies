6c7aa6c76dd23dc9da6e152d17e456ea776e2b71
Jackson Mades <jacksonmades@gmail.com>
2018-07-07 14:52:56 -0400

    Failure at iteration 4, 5, 6, 8 Success at iteration 7

---
+++ routes/movies.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const Movie = require('../models/movie');
+
+/* GET home page */
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+        .then((result) => {
+            res.render('movies/index', {result});//this result is making an array an object, which is the celebrity array
+        })
+});
