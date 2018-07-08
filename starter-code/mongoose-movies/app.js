b64375ab447db63950a8a02afdc8bddd04bc9179
Will Schaller <willschaller@gmail.com>
2018-07-08 15:17:27 +0200

    Set up morgan logging with rotating file stream

--- starter-code/mongoose-movies/app.js
+++ starter-code/mongoose-movies/app.js
@@ -6,6 +6,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const mongoose = require('mongoose');
+const fs = require('fs');
+const rfs = require('rotating-file-stream');
 
 // --- Instantiations
 const app = express();
@@ -17,9 +19,18 @@ mongoose.connect(`mongodb://localhost/${dbName}`);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+// logging
+const logDirectory = path.join(__dirname, 'log');
+// ensure log directory exists
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+// create a rotating write stream
+const accessLogStream = rfs('access.log', {
+  interval: '1d', // rotate daily
+  path: logDirectory
+});
 
 // --- Middleware
-app.use(logger('dev'));
+app.use(logger('dev', { stream: accessLogStream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
