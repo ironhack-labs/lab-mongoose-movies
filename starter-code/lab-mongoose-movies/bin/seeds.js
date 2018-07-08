610a54605602ae24bdfae75d483505cf1f2e1874
diegomendezp <diegomen15@gmail.com>
2018-07-08 18:05:18 +0200

    done

--- starter-code/lab-mongoose-movies/bin/seeds.js
+++ starter-code/lab-mongoose-movies/bin/seeds.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 const Celebrity = require('../models/celebrity');
-
+const Movie = require('../models/movie');
 
 const dbName = process.env.DBURL;
 mongoose.connect(dbName);
@@ -25,7 +25,34 @@ const celebrities = [
   }
 ]
 
+const movies = [
+  {
+    title : "The Leisure Seeker",
+    genre: "Drama",
+    plot: "A runaway couple goes on an unforgettable journey in the faithful old RV they call The Leisure Seeker, traveling from Boston to The Ernest Hemingway Home in Key West. They recapture their passion for life and their love for each other on a road trip that provides revelation and surprise right up to the very end.",
+  },
+  {
+    title : "Black Panther",
+    genre: "Action",
+    plot: "T'Challa, the King of Wakanda, rises to the throne in the isolated, technologically advanced African nation, but his claim is challenged by a vengeful outsider who was a childhood victim of T'Challa's father's mistake.",
+  },
+  {
+    title : "Red Sparrow",
+    genre: "Comedy",
+    plot: "Ballerina Dominika Egorova is recruited to 'Sparrow School,' a Russian intelligence service where she is forced to use her body as a weapon. Her first mission, targeting a C.I.A. agent, threatens to unravel the security of both nations.",
+   
+  },
+]
+
+Celebrity.collection.drop();
+Movie.collection.drop();
+
 Celebrity.create(celebrities, (err, data) => {
   if (err) { throw(err) }
   console.log(`Created ${celebrities.length} celebrities`);
+});
+
+Movie.create(movies, (err, data) => {
+  if (err) { throw(err) }
+  console.log(`Created ${movies.length} movies`);
 });
