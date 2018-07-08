4b8d206ae5efbd6ed8e7eed862b40b2249eb332e
Will Schaller <willschaller@gmail.com>
2018-07-08 15:10:47 +0200

    Require celebrities and users routes in routes/index.js rather than app.js

--- starter-code/mongoose-movies/app.js
+++ starter-code/mongoose-movies/app.js
@@ -5,8 +5,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const celebritiesRouter = require('./routes/celebrities');
 const mongoose = require('mongoose');
 
 // --- Instantiations
@@ -29,8 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // --- Routes
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/celebrities', celebritiesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- starter-code/mongoose-movies/routes/index.js
+++ starter-code/mongoose-movies/routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+router.use('/users', require('./users'));
+router.use('/celebrities', require('./celebrities'));
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Mongoose Movies' });
