9ef6f93dab57e83d19518a542e677850bbce92c1
Paola Arredondo <arredondop03@gmail.com>
2018-07-08 13:32:17 -0400

    missing deleting

---
+++ starter-code/models/celebrity.js
@@ -0,0 +1,26 @@
+//model = no s
+//route = s !! :)
+
+
+const mongoose = require("mongoose"); //always start with these two lines
+const Schema   = mongoose.Schema;
+
+
+//celebritySchema is the rule and put it as the second argument 
+//when creating the model
+
+
+const celebritySchema = new Schema({
+  name: String,
+  occupation: String,
+  catchPhrase: String,
+});
+
+
+//whatever is inside "" will be the name of the collection
+const Celebrity = mongoose.model("Celebritie", celebritySchema);
+
+
+//leting the oder files see this. when using require the others files 
+//will look at whaterver is equal to module.exports
+module.exports = Celebrity; //end with this one
---
+++ starter-code/routes/celebritiesRoutes.js
@@ -0,0 +1,99 @@
+const express = require('express'); //these two will always be the same two first lines
+const router  = express.Router();
+const Celebrity    = require('../models/celebrity'); //this will also be here but the model will change!
+
+
+//listOfCelebrities <-- this is the return value of the Celebrity.find
+//celebrities <-- this is what follows localhost:3000/celebrities
+//celebrityList is the name of the hbs file :)
+
+router.get('/celebrities', (req, res, next) => {
+  Celebrity.find()
+  .then((listOfCelebrities)=>{
+      res.render('celebrityList', {listOfCelebrities});
+  })
+  .catch((err)=>{
+      next(err); 
+   })
+});
+
+//this is a route for a single celeb
+
+router.get('/celebrities/new', (req, res, next) =>{
+  res.render('newCelebrity');
+});
+
+
+router.post('/celebrities/create', (req, res, next)=>{
+  const newCelebrity = new Celebrity({
+   name: req.body.name,
+   occupation: req.body.occupation,
+   catchPhrase: req.body.catchPhrase,
+  })
+
+  newCelebrity.save()
+.then((response)=>{
+    res.redirect('/celebrities')
+})
+.catch((err)=>{
+    next(err);
+}) 
+})
+
+
+router.get('/celebrities/:id/edit', (req, res, next)=>{
+    Celebrity.findById(req.params.id)
+    .then((theCeleb)=>{
+        res.render('editCeleb', {theCeleb})
+    })
+    .catch((err)=>{
+        next(err);
+    })
+ })
+ 
+ 
+ router.post('/celebrities/:id/update', (req, res, next)=>{
+     Celebrity.findByIdAndUpdate(req.params.id, {
+         name: req.body.name,
+         occupation: req.body.occupation,
+         catchPhrase: req.body.catchPhrase,
+     })
+     .then((theCeleb)=>{
+         res.redirect('/celebrities/'+theCeleb._id)
+     })
+     .catch((err)=>{
+         next(err);
+     })  
+ })
+
+router.post('/celebrities/:id/delete', (req, res, next)=>{
+    Celebrity.findByIdAndRemove(req.params.id)
+    .then((reponse)=>{
+        res.redirect('/celebrities');
+    })
+    .catch((err)=>{
+        next(err);
+    })
+})
+
+
+//this is a route for a single celeb. Always leave this at the end
+
+
+router.get('/celebrities/:id', (req, res, next) => {
+  const id = req.params.id;
+  Celebrity.findById(id)
+  .then((theCelebrity)=>{    
+      res.render('celebrityDetails',  theCelebrity);
+  })
+  .catch((err)=>{
+     next(err); 
+  })
+});
+
+
+
+
+
+
+module.exports = router;  //this is always the last line!!!!!
