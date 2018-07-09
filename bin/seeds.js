842dde698757aeab423774d1bcfb741610210ea2
Jackson Mades <jacksonmades@gmail.com>
2018-07-09 11:36:03 -0400

    uncommented the bin celebrities

--- bin/seeds.js
+++ bin/seeds.js
@@ -31,28 +31,28 @@ Movie.create(movies)
         console.log(err);
     });
 
-// const celebrities = [
-//     {
-//         name: "Buzz Lightyear",
-//         occupation: "Space Ranger",
-//         catchPhrase: "To infinity and beyond!"
-//     },
-//     {
-//         name: "Woody",
-//         occupation: "Sherif",
-//         catchPhrase: "There's a snake in my boot."
-//     },
-//     {
-//         name: "Master Chief",
-//         occupation: "Spartan",
-//         catchPhrase: "Thought I'd try shooting my way out. Mix things up a little."
-//     }
-// ];
-//
-// Celebrity.create(celebrities)
-//     .then((result) => {
-//         console.log('created ${result.length}')
-//     })
-// .catch((err)=>{
-//     console.log(err);
-// })
+const celebrities = [
+    {
+        name: "Buzz Lightyear",
+        occupation: "Space Ranger",
+        catchPhrase: "To infinity and beyond!"
+    },
+    {
+        name: "Woody",
+        occupation: "Sherif",
+        catchPhrase: "There's a snake in my boot."
+    },
+    {
+        name: "Master Chief",
+        occupation: "Spartan",
+        catchPhrase: "Thought I'd try shooting my way out. Mix things up a little."
+    }
+];
+
+Celebrity.create(celebrities)
+    .then((result) => {
+        console.log('created ${result.length}')
+    })
+.catch((err)=>{
+    console.log(err);
+})
