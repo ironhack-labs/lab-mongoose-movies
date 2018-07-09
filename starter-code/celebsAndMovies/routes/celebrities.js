a73cc8dfc717b88684eb001e8bef0fe719cd5cc2
Andrey Barrantes <abarrantes@clavisco.com>
2018-07-08 22:57:02 -0400

    having problems with the creation

--- starter-code/celebsAndMovies/routes/celebrities.js
+++ starter-code/celebsAndMovies/routes/celebrities.js
@@ -1,16 +1,46 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
-const Celeb    = require('../models/celeb');
+const Celeb = require('../models/celeb');
 
 /* GET celebs index page */
-router.get('/', (req, res, next) => {
+router.get('/index', (req, res, next) => {
   Celeb.find()
-  .then((theCelebs)=>{
-    res.render('celebs/index',{theCelebs});
-
-
-  })
+    .then((theCelebs) => {
+      res.render('celebs/index', { theCelebs });
+    })
+    .catch((err) => {
+      next(err);
+    })
 });
+
+router.post('/create', (req, res, next) => {
+  onsole.log("entre");
+  const newCeleb = new Celeb(req.body);
+  newCeleb.save()
+    .then((response) => {
+      res.redirect('/celebs/index');
+    })
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    })
+});
+
+router.get('/new', (req, res, next) => {
+  res.render('celebs/new');
+});
+
+router.get('/:id', (req, res, next) => {
+  Celeb.findById(req.params.id)
+    .then((theCeleb) => {
+      res.render('celebs/show', theCeleb);
+    })
+    .catch((err) => {
+      next(err);
+    })
+});
+
+
 
 module.exports = router;
