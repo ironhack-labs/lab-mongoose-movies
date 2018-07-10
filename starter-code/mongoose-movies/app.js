86e20589f456b0cfabca34f3fe6300d9f48aa9f6
Will Schaller <willschaller@gmail.com>
2018-07-10 18:24:43 +0200

    Simple middleware to set app.locals.currentUser to session currentUser

--- starter-code/mongoose-movies/app.js
+++ starter-code/mongoose-movies/app.js
@@ -49,6 +49,10 @@ app.use(session({
   })
 }));
 app.use(flash());
+app.use((req, res, next) => {
+  app.locals.currentUser = req.session.currentUser;
+  next();
+});
 
 // --- Routes
 app.use('/', indexRouter);
