89e4cbcccb1c5329ca80605d5cafa156e1ac71d2
Junior Mendez Mateo <junior_abigail@hotmail.com>
2018-07-11 07:39:09 -0400

    working on custom checkbox helper logic

--- starter-code/lab-mongoose-movies/bin/customHelpers.js
+++ starter-code/lab-mongoose-movies/bin/customHelpers.js
@@ -1,7 +1,20 @@
+const mongoose = require('mongoose');
+const Celebrities = require('../models/celebrity');
+const Movies = require('../models/movie');
+
 let customHelpers = [
 
-  celebrityCheckbox = function(celebrity) {
-    return `Hello ${celebrity}`;
+  //Take the celebrity id as parameter and return 'checked' 
+  //to be added to the checkbox properties if the 
+  //celebrity is part of the cast members of that movie
+  castCheckbox = function(celebrityId, movieId) {
+    console.log("celebrity id:",celebrityId);
+    console.log("movie id", movieId);
+    Movies.findById(movieId)
+    .then((movie) => {
+      console.log(movie);
+    })
+    return `checked`;
   }
 
 
