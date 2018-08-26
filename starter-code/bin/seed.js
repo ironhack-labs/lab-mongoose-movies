410c7ae3d7e4d8e253fc85f5911f225355c48aae
Cesar Vinken <cesarvinken@hotmail.com>
2018-08-26 20:32:15 +0200

    done

--- starter-code/bin/seed.js
+++ starter-code/bin/seed.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Celebrity = require("../models/celebrity");
+const Movie = require("../models/movie");
 
 const dbName = "celebs";
 mongoose.connect(`mongodb://localhost/${dbName}`);
@@ -31,10 +32,51 @@ const celebrities = [
   }
 ];
 
+const movies = [
+  {
+    title: "Se7en",
+    genre: "Crime",
+    plot:
+      "Two detectives, a rookie and a veteran, hunt a serial killer who uses the seven deadly sins as his motives."
+  },
+  {
+    title: "Nine",
+    genre: "Drama",
+    plot:
+      "Famous film director Guido Contini struggles to find harmony in his professional and personal lives, as he engages in dramatic relationships with his wife, his mistress, his muse, his agent, and his mother."
+  },
+  {
+    title: "8 Mile",
+    genre: "Music",
+    plot:
+      "Two detectives, a rookie and a veteran, hunt a serial killer who uses the seven deadly sins as his motives."
+  },
+  {
+    title: "Oceans Eleven",
+    genre: "Crime",
+    plot:
+      "Danny Ocean and his eleven accomplices plan to rob three Las Vegas casinos simultaneously."
+  },
+  {
+    title: "Twelve Monkeys",
+    genre: "Adventure",
+    plot:
+      "Follows the journey of a time traveler from the post-apocalyptic future who appears in present day on a mission to locate and eradicate the source of a deadly plague that will nearly destroy the human race."
+  }
+];
+
 Celebrity.create(celebrities, err => {
   if (err) {
     throw err;
   }
   console.log(`Created ${celebrities.length} celebrities`);
+  mongoose.connection.close();
+});
+
+Movie.create(movies, err => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Created ${movies.length} movies`);
   mongoose.connection.close();
 });
