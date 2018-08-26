2776b626c6cd81fb6fc7fbb002fb3421d94bf6ba
Tina Reuther <tina.reuther@freenet.de>
2018-08-26 18:31:44 +0200

    first six iterations done, no styling

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -25,6 +25,7 @@ router.get("/new", (req, res, next) => {
   res.render("celebrities/new")
 })
 
+
 router.get("/:id", (req, res, next) => {
   const query = req.params.id;
   Celeb.findById(query).then(star => {
@@ -33,6 +34,29 @@ router.get("/:id", (req, res, next) => {
 })
 
 
+router.post("/:id/delete", (req, res, next) => {
+  Celeb.findByIdAndRemove(req.params.id).then(star => {
+    res.redirect("/celebrities")
+  })
+})
+
+router.get("/:id/edit", (req, res, next) => {
+  const query = req.params.id;
+
+  Celeb.findById(query).then(star =>
+  res.render("celebrities/edit", {
+    star : star,
+    edited : true,
+  }))
+})
+
+router.post("/:id", (req, res, next) => {
+  Celeb.findByIdAndUpdate(req.params.id, req.body, {new: true}).then(star => {
+    res.render("celebrities/show", {
+      star : star,
+      edited: true})
+  })
+})
 
 
 module.exports = router;
