b078f707d6c15706dfcb1e9cbd4692540f2d4544
Eran Usha <eusha5@gmail.com>
2018-09-10 11:41:55 -0400

    added edit/update functionality

--- starter-code/mongoose-movies/routes/movies.js
+++ starter-code/mongoose-movies/routes/movies.js
@@ -12,4 +12,68 @@ router.get('/movies', (req, res, next) => {
   })
 })
 
+router.get('/movies/new', (req, res, next) => {
+  res.render('movies/new')
+})
+
+
+
+router.post('/movies/create', (req, res, next) => {
+  Movie.create({
+    title: req.body.title,
+    genre: req.body.genre,
+    plot: req.body.plot,
+  })
+  .then((response) => {
+    res.redirect('/movies')
+  })
+  .catch((err) => {
+    next(err);
+  })
+});
+
+router.post('/movies/delete/:id', (req, res, next) => {
+  Movie.findByIdAndRemove(req.params.id)
+  .then((response) => {
+    res.redirect('/movies')
+  })
+  .catch((err) => {
+    next(err)
+  })
+})
+
+router.post('/movies/update/:id', (req, res, next ) => {
+  Movie.findByIdAndUpdate(req.params.id, {
+    title: req.body.title,
+    genre: req.body.genre,
+    plot: req.body.plot
+  })
+  .then((response) => {
+    res.redirect('/movies/' + req.params.id)
+  })
+  .catch((err) => {
+    next(errs)
+  })
+})
+
+router.get('/movies/edit/:id', (req, res, next) => {
+  Movie.findById(req.params.id)
+  .then((aMovie) => {
+    res.render('movies/edit', {theMovie: aMovie})
+  })
+  .catch((err) => {
+    next(err)
+  })
+})
+
+router.get('/movies/:id', (req, res, next) => {
+  Movie.findById(req.params.id)
+  .then((movieInfo)=>{
+    res.render('movies/show', {movieDetails: movieInfo})
+  })
+  .catch((err)=>{
+    next(err);
+  })
+});
+
 module.exports = router;
