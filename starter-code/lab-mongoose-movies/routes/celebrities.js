1f763862b457bfbf365fe1713b95e09f9560319a
InnaYugay <yuinnaa@gmail.com>
2018-09-10 08:51:40 -0400

    celebrities done

--- starter-code/lab-mongoose-movies/routes/celebrities.js
+++ starter-code/lab-mongoose-movies/routes/celebrities.js
@@ -7,7 +7,7 @@ router.get('/celebrities', (req, res, next) => {
 
   Celebrity.find()
   .then((theThingsWeGet)=>{
-    console.log(theThingsWeGet)
+ 
     res.render('allCelebs', {listOfCelebs: theThingsWeGet});
   
   })
@@ -29,10 +29,8 @@ router.post('/celebrities/create', (req,res,next)=>{
   const celebOcc = req.body.newOccpation;
   const celebPhrase = req.body.newCatchphrase;
 
-  console.log(celebName,celebOcc,celebPhrase)
 
   Celebrity.create({
-
     name: celebName,
     occupation: celebOcc,
     catchphrase: celebPhrase
@@ -42,6 +40,50 @@ router.post('/celebrities/create', (req,res,next)=>{
   })
   .catch((err)=>{
     res.redirect('/celebrities/new')
+  })
+
+})
+
+router.post('/celebrities/delete/:celebID', (req, res, next)=>{
+  // console.log('=-=-=-=-=-=-=-=-=-=')
+  Celebrity.findByIdAndRemove(req.params.celebID)
+  .then((response)=>{
+    res.redirect('/celebrities')
+  })
+  .catch((err)=>{
+    next(err)
+  })
+})
+
+router.get('/celebrities/edit/:celebID', (req,res,next) =>{
+  // console.log('=-=-=-=-=-=-=-=-=-=')
+
+  Celebrity.findById(req.params.celebID)
+  .then((theThingWeGet)=>{
+    res.render('edit', {theID: theThingWeGet})
+  })
+  .catch((err)=>{
+    next(err)
+  })
+})
+
+
+router.post('/celebrities/update/:celebID', (req, res, next)=>{
+  const newName = req.body.updatedName
+  const newOccpation = req.body.updatedOccupation
+  const newCatchphrase = req.body.updatedCatchphrase
+  
+  console.log("=-=-=-=-=-=-", newName, newOccpation, newCatchphrase)
+  Celebrity.findByIdAndUpdate(req.params.celebID, {
+    name: newName,
+    occupation: newOccpation,
+    catchphrase: newCatchphrase
+  })
+  .then((response)=>{
+    res.redirect('/celebrities')
+  })
+  .catch((err)=>{
+    next(err)
   })
 
 })
