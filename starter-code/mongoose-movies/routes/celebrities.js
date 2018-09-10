ed666a2eb4453706bdeedbd291a6f4c4d0d4b2f6
Eran Usha <eusha5@gmail.com>
2018-09-10 10:35:38 -0400

    added update/delete feature

--- starter-code/mongoose-movies/routes/celebrities.js
+++ starter-code/mongoose-movies/routes/celebrities.js
@@ -17,6 +17,56 @@ router.get('/celebrities/new', (req, res, next) => {
   res.render('celebrities/newCeleb')
 })
 
+
+
+router.post('/celebrity/create', (req, res, next) => {
+  Celebrity.create({
+    name: req.body.celebName,
+    occupation: req.body.celebOccupation,
+    catchphrase: req.body.celebCatchphrase,
+  })
+  .then((response) => {
+    res.redirect('/celebrity')
+  })
+  .catch((err) => {
+    next(err);
+  })
+});
+
+router.post('/celebrities/delete/:id', (req, res, next) => {
+  Celebrity.findByIdAndRemove(req.params.id)
+  .then((response) => {
+    res.redirect('/celebrity')
+  })
+  .catch((err) => {
+    next(err)
+  })
+})
+
+router.get('/celebrities/edit/:id', (req, res, next) => {
+  Celebrity.findById(req.params.id)
+  .then((theCeleb) => {
+    res.render('celebrities/edit', {theCelebrity: theCeleb})
+  })
+  .catch((err) => {
+    next(err)
+  })
+})
+
+router.post('/celebrities/update/:id', (req, res, next ) => {
+  Celebrity.findByIdAndUpdate(req.params.id, {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchphrase: req.body.catchphrase
+  })
+  .then((response) => {
+    res.redirect('/celebrities/' + req.params.id)
+  })
+  .catch((err) => {
+    next(errs)
+  })
+})
+
 router.get('/celebrities/:id', (req, res, next) => {
   Celebrity.findById(req.params.id)
   .then((celebInfo)=>{
@@ -26,8 +76,5 @@ router.get('/celebrities/:id', (req, res, next) => {
     next(err);
   })
 });
-
-
-
 
 module.exports = router;
