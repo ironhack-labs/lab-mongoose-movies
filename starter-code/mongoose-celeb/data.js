886a2bc05aacb0d6b1971e0852d90463531b7697
Mapno <mariorvinas@gmail.com>
2018-09-14 17:55:38 +0200

    created db

---
+++ starter-code/mongoose-celeb/data.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        name: 'Carl Cox',
+        occupation: 'Music Producer & DJ',
+        catchPhrase: 'Oh yes! Oh yes! Ibiza!'
+    },
+    {
+        name: 'Karl Max',
+        occupation: 'Philosopher',
+        catchPhrase: 'To be radical is to grasp things by the root'
+    },
+    {
+        name: 'Mariano rajoy',
+        occupation: 'Politician',
+        catchPhrase: 'Como decia Galileo, el movimiento siempre se acelera cuando se va a detener'
+    }
+]
