d38ac69a0d6cf4480aaa9def3cd812c3a338bfd8
agdwm <almudenaguerraswm@gmail.com>
2018-09-15 21:30:00 +0200

    Iteration 3 done - The celebrity details page

--- starter-code/lab-express-celebrities/routes/celebrities.js
+++ starter-code/lab-express-celebrities/routes/celebrities.js
@@ -6,13 +6,25 @@ const Celebrity = require('../models/Celebrity');
 router.get('/', (req, res, next) => {
 	Celebrity.find()
 		.then(celebrities => {
-			console.log(celebrities);
 			res.render('celebrities/index', {celebrities, title: 'Celebrities'});
 		})
 		.catch( e => {
-			console.log('Error on retrieving the list of celebrities');
-			res.next(e);
+			console.log('Error on retrieving the list of celebrities', e);
+			next(e);
 		})
+});
+
+router.get('/:id', (req, res, next) => {
+	const id = req.params.id;
+	Celebrity.findById({_id:id})
+	.then(celebrity => {
+		console.log(celebrity);
+		res.render('celebrities/show', {celebrity, title: 'Celebrity Detail'});
+	})
+	.catch(e => {
+		console.log('Error on retrieving the detail of the celebrity', e);
+		next(e);
+	})
 });
 
 module.exports = router;
