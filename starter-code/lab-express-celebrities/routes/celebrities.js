55a6285cda797dfc04c6596d423674437a1e553d
agdwm <almudenaguerraswm@gmail.com>
2018-09-16 10:57:29 +0200

    Iteration 5 done - Deleting Celebrities

--- starter-code/lab-express-celebrities/routes/celebrities.js
+++ starter-code/lab-express-celebrities/routes/celebrities.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const Celebrity = require('../models/Celebrity');
 
+//C(R)UD -> Retrieve
 router.get('/', (req, res, next) => {
 	Celebrity.find()
 		.then(celebrities => {
@@ -19,6 +20,7 @@ router.get('/new', (req, res, next) => {
 	res.render('celebrities/new', {title: 'Create a new celebrity'});
 });
 
+//(C)RUD -> Create
 router.post('/', (req, res, next) => {
 	const { name, occupation, catchPhrase } = req.body;
 	const newCelebrity = new Celebrity({ name, occupation, catchPhrase });
@@ -32,7 +34,7 @@ router.post('/', (req, res, next) => {
 		})
 });
 
-
+//C(R)UD -> Retrieve One
 router.get('/:id', (req, res, next) => {
 	const id = req.params.id;
 	Celebrity.findById({_id:id})
@@ -46,6 +48,18 @@ router.get('/:id', (req, res, next) => {
 	})
 });
 
+//CRU(D) -> Delete
+router.post('/:id/delete', (req, res, next) => {
+	const id = req.params.id;
+	Celebrity.findByIdAndRemove({_id:id})
+		.then( celebrity => {
+			res.redirect('/celebrities');
+		})
+		.catch(e => {
+			console.log('Error on removing the celebrity', e);
+			next(e);
+		})
+});
 
 
 module.exports = router;
