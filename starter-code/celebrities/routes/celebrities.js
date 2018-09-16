7a5a3d1ed933274ce8d018e9f96bd5ac6efe6d6d
omar-nav <omar.navarro@g.austincc.edu>
2018-09-16 12:52:01 -0500

    post new records

--- starter-code/celebrities/routes/celebrities.js
+++ starter-code/celebrities/routes/celebrities.js
@@ -10,30 +10,30 @@ router.get('/', (req, res, next) => {
     }).catch(e => next(e))
 })
 
-//Celebrity datails
-// router.get('/detail/:id', (req, res, next) => {
-//   const { id } = req.params
-//   Celebrity.findById(id)
-//     .then(celebrity => {
-//       res.render('celebrities/show', celebrity)
-//     })
-//     .catch(e => {
-//       console.log(e)
-//       next(e)
-//     })
-// })
+//Celebrity details page
+router.get('/detail/:id', (req, res, next) => {
+  const { id } = req.params
+  Celebrity.findById(id)
+    .then(celebrity => {
+      res.render('celebrities/show', celebrity)
+    })
+    .catch(e => {
+      console.log(e)
+      next(e)
+    })
+})
 
-// // Create new celebrity and post
-// router.get('/new', (req, res, next) => {
-//   res.render('celebrities/new')
-// })
+// Create new celebrity and post
+router.get('/new', (req, res, next) => {
+  res.render('celebrities/new')
+})
+router.post('/new', (req, res, next) => {
+  Celebrity.create(req.body)
+    .then(celebrity => {
+      res.redirect('/celebrities')
+    }).catch(e => next(e))
+})
 
-// router.post('/new', (req, res, next) => {
-//   Celebrity.create(req.body)
-//     .then(celebrity => {
-//       res.redirect('/celebrities')
-//     }).catch(e => next(e))
-// })
 
 // //Delete celebrities
 // router.get('/:id/delete', (req, res, next) => {
