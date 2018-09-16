2fac79181691257e8ceccf0557b49365839ec23b
robotic-amoeba <raul.martin.majadas@gmail.com>
2018-09-16 20:41:20 +0200

    Movie model done

--- starter-code/movies-project/bin/seeds.js
+++ starter-code/movies-project/bin/seeds.js
@@ -1,6 +1,7 @@
 const Celebrity = require('../models/celebrity.js');
-const mongoose = require('mongoose');
-const data = [
+const Movie = require('../models/movie')
+ const mongoose = require('mongoose');
+/*const data = [
   {
     name: "Bruce Dickinson",
     occupation: "Singer",
@@ -21,7 +22,34 @@ const data = [
 mongoose.connect('mongodb://localhost/movies-project', { UseNewUrlParser: true })
   .then(data => {
     console.log("Connected to mongoDB: " + data.connections[0].name)
-    Celebrity.collection.drop()
+    //Celebrity.collection.drop()
   })
-  .then(() => Celebrity.insertMany(data))
+  .then(() => Celebrity.insertMany(data2))
+  .catch(e => console.log(e))
+*/
+
+const data2 = [
+  {
+    title: 'Dr Who',
+    genre: 'Sci-fi',
+    plot: 'An alien travels the universe through space and time because he is bored'
+  },
+  {
+    title: 'Spider-man',
+    genre: 'Action',
+    plot: 'Teenager ends jumping between buildings after being bit by a spider'
+  },
+  {
+    title: 'Robocop',
+    genre: 'Action',
+    plot: 'A cop gets transform into a tin and it turns out he is mad about it'
+  }
+]
+
+mongoose.connect('mongodb://localhost/movies-project', { UseNewUrlParser: true })
+  .then(data => {
+    console.log("Connected to mongoDB: " + data.connections[0].name)
+    //Movie.collection.drop()
+  })
+  .then(() => Movie.insertMany(data2))
   .catch(e => console.log(e))
