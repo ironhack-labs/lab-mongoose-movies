82ed0e828ebc1bd0e9c5935b56ae3e7a022193b7
TeoDiaz <tdiazsan@gmail.com>
2018-09-16 18:04:26 +0200

    iteration 6 (bonus)

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -32,28 +32,54 @@ router.get("/new", (req, res) => {
   res.render("./celebrities/new");
 });
 
-router.post('/new/create', (req, res, next) => {
-  const { name, occupation, catchPhrase} = req.body;
-  const newCelebrity = new Celebrity({ name, occupation, catchPhrase});
-  newCelebrity.save()
-  .then(() => {
-    res.redirect("/celebrity")
-  })
-  .catch((err) => {
-  console.log(err);
-  })
-
-  router.post('/celebrities/:id/delete', (req, res, next) => {
-    Celebrity.findByIdAndRemove(req.params.id)
+router.post("/new/create", (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
+  const newCelebrity = new Celebrity({ name, occupation, catchPhrase });
+  newCelebrity
+    .save()
     .then(() => {
-      res.redirect("/celebrity")
+      res.redirect("/celebrity");
     })
-    .catch((err) => {
-    console.log(err);
-    })
-  })
-  
+    .catch(err => {
+      console.log(err);
+    });
+});
 
+router.post("/celebrities/:id/delete", (req, res, next) => {
+  Celebrity.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.redirect("/celebrity");
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+router.get("/celebrities/:id/edit", (req, res) => {
+  Celebrity.findById(req.params.id)
+    .then(celebrities => {
+      console.log(celebrities);
+      res.render("./celebrities/edit", { celebrities });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+router.post("/celebrities/:id/editone", (req, res) => {
+  const { name, occupation, catchPhrase } = req.body;
+  Celebrity.update(
+    { _id: req.params.id },
+    { $set: { name: name, occupation: occupation, catchPhrase: catchPhrase } },
+    { new: true }
+  )
+    .then(() => {
+      console.log(Celebrity);
+      res.redirect("/celebrity");
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
