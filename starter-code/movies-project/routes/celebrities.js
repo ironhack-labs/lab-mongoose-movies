d2f239bf7ec647a68aae146dd4c220618c55088e
robotic-amoeba <raul.martin.majadas@gmail.com>
2018-09-16 13:09:29 +0200

    Celebrity Details Page done

--- starter-code/movies-project/routes/celebrities.js
+++ starter-code/movies-project/routes/celebrities.js
@@ -5,8 +5,16 @@ const Celebrity = require('../models/celebrity');
 
 router.get('/celebrities', (req, res, next) => {
   Celebrity.find({})
-  .then(celebrities => res.render('celebrities/index', {celebrities}))
-  .catch(e => console.log(e))
+    .then(celebrities => {
+      res.render('celebrities/index', { celebrities })
+    })
+    .catch(e => console.log(e))
+})
+
+router.get('/celebrities/:id', (req, res, next) => {
+  Celebrity.findById({ _id: req.params.id })
+    .then(celebrity => res.render('celebrities/show', { celebrity }))
+    .catch(e => console.log(e))
 })
 
 module.exports = router;
