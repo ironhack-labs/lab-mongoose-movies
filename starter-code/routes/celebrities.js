708ad8e1046c3e07e38c2dc1e7d086eb14d74ed2
FonRiego <fonriego@gmail.com>
2018-09-16 22:51:50 +0200

    Iteration 4

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -13,4 +13,30 @@ router.get ("/", (req, res, next) => {
   });
 });
 
+router.get ("/new", (req, res, next) => {
+  res.render("celebrities/new");
+});
+
+router.post ("/new", (req, res, next) => {
+  const {name, occupation, catchPhrase} = req.body;
+  new Celebrity({name, occupation, catchPhrase})
+  .save().then( celebrity => {
+    res.redirect("/celebrities")
+  })
+  .catch (() => {
+    res.render("celebrities/new")
+  });
+});
+
+router.get ("/:id", (req, res, next) => {
+  let celId = req.params.id;
+  Celebrity.findById(celId)
+  .then (celebrity => {
+    res.render("celebrities/show", {celebrity})
+  })
+  .catch (err => {
+    next(err)
+  });
+});
+
 module.exports = router;
