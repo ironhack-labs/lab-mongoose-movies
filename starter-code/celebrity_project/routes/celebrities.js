26aa1b8eafde9ee426e2b739b5fc5fa57693539e
Evenstarw1 <lauracorrallopez@gmail.com>
2018-09-16 18:55:00 +0200

    Hasta la iteracción 6

--- starter-code/celebrity_project/routes/celebrities.js
+++ starter-code/celebrity_project/routes/celebrities.js
@@ -12,6 +12,59 @@ router.get('/celebrities', (req, res, next) => {
       .catch(error => {
         console.log(error)
     })
+});
+
+
+//GET Details
+router.get('/celebrities/:id', (req, res, next) => {
+    Celebrity.findById(req.params.id)
+      .then(celebrities => {
+        res.render("celebrities/show", {celebrities});
+      })
+      .catch(error => {
+        console.log(error)
+    })
+});
+
+//GET New
+router.get('/new', (req, res, next) => {
+        res.render("celebrities/new");
+});
+
+router.post("/new", (req, res) => {
+    const { name, occupation, catchPhrase } = req.body;
+    const celebrity = new Celebrity({ name, occupation, catchPhrase });
+    celebrity.save( err => {
+        if (err) {
+            return next(err)
+        }
+        res.redirect('/celebrities');
   });
+});
+
+//DELETE celebrity
+router.post("/celebrities/:id/delete", (req, res) => {
+    Celebrity.findByIdAndRemove(req.params.id, () => res.redirect('/celebrities'))   
+});
+
+//EDIT celebrity
+router.get("/celebrities/edit/:id", (req, res, next) => {
+    let celebrityId = req.params.id;
+    Celebrity.findOne({'_id': celebrityId})
+    .then(celebrity => {
+        res.render("celebrities/edit", celebrity);
+    })
+    .catch(error => {
+        console.log(error)
+    })
+});
+
+router.post("/celebrities/:id", (req, res, next) => {
+    const { name, occupation, catchPhrase } = req.body;
+    Celebrity.findByIdAndUpdate(req.params.id, {$set: {name, occupation, catchPhrase}})
+    .then (() => {
+        res.redirect("/celebrities");
+    })
+});
 
 module.exports = router;
