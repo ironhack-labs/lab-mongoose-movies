77849d45a7296ad8cef05dd11b1c700a354238f0
Francis Wardley <fwardley90@gmail.com>
2018-09-16 11:45:28 +0200

    add form working

--- starter-code/lab-mongoose-movies/routes/celebrities.js
+++ starter-code/lab-mongoose-movies/routes/celebrities.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const router  = express.Router();
+// const Celebrity = require('../models/celebrity.js');
 
+// router.get('/celebrities', (req, res, next) => {
+//   Celebrity.find()
+//     .then(celebrities => {
+//       res.render('celebrities/index', {celebrities});
+//     })
+//     .catch(error => {
+//       console.log(error)
+//     })
+// });
 
+// router.get('/celebrity/:id', (req, res, next) => {
+//   let celebrityId = req.params.id;
+//   Celebrity.findOne({'_id': celebrityId})
+//     .then(celebrity => {
+//       res.render("celebrities/show", { celebrity })
+//     })
+//     .catch(error => {
+//       console.log(error)
+//     })
+// });
 
 module.exports = router;
--- starter-code/lab-mongoose-movies/routes/index.js
+++ starter-code/lab-mongoose-movies/routes/index.js
@@ -28,4 +28,20 @@ router.get('/celebrity/:id', (req, res, next) => {
     })
 });
 
+router.get('/celebrities/new', (req, res, next) => {
+  res.render('celebrities/new');
+});
+
+router.post('/celebrities/new', (req, res, next) => {
+  const { name, occupation, catchphrase } = req.body;
+  const newCelebrity = new Celebrity({ name, occupation, catchphrase })
+  newCelebrity.save()
+  .then((celebrity) => {
+    res.redirect('/celebrities')
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+});
+
 module.exports = router;
