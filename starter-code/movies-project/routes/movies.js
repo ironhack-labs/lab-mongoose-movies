db00c4bce6787c168b64d39c39229092f465d5ba
robotic-amoeba <raul.martin.majadas@gmail.com>
2018-09-16 21:43:52 +0200

    Add new movie done

--- starter-code/movies-project/routes/movies.js
+++ starter-code/movies-project/routes/movies.js
@@ -6,19 +6,32 @@ const Movie = require('../models/movie.js');
 router.get('/movies', (req, res, next) => {
   Movie.find({})
     .then(movies => {
-      res.render('movies/index', {movies})
+      res.render('movies/index', { movies })
     })
     .catch(e => console.log(e))
 })
 
-router.get('/movies/:id', (req, res, next) => {
-  let id = req.params.id;
-  Movie.findById({_id: id})
-  .then(movie => {
-    console.log(movie)
-    res.render('./movies/show', {movie})
-  })
-  
+router.get('/movies/new', (req, res, next) => {
+  res.render('./movies/new')
 })
+
+router.get('/movies/:id', (req, res, next) => {  //DETAILS menu
+  let id = req.params.id;
+  Movie.findById({ _id: id })
+    .then(movie => {
+      //console.log(movie)
+      res.render('./movies/show', { movie })
+        .catch(e => console.log(e))
+    })
+})
+
+router.post('/movies', (req, res, next) => {
+  let data = req.body;
+  let movie = new Movie({ title: data.title, genre: data.genre, plot: data.plot })
+  movie.save()
+    .then(() => res.redirect('/movies'))
+    .catch(() => res.render('/movies/new'))
+})
+
 
 module.exports = router;
