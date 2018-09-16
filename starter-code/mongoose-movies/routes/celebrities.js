74f42c38399f7b5207cb477cf309a7fa4fa9fc14
Gonlos <gonzalo.cortazar.kao@gmail.com>
2018-09-16 14:24:35 +0200

    The Celebrity Details Page

--- starter-code/mongoose-movies/routes/celebrities.js
+++ starter-code/mongoose-movies/routes/celebrities.js
@@ -2,10 +2,17 @@ const express = require("express");
 const router = express.Router();
 const Celebrity = require("../models/Celebrity");
 
-/* GET home page */
+/* GET celebrities list */
 router.get("/", (req, res, next) => {
   Celebrity.find()
     .then(celebrities => res.render("celebrities/index", {celebrities}))
+    .catch(e => next(e));
+});
+
+/* GET celebrity details */
+router.get("/:id", (req, res, next) => {
+  Celebrity.findById(req.params.id)
+    .then(celebrity => res.render("celebrities/show", {celebrity}))
     .catch(e => next(e));
 });
 
