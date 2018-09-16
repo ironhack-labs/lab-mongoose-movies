e6a380c046376aa181179513182994ac66e247d5
Gonlos <gonzalo.cortazar.kao@gmail.com>
2018-09-16 12:58:56 +0200

    fix directory name

--- starter-code/mongoose-movies/app.js
+++ starter-code/mongoose-movies/app.js
@@ -11,7 +11,7 @@ const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/mogoose-movies', {useNewUrlParser: true})
+  .connect('mongodb://localhost/mongoose-movies', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
