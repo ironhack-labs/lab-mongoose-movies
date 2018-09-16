cc3c78f2acdad221d9572e3fb96ba47d0ada687f
Gonlos <gonzalo.cortazar.kao@gmail.com>
2018-09-16 16:27:10 +0200

    Editing Celebrities - Celebrities Done

--- starter-code/mongoose-movies/routes/celebrities.js
+++ starter-code/mongoose-movies/routes/celebrities.js
@@ -14,10 +14,18 @@ router.get("/new", (req, res, next) => {
   res.render("celebrities/new")
 });
 
+  /* GET edit celebrities*/
+  router.get("/:id/edit", (req, res, next) => {
+    console.log(req.params.id)
+    Celebrity.findById(req.params.id)
+      .then(celebrity => res.render("celebrities/edit", celebrity ))
+      .catch(e => next(e));
+  });
+
   /* GET celebrity details */
   router.get("/:id", (req, res, next) => {
     Celebrity.findById(req.params.id)
-      .then(celebrity => res.render("celebrities/show", { celebrity }))
+      .then(celebrity => res.render("celebrities/show", celebrity ))
       .catch(e => next(e));
   });
 
@@ -30,10 +38,22 @@ router.get("/new", (req, res, next) => {
       })
       .catch(error => res.render("celebrities/new", { error }));
   });
+  
   /* POST delete celebrity */
-router.post("/:id/delete", (req, res, next) => {
+  router.post("/:id/delete", (req, res, next) => {
     Celebrity
       .findByIdAndDelete(req.params.id)
+      .then(() => {
+        res.redirect('/celebrities')
+      })
+      .catch(e => next(e));
+  });
+  
+  /* POST edit celebrity */
+  router.post("/:id", (req, res, next) => {
+    const { name, occupation, catchPhrase } = req.body
+    Celebrity
+      .findByIdAndUpdate(req.params.id,{ name, occupation, catchPhrase })
       .then(() => {
         res.redirect('/celebrities')
       })
