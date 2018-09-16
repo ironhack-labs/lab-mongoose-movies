7bad3a68e454571485e657928ab11637bd710d1f
omar-nav <omar.navarro@g.austincc.edu>
2018-09-16 16:26:22 -0500

    add movies to dB within seeds file

--- starter-code/celebrities/app.js
+++ starter-code/celebrities/app.js
@@ -57,7 +57,7 @@ app.use('/', index);
 const celebrities = require('./routes/celebrities');
 app.use('/celebrities', celebrities);
 
-// const movies = require('./routes/movies');
-// app.use('/movies', movies);
+const movies = require('./routes/movies');
+app.use('/movies', movies);
 
 module.exports = app;
--- starter-code/celebrities/bin/seeds.js
+++ starter-code/celebrities/bin/seeds.js
@@ -1,53 +1,53 @@
 const mongoose = require('mongoose');
 const Celebrity = require('../models/Celebrity');
-// const Movie = require('../models/Movie');
+const Movie = require('../models/Movie');
 
 const dbName = 'celebs';
 mongoose.connect(`mongodb://localhost/${dbName}`);
 
-const celebrities = [
-  {
-    name: "BlisS",
-    occupation: "Teacher",
-    catchPhrase: "BlisS"
-  },
-  {
-    name: "Diego",
-    occupation: "TA",
-    catchPhrase: "JS no te espera!"
-  },
-  {
-    name: "Julian",
-    occupation: "Entrepreneur",
-    catchPhrase: "tecnología"
-  }
-]
-
-Celebrity.create(celebrities, (err) => {
-  if (err) { throw (err) }
-  console.log(`Created ${celebrities.length} celebrities`)
-  mongoose.connection.close()
-})
-// const movies = [
+// const celebrities = [
 //   {
-//     title: "Guardians of the Galaxy",
-//     genre: "Superhero Film",
-//     plot: "In 1988, following his mother's death, a young Peter Quill is abducted from Earth by the Ravagers, a group of space pirates led by Yondu Udonta. Twenty-six years later on the planet Morag, Quill steals an orb but is attacked by Korath, a subordinate to the fanatical Kree, Ronan. Although Quill escapes with the orb, Yondu discovers his theft and issues a bounty for his capture, while Ronan sends the assassin Gamora after the orb."
+//     name: "BlisS",
+//     occupation: "Teacher",
+//     catchPhrase: "BlisS"
 //   },
 //   {
-//     title: "Doctor Strange",
-//     genre: "Superhero Film",
-//     plot: "In Kathmandu, the sorcerer Kaecilius and his zealots enter the secret compound Kamar-Taj and behead its librarian. They steal a few pages from an ancient, mystical text belonging to the Ancient One, a long-lived sorcerer who has taught every student at Kamar-Taj, including Kaecilius, in the mystic arts. The Ancient One pursues the traitors, but Kaecilius and his followers escape."
+//     name: "Diego",
+//     occupation: "TA",
+//     catchPhrase: "JS no te espera!"
 //   },
 //   {
-//     title: "Iron Man",
-//     genre: "Superhero Film",
-//     plot: "Genius, billionaire, playboy and philanthropist Tony Stark, who has inherited the defense contractor Stark Industries from his father, is in war-torn Afghanistan with his friend and military liaison Lieutenant Colonel James Rhodes to demonstrate the new Jericho missile. After the demonstration, the convoy is ambushed and Stark is critically wounded by one of his own company's rocket-propelled grenades, used by the attackers."
+//     name: "Julian",
+//     occupation: "Entrepreneur",
+//     catchPhrase: "tecnología"
 //   }
 // ]
 
-// Movie.create(movies, (err) => {
-//   if (err) { throw(err) }
-//   console.log(`Created ${movies.length} movies`)
+// Celebrity.create(celebrities, (err) => {
+//   if (err) { throw (err) }
+//   console.log(`Created ${celebrities.length} celebrities`)
 //   mongoose.connection.close()
 // })
+const movies = [
+  {
+    title: "fight club",
+    genre: "drama",
+    plot: "An insomniac office worker and a devil-may-care soapmaker form an underground fight club that evolves into something much, much more."
+  },
+  {
+    title: "amores perros",
+    genre: "drama",
+    plot: "A horrific car accident connects three stories, each involving characters dealing with loss, regret, and life's harsh realities, all in the name of love."
+  },
+  {
+    title: "Y tu mamá también",
+    genre: "drama",
+    plot: "In Mexico, two teenage boys and an attractive older woman embark on a road trip and learn a thing or two about life, friendship, sex, and each other."
+  }
+]
+
+Movie.create(movies, (err) => {
+  if (err) { throw (err) }
+  console.log(`Created ${movies.length} movies`)
+  mongoose.connection.close()
+})
