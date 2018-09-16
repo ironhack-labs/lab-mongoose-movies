306bcf905ebb35d7b2e5772044f18659efadcf51
agdwm <almudenaguerraswm@gmail.com>
2018-09-16 12:03:09 +0200

    Iteration 6 done - Editing Celebrities

--- starter-code/lab-express-celebrities/routes/celebrities.js
+++ starter-code/lab-express-celebrities/routes/celebrities.js
@@ -20,20 +20,6 @@ router.get('/new', (req, res, next) => {
 	res.render('celebrities/new', {title: 'Create a new celebrity'});
 });
 
-//(C)RUD -> Create
-router.post('/', (req, res, next) => {
-	const { name, occupation, catchPhrase } = req.body;
-	const newCelebrity = new Celebrity({ name, occupation, catchPhrase });
-	newCelebrity.save()
-		.then( celebrity => {
-			res.redirect('/celebrities');
-		})
-		.catch( e => {
-			console.log('Error on creating a new celebrity', e);
-			res.render('celebrities/new', {title: 'Create a new celebrity'});
-		})
-});
-
 //C(R)UD -> Retrieve One
 router.get('/:id', (req, res, next) => {
 	const id = req.params.id;
@@ -48,6 +34,50 @@ router.get('/:id', (req, res, next) => {
 	})
 });
 
+//CR(U)D -> Update Show
+router.get('/:id/edit', (req, res, next) => {
+	const id = req.params.id;
+	Celebrity.findById({_id:id})
+		.then(celebrity => {
+			console.log('celebrity', celebrity);
+			res.render('celebrities/edit', {celebrity, title: 'Edit a celebrity'});
+		})
+		.catch( e => {
+			console.log('Error on showing the form to update a celebrity', e);
+			next(e);
+		})
+})
+
+//(C)RUD -> Create
+router.post('/', (req, res, next) => {
+	const { name, occupation, catchPhrase } = req.body;
+	const newCelebrity = new Celebrity({ name, occupation, catchPhrase });
+	newCelebrity.save()
+		.then( celebrity => {
+			res.redirect('/celebrities');
+		})
+		.catch( e => {
+			console.log('Error on creating a new celebrity', e);
+			res.render('celebrities/new', {title: 'Create a new celebrity'});
+		})
+});
+
+//CR(U)D -> Update
+router.post('/:id', (req, res, next) => {
+	const id = req.params.id;
+	const { name, occupation, catchPhrase } = req.body;
+
+	Celebrity.update({_id: id}, {$set: {name, occupation, catchPhrase}}, { new: true })
+		.then( celebrity => {
+			console.log('celebrity', celebrity);
+			res.redirect('/celebrities');
+		})
+		.catch( e => {
+			console.log('Error on updating the celebrity', e);
+	 		next(e);
+		})
+});
+
 //CRU(D) -> Delete
 router.post('/:id/delete', (req, res, next) => {
 	const id = req.params.id;
@@ -60,6 +90,5 @@ router.post('/:id/delete', (req, res, next) => {
 			next(e);
 		})
 });
-
 
 module.exports = router;
