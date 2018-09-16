1e126332266a8ee010fca632ed0d0bdd76a24794
agdwm <almudenaguerraswm@gmail.com>
2018-09-16 20:06:37 +0200

    Iteration 9 - The Movie Details Page

--- starter-code/lab-express-celebrities/routes/movies.js
+++ starter-code/lab-express-celebrities/routes/movies.js
@@ -15,5 +15,18 @@ router.get('/', (req, res, next) => {
 		})
 });
 
+//C(R)UD -> Retrieve One
+router.get('/:id', (req, res, next) => {
+	const id = req.params.id;
+	Movie.findById({_id:id})
+	.then(movie => {
+		console.log(movie);
+		res.render('movies/show', {movie, title: 'Movie Detail'});
+	})
+	.catch(e => {
+		console.log('Error on retrieving the detail of the movie', e);
+		next(e);
+	})
+});
 
 module.exports = router;
