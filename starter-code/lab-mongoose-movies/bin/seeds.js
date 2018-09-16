15dabdbf1f7a95e433f1ac30d521a7d359e90a2b
RaulG <guiaraul@gmail.com>
2018-09-16 15:36:06 +0200

    Celebrities Done, Movies seeded

--- starter-code/lab-mongoose-movies/bin/seeds.js
+++ starter-code/lab-mongoose-movies/bin/seeds.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Celebrity = require("../models/Celebrity");
+const Movie = require("../models/Movie");
 
 const celebrities = [
   {
@@ -19,6 +20,24 @@ const celebrities = [
   }
 ];
 
+const movies = [
+  {
+      title: "Mission: Imposible",
+      genre: "Action",
+      plot: "Tom Cruise is a fucker"
+  },
+  {
+      title: "Seinfeld",
+      genre: "Comedia",
+      plot: "A show about nothing"
+    },
+  {
+      title: "The Boxer",
+      genre: "Action",
+      plot: "A boxes bites someone's ear"
+  }
+];
+
 mongoose
   .connect('mongodb://localhost/lab-mongoose-movies', { useNewUrlParser: true })
   .then(x => {
@@ -26,6 +45,12 @@ mongoose
     }).catch(e => console.log(e))
     Celebrity.insertMany(celebrities).then(celebrities => {
       console.log(celebrities);
+    }).catch(e => console.log(e))
+
+    Movie.collection.drop().then(() => {
+    }).catch(e => console.log(e))
+    Movie.insertMany(movies).then(movies => {
+      console.log(movies);
     }).catch(e => console.log(e))
   })
   .catch(err => {
--- starter-code/lab-mongoose-movies/routes/celebrities.js
+++ starter-code/lab-mongoose-movies/routes/celebrities.js
@@ -2,12 +2,11 @@ const express = require('express');
 const router = express.Router();
 const Celebrity = require('../models/Celebrity')
 
-
 //GET CREATE
 router.get('/new', (req, res, next) => {
   res.render('celebrities/new')
 })
-//POSt CREATE
+//POST CREATE
 router.post('/new', (req, res, next) => {
   const { name, occupation, catchPhrase} = req.body;
   Celebrity.create({name, occupation, catchPhrase}).then(celeb => {
@@ -15,7 +14,6 @@ router.post('/new', (req, res, next) => {
     res.redirect('/celebrities')
   }).catch(e => console.log(e))
 });
-
 
 // GET READ LIST
 router.get('/', (req, res, next) => {
@@ -30,6 +28,36 @@ router.get('/:id', (req, res, next) => {
   Celebrity.findById(req.params.id)
     .then(celeb => {
       res.render('celebrities/show', {celeb})
+    }).catch(next)
+    
+})
+
+//GET EDIT
+router.get('/:id/edit', (req, res, next) => {
+  Celebrity.findById(req.params.id)
+    .then(celeb => {
+      res.render('celebrities/edit', {celeb})
+    }).catch(next)
+    
+})
+//POST EDIT
+router.post('/:id', (req, res, next) => {
+  const {name, occupation, catchPhrase} = req.body;
+  Celebrity.findByIdAndUpdate(req.params.id, {name :name, occupation: occupation, catchPhrase: catchPhrase})
+    .then(celeb => {
+      let stringId = encodeURIComponent(celeb._id);
+      res.redirect('/celebrities/' + stringId);
+    }).catch(next)
+    
+})
+
+//POST DELETE CELEBRITY
+router.post('/:id/delete', (req, res, next) => {
+  const {celebId} = req.body;
+  Celebrity.findByIdAndRemove(celebId)
+    .then(removed => {
+      console.log('Removed Celebrity', removed);
+      res.redirect('/celebrities');
     }).catch(next)
     
 })
