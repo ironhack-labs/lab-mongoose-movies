a574871bb83a75a82b25623d3c05cae4c52bc9c6
Celia Arnelas <carnelas@gmail.com>
2018-09-17 09:25:51 +0200

    6 iteracion

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const router  = express.Router();
-const Celebrity = require("../models/Celebrity");
+const Celebrity = require("../models/celebrity");
 
-
-/* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
-});
 
 //GET celebrities
 router.get('/celebrities', (req, res, next) => {
@@ -17,4 +12,40 @@ router.get('/celebrities', (req, res, next) => {
       .catch(error => {
         console.log(error)
     })
+});
+
+
+
+//GET Details
+router.get('/celebrities/:id', (req, res, next) => {
+  Celebrity.findById(req.params.id)
+    .then(celebrities => {
+      res.render("celebrities/show", {celebrities});
+    })
+    .catch(error => {
+      console.log(error)
+  })
+});
+
+//GET New
+router.get('/new', (req, res, next) => {
+  res.render("celebrities/new");
+});
+
+router.post("/new", (req, res) => {
+  const { name, occupation, catchPhrase } = req.body;
+  const celebrity = new Celebrity({ name, occupation, catchPhrase });
+  celebrity.save( err => {
+      if (err) {
+          return next(err)
+      }
+      res.redirect('/celebrities');
+});
+});
+
+
+
+//DELETE celebrity
+router.post("/celebrities/:id/delete", (req, res) => {
+  Celebrity.findByIdAndRemove(req.params.id, () => res.redirect('/celebrities'))   
 });
