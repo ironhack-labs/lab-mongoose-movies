5af09a4b8e99e515aaced18f7d7db83a4af61732
Mapno <mariorvinas@gmail.com>
2018-09-17 08:34:31 +0200

    movie collection done

--- starter-code/mongoose-celeb/data.js
+++ starter-code/mongoose-celeb/data.js
@@ -1,17 +1,32 @@
 module.exports = [
+    // {
+    //     name: 'Carl Cox',
+    //     occupation: 'Music Producer & DJ',
+    //     catchPhrase: 'Oh yes! Oh yes! Ibiza!'
+    // },
+    // {
+    //     name: 'Karl Max',
+    //     occupation: 'Philosopher',
+    //     catchPhrase: 'To be radical is to grasp things by the root'
+    // },
+    // {
+    //     name: 'Mariano rajoy',
+    //     occupation: 'Politician',
+    //     catchPhrase: 'Como decia Galileo, el movimiento siempre se acelera cuando se va a detener'
+    // }
     {
-        name: 'Carl Cox',
-        occupation: 'Music Producer & DJ',
-        catchPhrase: 'Oh yes! Oh yes! Ibiza!'
+        title: 'Inglorious Basterds',
+        genre: 'War',
+        plot: 'In Nazi-occupied France during World War II, a plan to assassinate Nazi leaders by a group of Jewish U.S. soldiers coincides with a theatre owner\'s vengeful plans for the same.'
     },
     {
-        name: 'Karl Max',
-        occupation: 'Philosopher',
-        catchPhrase: 'To be radical is to grasp things by the root'
+        title: 'The Lord of the Rings: The Return of the King',
+        genre: 'Fantasy',
+        plot: 'Gandalf and Aragorn lead the World of Men against Sauron\'s army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.'
     },
     {
-        name: 'Mariano rajoy',
-        occupation: 'Politician',
-        catchPhrase: 'Como decia Galileo, el movimiento siempre se acelera cuando se va a detener'
+        title: 'The Pianist',
+        genre: 'Drama',
+        plot: 'A Polish Jewish musician struggles to survive the destruction of the Warsaw ghetto of World War II.'
     }
 ]
