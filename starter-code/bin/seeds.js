445cad891cb0e8ae6c35e87b9a112b8dcbdaec32
wergilia <weronika.zdrojewska@gmail.com>
2018-09-17 09:33:10 +0200

    Celebrities done, movies just started

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,31 +1,63 @@
+// const mongoose = require('mongoose');
+// const Celebrity = require('../models/Celebrity');
+
+// const dbName = 'my-celebrities';
+// mongoose.connect(`mongodb://localhost/${dbName}`);
+
+// const celebrities = [
+//   {
+//     name: "Sylwia Poplawska",
+//     occupation: "actor",
+//     catchPhrase: "I want to be Marilyn Monroe of 21th century"
+//     },
+//   {
+//     name: "Monika Szadkowska",
+//     occupation: "kite surfer",
+//     catchPhrase: "Zycie jest jak rower "    
+//   },
+//   {
+//     name: "Brodka ",
+//     occupation: "singer",
+//     catchPhrase: "Lubie lato w Varsovie"  
+//   }  
+// ]
+
+// Celebrity.collection.drop();
+
+// Celebrity.create(celebrities, (err) => {
+//   if (err) { throw(err) }
+//   console.log(`Created ${celebrities.length} celebrities`)
+//   mongoose.connection.close()
+// });
+
 const mongoose = require('mongoose');
-const Celebrity = require('../models/Celebrity');
+const Movie = require('../models/Movie');
 
 const dbName = 'my-celebrities';
 mongoose.connect(`mongodb://localhost/${dbName}`);
 
-const celebrities = [
+const movies = [
   {
-    name: "Sylwia Poplawska",
-    occupation: "actor",
-    catchPhrase: "I want to be Marilyn Monroe of 21th century"
+    title: "Perfect Day",
+    genre: "drama",
+    plot: "War in Balkans"
     },
   {
-    name: "Monika Szadkowska",
-    occupation: "kite surfer",
-    catchPhrase: "Zycie jest jak rower "    
+    title: "buu",
+    genre: "comedy",
+    plot: "Zycie jest jak rower "    
   },
   {
-    name: "Brodka ",
-    occupation: "singer",
-    catchPhrase: "Lubie lato w Varsovie"  
+    title: "show ",
+    genre: "drama",
+    plot: "Lubie lato w Varsovie"  
   }  
 ]
 
-Celebrity.collection.drop();
+Movie.collection.drop();
 
-Celebrity.create(celebrities, (err) => {
+Movie.create(movies, (err) => {
   if (err) { throw(err) }
-  console.log(`Created ${celebrities.length} celebrities`)
+  console.log(`Created ${movies.length} movies`)
   mongoose.connection.close()
 });
