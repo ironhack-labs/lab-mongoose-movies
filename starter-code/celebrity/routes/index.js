1a513e6afc692818bfda1b4ea5d2567e0b973d8c
Adolfo Vela <vela@tekplanner.com>
2018-10-05 17:19:13 -0500

    Listo iteracion 2

--- starter-code/celebrity/routes/index.js
+++ starter-code/celebrity/routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router  = express.Router();
+const Celebrity = require('../models/Celebrity')
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+//RUTA CELEBRITY
+router.get('/celebrities', (req, res) => {
+  Celebrity.find()
+    .then(celebritytables => {
+      res.render('celebrities', {
+        header: "Lista de Celebridades",
+        celebritytables
+      })
+    })
+})
 
 module.exports = router;
