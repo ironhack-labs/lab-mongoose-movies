8b9a25aac0fc61d9724202600fbea1189ac7fd07
tomjohnburton <tomjohnburton@users.noreply.github.com>
2018-11-03 21:53:21 +0100

    finished

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -8,6 +8,8 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+// /celebrity GET page
+
 router.get('/celebrities', (req,res,next) => {
   Celebrity.find()
   .then(celebrities=>{
@@ -26,4 +28,56 @@ router.get('/celebrities/:id', (req,res,next) => {
     res.render('show',{celebrities})
   })
 })
+
+
+// /add-new GET page
+
+router.get('/add-new', (req,res,next)=>{
+  res.render('add-new')
+});
+
+router.post('/add-new', (req,res,next)=>{
+  Celebrity.create({
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase
+  })
+  .then(celebrities => {
+    res.render('show-new',{celebrities})
+  })
+})
+
+// delete celebs
+
+router.get('/celebrities/:id/delete', (req,res,next) => {
+  let id = req.params.id;
+  Celebrity.findByIdAndDelete(id)
+  .then(celebrities =>{
+    res.render('show-new',{celebrities})
+  })
+})
+
+// edit celebs
+
+router.get('/celebrities/:id/edit', (req,res,next)=>{
+  Celebrity.findById(req.params.id)
+  .then(celebrities =>{
+
+    res.render('edit', {celebrities})
+  })
+});
+
+router.post('/celebrities/:id', (req,res,next) => {
+  let id = req.params.id;
+  Celebrity.findByIdAndUpdate(id,     
+    {name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase
+  })
+  .then(celebrities =>{
+    res.redirect('/celebrities/' +celebrities._id)
+  })
+})
+
+
 module.exports = router;
