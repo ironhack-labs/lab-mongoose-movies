487594075a58793b5eacadf0643183adec68f2d3
Axel <axdahlin@gmail.com>
2018-11-03 21:57:02 +0100

    add almost done with iteration 5

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -12,7 +12,7 @@ router.get('/', (req, res, next) => {
 router.get('/celebrities', (req, res, next) => {
   celebrityModel.find()
     .then(data => {
-  res.render('celebrities/celebrities', {data});
+      res.render('celebrities/celebrities', {data});
 
 
     })
@@ -21,5 +21,73 @@ router.get('/celebrities', (req, res, next) => {
     })
   console.log('SUCCESS')
 })
+
+
+
+
+
+router.get('/celebrities/:id', (req, res, next) => {
+  
+  let id = req.params.id
+  
+  celebrityModel.findById(id)
+  .then(data => {
+    console.log(data)
+    res.render('celebrities/show', {data});
+  })
+  .catch(err => {
+    next()
+    console.log(err)
+  })
+  
+})
+
+
+
+router.get('/celebrities/new', (req, res, next) => {
+  res.render('celebrities/new');
+});
+
+
+
+router.post('/celebrities', (req, res) => {
+  // What ES6 feature could we use to clean these two lines up?
+
+  let celebInfo = {
+     name: req.body.name,
+     occupation: req.body.occupation,
+     catchPhrase: req.body.catchPhrase
+  }
+
+  var newInfo = new celebrityModel(celebInfo);
+  newInfo.save(function (err) {
+  if (err) {
+
+    res.render('celebrities/new');
+
+  } else {
+
+   celebrityModel.find()
+    .then(data => {
+      res.render('celebrities/celebrities', {data});
+
+
+    })
+  }
+  // saved!
+});
+
+
+
+});
+
+
+router.get('/celebrities/:id/delete', (req, res, next) => {
+  
+  let id = req.params.id
+  
+
+})
+
 
 module.exports = router;
