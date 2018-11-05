c07fbdc46c9c3db0909b8e17f7d8d64b0f344f16
Johanna <johanna.skuhra@gmail.com>
2018-11-05 14:17:11 +0100

    done iteration 6

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -20,6 +20,10 @@ router.get('/celebrities', (req, res, next) => {
 
 });
 
+router.get('/celebrities/new', (req, res, next) => {
+    res.render("celebrities/new")
+});
+
 router.get('/celebrities/:id', (req, res, next) => {
     let id = req.params.id
     Celebrity.findById(id)
@@ -34,9 +38,6 @@ router.get('/celebrities/:id', (req, res, next) => {
 
 });
 
-router.get('/celebrities/new', (req, res, next) => {
-    res.render("celebrities/new")
-});
 
 router.post('/celebrities', (req, res, next) => {
     // If there is an empty title
@@ -60,18 +61,25 @@ router.post('/celebrities/:id/delete', (req, res, next) => {
         })
 })
 
-//Route to handle the edit form submission BONUS
-//router.post('/celebrities/:id/edit', (req, res, next) => {
-// Find the celebrity and update it with the info from the form
-// Celebrity.findByIdAndUpdate(req.params.id, {
-//     name: req.body.name,
-//     occupation: req.body.occupation,
-//     catchPhrase: req.body.catchPhrase,
-// })
-//     .then(celebrity => {
-//         res.redirect('/celebrities/')
-//     })
-//
+router.get('/celebrities/:id/edit', (req, res, next) => {
+    Celebrity.findById(req.params.id)
+        .then(celebrity => {
+            res.render('celebrities/edit', { celebrity })
+        })
+});
 
+
+//Route to handle the edit form submission BONUS
+router.post('/celebrities/:id/edit', (req, res, next) => {
+    //Find the celebrity and update it with the info from the form
+    Celebrity.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        occupation: req.body.occupation,
+        catchPhrase: req.body.catchPhrase,
+    })
+        .then(celebrity => {
+            res.redirect('/celebrities', { celebrity })
+        })
+});
 
 module.exports = router;
