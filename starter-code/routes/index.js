56869861c344fe37aad495406123051f14742bc6
Rachel Hofer <rhofer3@gmail.com>
2018-11-14 15:13:17 -0500

    done with iteration 4

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -17,14 +17,35 @@ router.get('/celebrities', (req, res, next) => {
     })
 });
 
-router.get('/celebrities/:theID', (req, res, next)=>{
-  Celeb.findById(req.params.theID)
-  .then((specificsFromDB)=>{
-      res.render('celeb-det', {theSpecifics: specificsFromDB})
+router.get('/celebrities/new', (req, res, next) => {
+  Celeb.find()
+  .then((allTheCelebs)=>{
+      res.render('new-celeb', {allTheCelebs})
   })
   .catch((err)=>{
       next(err);
   })
+});
+
+router.post('/celebrities/create', (req, res, next)=>{
+  Celeb.create(req.body)
+  .then(()=>{
+      res.redirect('/celebrities');
+  })
+  .catch((err)=>{
+      next(err)
+  })
 })
+
+router.get('/celebrities/:theID', (req, res, next)=>{
+  Celeb.findById(req.params.theID)
+    .then((specificsFromDB)=>{
+      res.render('celeb-det', {theSpecifics: specificsFromDB})
+  })
+    .catch((err)=>{
+      next(err);
+  })
+})
+
 
 module.exports = router;
