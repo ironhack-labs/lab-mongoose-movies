f80b962dc0d13bbbaeb7e566e3ec78118788b730
Rachel Hofer <rhofer3@gmail.com>
2018-11-15 09:22:15 -0500

    done through iteration 10

--- starter-code/routes/indexMovie.js
+++ starter-code/routes/indexMovie.js
@@ -13,4 +13,35 @@ router.get('/movies', (req, res, next) => {
     })
 });
 
+router.get('/movies/new', (req, res, next) => {
+    Movies.find()
+    .then((allTheMovies)=>{
+        res.render('views-movies/new-movie', {allTheMovies})
+    })
+    .catch((err)=>{
+        next(err);
+    })
+  });
+
+router.post('/movies/create', (req, res, next)=>{
+    Movies.create(req.body)
+    .then(()=>{
+        res.redirect('/movies');
+    })
+    .catch((err)=>{
+        next(err)
+    })
+  })
+
+
+router.get('/movies/:theID', (req, res, next)=>{
+    Movies.findById(req.params.theID)
+      .then((specificsFromDB)=>{
+        res.render('views-movies/movies-det', {theSpecifics: specificsFromDB})
+    })
+      .catch((err)=>{
+        next(err);
+    })
+  })
+
 module.exports = router;
