f898a1643fc2e3e56b52ec58a2bf3c72e87032f9
DSauthier <danielsauthier@live.com>
2018-11-15 08:33:25 -0500

    models added

---
+++ starter-code/models/celebrityModels.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema   = mongoose.Schema;
+
+
+const celebritySchema = new Schema({
+    name: String,
+    occupation: String,
+    catchPhrase: String
+  });
+
+
+const Celebrity = mongoose.model('celebrityModels', celebritySchema);
+
+
+module.exports = Celebrity;
