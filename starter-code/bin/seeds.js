f66b210ad5c5522acebad38363ae7936b4da7225
aemartos <aemartos@gmail.com>
2018-11-16 13:34:13 +0100

    Create Movie model and movie seeds

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -25,7 +25,25 @@ const celebs = [
     occupation:"Actress",
     catchPhrase:"People think that the directors direct actors. No. Really, what the director's doing is directing the audience's eye through the film."
   }
-]
+];
+
+const movies = [
+  {
+    title:"Crooked House",
+    genre:"Crime",
+    plot:"In Agatha Christie's most twisted tale, a spy-turned-private-detective is lured by his former lover to catch her grandfather's murderer before Scotland Yard exposes dark family secrets."
+  },
+  {
+    title:"The Post",
+    genre:"Biography",
+    plot:"A cover-up that spanned four U.S. Presidents pushed the country's first female newspaper publisher and a hard-driving editor to join an unprecedented battle between the press and the government."
+  },
+  {
+    title:"The Hours",
+    genre:"Drama",
+    plot:'The story of how the novel "Mrs. Dalloway" affects three generations of women, all of whom, in one way or another, have had to deal with suicide in their lives.'
+  }
+];
 
 
 
@@ -34,5 +52,9 @@ Celebrity.collection.drop();
 Celebrity.create(celebs)
   .then(celebs => {
     console.log(`Created ${celebs.length} celebs!`);
-  })
-    .then(() => {mongoose.disconnect()});
+  }).then(() => {mongoose.disconnect()});
+
+Movie.create(movies)
+  .then(movies => {
+    console.log(`Created ${movies.length} movies!`);
+  }).then(() => {mongoose.disconnect()});
---
+++ starter-code/models/Movie.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const MovieSchema = new Schema({
+  title: String,
+  genre: String,
+  plot: String
+});
+
+module.exports = mongoose.model('Movie', MovieSchema);
