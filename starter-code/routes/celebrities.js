d8f280147df90ec96fed0f9df32e7bf70349a812
Christian Romero <christian@MacBook-Air-de-Christian.local>
2018-11-17 21:03:47 +0100

    Iteration 3

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -5,9 +5,17 @@ const Celebrity = require('../models/Celebrity.js');
 router.get('/', (req, res, next) => {
   Celebrity.find({})
     .then(celebrities => {
-      res.render('celebrities/index', {celebrities})
+      res.render('celebrities/index', { celebrities })
+    })
+    .catch(error => console.log("Error to find celebrities" + error))
+});
+
+router.get('/:_id', (req, res, next) => {
+  Celebrity.findById(req.params._id)
+    .then(celebrity => {
+      res.render('celebrities/show', { celebrity })
     })
     .catch(error => console.log("Error to find a celebrity" + error))
-});
+})
 
 module.exports = router;
