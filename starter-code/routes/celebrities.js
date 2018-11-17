20db76bd9bca6e14936afc0789f90663dca9b96e
Rodrigo.Munoz <rodrigomunozga@gmail.com>
2018-11-17 19:29:27 +0100

    Iteration 2,

---
+++ starter-code/routes/celebrities.js
@@ -0,0 +1,28 @@
+// const express = require('express');
+// const router  = express.Router();
+// const data = require('../models/Celebrity')
+
+// /* GET celebrities page */
+// router.get('/celebrities', (req, res, next) => {
+  
+// })
+// .then(() =>{
+//   res.render('celebrities/index');
+// })
+
+// router.post('/celebrities', (req, res, next) => {
+
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+// module.exports = router;
