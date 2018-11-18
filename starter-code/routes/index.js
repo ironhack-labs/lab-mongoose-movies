822fe9371faca15524dd2ab20927b64c26a29344
cgandiaf <c.gandia.f@gmail.com>
2018-11-18 13:30:43 +0100

    Iteration #3: The Celebrity Details Page

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -11,11 +11,24 @@ router.get('/celebrities', (req, res, next) => {
   Celebrity.find({})
     .then(list => {
       console.log(list);
-      res.render('celebrities', {list});
+      res.render('celebrities', { list });
     })
     .catch(err => {
       res.redirect('/index')
     });
+
+});
+
+router.get('/celebrities/:id', (req, res, next) => {
+
+  Celebrity.findById(req.params.id)
+    .then(celebrity => {
+      console.log(celebrity);
+      res.render('celebrity', {celebrity});
+    })
+    .catch(err => {
+      console.log('No encuentra el personaje buscado')
+    })
 
 });
 
