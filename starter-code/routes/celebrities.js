d1aa03ec67fb54716b3d9b3ed6651530e0e14f3c
Fer Perez Ayala <ferp@protonmail.ch>
2018-11-18 19:30:17 +0100

    Thid iteration complete.

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -19,4 +19,19 @@ router.get('/celebrities', (req, res, next) => {
         })
 });
 
+router.get('/celebrities/:id', (req, res, next) => {
+
+    let celebId = req.params.id;
+    Celeb.findById(celebId)
+    .then((details) => {
+        res.render('celebrities/show', {details});
+    })
+    .catch((err) => {
+        if (err) {
+            next();
+            return err
+        }
+    })
+})
+
 module.exports = router;
