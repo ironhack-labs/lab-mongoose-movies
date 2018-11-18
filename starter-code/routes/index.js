f98b86b6ae0ef8230cede80f9570a1c13f75bd6c
Eduardo Gandía Fernández <eduardogandiafernandez@mbp-de-eduardo.home>
2018-11-18 16:54:21 +0100

    Iteration 3

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router  = express.Router();
 const Celebrity = require("../models/Celebrity")
+const genericCeleb = new Celebrity();
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -25,6 +27,23 @@ router.get('/celebrities/:id', (req, res, next) => {
     res.render('celebrity',{celeb: celeb});
   })
   .catch(err=>{
+    
+  })
+});
+
+router.get('/new', (req, res, next) => {
+  res.render('new');
+});
+
+router.post('/new', (req, res) => {
+  genericCeleb.name = req.body.name;
+  genericCeleb.occupation = req.body.occupation;
+  genericCeleb.catchPhrase = req.body.catchPhrase;
+  genericCeleb.save()
+  .then(()=>{
+    res.redirect("celebrities")
+  })
+  .catch(()=>{
 
   })
 });
