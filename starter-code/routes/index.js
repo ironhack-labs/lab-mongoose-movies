6cf638f31f391e6174cc732f9a7298e9d715cc2f
aterron84 <aterron84@gmail.com>
2018-11-18 22:49:45 +0100

    Iteration 2, modificararchivos según learning, aunque funciona correctamente

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router  = express.Router();
+const Celebrity = require('../models/Celebrity');
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-module.exports = router;
+
+ /* GET celebrities page */
+router.get('/celebrities', (req, res, next) => {
+  Celebrity.find()
+    .then(celebrities => res.render('celebrities', { celebrities }))
+    .catch(err => next(err));
+});
+ module.exports = router;
