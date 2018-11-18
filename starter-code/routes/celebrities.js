f98d2aa73e00eb4e8ba6467c3121a8a65edf93bb
Jorge Devesa <jrgdevesa@gmail.com>
2018-11-18 15:31:20 +0100

    Iteration 3

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,12 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Celebrities = require('../models/celebrity');
+const Celebrities = require("../models/celebrity");
 
-router.get('/celebrities',(req,res,next) => {
-    Celebrities.find()
-      .then(celebrities => res.render("celebrities/index", { celebrities }) )
-      .catch(err => { next(err) })
-  
-})
+router.get("/celebrities", (req, res, next) => {
+  Celebrities.find()
+  .then(celebrities => res.render("celebrities/index", { celebrities }))
+  .catch(err => { next(err) });
+});
+
+router.get("/celebrities/:id", (req, res, next) => {
+  Celebrities.findById(req.params.id)
+    .then(celebrities => res.render("celebrities/show", { celebrities }))
+    .catch(err => { next(err) })
+});
 
 module.exports = router;
