8da15ee684f4aed882fa3663dc6f17c4a18b8252
Sandra Luengo <luchosandra@gmail.com>
2018-11-18 19:24:41 +0100

    Iteration #2: Listing Our Celebrities

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router  = express.Router();
-const Celebrity = require('../models/Celebrity')
-const mongoose = require('mongoose');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
