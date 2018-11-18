7061c5f38dff717da4ce2346ed04fdc4ac93b12a
Zeswen <zeswentyrael@gmail.com>
2018-11-18 12:51:21 +0100

    Iteration 3

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -4,10 +4,15 @@ const router  = express.Router();
 
 const Celebrity = require('../models/Celebrity');
 
-/* GET home page */
 router.get('/celebrities', (req, res, next) => {
   Celebrity.find()
     .then(celebrities => res.render('celebrities/index', { celebrities }))
+    .catch(err => next(err));
+});
+
+router.get('/celebrities/:id', (req, res, next) => {
+  Celebrity.findById(req.params.id)
+    .then(celebrity => res.render('celebrities/show', { celebrity }))
     .catch(err => next(err));
 });
 
