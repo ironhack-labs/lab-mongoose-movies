19de1ad959abed69420d20e803e597826a125d19
Sandra Luengo <luchosandra@gmail.com>
2018-11-18 20:11:59 +0100

    Iteration #3: The Celebrity Details Page

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -9,17 +9,29 @@ router.get('/', (req, res, next) => {
     Celebrity.find({})
         .then(allCelebrities => {
             console.log(allCelebrities);
-            return res.render('celebrities/', { allCelebrities });
-        })
-        .then(() => {
-            mongoose.connection.close()
-            console.log('Conection close');
+            return res.render('celebrities', { allCelebrities });
         })
         .catch(err => {
             console.log(err);
             next();
+            return err;
         })
+});
 
+
+router.get('/show/:celebrityId', (req, res, next) => {
+
+    let id = req.params.celebrityId;
+
+    Celebrity.findById(id)
+        .then(celebrityData => {
+            console.log(celebrityData);
+            return res.render('celebrities/show', { celebrityData });
+        })
+        .catch(err => {
+            next();
+            return err;
+        })
 
 });
 
