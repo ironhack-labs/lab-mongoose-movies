edc206e2dad4167abc8315ab705edba3defb3bb1
Eduardo Gandía Fernández <eduardogandiafernandez@mbp-de-eduardo.home>
2018-11-18 17:19:37 +0100

    Iteration 5

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,51 +1,54 @@
-const express = require('express');
-const router  = express.Router();
-const Celebrity = require("../models/Celebrity")
+const express = require("express");
+const router = express.Router();
+const Celebrity = require("../models/Celebrity");
 const genericCeleb = new Celebrity();
 
-
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
-router.get('/celebrities', (req, res, next) => {
+router.get("/celebrities", (req, res, next) => {
   Celebrity.find({})
-  .then(celebs=>{
-    console.log(celebs)
-    res.render('celebrities',{celebs: celebs});
-  })
-  .catch(err=>{
-
-  })
+    .then(celebs => {
+      console.log(celebs);
+      res.render("celebrities", { celebs: celebs });
+    })
+    .catch(err => {});
 });
 
-router.get('/celebrities/:id', (req, res, next) => {
+router.get("/celebrities/:id", (req, res, next) => {
   Celebrity.findById(req.params.id)
-  .then(celeb=>{
-    console.log(celeb)
-    res.render('celebrity',{celeb: celeb});
-  })
-  .catch(err=>{
-    
-  })
+    .then(celeb => {
+      console.log(celeb);
+      res.render("celebrity", { celeb: celeb });
+    })
+    .catch(err => {});
 });
 
-router.get('/new', (req, res, next) => {
-  res.render('new');
+router.get("/new", (req, res, next) => {
+  res.render("new");
 });
 
-router.post('/new', (req, res) => {
+router.post("/new", (req, res) => {
   genericCeleb.name = req.body.name;
   genericCeleb.occupation = req.body.occupation;
   genericCeleb.catchPhrase = req.body.catchPhrase;
-  genericCeleb.save()
-  .then(()=>{
-    res.redirect("celebrities")
-  })
-  .catch(()=>{
+  genericCeleb
+    .save()
+    .then(() => {
+      res.redirect("/celebrities");
+    })
+    .catch(() => {});
+});
 
-  })
+router.post("/celebrities/:id/delete", (req, res, next) => {
+  Celebrity.findByIdAndRemove(req.params.id)
+    .then(celeb => {
+      console.log(celeb);
+      res.redirect("/celebrities");
+    })
+    .catch(err => {});
 });
 
 module.exports = router;
