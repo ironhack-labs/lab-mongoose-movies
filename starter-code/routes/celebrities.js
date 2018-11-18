35852580b12d51d674b5a7122b58948b606bf6db
Sandra Luengo <luchosandra@gmail.com>
2018-11-18 21:40:22 +0100

    Iteration #4: Adding New Celebrities

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -3,21 +3,34 @@ const router = express.Router();
 const Celebrity = require('../models/Celebrity')
 const mongoose = require('mongoose');
 
-/* GET home page */
+
 router.get('/', (req, res, next) => {
 
     Celebrity.find({})
         .then(allCelebrities => {
-            console.log(allCelebrities);
             return res.render('celebrities', { allCelebrities });
         })
         .catch(err => {
-            console.log(err);
             next();
             return err;
         })
 });
 
+router.post('/new', (req, res, next) => {
+
+    const { name, occupation, catchPhrase } = req.body;
+    const newCelebrity = new Celebrity({ name, occupation, catchPhrase });
+
+    newCelebrity.save()
+        .then((celebrity) => {
+            console.log(celebrity);
+            res.redirect('/celebrities/');
+        })
+        .catch(err => {
+            console.log(err);
+            return res.render('celebrities/new', { errorMessage: "There was an error, please resend the form" });
+        })
+});
 
 router.get('/show/:celebrityId', (req, res, next) => {
 
@@ -25,14 +38,19 @@ router.get('/show/:celebrityId', (req, res, next) => {
 
     Celebrity.findById(id)
         .then(celebrityData => {
-            console.log(celebrityData);
             return res.render('celebrities/show', { celebrityData });
         })
         .catch(err => {
             next();
             return err;
         })
-
 });
+
+router.get('/new', (req, res, next) => {
+    console.log('entro');
+    res.render('celebrities/new');
+});
+
+
 
 module.exports = router;
