3424b2ab8fdcd8a4c89bcfa55650f470065d6a3f
Steven M <steve5mv5@gmail.com>
2018-11-18 13:15:33 +0100

    iteration 3 & 4

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -23,3 +23,52 @@ router.get('/celebrities', (req,res,next) => {
     .catch(next)
 
 })
+
+
+
+/* GET form new celebrity */
+
+router.get('/celebrities/new',(req,res,next) =>{
+  res.render('celebrities/new');
+})
+
+/* GET specicy celebritie */
+
+router.get('/celebrities/:id', (req,res,next) =>{
+  let celebritieId = req.params.id;
+
+  Celebrity.findOne({'_id':celebritieId})
+    .then(celebrity =>{
+        res.render('celebrities/show', celebrity);
+    })
+    .catch(next)
+})
+
+
+
+/* POST create new Celebrity */
+
+router.post('/celebrities', (req,res,next) => {
+  
+   var objCelebrity = {
+     name: req.body.name,
+     occupation: req.body.occupation,
+     catchPhrase: req.body.catchPhrase
+   }
+
+   console.log(objCelebrity);
+
+   const newCelebrity = new Celebrity();
+
+   newCelebrity.name = objCelebrity.name;
+   newCelebrity.occupation = objCelebrity.occupation;
+   newCelebrity.catchPhrase = objCelebrity.catchPhrase;
+
+   newCelebrity.save()
+    .then(()=>{
+      res.redirect('/celebrities');
+    })
+    .catch(()=>{
+      req.redirect('/celebrities');
+    })
+})
