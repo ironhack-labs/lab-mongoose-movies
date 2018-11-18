8d161c9d370834a6bb7fab88ff498b451aa27ef8
Christian Romero <christian@MacBook-Air-de-Christian.local>
2018-11-18 20:34:34 +0100

    Itertion 7

--- starter-code/app.js
+++ starter-code/app.js
@@ -11,7 +11,7 @@ const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/celebrities', {useNewUrlParser: true})
+  .connect(`mongodb://localhost/${process.env.DB}`, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,27 +1,56 @@
-const mongoose = require('mongoose');
-const Celebrity = require('../models/celebrity');
-mongoose.connect(`mongodb://localhost/celebrities`);
+require('dotenv').config();
+// const mongoose = require('mongoose');
+// const Celebrity = require('../models/celebrity');
+// mongoose.connect(`mongodb://localhost/${process.env.DB}`);
 
-const celebrities = [
+// const celebrities = [
+//   {
+//     name: "celebrity1",
+//     occupation: "Actor1",
+//     catchPhrase: "Phrase1"
+//   },
+//   {
+//     name: "celebrity2",
+//     occupation: "Actor2",
+//     catchPhrase: "Phrase2"
+//   },
+//   {
+//     name: "celebrity3",
+//     occupation: "Actor3",
+//     catchPhrase: "Phrase3"
+//   }
+// ]
+
+// Celebrity.create(celebrities, (err) => {
+//   if (err) { throw (err) }
+//   console.log(`Created ${celebrities.length} celebrities`)
+//   mongoose.connection.close()
+// });
+
+const mongoose = require('mongoose');
+const Movie = require('../models/movie');
+mongoose.connect(`mongodb://localhost/${process.env.DB}`);
+
+const movies = [
   {
-    name: "celebrity1",
-    occupation: "Actor1",
-    catchPhrase: "Phrase1"
+    title: "movie1",
+    genre: "genre1",
+    plot: "plot1"
   },
   {
-    name: "celebrity2",
-    occupation: "Actor2",
-    catchPhrase: "Phrase2"
+    title: "movie2",
+    genre: "genre2",
+    plot: "plot2"
   },
   {
-    name: "celebrity3",
-    occupation: "Actor3",
-    catchPhrase: "Phrase3"
+    title: "movie3",
+    genre: "genre3",
+    plot: "plot3"
   }
 ]
 
-Celebrity.create(celebrities, (err) => {
+Movie.create(movies, (err) => {
   if (err) { throw (err) }
-  console.log(`Created ${celebrities.length} celebrities`)
+  console.log(`Created ${movies.length} movies`)
   mongoose.connection.close()
 });
