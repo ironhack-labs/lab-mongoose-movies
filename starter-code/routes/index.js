59bf631a19f05c8752357683c5b3fa9d81d897e8
Eduardo Gandía Fernández <eduardogandiafernandez@MacBook-Pro-de-Eduardo.local>
2018-11-18 12:37:03 +0100

    Iteration 2

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router  = express.Router();
+const Celebrity = require("../models/Celebrity")
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/celebrities', (req, res, next) => {
+  Celebrity.find({})
+  .then(celeb=>{
+    console.log(celeb)
+    res.render('celebrities',{celeb: celeb});
+  })
+  .catch(err=>{
+
+  })
 });
 
 module.exports = router;
