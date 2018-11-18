0d421cc1b47759221845a450d54b0ebeb0fad094
Steven M <steve5mv5@gmail.com>
2018-11-18 12:15:49 +0100

    Iteration 2: Listing Our Celebrities

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router  = express.Router();
 
+const Celebrity = require('../models/Celebrity');
+
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
 module.exports = router;
+
+
+/* GET Celebrities list  */
+
+
+router.get('/celebrities', (req,res,next) => {
+   Celebrity.find()
+    .then(celebrities => {
+       res.render('celebrities/index', {celebrities});
+    })
+    .catch(next)
+
+})
