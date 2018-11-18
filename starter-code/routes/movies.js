cbb26ce191d1bfd8b0eedae9c7e7b2c06063d54b
Zeswen <zeswentyrael@gmail.com>
2018-11-18 15:03:53 +0100

    Iteration 10

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -10,10 +10,29 @@ router.get('/movies', (req, res, next) => {
     .catch(err => next(err));
 });
 
+router.get('/movies/new', (req, res, next) => {
+  res.render('movies/new');
+});
+
 router.get('/movies/:id', (req, res, next) => {
   Movie.findById(req.params.id)
     .then(movie => res.render('movies/show', { movie }))
     .catch(err => next(err));
+});
+
+router.post('/movies', (req, res, next) => {
+  const addedMovie = new Movie();
+
+  if (req.body.title == '' || req.body.genre == '' || req.body.plot == '') res.redirect('/movies/new');
+
+  addedMovie.title = req.body.title;
+  addedMovie.genre = req.body.genre;
+  addedMovie.plot = req.body.plot;
+
+  console.log(addedMovie);
+  addedMovie.save()
+    .then(() => res.redirect('/movies'))
+    .catch(() => res.redirect('/movies/new'));
 });
 
 module.exports = router;
