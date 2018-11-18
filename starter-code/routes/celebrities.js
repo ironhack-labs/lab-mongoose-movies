ce4277b0e164ca7ba7b4886a6d3e7a453ed20cb5
jrecio <recio.juan@gmail.com>
2018-11-18 17:39:03 +0100

    Iteration 4

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 const Celebrity = require('../models/celebrity.js');
+
+const genericCelebrity = new Celebrity();
 
 router.get('/', (req, res, next) => {
   Celebrity.find({})
@@ -10,19 +12,40 @@ router.get('/', (req, res, next) => {
     })
     .catch(error => {
       console.log(error)
-    }) 
+    })
+});
+
+
+router.get('/new', (req, res, next) => {
+  res.render('celebrities/new');
 });
 
 router.get('/:id', (req, res, next) => {
   let celebrityId = req.params.id;
   Celebrity.findById(celebrityId)
     .then(celebrity => {
-      res.render("celebrities/celebrity", celebrity);
+      res.render("celebrities/show", celebrity);
     })
     .catch(error => {
       console.log(error)
-    }) 
+    })
 });
+
+
+router.post('/', (req, res, next) => {
+  genericCelebrity.name = req.body.name;
+  genericCelebrity.occupation = req.body.occupation;
+  genericCelebrity.catchPhrase = req.body.catchPhrase;
+  genericCelebrity.save()
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(() => {
+      res.redirect('/new');
+  })
+})
+
+
 
 
 module.exports = router;
