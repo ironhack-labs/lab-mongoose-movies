d72fbc2fcbeafb9c43b2acb96bf27551aa0f9d48
Yared Santiago <yaredmyers@gmail.com>
2018-11-19 09:03:41 +0100

    iteration 3 error env solucionado

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router  = express.Router();
 const Celebrity = require('../models/Celebrity')
 
-router.get('/celebrities', (req, res, next) => {
+router.get('/', (req, res, next) => {
   Celebrity.find({})
   .then(celebrities => {
     res.render('celebrities/index',  { celebrities })
