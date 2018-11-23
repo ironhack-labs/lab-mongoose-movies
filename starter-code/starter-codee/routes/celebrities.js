3ff559e65c5a667ce2794e3ad09efc9ede0ab5dd
Rene Manzano <rene@Renes-MacBook.local>
2018-11-23 15:00:27 -0600

    Celebridades hecho

--- starter-code/starter-codee/routes/celebrities.js
+++ starter-code/starter-codee/routes/celebrities.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const Celebrity = require('../models/Celebrity')
+
+router.get('/new',(req,res,next)=>{
+  res.render('celebrities/new')
+})
+router.post('/new',(req,res)=>{
+  Celebrity.create(req.body)
+  .then(celebridad=>{
+    res.render('celebrities/index',celebridad)
+  }).catch(error=>{
+    res.render('celebrities/detail',{celebridad:req.body,error})
+  })
+})
 //lsitar
 router.get('/index',(req,res,next)=>{
   Celebrity.find()
@@ -14,7 +26,26 @@ router.get('/:id',(req,res,next)=>{
   Celebrity.findById(id)
   .then(celebridad =>{
     res.render('celebrities/detail',celebridad)
-  }).catch(error=>next(e))
+  }).catch(error=>next(error))
 })
+//borrar
+router.get('/delete/:id',(req,res,next)=>{
+  const {id}= req.params
+  Celebrity.findById(id)
+  .then(celebridad=>{
+    res.render('celebrities/delete',celebridad)
+  }).catch(e=>next(e))
+})
+router.post('/delete/:id',(req,res,next)=>{
+  const {id} =req.params
+  Celebrity.findByIdAndRemove(id)
+  .then(celebridad=>{
+    
+    res.redirect('/celebrities/index')
+  }).catch(e=>next(e))
+})
+
+
+
 
 module.exports = router
