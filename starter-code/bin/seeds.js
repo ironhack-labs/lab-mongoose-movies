45aabd452cc81e1b37959876760f5409f21c45ad
Carlos Rivera <carlos.rivera.cabrera@gmail.com>
2018-11-24 11:24:14 -0600

    Iteration 7. The Movie Model. Done

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,32 +1,58 @@
 const mongoose = require('mongoose')
 const Celebrity = require('../models/Celebrity')
+const Movie = require('../models/Movie')
 
 const dbName = 'celebrities'
 
 mongoose.connect(`mongodb://localhost/${dbName}`)
 
 
-const celebrities = [
+// const celebrities = [
+//   {
+//     name : "Brendan Fraser",
+//     occupation: "Actor",
+//     catchPhrase: "oh my god"
+//   },
+//   {
+//     name : "Rene Stiller",
+//     occupation: "Astromático",
+//     catchPhrase: "a huevo"
+//   },
+//   {
+//     name : "BlisS",
+//     occupation: "Dev",
+//     catchPhrase: "polloyón"
+//   }
+// ];
+
+// Celebrity.create(celebrities)
+//     .then(celebrities=>{
+//       console.log(`${celebrities.length} celebrities created`)
+//       mongoose.connection.close()
+//     })
+//     .catch(err=>console.log('Something went wrong', err))
+
+const movies = [
   {
-    name : "Brendan Fraser",
-    occupation: "Actor",
-    catchPhrase: "oh my god"
+    title : "Alice",
+    genre: "Animation",
+    plot: "A girl is lost in a wonderful world"
   },
   {
-    name : "Rene Stiller",
-    occupation: "Astromático",
-    catchPhrase: "a huevo"
+    title : "Le planete sauvage",
+    genre: "Anime",
+    plot: "Cruel alien world"
   },
   {
-    name : "BlisS",
-    occupation: "Dev",
-    catchPhrase: "polloyón"
-  }
+    title : "A clockwork orange",
+    genre: "Distopian fiction",
+    plot: "A man is brainwashed"
+  },
 ];
 
-Celebrity.create(celebrities)
-    .then(celebrities=>{
-      console.log(`${celebrities.length} celebrities created`)
+Movie.create(movies)
+    .then(movies=>{
+      console.log(`${movies.length} movies created`)
       mongoose.connection.close()
     })
     .catch(err=>console.log('Something went wrong', err))
