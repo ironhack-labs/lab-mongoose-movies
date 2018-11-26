1a70251d0af3916d5907c3f3061e7254cccbc0f6
jaqusa <dev@javierquinte.com>
2018-11-25 22:45:50 -0600

    iteration 5

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -35,5 +35,25 @@ router.get('/new',(req,res)=>{
 })
 
 
+router.get('/:id/delete',(req, res, next)=>{
+  const {id} = req.params
+  Celebrity.findById(id)
+    .then(celebrity=>{
+      res.render('celebrities/delete',celebrity)
+    }).catch(e=>next(e))
+})
+ router.post('/:id/delete',(req, res)=>{
+  const {id} = req.params
+  Celebrity.findByIdAndRemove(id)
+    .then(celebrity=>{
+      res.redirect('/celebrities')
+    }).catch(e=>next(e))
+})
+
+
+
+
+
+
 
 module.exports = router
