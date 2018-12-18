0b5bcd0881ee3a91159eeb4fa6a8c5b1f2306880
justinezahiri <justinezahiri@gmail.com>
2018-12-18 21:28:38 +0100

    iteration 7

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,30 +1,55 @@
 
 const mongoose = require('mongoose');
-const Celeb = require('../models/celebrity');
+//const Celeb = require('../models/celebrity');
+const Movie = require('../models/movie');
 
 const dbName = 'lab-celebrities';
 mongoose.connect(`mongodb://localhost/${dbName}`);
 
-const celebrities = [
-  {
-    name: "Michael Jackson",
-    occupation: "Singer",
-    catchPhrase: "I love you",
-  },
-  {
-    name: "Mariah Carey",
-    occupation: "Singer",
-    catchPhrase: "All I want for Christmas is you",
-  },
-  {
-    name: "Leonardo Dicaprio",
-    occupation: "Actor",
-    catchPhrase: "I don't know",
-  }, 
-]
+// const celebrities = [
+//   {
+//     name: "Michael Jackson",
+//     occupation: "Singer",
+//     catchPhrase: "I love you",
+//   },
+//   {
+//     name: "Mariah Carey",
+//     occupation: "Singer",
+//     catchPhrase: "All I want for Christmas is you",
+//   },
+//   {
+//     name: "Leonardo Dicaprio",
+//     occupation: "Actor",
+//     catchPhrase: "I don't know",
+//   }, 
+// ]
 
-Celeb.create(celebrities, (err) => {
-  if (err) { throw(err) }
-  console.log(`Created ${celebrities.length} celebrities`)
-  mongoose.connection.close()
-});
+// Celeb.create(celebrities, (err) => {
+//   if (err) { throw(err) }
+//   console.log(`Created ${celebrities.length} celebrities`)
+//   mongoose.connection.close()
+// });
+
+const movies = [
+    {
+      title: "Ocean",
+      genre: "Drama",
+      plot: "France",
+    },
+    {
+      title: "Clown",
+      genre: "Comedy",
+      plot: "US",
+    },
+    {
+      title: "The Bear",
+      genre: "Family",
+      plot: "France",
+    }, 
+  ]
+
+  Movie.create(movies, (err) => {
+    if (err) { throw(err) }
+    console.log(`Created ${movies.length} movies`)
+    mongoose.connection.close()
+  });
