2b151154015ca9f7c6ac60958180723d74231527
justinezahiri <justinezahiri@gmail.com>
2018-12-30 11:32:29 +0100

    iteration 7

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,34 +1,34 @@
 
 const mongoose = require('mongoose');
-//const Celeb = require('../models/celebrity');
+const Celeb = require('../models/celebrity');
 const Movie = require('../models/movie');
 
 const dbName = 'lab-celebrities';
 mongoose.connect(`mongodb://localhost/${dbName}`);
 
-// const celebrities = [
-//   {
-//     name: "Michael Jackson",
-//     occupation: "Singer",
-//     catchPhrase: "I love you",
-//   },
-//   {
-//     name: "Mariah Carey",
-//     occupation: "Singer",
-//     catchPhrase: "All I want for Christmas is you",
-//   },
-//   {
-//     name: "Leonardo Dicaprio",
-//     occupation: "Actor",
-//     catchPhrase: "I don't know",
-//   }, 
-// ]
+const celebrities = [
+  {
+    name: "Michael Jackson",
+    occupation: "Singer",
+    catchPhrase: "I love you",
+  },
+  {
+    name: "Mariah Carey",
+    occupation: "Singer",
+    catchPhrase: "All I want for Christmas is you",
+  },
+  {
+    name: "Leonardo Dicaprio",
+    occupation: "Actor",
+    catchPhrase: "I don't know",
+  }, 
+]
 
-// Celeb.create(celebrities, (err) => {
-//   if (err) { throw(err) }
-//   console.log(`Created ${celebrities.length} celebrities`)
-//   mongoose.connection.close()
-// });
+Celeb.create(celebrities, (err) => {
+  if (err) { throw(err) }
+  console.log(`Created ${celebrities.length} celebrities`)
+  mongoose.connection.close()
+});
 
 const movies = [
     {
