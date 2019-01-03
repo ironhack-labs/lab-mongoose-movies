faa832242e3fafe1effb1f5df33d5b6fc3c2c161
Luise Schneider <luise.schneider@berlin.de>
2019-01-03 21:44:50 +0100

    iteration 1-3 done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,31 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Celebrity = require("../models/celebrity.js");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/celebrities", (req, res, next) => {
+  Celebrity.find()
+    .then(celebrities => {
+      console.log(celebrities);
+      res.render("celebrities/index", { celebrityArray: celebrities });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+router.get("/celebrities/:celebrityId", (req, res, next) => {
+  Celebrity.findOne({ _id: req.params.celebrityId })
+    .then(document => {
+      res.render("celebrities/show", { celebrity: document });
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 module.exports = router;
