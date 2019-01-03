16f44ce4e108ff789781c658f1b52c1f8de78519
annearbor <betting.anne@gmail.com>
2019-01-03 22:06:36 +0100

    nearly done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,20 @@
-const express = require('express');
-const router  = express.Router();
-
+const express = require("express");
+const router = express.Router();
+const Celebrity = require("../models/Celebrity");
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
+router.get("/celebrities", (req, res, next) => {
+  Celebrity.find().then(celebrities => {
+    res.render("celebrities", { celebritiesArray: celebrities });
+  });
+});
+
+router.get("/celebrities/:id", (req, res, next) => {
+  Celebrity.findOne({ _id: req.params.id }).then(celebrity => {
+    res.render("celebrities/show", { celebrity: celebrity });
+  });
+});
 module.exports = router;
