e9b384beee5bbc7e32adbe1c8bf803e1ba5fad1e
StephGidde <steph.gidde@gmx.de>
2019-01-04 13:47:55 +0100

    iteration 5 done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -17,12 +17,49 @@ router.get("/celebrities", (req, res) => {
     });
 });
 
+router.get("/celebrities/new", (req, res) => {
+  res.render("celebrities/new");
+});
+
+router.post("/celebrities", (req, res) => {
+  const { name, ocupation, catchPhrase } = req.body;
+  const newCeleb = new Celebrity({
+    name: name,
+    ocupation: ocupation,
+    catchPhrase: catchPhrase
+  });
+  newCeleb
+    .save()
+    .then(() => {
+      res.redirect("celebrities");
+    })
+    // .then(celebrities => {
+    //   res.render("celebrities/index", { celebrities });
+    // })
+    .catch(error => {
+      console.log(error);
+      res.render("/celebrities/new");
+    });
+});
+
 router.get("/celebrities/:celebId", (req, res) => {
   Celebrity.find({
     _id: req.params.celebId
   })
     .then(celebrities => {
       res.render("celebrities/show", { celebrities });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+router.post("/celebrities/:celebId/delete", (req, res) => {
+  Celebrity.findByIdAndRemove({
+    _id: req.params.celebId
+  })
+    .then(() => {
+      res.redirect("/celebrities");
     })
     .catch(error => {
       console.log(error);
