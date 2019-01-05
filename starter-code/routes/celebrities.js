4d89b411c1a1691b722b9e55ce7f92044c9d1a63
Juan Ignacio Benito <juanignaciobenito@Juans-Air.fritz.box>
2019-01-05 01:42:17 +0100

    finished

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -12,4 +12,40 @@ router.get('/',(req, res)=>{
   })
 })
 
+router.get('/new',(req,res)=>{
+  res.render('./celebrities/new')
+})
+
+router.get('/:celebritieId',(req,res)=>{
+  Celebrity.findOne({_id:req.params.celebritieId}).then((celeb)=>{
+    res.render('./celebrities/show',{celeb})
+  })
+  .catch(error=>{
+    res.send(error)
+  })
+})
+
+router.post('/new',(req,res)=>{
+  const {name,occupation,catchPhrase} = req.body;
+  const newCelebrity = new Celebrity({
+    name:name,
+    occupation:occupation,
+    catchPhrase:catchPhrase})
+
+    newCelebrity.save()
+    .then(()=>{
+      res.redirect('./')
+    })
+    .catch((error) => {
+      res.redirect('./new');
+    })
+})
+
+router.post('/:id/delete',(req,res)=>{
+    Celebrity.findOneAndRemove({_id:req.params.id}).then(()=>{
+      res.redirect('/')
+    })
+})
+
+
 module.exports = router;
