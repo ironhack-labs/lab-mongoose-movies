04c821225b55a90280b78bbc714ab05f626b06a6
Cdelatorre <carlosdelatorrediaz@gmail.com>
2019-01-13 17:12:44 +0100

    home page style and responsive done

---
+++ starter-code/public/js/index.js
@@ -0,0 +1,13 @@
+
+
+$(".btn-movies").hover(function(){
+  $('.part-one-back').css("-webkit-filter", "none");
+  }, function(){
+  $('.part-one-back').css("-webkit-filter", "grayscale(100%)");
+});
+
+$(".btn-celebrities").hover(function(){
+  $('.part-two-back').css("-webkit-filter", "none");
+  }, function(){
+  $('.part-two-back').css("-webkit-filter", "grayscale(100%)");
+});
