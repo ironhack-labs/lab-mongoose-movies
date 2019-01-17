f15a2e5d8c9274559fe208f764cb91a9308bab13
sogchs <i2implantologia@Mac-Pro-de-i2.local>
2019-01-17 10:14:11 +0100

    Finish exercice

---
+++ models/celebrity.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    occupation: {
+        type: String,
+        required: true
+    },
+    catchPhrase: {
+        type: String,
+        required: true
+    },
+    photo: {
+        type: String
+    }
+}, {
+    timestamps: true
+}); 
+
+module.exports = mongoose.model("Celebrity", schema);
---
+++ routes/celebrities.routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const celebritiesController = require('../controllers/celebrities.controller');
+
+router.get('/', celebritiesController.list)
+router.get('/create', celebritiesController.create)
+
+router.post('/create', celebritiesController.doCreate)
+
+router.get('/:id', celebritiesController.get)
+router.get('/:id/edit', celebritiesController.edit)
+router.post('/:id/edit', celebritiesController.doEdit)
+router.post('/:id/delete', celebritiesController.delete)
+
+module.exports = router;
