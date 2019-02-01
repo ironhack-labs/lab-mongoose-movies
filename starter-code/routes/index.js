78ba54a222294742ca623e4f11433e2879a871ef
Pedro Arantes <pedro.hga90@gmail.com>
2019-02-01 16:21:48 +0100

    done to 6th iteration

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const router  = express.Router();
-
+const Celebrity = require('../models/Celebrity')
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+ res.render('index')
 });
+
+router.get('/celebrities',(req,res,next)=>{
+  Celebrity.find()
+  .then(celebrity =>{
+    res.render('celebrities/index', {celebrity});
+  })
+})
+
+router.get('/celebrities/new', (req,res,next)=>{
+  res.render('celebrities/new')
+})
+
+router.post('/celebrities/new', (req,res,next)=>{
+  const {name,occupation,catchPhrase} = req.body;
+  const newCelebrity = new Celebrity({name,occupation,catchPhrase})
+  newCelebrity.save()
+    .then((celebrity)=>{
+      res.redirect('/celebrities/')
+    })
+    .catch(err=>console.log(err))
+})
+
+router.get('/celebrities/:id',(req,res,next)=>{
+  Celebrity.findById(req.params.id)
+    .then(celebrityInfo => {
+      res.render('celebrities/show', {celebrityInfo})
+    })
+})
 
 module.exports = router;
