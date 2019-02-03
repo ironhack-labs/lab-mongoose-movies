fed30bbdb217d23bf4d0eab48dd64be921b85eff
Didac Bigorda <didacbigorda@gmail.com>
2019-02-03 17:17:52 +0100

    Add movie model and seeder

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,27 +1,51 @@
 const mongoose = require('mongoose');
 const Celebrity = require('../models/celebrity.js');
+const Movie = require('../models/movie.js');
 
 const dbName = 'lab-mongoose-movies';
 mongoose.connect(`mongodb://localhost/${dbName}`);
 
-const celebArray = [
+//const celebArray = [
+//  {
+//    name: 'Tom Cruise',
+//    occupation: 'actor',
+//    catchPhrase: 'Danger Zone',
+//  },
+//  {
+//    name: 'Gillian Flynn',
+//    occupation: 'author',
+//    catchPhrase: 'Does anyone do anything profusely except apologize? Sweat, I guess.',
+//  },
+//  {
+//    name: 'Frank Herbert',
+//    occupation: 'author',
+//    catchPhrase: 'The spice must flow',
+//  },
+//];
+//
+//Celebrity.create(celebArray, () => {
+//  console.log('celebrity created');
+//});
+
+
+const movieArray = [
   {
-    name: 'Tom Cruise',
-    occupation: 'actor',
-    catchPhrase: 'Danger Zone',
+    title: 'Star Wars: A Forlorn Saga',
+    genre: 'Action',
+    plot: 'A long time ago in a galaxy far, far away...',
   },
   {
-    name: 'Gillian Flynn',
-    occupation: 'author',
-    catchPhrase: 'Does anyone do anything profusely except apologize? Sweat, I guess.',
+    title: 'Star Wars: Where Did I Park My Falcon?',
+    genre: 'Action',
+    plot: 'A long time ago in a galaxy far, far away...',
   },
   {
-    name: 'Frank Herbert',
-    occupation: 'author',
-    catchPhrase: 'The spice must flow',
+    title: 'Star Wars: Another One',
+    genre: 'Action',
+    plot: 'A long time ago in a galaxy far, far away...',
   },
 ];
 
-Celebrity.create(celebArray, () => {
-  console.log('celebrity created');
+Movie.create(movieArray, () => {
+  console.log('movies created');
 });
