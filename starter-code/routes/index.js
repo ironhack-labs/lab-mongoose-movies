a72ea49996d14872927190ef0af14c4a16c455b6
j0nl1 <jonlitech@gmail.com>
2019-02-03 21:24:40 +0100

    3 Iteration

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -14,11 +14,14 @@ router.get('/celebrities/index', (req, res, next) => {
     .catch(err => console.log('An error ocurred: ', err));
 });
 
+router.get('/celebrities/:id', (req, res, next) => {
+  celebritiesSchema
+    .findById(req.params.id, { _id: 0 })
+    .then(celebrity => res.render('celebrities/show', { celebrity }))
+    .catch(err => console.log('An error ocurred: ', err));
+});
+
 module.exports = router;
 
 /*
-In the views / celebrities / index.hbs view file:
-Add an < h2 > tag for the page's heading.
-Use a forEach loop to display tags with each celebrity's name.
-In the views / index.hbs(homepage) file:
-Add a link that goes to the / celebrities route. */
+ */
