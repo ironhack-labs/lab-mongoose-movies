3fbc06052ccd1ace43439e88732bcaecaddf1694
Sofía <sofianombela@gmail.com>
2019-02-03 15:28:11 +0100

    Iteration 2:Get routes

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router  = express.Router();
 
+const Celebrity = require('../models/celebrity.js')
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/celebrities', (req, res, next) => {
+  Celebrity.find()
+    .then(allCelebritiesFromDB => {
+      res.render('celebrities/index', {celebrities: allCelebritiesFromDB})
+    })
+    .catch(error => {
+      console.log('Error', error)
+    })
+});
+
+
+
 
 module.exports = router;
