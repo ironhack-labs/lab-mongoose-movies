790c95ed146f9023e94d5c4df5d2b3d797401b54
Sofía <sofianombela@gmail.com>
2019-02-03 13:56:14 +0100

    Iteration done: Create model and seeds

---
+++ starter-code/bin/seeds.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const Celebrity = require('../models/celebrity')
+
+const dbName = 'celebrity-project';
+mongoose.connect(`mongodb://localhost/${dbName}`)
+
+const celebrities = [
+    {
+        name: "Beyonce",
+        occupation: "Singer",
+        catchPhrase: "Power means happiness; power means hard work and sacrifice"
+    },
+    {
+        name: "Lady Gaga",
+        occupation: "Singer",
+        catchPhrase: "Don't you ever let a soul in the world tell you that you can't be exactly who you are."
+    },
+    {
+        name: "Edward Norton",
+        occupation: "Actor",
+        catchPhrase: "I don't smoke and I don't want to smoke."
+    }
+]
+
+Celebrity.create(celebrities, (err) => {
+    if (err) {throw(err)}
+    console.log(`Create ${celebrities.length} celebrities`)
+    mongoose.connection.close();
+});
