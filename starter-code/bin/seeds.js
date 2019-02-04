3c72352aaf8dd2df9c0c38878a7e182347f45c52
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2019-02-04 22:00:18 +0100

    done Iteration #7

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,31 +1,66 @@
 const mongoose = require('mongoose');
 const Celebrity = require('../models/celebrity');
+const Movie = require('../models/movie');
 
-const celebrities = [
+// const celebrities = [
+//   {
+//     name: 'Tom Cruise',
+//     occupation: 'Actor',
+//     catchPhrase: 'Top gun',
+//   },
+//   {
+//     name: 'Beyonce',
+//     occupation: 'Singer',
+//     catchPhrase: 'wow',
+//   },
+//   {
+//     name: 'Daffy Duck',
+//     occupation: 'Comedian',
+//     catchPhrase: 'hahaha',
+//   },
+// ];
+
+// mongoose.connect('mongodb://localhost:27017/mongoose-movies', { useNewUrlParser: true })
+//   .then(() => {
+//     console.log('connected to db');
+//     return Celebrity.create(celebrities);
+//   })
+//   .then((data) => {
+//     console.log('Created initial celebrity', data);
+//   })
+//   .then(() => {
+//     mongoose.connection.close();
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//     mongoose.connection.close();
+//   });
+
+const movies = [
   {
-    name: 'Tom Cruise',
-    occupation: 'Actor',
-    catchPhrase: 'Top gun',
+    title: 'aaa',
+    genre: 'aaa',
+    plot: 'aaa',
   },
   {
-    name: 'Beyonce',
-    occupation: 'Singer',
-    catchPhrase: 'wow',
+    title: 'bbb',
+    genre: 'bbb',
+    plot: 'bbb',
   },
   {
-    name: 'Daffy Duck',
-    occupation: 'Comedian',
-    catchPhrase: 'hahaha',
+    title: 'ccc',
+    genre: 'ccc',
+    plot: 'ccc',
   },
 ];
 
 mongoose.connect('mongodb://localhost:27017/mongoose-movies', { useNewUrlParser: true })
   .then(() => {
     console.log('connected to db');
-    return Celebrity.create(celebrities);
+    return Movie.create(movies);
   })
   .then((data) => {
-    console.log('Created initial celebrity', data);
+    console.log('Created initial movie', data);
   })
   .then(() => {
     mongoose.connection.close();
