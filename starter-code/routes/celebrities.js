4eb8da765747b52f21cf190f38363b35babadad8
Gustavo Galvez <gustavo.galvez09@gmail.com>
2019-02-04 00:35:46 -0500

    Done with Iteration 4

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 const Celebrity = require('../models/Celebrity');
 
+
+//This route display the names of all celebrities
 router.get('/celebrities', (req,res,next) =>{
    Celebrity.find()
     .then (allCelebrities =>{
@@ -13,6 +15,7 @@ router.get('/celebrities', (req,res,next) =>{
     .catch (err => console.log("error while getting all celebrities from DB", err))
 })
 
+//This route display the details of each celebrity based on the elebrity ID
 router.get('/celebrities/:celebrityId', (req,res,next) =>{
   const celebrityId = req.params.celebrityId
 
@@ -23,5 +26,26 @@ router.get('/celebrities/:celebrityId', (req,res,next) =>{
    })
    .catch (err => console.log("error while getting all celebrities from DB", err))
 })
+
+//Route to display form to add new celebrity
+router.get('/newCelebrity', (req, res, next) =>{
+    res.render('celebrities/new')
+})
+
+router.post('/newCelebrity', (req,res,next) => {
+    const {name, occupation, catchPhrase} = req.body;
+    const newCeleb = new Celebrity({name, occupation, catchPhrase});
+
+    newCeleb.save()
+    .then ((celeb) =>{
+        console.log("New Celebrity created: ", celeb);
+        res.redirect('/celebrities');
+    })
+    .catch (err => console.log("Error while creating a new celebrity"))
+})
+
+
+
+
 
 module.exports = router;
