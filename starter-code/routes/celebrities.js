7e45a2dbf8e1a251f2e58052d06ec450b5376caa
Gustavo Galvez <gustavo.galvez09@gmail.com>
2019-02-03 19:46:52 -0500

    Done with iteration 3

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -4,13 +4,24 @@ const router = express.Router();
 
 const Celebrity = require('../models/Celebrity');
 
-router.get('/routes/celebrities', (req,res,next) =>{
+router.get('/celebrities', (req,res,next) =>{
    Celebrity.find()
     .then (allCelebrities =>{
       res.render('celebrities/index', {celebritiesFromDB: allCelebrities})
 
     })
     .catch (err => console.log("error while getting all celebrities from DB", err))
+})
+
+router.get('/celebrities/:celebrityId', (req,res,next) =>{
+  const celebrityId = req.params.celebrityId
+
+  Celebrity.findById(celebrityId).populate('celebrity')
+   .then (theCelebrity =>{
+     res.render('celebrities/show', {celebrity: theCelebrity})
+
+   })
+   .catch (err => console.log("error while getting all celebrities from DB", err))
 })
 
 module.exports = router;
