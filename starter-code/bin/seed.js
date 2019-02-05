84ef3b7ff3483132418f2c921417b136ebf4505b
Madhur Suria Mata Garcia <madhurmata@Madhurs-MacBook-Pro.local>
2019-02-05 23:21:27 +0100

    celebrities done

--- starter-code/bin/seed.js
+++ starter-code/bin/seed.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 const Celebrity = require('../models/celebrity');
 
-const products = [
+const celebrities = [
   { name: 'Yoshi', ocupation: 'Student', catchPhrase: 'whatever' },
   { name: 'Not-pedro', ocupation: 'Layer', catchPhrase: 'caboom' },
   { name: 'Isma', ocupation: 'Robar carteras', catchPhrase: 'ijbcjdkcn' },
 ];
 
-mongoose.connect('mongodb://localhost:27017/iron-amazon', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost:27017/starter-code', { useNewUrlParser: true })
   .then(() => {
     console.log('connected to db');
-    return Celebrity.create(products);
+    return Celebrity.create(celebrities);
   }).then((data) => {
     console.log('created data', data);
   }).then(() => {
--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -6,14 +6,72 @@ const router = express.Router();
 /* GET products listing. */
 
 router.get('/', (req, res, next) => {
-
   Celebrity.find({})
     .then((celebrities) => {
-      res.render('celebrities/celebrities', { celebrities });
+      res.render('celebrities/celebrities', { celebrities: celebrities });
     })
     .catch((error) => {
       next(error);
     });
 });
+
+router.get('/new', (req, res, next) => {
+  res.render('celebrities/new');
+});
+
+router.post('/', (req, res, next) => {
+  const { name, ocupation, catchPhrase } = req.body;
+  Celebrity.create({ name, ocupation, catchPhrase })
+    .then(() => {
+      res.redirect('/celebrities');
+    })
+    .catch(next);
+});
+
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+  Celebrity.findById(id)
+    .then((celebrity) => {
+      res.render('celebrities/show', { celebrity });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.post('/:id/delete', (req, res, next) => {
+  const { id } = req.params;
+  Celebrity.findByIdAndDelete(id)
+    .then((celebrity) => {
+      res.redirect('/celebrities');
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.get('/:id/edit', (req, res, next) => {
+  const { id } = req.params;
+  Celebrity.findById(id)
+    .then((celebrity) => {
+      res.render('celebrities/edit', { celebrity });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.post('/:id/update', (req, res, next) => {
+  const { id } = req.params;
+  const { name, ocupation, catchPhrase } = req.body;
+  Celebrity.findByIdAndUpdate(id, { name, ocupation, catchPhrase })
+    .then((celebrity) => {
+      res.redirect('/celebrities');
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
 
 module.exports = router;
