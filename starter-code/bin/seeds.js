8fb2f73d8591f7f7d42d7efacce9d6aea049a73c
Bastien Tran <bastien.tran@gmail.com>
2019-02-06 16:31:04 +0100

    seeds.js is populated.not 100% sure about the syntax

---
+++ starter-code/bin/seeds.js
@@ -0,0 +1,17 @@
+[
+  {
+    name: "Bugs Bunny",
+    occupation: "Eating carrots",
+    catchPhrase: "What's up doc?"
+  },
+  {
+    name: "Baloo",
+    occupation: "Chillin'",
+    catchPhrase: "Look for the 'bear' necessities"
+  },
+  {
+    name: "Chad Jones",
+    occupation: "Pro Golfer",
+    catchPhrase: "Look what I putt up with."
+  }
+];
