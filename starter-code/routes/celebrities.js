e99118be251bd50916ec5e4d32f09789f987994b
Raja Prahara <raja.ardri@gmail.com>
2019-02-06 18:08:23 +0100

    iteration 3

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -5,6 +5,8 @@ const Celebrity = require("../models/celebrity-model.js");
 const router = express.Router();
 
 /* CRUD */
+
+/* Celebrities index */
 router.get("/celebrities", (req, res, next) => {
   // whenever a user visits "/books" finds all the books sorted by rating
   Celebrity.find()
@@ -14,6 +16,21 @@ router.get("/celebrities", (req, res, next) => {
 
       res.locals.celebrityArray = queryResult;
       res.render("celebrities/index.hbs");
+    })
+    // next(err) skips to the error handler in "bin/www" (error.hbs)
+    .catch(err => next(err));
+});
+
+/* Celebrity detail */
+router.get("/celebrities/:celebrityId", (req, res, next) => {
+  const { celebrityId } = req.params;
+
+  // find the book in the database using the ID from the address
+  Celebrity.findById(celebrityId)
+    .then(queryResult => {
+      // send the database query result to the HBS file as "bookItem"
+      res.locals.celebrityItem = queryResult;
+      res.render("celebrities/show.hbs");
     })
     // next(err) skips to the error handler in "bin/www" (error.hbs)
     .catch(err => next(err));
