cdaf73b4cb72ec57124e27d5954a2b796942d4ad
Christina Bailey <christinaibailey@live.com>
2019-02-06 18:09:37 +0100

    finished iteration four

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -16,6 +16,10 @@ router.get("/celebrities", (req, res, next) => {
     .catch(err => next(err));
 });
 
+router.get("/celebrities/new", (req, res, next) => {
+  res.render("celebrities/new.hbs");
+});
+
 router.get("/celebrities/:celebrityId", (req, res, next) => {
   //get the info printed:
   // res.send(req.params);
@@ -31,8 +35,18 @@ router.get("/celebrities/:celebrityId", (req, res, next) => {
     .catch(err => next(err));
 });
 
-module.exports = router;
-
-router.get("/celebrities/new", (req, res, next) => {
-  res.render("celebrities/new.hbs");
+router.post("/celebrities", (req, res, next) => {
+  // make an object with the information that the user gave
+  const { name, occupation, catchPhrase } = req.body;
+  //create saves the input information the user gave us and creates anew object in the DB)
+  //ALWAYS redirect only if its successful to avoid DUPLICATE DATA on refresh
+  // redirect only to ADDRESSES - not HBS files
+  Celebrity.create({ name, occupation, catchPhrase })
+    .then(celebrityDoc => {
+      ///the following line defines where you will go afterwords
+      res.redirect(`/celebrities/${celebrityDoc._id}`);
+    })
+    .catch(err => next(err));
 });
+
+module.exports = router;
