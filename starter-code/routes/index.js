4761af64d3a9ef10e9afcc8749f3272f25f779c1
Christina Bailey <christinaibailey@live.com>
2019-02-06 17:37:27 +0100

    iteration 3 done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -16,6 +16,21 @@ router.get("/celebrities", (req, res, next) => {
     .catch(err => next(err));
 });
 
+router.get("/celebrities/:celebrityId", (req, res, next) => {
+  //get the info printed:
+  // res.send(req.params);
+  const { celebrityId } = req.params;
+  //find the book in the database using the ID from the address below
+  Celebrity.findById(celebrityId)
+
+    .then(celebrityDoc => {
+      res.locals.celebrityItem = celebrityDoc;
+      res.render("celebrities/show.hbs");
+    })
+    //next(err) skips to the error handler in "bin/www" (error.hbs)
+    .catch(err => next(err));
+});
+
 module.exports = router;
 
 // router.get("/celebrities/:id", (req, res, next) => {
