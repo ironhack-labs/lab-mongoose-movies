9536d3c4dac704a989b1b4c4fef0cf1cf2c8dcda
Bastien Tran <bastien.tran@gmail.com>
2019-02-06 17:10:16 +0100

    changed seeds.js, need to run it

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,17 +1,33 @@
-[
-  {
-    name: "Bugs Bunny",
-    occupation: "Eating carrots",
-    catchPhrase: "What's up doc?"
-  },
-  {
-    name: "Baloo",
-    occupation: "Chillin'",
-    catchPhrase: "Look for the 'bear' necessities"
-  },
-  {
-    name: "Chad Jones",
-    occupation: "Pro Golfer",
-    catchPhrase: "Look what I putt up with."
-  }
-];
+// Seed File (run this to insert more Celebs into the database)
+// -----------------------------------
+// connects seed.js to the .env file
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+// Get the Celebrity model to do our databse query
+const Celebrity = require("../models/celebrity-model.js");
+
+const celebrityData = require("./celebrities.json");
+
+// !!! CONNECT TO THE SAME DATABASE AS app.js !!!
+// connects to MongoDB server with the DB name equal to the project name
+// (also has console.logs for successful and failed connections)
+mongoose
+  .connect("mongodb://localhost/mongoose-movies", { useNewUrlParser: true })
+  .then(x => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
+  });
+
+Book.create(celebrityData)
+  .then(celebrityResults => {
+    console.log(`Inserted ${celebrityResults.length} CELEBRITIES!`);
+  })
+  .catch(err => {
+    console.log("Insert FAILURE!!", err);
+  });
