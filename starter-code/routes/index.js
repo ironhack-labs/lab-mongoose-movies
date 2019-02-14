7a7fdf0454b39c91b63c23572deb4e6b251d3055
cameron-chromiak <cam.chromiak@hotmail.com>
2019-02-14 16:14:45 -0500

    stuff goes onto page

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -8,4 +8,12 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get('/celebs', (req, res)=>{
+  Celeberty.find({}).then(myCelebs => {
+    // console.log(myCelebs);
+      res.render('celeberties', {celebs:myCelebs})
+  }).catch(err => {console.log(err)})
+
+})
+
 module.exports = router;
