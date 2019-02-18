38e6f54cde9108d7ab6da96e0fa1bf88d9402e96
Ric Taglietti <rictaglietti@gmail.com>
2019-02-17 22:32:58 -0500

    WIP

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router  = express.Router();
+const Celebrity = require('../models/celebrity')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/celebrities', (req, res, next) => {
+  Celebrity.find().then(allCelebrities => {
+    console.log(allCelebrities)
+    res.render('celebrities.hbs', {celebrities: allCelebrities});
+  })
+})
+
+router.get('/celebrity/:id', (req, res, next) => {
+  console.log(req.params)
+  Celebrity.findOne({ _id: req.params.id }).then(oneCelebrity => {
+    console.log(oneCelebrity)
+    res.render('celebrity-details.hbs', { celebrityDetails: oneCelebrity });
+  })
+
+})
 
 module.exports = router;
