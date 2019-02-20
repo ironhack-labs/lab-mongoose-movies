07f14ce3d9febc9ad63fb2c16ba0c478913ec21f
Manuel Garaboa <mannygaraboa@gmail.com>
2019-02-20 18:03:54 -0500

    Iteration #3

--- starter-code/app.js
+++ starter-code/app.js
@@ -11,7 +11,7 @@ const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/celebrities-db', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -45,14 +45,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
-
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
-
-
-
 const index = require('./routes/index');
 app.use('/', index);
-
 
 module.exports = app;
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router  = express.Router();
-
+const Celebrity = require('../models/celebrity.js');
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/celebs', (req, res, next) => {
+  
+  Celebrity.find().then(celebsFromDb=>{
+    console.log(celebsFromDb)
+    res.render('celebs.hbs', {celebsFromDb})
+  })
+});
+
+router.get('/celebrity-detail/:id', (req, res, next) => {
+  Celebrity.findOne({_id: req.params.id}).then(celeb => {
+    console.log(celeb)
+    res.render('details.hbs', { celeb: celeb })
+  })
+  .catch(err => {
+    console.log(err)
+  })
 });
 
 module.exports = router;
