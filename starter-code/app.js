3d1845e2bb2a263ac36cb99f01dc123091109af8
Jelte Homminga <jeltehomminga@gmail.com>
2019-02-21 17:29:37 +0100

    read and delete movies

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,58 +1,74 @@
-require('dotenv').config();
-
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
 const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
 const mongoose     = require('mongoose');
-const logger       = require('morgan');
 const path         = require('path');
+const Schema       = mongoose.Schema;
+const app          = express();
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/Movies', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
   .catch(err => {
     console.error('Error connecting to mongo', err)
-  });
-
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
-
-const app = express();
-
-// Middleware Setup
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-// Express View engine setup
-
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
+  });     
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
+
+const MyMovieModel = mongoose.model('movies',
+    new Schema({
+    title: String,
+    rating: Number
+  })
+);
+
+MyMovieModel.create({
+  title: 'Nog een film',
+  rating: 3
+}, (err) => {console.log(err)})
+
+app.get('/', (req, res) =>{ 
+MyMovieModel.find({},  (err, movies) => {
+    if(err) res.send(err)
+    else {
+      console.log(movies)
+      res.render('index', {movies})
+    } 
+  })})
+
+  app.get('/search', (req, res) => {
+    const userInput = req.query;
+
+    var newMovie = new MyMovieModel({
+      title: userInput.title
+    })
+
+    newMovie.save((err) => {
+      console.log(err);
+    })
+
+    res.send('movie saved');
+
+  })
+
+  app.get('/delete/:id', (req,res) => {
+    // const userInput = req.query;
+
+    MyMovieModel.findByIdAndDelete(req.params.id, (err) => {
+      if(err) {
+        console.log(err)
+      } else {
+        res.send('Deleted')
+      }
+    })
+  })
+
+app.listen(3000, ()=> console.log("ja? movies graag!"))
 
 
 
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
 
-
-
-const index = require('./routes/index');
-app.use('/', index);
-
-
-module.exports = app;
