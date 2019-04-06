83b83b614c010a86e883cdc61315e13385174a79
Eva Francés <evafrances@MacBook-Pro-de-Eva.local>
2019-04-06 09:26:34 +0200

    in process

---
+++ starter-code/bin/seeds.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Celebrity = require('../models/celebrity.js');
+
+const dbName = 'lab-mongoose-movies';
+mongoose.connect(`mongodb://localhost/${dbName}`);
+
+const celebArray = [
+  {
+    name: 'Tom Cruise',
+    occupation: 'actor',
+    catchPhrase: 'Danger Zone',
+  },
+  {
+    name: 'Gillian Flynn',
+    occupation: 'author',
+    catchPhrase: 'Does anyone do anything profusely except apologize? Sweat, I guess.',
+  },
+  {
+    name: 'Frank Herbert',
+    occupation: 'author',
+    catchPhrase: 'The spice must flow',
+  },
+];
+
+Celebrity.create(celebArray, () => {
+  console.log('celebrity created');
+})
