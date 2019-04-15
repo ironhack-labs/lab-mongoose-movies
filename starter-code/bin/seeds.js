11ca26ee3921357ae1ece34afbb4c802d45846a4
Fernando Lopez <fnandolopezsanchez@gmail.com>
2019-04-15 07:25:31 +0200

    Hecho sin maquetar

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,71 +1,31 @@
-// const mongoose = require('mongoose');
-// const Celebrity = require('../models/Celebrity');
-
-
-// mongoose
-//   .connect("mongodb://localhost/starter-code", {useNewUrlParser: true })
-//   .then(x => {
-//     //limpiar la coleccion
-//     Celebrity.collection.drop();
-//     Celebrity.create([
-//       {
-//         name: "Mickey Mouse",
-//         occupation: "Ava DuVernay",
-//         catchPhrase: ""
-//       },
-//       {
-//         name: "Donald Duck",
-//         occupation: "Ava DuVernay",
-//         catchPhrase: ""
-//       },
-//       {
-//         name: "Harry Potter",
-//         occupation: "Ava DuVernay",
-//         catchPhrase: ""
-//       }
-//     ])
-//     .then(celebrityInserted =>{
-//       console.log(celebrityInserted);
-//       mongoose.disconnect();
-//     })
-//     .catch(err =>{
-//       console.log(err);
-//     })
-//   console.log(`Connect to Mongo! Database name: "${x.connections[0].name}"`)
-// })
-// .catch(err => {
-//   console.error('Err connecting to mongo', err)
-// });
-
-
 const mongoose = require('mongoose');
-const Movie = require('../models/Movie');
+const Celebrity = require('../models/Celebrity');
 
 
 mongoose
   .connect("mongodb://localhost/starter-code", {useNewUrlParser: true })
   .then(x => {
     //limpiar la coleccion
-    Movie.collection.drop();
-    Movie.create([
+    Celebrity.collection.drop();
+    Celebrity.create([
       {
-        title: "The imitation game",
-        genre: "historical non-ficcion",
-        plot: "Breaking enigma"
+        name: "Mickey Mouse",
+        occupation: "Ava DuVernay",
+        catchPhrase: ""
       },
       {
-        title: "Harry Potter",
-        genre: "Fantasy",
-        plot: "boy with scar"
+        name: "Donald Duck",
+        occupation: "Ava DuVernay",
+        catchPhrase: ""
       },
       {
-        title: "Jungle Book",
-        genre: "Fantasy animation",
-        plot: "Boy without clothes"
+        name: "Harry Potter",
+        occupation: "Ava DuVernay",
+        catchPhrase: ""
       }
     ])
-    .then(movieInserted =>{
-      console.log(movieInserted);
+    .then(celebrityInserted =>{
+      console.log(celebrityInserted);
       mongoose.disconnect();
     })
     .catch(err =>{
@@ -76,3 +36,43 @@ mongoose
 .catch(err => {
   console.error('Err connecting to mongo', err)
 });
+
+
+// const mongoose = require('mongoose');
+// const Movie = require('../models/Movie');
+
+
+// mongoose
+//   .connect("mongodb://localhost/starter-code", {useNewUrlParser: true })
+//   .then(x => {
+//     //limpiar la coleccion
+//     Movie.collection.drop();
+//     Movie.create([
+//       {
+//         title: "The imitation game",
+//         genre: "historical non-ficcion",
+//         plot: "Breaking enigma"
+//       },
+//       {
+//         title: "Harry Potter",
+//         genre: "Fantasy",
+//         plot: "boy with scar"
+//       },
+//       {
+//         title: "Jungle Book",
+//         genre: "Fantasy animation",
+//         plot: "Boy without clothes"
+//       }
+//     ])
+//     .then(movieInserted =>{
+//       console.log(movieInserted);
+//       mongoose.disconnect();
+//     })
+//     .catch(err =>{
+//       console.log(err);
+//     })
+//   console.log(`Connect to Mongo! Database name: "${x.connections[0].name}"`)
+// })
+// .catch(err => {
+//   console.error('Err connecting to mongo', err)
+// });
