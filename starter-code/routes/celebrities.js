59c507ec199cb13cb66ef44d78c3fa7fa260fd97
Dnox <david@nestrategia.com>
2019-04-19 12:31:18 +0200

    iteration 6 done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -5,13 +5,38 @@ const router = express.Router();
 const Celebrity = require('../models/celebrity');
 // const celebs = require('../data/celebs.json');
 
+
+
 router.post('/celebrities/:id/delete', (req, res, next) => {
   const id = req.params.id
-  console.log(id)
-  Celebrity.findByIdAndRemove(id)
+  // console.log(id)
+  Celebrity.findByIdAndDelete(id)
   .then(() => {res.redirect('/celebrities')}
   )
   .catch(err => next(err));
+});
+
+router.get('/celebrities/new', (req, res, next) => {
+  res.render('celebrities/new')
+});
+
+router.get('/celebrities/:id/edit', (req, res, next) => {
+  const id = req.params.id
+  Celebrity.findByIdAndUpdate(id)
+    .then(celeb => {
+      //res.send(celeb)
+      res.render('celebrities/edit', {celeb})
+    })
+    .catch(err => next(err))
+})
+
+router.get('/celebrities/:id', (req, res, next) => {
+  const id = req.params.id;
+  Celebrity.findById(id)
+    .then(celeb => {
+      res.render('celebrities/show', {celeb})
+    })
+    .catch(err => next(err));
 });
 
 router.get('/celebrities', (req, res, next) => {
@@ -23,6 +48,19 @@ router.get('/celebrities', (req, res, next) => {
     .catch(err => next(err));
 });
 
+router.post('/celebrities/:id', (req, res, next) => {
+  
+  //res.send(req.params)
+  // const id = req.params.id
+  const { name, ocupation, catchPhrase } = req.body;
+  Celebrity.findByIdAndUpdate({ _id: req.params.id }, { $set: { name, ocupation, catchPhrase }}, {new: true})
+    .then(() => {
+      res.redirect('/celebrities')
+
+    })
+    .catch(err => next(err))
+})
+
 router.post('/celebrities', (req, res, next) => {
   const { name, ocupation, catchPhrase } = req.body;
   const newCelebrity = new Celebrity({ name, ocupation, catchPhrase})
@@ -33,23 +71,10 @@ router.post('/celebrities', (req, res, next) => {
     })
     .catch( 
       res.render('celebrities/new'),
+      err => next(err)
       );
 });
 
-router.get('/celebrities/new', (req, res, next) => {
-  res.render('celebrities/new')
-});
-
-
-
-router.get('/celebrities/:id', (req, res, next) => {
-  const id = req.params.id;
-  Celebrity.findById(id)
-    .then(celeb => {
-      res.render('celebrities/show', {celeb})
-    })
-    .catch(err => next(err));
-});
 
 
 
