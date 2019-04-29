0f70de1d6b2273cdc43e369b1e2bb81c6e02a088
Chris Cardelle <chris.cardelle@gmail.com>
2019-04-29 16:39:33 -0400

    Celebrities finished

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -11,13 +11,53 @@ router.get('/', (req, res, next) => {
 router.get('/celebrities', (req, res, next) => {
   Celebrity.find().then (data => {
     
-    console.log(`This is the gathered data:`);
-    console.log(data);
+    // console.log(`This is the gathered data:`);
+    // console.log(data);
     
     
     res.render('../views/celebrities/index', {data : data});
   })
   
+  
+});
+
+router.get('/celebrities/delete/:id', (req, res, next) => {
+  Celebrity.findByIdAndRemove(req.params.id).then (data => {
+    res.redirect('/celebrities');
+  })
+  .catch(error => {
+    res.render ('../views/celebrities');
+    console.log('Error while retrieving celeb details: ', error);
+  })
+  
+});
+
+router.get('/celebrities/new', (req, res, next) => {
+  res.render('../views/celebrities/new')
+  });
+
+router.post('/celebrities', (req, res, next) => {
+  const { name, occupation, catchPhrase} = req.body;
+  const newCeleb = new Celebrity({  name, occupation, catchPhrase})
+  newCeleb.save()
+  .then((data) => {
+    res.redirect('/celebrities');
+  })
+  .catch((error) => {
+    console.log(error);
+    res.redirect('/celebrities/new')
+  })
+});
+
+router.get('/celebrities/:id', (req, res, next) => {
+  Celebrity.findById(req.params.id).then (data => {
+    console.log(`This is the selected celeb: ${data}`)
+    res.render('../views/celebrities/show', data);
+  })
+  .catch(error => {
+    res.render ('../views/celebrities/show');
+    console.log('Error while retrieving celeb details: ', error);
+  })
   
 });
 
