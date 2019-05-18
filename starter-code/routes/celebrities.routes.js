9453487256f6ced691c8933e31baf73c96aad185
Tino Gutiérrez <tgf1987@gmail.com>
2019-05-18 14:50:43 +0200

    Iteration 3. The Celebrity Details Page

--- starter-code/routes/celebrities.routes.js
+++ starter-code/routes/celebrities.routes.js
@@ -9,15 +9,24 @@ const celebrity = require('../models/celebrity')
 // Listado de celebrities
 router.get('/', (req, res, next) => {                                                           // ESTO ES EL CONTROLADOR                                                                                                  
   celebrity.find()                                                                              // ESTO ES EL MODELO
-    .then(allCelebrities => {                                                                   // ESTO ES LA VISTA
+    .then(allCelebrities => {                                                                    // ESTO ES LA VISTA
       res.render('celebrities/index', { celebrities: allCelebrities })
     })   
     .catch(error => console.log(error))
 })
 
+router.get("/:id", (req, res) => {
+  console.log("************ El id es:", req.params.id)
+  celebrity.findById(req.params.id)
+    .then(thisCeleb => { 
+      console.log("************ INFO DE CELEBRITY:", thisCeleb)
+      res.render('celebrities/show', { celebrity: thisCeleb })
+    })
+    .catch(error => console.log(error))
+})
 
 
+router.get('/view/:book_id', (req, res) => {
 
-
-
+})
 module.exports = router
