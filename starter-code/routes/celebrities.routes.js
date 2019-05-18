44c70446da3944e801824306b10432cb613500d8
Tino Gutiérrez <tgf1987@gmail.com>
2019-05-18 18:19:19 +0200

    Iteration #4: Adding New Celebrities

--- starter-code/routes/celebrities.routes.js
+++ starter-code/routes/celebrities.routes.js
@@ -15,18 +15,38 @@ router.get('/', (req, res, next) => {                                           
     .catch(error => console.log(error))
 })
 
+//Nueva celebrity ****** Por algún motivo esta linea peta si la pongo después del "Detalle de celebrity" (¿?)
+router.get('/new', (req, res) => res.render('celebrities/new', req))
+router.post('/new', (req, res) => {
+  console.log("********************************** Llega aquí ********************************")
+  const { name, occupation, catchPhrase } = req.body
+  if (!name || !occupation || !catchPhrase){
+    res.render("celebrities/new", {errMsg: "Faltan campos"})
+    return
+  }
+  celebrity.findOne({name})
+  .then(foundCeleb =>{
+      if (foundCeleb) {
+          res.render("celebrities/new", {errMsg: "La celebrity ya existe"})
+          return
+      }
+      const newCelebrity = new celebrity({ name, occupation, catchPhrase })
+      newCelebrity.save()
+        .then(theCelebrity => res.redirect('/celebrities'))
+        .catch(error => console.log(error))
+  })
+  .catch(err => console.log("**** Error en celebrity.findOne", err))
+})
+
+//Detalle de celebrity
 router.get("/:id", (req, res) => {
-  console.log("************ El id es:", req.params.id)
+  //console.log("************ El id es:", req.params.id)
   celebrity.findById(req.params.id)
-    .then(thisCeleb => { 
-      console.log("************ INFO DE CELEBRITY:", thisCeleb)
-      res.render('celebrities/show', { celebrity: thisCeleb })
-    })
-    .catch(error => console.log(error))
+  .then(thisCeleb => { 
+    //console.log("************ INFO DE CELEBRITY:", thisCeleb)
+    res.render('celebrities/show', { celebrity: thisCeleb })
+  })
+  .catch(error => console.log(error))
 })
 
-
-router.get('/view/:book_id', (req, res) => {
-
-})
 module.exports = router
