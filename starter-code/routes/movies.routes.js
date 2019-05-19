a77beb7e353528c853e52380d56faa72e45cc72c
Tino Gutiérrez <tgf1987@gmail.com>
2019-05-19 12:46:54 +0200

    Iteration #12 (Bonus): Editing Movies

--- starter-code/routes/movies.routes.js
+++ starter-code/routes/movies.routes.js
@@ -19,59 +19,56 @@ router.get('/', (req, res, next) => {                                           
     .catch(error => console.log(error))
 })
 
-// //Nueva celebrity ****** Por algún motivo esta linea peta si la pongo después del "Detalle de celebrity" (¿?)
-// router.get('/new', (req, res) => res.render('celebrities/new', req))
-// router.post('/new', (req, res) => {
-//   console.log("********************************** Llega aquí ********************************")
-//   const { name, occupation, catchPhrase } = req.body
-//   if (!name || !occupation || !catchPhrase){
-//     res.render("celebrities/new", {errMsg: "Faltan campos"})
-//     return
-//   }
-//   celebrity.findOne({name})
-//   .then(foundCeleb =>{
-//       if (foundCeleb) {
-//           res.render("celebrities/new", {errMsg: "La celebrity ya existe"})
-//           return
-//       }
-//       const newCelebrity = new celebrity({ name, occupation, catchPhrase })
-//       newCelebrity.save()
-//         .then(theCelebrity => res.redirect('/celebrities'))
-//         .catch(error => console.log(error))
-//   })
-//   .catch(err => console.log("**** Error en celebrity.findOne", err))
-// })
+//Nueva película
+router.get('/new', (req, res) => res.render('movies/new', req))
+router.post('/new', (req, res) => {
+  const { title, genre, plot } = req.body
+  if (!title || !genre || !plot){
+    res.render("movies/new", {errMsg: "Faltan campos"})
+    return
+  }
+  movie.findOne({title})
+  .then(foundMovie =>{
+      if (foundMovie) {
+          res.render("movies/new", {errMsg: "La película ya existe"})
+          return
+      }
+      const newMovie = new movie({ title, genre, plot })
+      newMovie.save()
+        .then(theMovie => res.redirect('/movies'))
+        .catch(error => console.log(error))
+  })
+  .catch(err => console.log("**** Error en movie.findOne", err))
+})
 
-// //Borrar celebrity
-// router.get('/:id/delete', (req, res) => {
-//   celebrity.findByIdAndRemove(req.params.id)    
-//     .then(theCeleb => res.redirect('/celebrities'))
-//     .catch(error => console.log(error))
-// })
+//Borrar película
+router.get('/:id/delete', (req, res) => {
+  movie.findByIdAndRemove(req.params.id)    
+    .then(theMovie => res.redirect('/movies'))
+    .catch(error => console.log(error))
+})
 
-// //Editar celebrity
-// router.get('/edit', (req, res) => {
-//   celebrity.findOne({ _id: req.query.celebrity_id })    
-//     .then(celeb => res.render("celebrities/edit", { celeb }))
-//     .catch(error => console.log(error))
-// })
+//Editar película
+router.get('/edit', (req, res) => {
+  movie.findOne({ _id: req.query.movie_id })    
+    .then(theMovie => res.render("movies/edit", { theMovie }))
+    .catch(error => console.log(error))
+})
 
-// router.post('/edit', (req, res) => {
-//   const { name, occupation, catchPhrase } = req.body
-//   celebrity.update({ _id: req.query.celebrity_id }, { $set: { name, occupation, catchPhrase } })
-//     .then(celeb => res.redirect('/celebrities'))
-//     .catch(error => console.log(error))
-// })
+router.post('/edit', (req, res) => {
+  const { title, genre, plot } = req.body
+  movie.update({ _id: req.query.movie_id }, { $set: { title, genre, plot } })
+    .then(theMovie => res.redirect('/movies'))
+    .catch(error => console.log(error))
+})
 
-// //Detalle de celebrity
-// router.get("/:id", (req, res) => {
-//   //console.log("************ El id es:", req.params.id)
-//   celebrity.findById(req.params.id)
-//   .then(thisCeleb => { 
-//     //console.log("************ INFO DE CELEBRITY:", thisCeleb)
-//     res.render('celebrities/show', { celebrity: thisCeleb })
-//   })
-//   .catch(error => console.log(error))
-// })
+//Detalle de la película
+router.get("/:id", (req, res) => {
+  movie.findById(req.params.id)
+  .then(thisMovie => { 
+    res.render('movies/show', { movie: thisMovie })
+  })
+  .catch(error => console.log(error))
+})
 
 module.exports = router
