ea0c317e407c2b9fb4d2cb8c23a8cf3d78fb812b
Virginie Beernaert <vbeernaert@yahoo.fr>
2019-06-23 16:49:01 +0200

    celebrities part done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -13,22 +13,13 @@ router.get("/celebrities", (req, res) => {
     });
 });
 
-router.get("/celebrities/:id", (req, res, next) => {
-  Celebrity.findById(req.params.id)
-    .then(celebrity => {
-      res.render("celebrities/show", { celebrity });
-    })
-    .catch(err => {
-      console.log(err);
-      next(err);
-    });
-});
+/* CREATE a new celebrity with a form */
 
-router.get("/new", (req, res) => {
+router.get("/celebrities/new", (req, res) => {
   res.render("celebrities/new");
 });
 
-router.post("/new", (req, res) => {
+router.post("/celebrities/new", (req, res) => {
   const { name, occupation, catchPhrase } = req.body;
   Celebrity.create({
     name,
@@ -41,7 +32,60 @@ router.post("/new", (req, res) => {
     })
     .catch(err => {
       console.log("create celebrity not ok", err);
-      res.redirect("/new");
+      res.redirect("/celebrities/new");
+    });
+});
+
+/*EDIT a celebrity */
+router.get("/celebrities/:id/edit", (req, res, next) => {
+  Celebrity.findById(req.params.id)
+    .then(celebrity => {
+      res.render("celebrities/edit", { celebrity });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.post("/celebrities/:id", (req, res) => {
+  const { name, occupation, catchPhrase } = req.body;
+  Celebrity.update({
+    name,
+    occupation,
+    catchPhrase
+  })
+    .then(celebrity => {
+      console.log("update celebrity ok", celebrity);
+      res.redirect("/celebrities");
+    })
+    .catch(err => {
+      console.log("update celebrity not ok", err);
+      next(err);
+    });
+});
+
+/* GET a specific celebrity's details */
+router.get("/celebrities/:id", (req, res, next) => {
+  Celebrity.findById(req.params.id)
+    .then(celebrity => {
+      res.render("celebrities/show", { celebrity });
+    })
+    .catch(err => {
+      console.log(err);
+      next(err);
+    });
+});
+
+/*DELETE a celebrity */
+router.post("/celebrities/:id/delete", (req, res, next) => {
+  Celebrity.findByIdAndRemove(req.params.id)
+    .then(celebrity => {
+      console.log("celebrity successfully deleted", celebrity);
+      res.redirect("/celebrities");
+    })
+    .catch(err => {
+      console.log(err);
+      next(err);
     });
 });
 
