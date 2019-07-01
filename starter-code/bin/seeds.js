efb651f8197706834cb0471ad440fb348acf67de
Edgar Herera <contactoedgarherrera@gmail.com>
2019-06-30 19:26:07 -0500

    Iteration 7 | Done.

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -2,30 +2,32 @@ require('dotenv').config();
 
 const mongoose = require('mongoose')
 const Celebrity = require('../models/celebrity')
+const Movie = require('../models/movie')
 
-const celebrity = [
+const movies = [
   {
-    name: 'Michael Jackson',
-    occupation: 'Singer',
-    catchPhrase: `They don't really care about us`
+    title: 'Toy Story 4',
+    genre: 'Family',
+    plot: `Woody (voice of Tom Hanks) has always been confident about his place in the world, and that his priority is taking care of his kid, whether that's Andy or Bonnie.`
   },
   {
-    name: 'Malala',
-    occupation: 'Activist',
-    catchPhrase: `When someone takes away your pens you realize quite how important education is.`
+    title: 'Aladdin',
+    genre: 'Family',
+    plot: `A street rat frees a genie from a lamp, granting all of his wishes and transforming himself into a charming prince in order to marry a beautiful princess. But soon, an evil sorcerer becomes hell-bent on securing the lamp for his own sinister purposes.`
   },
   {
-    name: 'WesBoss',
-    occupation: 'Developer',
-    catchPhrase: `I'm Wes Bos, Ask Me Anything!`
+    title: 'Avengers END GAME',
+    genre: 'Action',
+    plot: `The grave course of events set in motion by Thanos that wiped out half the universe and fractured the Avengers ranks compels the remaining Avengers to take one final stand in Marvel Studios' grand conclusion to twenty-two films, "Avengers: Endgame."`
   }
 ]
 
 mongoose.connect(process.env.DB, { useNewUrlParser: true })
 
-Celebrity.create(celebrity)
-  .then(celebrity => {
-    console.log('IT WORKS', celebrity)
+
+Movie.create(movies)
+  .then(movies => {
+    console.log('IT WORKS (MOVIES)', movies)
     mongoose.connection.close()
   })
   .catch(err => {
---
+++ starter-code/models/movie.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const movieSchema = new Schema({
+  title: String,
+  genre: String,
+  plot: String
+}, 
+{
+  versionKey: false,
+  timestamps: true
+})
+
+module.exports = mongoose.model('Movie', movieSchema)
