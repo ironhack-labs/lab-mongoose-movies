eb429d24b54f68e4324ff6b516b07b2c098c4ad9
Pedro Morissawa <pmorissawa@gmail.com>
2019-07-07 10:32:03 -0300

    done minus extras

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,29 +1,55 @@
 const mongoose = require('mongoose');
 const Celebrity = require('../models/celebrity');
+const Movie = require('../models/movie');
 
 const dbtitle = 'celebrity-lab';
 mongoose.connect(`mongodb://localhost/${dbtitle}`);
 
-const celebrities = [
+// const celebrities = [
+//   {
+//     name: 'Malandro Memo',
+//     occupation: 'Other',
+//     catchPhrase: 'Malandro é malandro, mané é mané.',
+//   },
+//   {
+//     name: 'Zé Carioca',
+//     occupation: 'Sambista',
+//     catchPhrase: 'Piu!',
+//   },
+//   {
+//     name: 'Nazareth Tedesco',
+//     occupation: 'Matemática',
+//     catchPhrase: 'Fica esperto nos degraus, parsa.',
+//   },
+// ];
+
+// Celebrity.create(celebrities, (err) => {
+//   if (err) { throw(err) }
+//   console.log(`Created ${celebrities.length} celebrities`);
+//   mongoose.connection.close();
+// });
+
+
+const movies = [
   {
-    name: 'Malandro Memo',
-    occupation: 'Other',
-    catchPhrase: 'Malandro é malandro, mané é mané.',
+    title: 'Die Hard',
+    genre: 'Action',
+    plot: 'Something happens. A lot of things explode.',
   },
   {
-    name: 'Zé Carioca',
-    occupation: 'Sambista',
-    catchPhrase: 'Piu!',
+    title: 'The Exorcist',
+    genre: 'Horror',
+    plot: 'Girl gets possessed. Exorcist walks in.',
   },
   {
-    name: 'Nazareth Tedesco',
-    occupation: 'Matemática',
-    catchPhrase: 'Fica esperto nos degraus, parsa.',
+    title: 'Fear and Loathing in Las Vegas',
+    genre: 'Drama',
+    plot: 'Gonzo Journalism in a movie.',
   },
 ];
 
-Celebrity.create(celebrities, (err) => {
+Movie.create(movies, (err) => {
   if (err) { throw(err) }
-  console.log(`Created ${celebrities.length} celebrities`);
+  console.log(`Created ${movies.length} movies`);
   mongoose.connection.close();
 });
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const router  = express.Router();
 const Celebrity = require('../models/celebrity');
+const Movie = require('../models/movie');
+
+const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+// Celebrities
 router.get('/celebrities/index', (req, res) => {
   Celebrity.find()
     .then((celebrities) => {
@@ -32,12 +35,66 @@ router.post('/celebrities/new', (req, res) => {
     });
 });
 
+router.post('/celebrities/:id/delete', (req, res) => {
+  const { id } = req.params;
+  Celebrity.findByIdAndRemove(id)
+    .then((celebrity) => {
+      res.redirect('/celebrities/index');
+    })
+    .catch(err => console.log(err));
+});
+
 router.get('/celebrities/:id', (req, res) => {
   const celebrityId = req.params.id;
   Celebrity.findById(celebrityId)
     .then((celebrity) => {
       console.log('Details:', celebrity);
       res.render('celebrities/show', { celebrity });
+    })
+    .catch(err => console.log("ERROR:", err));
+});
+
+// Movies
+router.get('/movies/index', (req, res) => {
+  Movie.find()
+    .then((movies) => {
+      res.render('movies/index', { movies });
+    })
+    .catch(err => console.log('Error:', err));
+});
+
+router.get('/movies/new', (req, res) => {
+  res.render('movies/new');
+});
+
+router.post('/movies/new', (req, res) => {
+  const { title, genre, plot } = req.body;
+  console.log(req.body);
+  const newMovie = new Movie({ title, genre, plot });
+  newMovie.save()
+    .then((movie) => {
+      res.redirect('/movies/index');
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+router.post('/movies/:id/delete', (req, res) => {
+  const { id } = req.params;
+  Movie.findByIdAndRemove(id)
+    .then((movie) => {
+      res.redirect('/movies/index');
+    })
+    .catch(err => console.log(err));
+});
+
+router.get('/movies/:id', (req, res) => {
+  const { id } = req.params;
+  Movie.findById(id)
+    .then((movie) => {
+      console.log('Details:', movie);
+      res.render('movies/show', { movie });
     })
     .catch(err => console.log("ERROR:", err));
 });
