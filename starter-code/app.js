6d3740610cae2cb75e8b5756d222e65d87787fbc
confidenti-al <steve@confidenti.al>
2019-07-08 17:35:54 -0400

    Fixed a few issues with nav, added route to app.js for movies.

--- starter-code/app.js
+++ starter-code/app.js
@@ -57,5 +57,9 @@ app.use('/', index);
 const celebrities = require('./routes/celebrities');
 app.use('/', celebrities);
 
+const movies = require('./routes/movies');
+app.use('/', movies);
+
+
 
 module.exports = app;
