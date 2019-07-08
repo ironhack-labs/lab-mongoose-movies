bd0cd3aaf8445e1a99e76a9e20cb5663ebcc2ae1
mario salvatierra <msalv006@fiu.edu>
2019-07-08 17:59:59 -0400

    done added movies

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -2,92 +2,92 @@ const express = require('express');
 const movieRouter  = express.Router();
 const Movie = require('../models/Movie');
 
-// celebrityRouter.post('/celebrities/:id', (req, res, next) => {
+movieRouter.post('/movies/:id', (req, res, next) => {
 
-//   console.log('POST celebrities/:id  inside router..' );
-//    Celebrity.findByIdAndUpdate(req.params.id, req.body)
-//       .then((thingFromDB)=>{
+  console.log('POST movies/:id  inside router..' );
+   Movie.findByIdAndUpdate(req.params.id, req.body)
+      .then((thingFromDB)=>{
 
-//         // res.redirect('/celebrities/' + req.params.id );
-//         res.redirect('/celebrities');
+        // res.redirect('/celebrities/' + req.params.id );
+        res.redirect('/movies');
 
-//       })
-//       .catch((err)=>{
-//         console.log(err);
-//         next(err);
-//       });
-// });
+      })
+      .catch((err)=>{
+        console.log(err);
+        next(err);
+      });
+});
 
-// celebrityRouter.get('/celebrities/:id/edit', (req, res, next) => {
-//   console.log('you are in edit route..');
-//     Celebrity.findById(req.params.id)
-//       .then((celebrityx)=>{
-//           console.log("object from db from edit is:   " + celebrityx)
-//           res.render('celebrities/edit', {thecelebrityx:celebrityx});
-//       })
-//       .catch((err)=>{
-//         console.log('error ' + err);
-//         next(err);
-//       });
-// });
+movieRouter.get('/movies/:id/edit', (req, res, next) => {
+  console.log('you are in movie edit route..');
+    Movie.findById(req.params.id)
+      .then((moviex)=>{
+          console.log("object from db from edit is:   " + moviex)
+          res.render('movies/edit', {themoviex:moviex});
+      })
+      .catch((err)=>{
+        console.log('error ' + err);
+        next(err);
+      });
+});
 
 
-// celebrityRouter.post('/celebrities/:id/delete', (req, res, next) => {
-//   console.log("you are delete in celebreties/delete  route");
-//   Celebrity.findByIdAndRemove(req.params.id)
-//     .then(()=>{
-//       res.redirect('/celebrities');
-//     })
-//     .catch((err)=>{
-//       console.log(err);
-//       next(err);
-//     });
+movieRouter.post('/movies/:id/delete', (req, res, next) => {
+  console.log("you are delete in movies /delete  route");
+  Movie.findByIdAndRemove(req.params.id)
+    .then(()=>{
+      res.redirect('/movies');
+    })
+    .catch((err)=>{
+      console.log(err);
+      next(err);
+    });
 
     
-// });
+});
 
-// celebrityRouter.get('/celebrities/new', (req, res, next) => {
-//   console.log("you are in celebreties/new GET route");
+movieRouter.get('/movies/new', (req, res, next) => {
+  console.log("you are in celebreties/new GET route");
 
-//     res.render('celebrities/new')
-//     .catch((err)=>{
-//       console.log(err);
-//       next(err);
-//     });
+    res.render('movies/new')
+    .catch((err)=>{
+      console.log(err);
+      next(err);
+    });
   
-// });
+});
 
-// celebrityRouter.post('/celebrities', (req, res, next) => {
-//   console.log("you are POST in celebreties/new  route");
+movieRouter.post('/movies', (req, res, next) => {
+  console.log("you are POST in celebreties/new  route");
 
-//     const newCelebrity = new Celebrity( req.body);
+    const newMOvie = new Movie( req.body);
 
-//     newCelebrity.save()
-//       .then((celebrityx)=>{
-//         res.redirect('celebrities');
-//       })
-//       .catch((err)=>{
-//         console.log(err);
-//         res.redirect('celebrities/new',{
-//           errorMessage: "Error when adding to DB try again!"
-//         });
-//         next(err);
-//       });
-// });
+    newMOvie.save()
+      .then((moviex)=>{
+        res.redirect('movies')
+      })
+      .catch((err)=>{
+        console.log(err);
+        res.redirect('movies/new',{
+          errorMessage: "Error when adding movie to DB try again!"
+        });
+        next(err);
+      });
+});
 
-// celebrityRouter.get('/movies/:id', (req, res, next) => {
+movieRouter.get('/movies/:id', (req, res, next) => {
 
-//   console.log('GET movies/:id  inside router..' );
-//   Movie.findById(req.params.id)
-//   .then((thingFromDB)=>{
+  console.log('GET movies/:id  inside router..' );
+  Movie.findById(req.params.id)
+  .then((thingFromDB)=>{
 
-//     res.render('movies/show',{theCelebrity :thingFromDB});
-//   })
-//   .catch((err)=>{
-//     console.log(err);
-//     next(err);
-//   });
-// });
+    res.render('movies/show',{theMovie :thingFromDB});
+  })
+  .catch((err)=>{
+    console.log(err);
+    next(err);
+  });
+});
 
 
 movieRouter.get('/movies', (req, res, next) => {
