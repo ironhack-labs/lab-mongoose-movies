c140e1d225173fb09ec959c0c5d9b03b372796bf
davidh <iq.davidh@gmail.com>
2019-07-09 19:14:20 -0500

    agregado para crud

--- back/dist/server.js
+++ back/dist/server.js
@@ -1,8 +1,13 @@
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
-const path = require('path');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", function (req, res) {
+    res.json({ "ope": "i´m back :)" });
+});
 const port = 3006;
 app.listen(port, () => console.log(`app on port ${port}!`));
 //# sourceMappingURL=server.js.map
--- back/src/server.ts
+++ back/src/server.ts
@@ -4,9 +4,20 @@ import {Application, Request, Response} from "express";
 
 
 const express = require('express');
+
+const bodyParser   = require("body-parser");
+
 const app: Application = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+
+
+
+app.get("/", function(req, res) {
+    res.json({"ope":"i´m back :)"});
+});
 
 
 const port: number = 3006;
