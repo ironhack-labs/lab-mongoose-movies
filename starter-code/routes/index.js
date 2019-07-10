79175a7f16346a1f9c5be08e6430a81fc2baf91d
arstrel <arstrel@gmail.com>
2019-07-09 20:40:27 -0400

    Switched to passport and flash. Most error messages working

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -3,11 +3,8 @@ const router = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  if (req.session.errorCount <= 0) {
-    req.session.loggedout_msg = null;
-  }
-  req.session.errorCount -= 1;
-  res.render("index", { msg: req.session.loggedout_msg });
+ 
+  res.render("index");
 });
 
 module.exports = router;
--- starter-code/routes/userRoutes.js
+++ starter-code/routes/userRoutes.js
@@ -1,89 +1,77 @@
-const express = require('express');
-const router  = express.Router();
-const User = require('../models/User');
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const flash = require("connect-flash");
+const passport = require("passport");
 
+router.get("/signup", (req, res, next) => {
+  res.render("user-views/signup", { msg: req.session.msg });
+});
 
-router.get('/signup', (req, res, next) => {
-  res.render('user-views/signup')
-})
+router.get("/login", (req, res, next) => {
+  res.render("user-views/login");
+});
 
-router.get('/login', (req, res, next) => {
-  res.render('user-views/login')
-})
+router.get("/user-view/profile", (req, res, next) => {
+  req.flash("success", "Logged in successfully");
+  console.log(req.user);
+  res.render("user-views/profile", { user: req.user });
+});
 
-router.get('/user-view/profile', (req, res, next) => {
-
-  res.render('user-views/profile', {loggedin: req.session.loggedin_msg})
-})
-
-router.post('/user/create', (req, res, next) => {
-  const thePassword = req.body.thePassword;
-  const theUsername = req.body.theUsername;
-
+router.post("/user/create", (req, res, next) => {
+  const thePassword = req.body.password;
+  const theUsername = req.body.username;
+  if (theUsername === "" || thePassword === "") {
+    req.session.msg = "Indicate username and password";
+    res.redirect("/signup");
+  }
+  User.findOne({ username: theUsername }).then(user => {
+    if (user !== null) {
+      req.session.msg = "The username already exists";
+      res.redirect("/signup");
+    }
+  });
   const salt = bcrypt.genSaltSync(10);
   const hashedPassword = bcrypt.hashSync(thePassword, salt);
-
   User.create({
     username: theUsername,
     password: hashedPassword
   })
-  
-  .then(()=> {
-    console.log(`New user created`);
-    req.session.loggedin_msg = "You are logged in now";
-    res.redirect('/user-view/profile')
-  })
-  .catch(err=> {
-    next(err)
-  })
-})
-
-router.post('/user/login', (req, res, next) => {
-  const thePassword = req.body.thePassword;
-  const theUsername = req.body.theUsername;
-
-  User.findOne({"username": theUsername})
-    .then(user => {
-      if(!user) {
-        res.redirect('/login')
-        //error user not found
-        return;
-      }
-      if( bcrypt.compareSync(thePassword, user.password)) {
-        req.session.currentUser = user;
-        req.session.loggedin_msg = "You are logged in now";
-        res.redirect('/user-view/profile')
-      } else {
-        res.redirect('/login')
-        //error wrong password
-      }
+    .then((user) => {
+      req.user = user;
+      console.log(`New user created`);
+      res.redirect("/user-view/profile");
     })
-    .catch(err=> {
-      next(err)
+    .catch(err => {
+      next(err);
     });
-})
+});
 
-router.get('/profile', (req,res, next) => {
+router.post(
+  "/user/login",
+  passport.authenticate("local", {
+    successRedirect: "/user-view/profile",
+    failureRedirect: "/login",
+    failureFlash: true,
+    successFlash: "Logged in successfully",
+    passReqToCallback: true
+  })
+);
 
-  if(req.session.currentUser) {
-    res.render('user-views/profile', {user: req.session.currentUser})
-    
+router.get("/profile", (req, res, next) => {
+  if (req.user) {
+    res.render("user-views/profile", { user: req.user });
   } else {
-    req.session.errorCount = 1;
-    req.session.errorMessage = "Sorry, you have to login first"
-    res.redirect('/')
+    req.flash("error", "Please log in to access your profile");
+    res.redirect("/login");
   }
+});
 
-})
-
-router.post('/logout', (req,res,next) => {
-  req.session.currentUser = null; 
-  req.session.loggedin_msg = null;
-  req.session.loggedout_msg = `You are now on your own`
-  req.session.errorCount = 1;
-  res.redirect('/')
-  
-})
+router.post("/logout", (req, res, next) => {
+  req.logout();
+  req.flash("error", "Logged out successfully");
+  res.redirect("/");
+});
 
 module.exports = router;
