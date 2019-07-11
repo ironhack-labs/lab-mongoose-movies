9df7faf7839849d1699b8d5e67f56862b2ca5f42
confidenti-al <steve@confidenti.al>
2019-07-11 13:08:29 -0400

    Add an API for Celebrities where the page loads and then waits a few seconds before showing the list of celebs, or when a new on is submitted, refresh the list immediately using AJAX

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -1,5 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  console.log('IronGenerator JS imported successfully!');
+  function getAllTheCelebsAndPutThemOnThePage(){
+    let list = document.getElementById('celeb-list');
+    axios.get('http://localhost:3000/celebrities/new-celeb/api')
+    .then((response)=>{
+      let arrayOfStuff = response.data.reverse();
+      list.innerHTML = "";
+      arrayOfStuff.forEach((theCeleb)=>{
+        let newDiv = document.createElement('div');
+        newDiv.innerHTML = `
+        <h4> ${theCeleb.name} </h4>
+        <h6> ${theCeleb.occupation} </h6>
+        <p> ${theCeleb.catchPhrase} </p>
+        `
+        list.appendChild(newDiv);
+      })
+    })
+    .catch((err)=>{
+    })
+  }
+
+  setTimeout(getAllTheCelebsAndPutThemOnThePage, 7000);
+
+  let celebButton = document.getElementById('new-celeb-button');
+
+  celebButton.onclick = ()=>{
+    let name = document.getElementById('name');
+    let occupation = document.getElementById('occupation');
+    let catchPhrase = document.getElementById('catchPhrase');
+    axios.post('http://localhost:3000/celebrities/new-celeb/api', {name: name.value, occupation: occupation.value, catchPhrase: catchPhrase.value})
+    .then((res)=>{
+        getAllTheCelebsAndPutThemOnThePage();
+    })
+    .catch((err)=>{
+    })
+
+    name.value = "";
+    occupation.value = "";
+    catchPhrase.value = ""; 
+
+  }
 
 }, false);
--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -2,7 +2,42 @@ const express = require('express');
 const celebrityRouter  = express.Router();
 const Celebrity = require('../models/Celebrity');
 
+// adding API here
+celebrityRouter.get('/celebrities/new-celeb/api', (req, res, next)=>{
+  Celebrity.find()
+  .then((listOfCelebs)=>{
+    res.json(listOfCelebs)
+  })
+  .catch((err)=>{
+      next(err);
+  })
+})
+  
+celebrityRouter.post('/celebrities/new-celeb/api', (req, res, next)=>{
+  let name = req.body.name;
+  let occupation = req.body.occupation;
+  let catchPhrase = req.body.catchPhrase;
+  console.log(name, occupation, catchPhrase)
 
+  Celebrity.create({
+      name: name,
+      occupation: occupation,
+      catchPhrase: catchPhrase
+  })
+  .then((response)=>{
+    console.log(response);
+      res.json({message: 'Successfully Created Celeb'});
+  })
+  .catch((err)=>{
+    console.log(err);
+      res.json(err);
+  })
+})
+// ending API here
+
+celebrityRouter.get('/celebrities/new-celeb', (req, res, next)=>{
+  res.render('celebrities/new-celeb');
+});
 
 celebrityRouter.get('/celebrities/', (req, res, next) => {
   
@@ -17,12 +52,9 @@ celebrityRouter.get('/celebrities/', (req, res, next) => {
 });
 
 
-
-
 celebrityRouter.get('/celebrities/new', (req, res, next)=>{
   res.render('celebrities/new');
 });
-
 
 celebrityRouter.post('/celebrities', (req, res, next)=>{
   const {theName, theOccupation, theCatchPhrase} = req.body;
@@ -33,19 +65,6 @@ celebrityRouter.post('/celebrities', (req, res, next)=>{
   })
   .catch ((err)=>{
     res.render('celebrities/new')
-    next(err);
-  })
-});
-
-
-celebrityRouter.get('/celebrities/:id', (req, res, next)=>{
-  
-  let theID = req.params.id;
-  Celebrity.findById(theID)
-  .then((oneSingleCelebrity)=>{
-    res.render('celebrities/show', {theCelebrity: oneSingleCelebrity})
-  })
-  .catch((err)=>{
     next(err);
   })
 });
@@ -67,9 +86,21 @@ celebrityRouter.get('/celebrities/:id/edit', (req, res, next)=>{
   Celebrity.findById(req.params.id)
   .then((celebrityFromDb)=>{
     res.render('celebrities/edit', {celebrity: celebrityFromDb})
+    .catch((err)=>{
+      next(err);
+    })
+  })
+});
+
+celebrityRouter.get('/celebrities/:id', (req, res, next)=>{
+  
+  let theID = req.params.id;
+  Celebrity.findById(theID)
+  .then((oneSingleCelebrity)=>{
+    res.render('celebrities/show', {theCelebrity: oneSingleCelebrity})
+  })
   .catch((err)=>{
     next(err);
-  })
   })
 });
 
