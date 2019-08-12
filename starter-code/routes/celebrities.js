142a13d4d34a8097a50adc1ad6be1304db09bddd
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-08-12 16:28:05 -0400

    added a route display the details for a single celebrity

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -6,10 +6,18 @@ router.get("/celebrities", (req, res, next) => {
   Celebrity
     .find()
     .then(celebrities => {
-      console.log(celebrities)
       res.render('celebrities/index', celebrities)
     })
     .catch(err => console.log("There was an error returning the celebrities", err))
+})
+
+router.get("/celebrities/:id", (req, res, next) => {
+  Celebrity
+    .findById(req.params.id)
+    .then(celebrity => {
+      res.render('celebrities/show', {celebrity})
+    })
+    .catch(err => console.log("There's was an error finding the celebrity", err))
 })
 
 module.exports = router;
