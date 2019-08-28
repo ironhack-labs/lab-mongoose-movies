1b533dafa890b76ba9ea8677fc4317b7f6c4232e
joelmzramos <joel.ramos@accenture.com>
2019-08-27 21:54:33 -0300

    Finalizado Movies

--- starter-code/controllers/celebrities-controller.js
+++ starter-code/controllers/celebrities-controller.js
@@ -8,17 +8,79 @@ const index = async (req, res) => {
     throw new Error(error);
   }
 };
+
 const detail = async (req, res) => {
   try {
     const detCelebrity = await Celebrity.findById(req.params.id);
-    console.log(detCelebrity);
     res.render('celebrities/detail', detCelebrity);
   } catch (error) {
     throw new Error(error);
   }
 };
 
+const newCelebrityForm = async (req, res) => {
+  try {
+    res.render('celebrities/new');
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const createCelebrity = (req, res) => {
+  const { name, occupation, catchPhrase } = req.body;
+  const newCelebrity = new Celebrity({ name, occupation, catchPhrase });
+  newCelebrity.save()
+    .then(() => {
+      res.redirect('/celebrities');
+    })
+    .catch((err) => {
+      res.redirect('/celebrities/new');
+    });
+};
+
+const deleteCelebrity = async (req, res) => {
+  const { id } = req.body;
+  Celebrity.findByIdAndRemove(id)
+    .then(() => {
+      res.redirect('/celebrities');
+    })
+    .catch((err) => {
+      res.redirect('/celebrities');
+    });
+};
+
+const formEditCelebrity = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const celebrityEdit = await Celebrity.findById(id);
+    console.log(celebrityEdit);
+    res.render('celebrities/edit', celebrityEdit);
+  } catch (error) {
+    res.redirect(`/celebrities/${id}`);
+  }
+};
+
+const editCelebrity = async (req, res) => {
+  const { id } = req.params;
+  const {
+    name,
+    occupation,
+    catchPhrase,
+  } = req.body;
+  try {
+    await Celebrity.findByIdAndUpdate({ _id: id }, { name, occupation, catchPhrase });
+    res.redirect(`/celebrities/${id}`);
+  } catch (error) {
+    res.redirect(`/celebrities/${id}`);
+  }
+};
+
 module.exports = {
   index,
   detail,
+  newCelebrityForm,
+  createCelebrity,
+  deleteCelebrity,
+  editCelebrity,
+  formEditCelebrity,
 };
--- starter-code/routes/celebrities-route.js
+++ starter-code/routes/celebrities-route.js
@@ -3,12 +3,22 @@ const express = require('express');
 const {
   index,
   detail,
+  newCelebrityForm,
+  createCelebrity,
+  deleteCelebrity,
+  formEditCelebrity,
+  editCelebrity,
 } = require('../controllers/celebrities-controller');
 
 const router = express.Router();
 
 /* GET home page */
 router.get('/', index);
+router.get('/new', newCelebrityForm);
+router.get('/:id/edit', formEditCelebrity);
 router.get('/:id', detail);
+router.post('/new/createCelebrity', createCelebrity);
+router.post('/delete', deleteCelebrity);
+router.post('/:id/edit', editCelebrity);
 
 module.exports = router;
