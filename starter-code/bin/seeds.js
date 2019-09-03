bb1bb0b4407db80845b8d8cdd19d312de4b4bd61
gramsco <antoningauth@gmail.com>
2019-09-03 10:59:36 +0200

    done

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,39 +1,59 @@
-let Celebrity = require("../models/celebrity")
+// let Celebrity = require("../models/celebrity")
+let Movie = require("../models/movie.js");
+// let Star = require("../models/celebrity.js")
 let mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost/celebrities")
+  .connect("mongodb://localhost/movies")
   .then(console.log("works!"))
   .catch((err) => console.log("marche pas :" + err))
 
-let data = [
+// mongoose
+//   .connect("mongodb://localhost/celebrities")
+//   .then(console.log("works!"))
+//   .catch((err) => console.log("marche pas :" + err))
+
+// let data = [
+//   {
+//     name: "Machin",
+//     occupation: "Actor",
+//     catchPhrase: "I am an actor !"
+//   },
+//   {
+//     name: "Truc",
+//     occupation: "Actress",
+//     catchPhrase: "I am an actress !"
+//   },
+//   {
+//     name: "Globor",
+//     occupation: "Destroyer of worlds",
+//     catchPhrase: "I am Globor, destroyer of worlds"
+//   }
+// ]
+
+const movies = [
   {
-    name: "Machin",
-    occupation: "Actor",
-    catchPhrase: "I am an actor !"
+    title: "The anxious developer",
+    genre: "suspense",
+    plot:"A developer has to survive"
   },
   {
-    name: "Truc",
-    occupation: "Actress",
-    catchPhrase: "I am an actress !"
+    title: "The happy developer",
+    genre: "romantic comedy",
+    plot:"A developer had a boring life until he met a fancy robot"
   },
   {
-    name: "Globor",
-    occupation: "Destroyer of worlds",
-    catchPhrase: "I am Globor, destroyer of worlds"
+    title: "Deadvloper",
+    genre: "horror",
+    plot:"A developer arose from the dead"
   }
 ]
 
-console.log(data)
-
-
-Celebrity.create(data, function (err, celebrity) {
+Movie.create(movies, (err, movie) => {
   
-  if (err) console.log(err)
+  if(err) console.log(err)
+  console.log(movie +"imported")
 
-  
-  // ...
-});
-
+})
 
 
