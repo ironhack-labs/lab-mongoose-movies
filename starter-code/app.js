bf631352ff13b0429409ee92832ed5a4359ba49c
kidistca <kidistca@gmail.com>
2019-09-08 18:08:10 +0100

    Iteration 0-6 completed

--- starter-code/app.js
+++ starter-code/app.js
@@ -10,8 +10,7 @@ const logger       = require('morgan');
 const path         = require('path');
 
 
-mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+mongoose.connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -45,14 +44,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
-
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
-
 
 
 const index = require('./routes/index');
 app.use('/', index);
 
+const celebrities = require('./routes/celebrities');
+app.use('/', celebrities);
+
+
+// const routes = require('./routes');
+// app.use(routes);
 
 module.exports = app;
+
+
+// app.listen(3000);
