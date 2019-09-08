feaf5694cd2c84b55c785fc9c27cdc6c9a13d617
Elisa <elisam.esquivel@gmail.com>
2019-09-08 14:28:52 -0500

    All interations finished, with BONUS

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -4,32 +4,32 @@ const Movie = require('../models/Movie');
 const dbName = 'celebrities';
 mongoose.connect(`mongodb://localhost/${dbName}`);
 
-const celebrities = [
-  {
-    name: 'Mariah Carey',
-    occupation: 'Singer',
-    catchPhrase:
-      'Whenever I watch TV and see those poor starving kids all over the world, I cant help but cry. I mean Id love to be skinny like that, but not with all those flies and death and stuff.'
-  },
-  {
-    name: 'Marilyn Monroe',
-    occupation: 'Model',
-    catchPhrase: 'Happy Birthday to you'
-  },
-  {
-    name: 'Paris Hilton',
-    occupation: 'Model',
-    catchPhrase: 'Thats hot!'
-  }
-];
+// const celebrities = [
+//   {
+//     name: 'Mariah Carey',
+//     occupation: 'Singer',
+//     catchPhrase:
+//       'Whenever I watch TV and see those poor starving kids all over the world, I cant help but cry. I mean Id love to be skinny like that, but not with all those flies and death and stuff.'
+//   },
+//   {
+//     name: 'Marilyn Monroe',
+//     occupation: 'Model',
+//     catchPhrase: 'Happy Birthday to you'
+//   },
+//   {
+//     name: 'Paris Hilton',
+//     occupation: 'Model',
+//     catchPhrase: 'Thats hot!'
+//   }
+// ];
 
-Celebrity.create(celebrities, err => {
-  if (err) {
-    throw err;
-  }
-  console.log(`${celebrities.length} "celebrities" created.`);
-  mongoose.connection.close();
-});
+// Celebrity.create(celebrities, err => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log(`${celebrities.length} "celebrities" created.`);
+//   mongoose.connection.close();
+// });
 
 // const movies = [
 //   {
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -57,4 +57,91 @@ router.post('/celebrities/:id/delete', (req, res, next) => {
   res.redirect('/celebrities/index');
 });
 
+router.get('/celebrities/:id/edit', (req,res,next) =>{
+  Celebrity.findById({ _id: req.params.id })
+    .then(celebrity => {
+      res.render('celebrities/edit', { celebrity });
+    })
+    .catch(error => {
+      console.log('Error occured:', error);
+    });
+});
+
+router.post('/celebrities/:id/edit', async (req,res) =>{
+  const { name, occupation, catchPhrase } = req.body
+  await Celebrity.findByIdAndUpdate({ _id: req.params.id }, {
+    name,
+    occupation,
+    catchPhrase })
+  res.redirect('/celebrities/index')
+});
+
+//Movies
+router.get('/movies/new', (req, res, next) => {
+  res.render('movies/new');
+});
+
+router.post('/movies/new', (req, res, next) => {
+  const { title, genre, plot } = req.body;
+  const movie = new Movies({ title, genre, plot });
+  movie
+    .save()
+    .then()
+    .catch(error => {
+      console.log('Error occured:', error);
+      res.redirect('/movies/index');
+    });
+  res.redirect('/movies/index');
+});
+
+router.get('/movies/index', (req, res, next) => {
+  Movies.find()
+    .then(movies => {
+      res.render('movies/index', { movies });
+    })
+    .catch(error => {
+      console.log('Error', error);
+    });
+});
+
+router.get('/movies/:id', (req, res, next) => {
+  Movies.findById({ _id: req.params.id })
+    .then(movie => {
+      res.render('movies/show', { movie });
+    })
+    .catch(error => {
+      console.log('Error', error);
+    });
+});
+
+router.post('/movies/:id/delete', (req, res, next) => {
+  Movies.findByIdAndRemove({ _id: req.params.id })
+    .then()
+    .catch(error => {
+      console.log('Error occured:', error);
+      res.redirect('/movies/index');
+    });
+
+  res.redirect('/movies/index');
+});
+
+router.get('/movies/:id/edit', (req,res,next) =>{
+  Movies.findById({ _id: req.params.id })
+    .then(movie => {
+      res.render('movies/edit', { movie });
+    })
+    .catch(error => {
+      console.log('Error occured:', error);
+    });
+});
+
+router.post('/movies/:id/edit', async (req,res) =>{
+  const { title, genre, plot } = req.body
+  await Movies.findByIdAndUpdate({ _id: req.params.id }, {
+    title,
+    genre,
+    plot })
+  res.redirect('/movies/index')
+});
+
 module.exports = router;
