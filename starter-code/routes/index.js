282410693051dad263522ab80ec25fa0924be838
Nelly Schneider <nellschge@stadtlengsfeld.net>
2019-09-10 12:46:48 +0200

    Iteration 2 done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,30 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Celebrity = require("../models/Celebrity");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
+
+//gets celebrity data from db and renders on success
+router.get("/celebrities", (req, res, next) => {
+  Celebrity.find()
+    .then(celebrities => {
+      console.log(celebrities);
+
+      //grabbing index.hbs from celebrities folder in views
+      res.render("celebrities/index", { celebrities });
+    })
+    .catch(err => {
+      console.log("ERROR: Data could not be loaded", err);
+      next(err);
+    });
+});
+
+// router.post("/celebrities", (req, res, next) => {
+//   const celebrityQuery = req.body;
+
+// });
 
 module.exports = router;
