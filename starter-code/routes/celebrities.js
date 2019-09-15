2f9987e03c526e2d175392836bcbd43e0d2f305f
Brais Vidal <brais.vidal.rodriguez@gmail.com>
2019-09-15 13:15:51 +0200

    iteration 3

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -9,4 +9,11 @@ router.get('/', (req, res, next) => {
     .catch(err => console.log('Celeb can not be found', err))
 });
 
+router.get('/:id', (req, res, next) => {
+  const celebId = req.params.id;
+  Celebrity.findById(celebId)
+    .then(celeb => res.render('celebrities/show', {celeb}))
+    .catch(err => console.log('Invalid ID', err))
+});
+
 module.exports = router;
