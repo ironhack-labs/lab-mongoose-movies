f50c68842f89f371391f302402fcf4fa2fb1ab0c
Elba Chimilio <elbachimilio@gmail.com>
2019-09-17 11:11:06 -0400

    Finished celebrities iterations

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -26,4 +26,75 @@ router.get('/show/:id', (req, res, next)=>{
 
 })
 
+router.get('/new-celeb', (req, res, next)=>{
+  res.render('celebrities/new');
+})
+
+router.post('/created', (req, res, next)=>{
+
+  let name = req.body.name;
+  let occupation = req.body.occupation;
+  let catchPhrase = req.body.catchPhrase;
+
+
+  Celebrity.create({
+      name: name,
+      occupation: occupation,
+      catchPhrase: catchPhrase
+  })
+  .then((result)=>{
+      res.redirect('/index')
+  })
+  .catch((err)=>{
+      next(err);
+  })
+})
+
+
+router.post('/delete/:id', (req, res, next)=>{
+  let id = req.params.id;
+
+  Celebrity.findByIdAndRemove(id)
+  .then((result)=>{
+      res.redirect('/index')
+  })
+  .catch((err)=>{
+      next(err)
+  })
+})
+
+
+router.get('/edit/:id', (req, res, next)=>{
+
+  let id=req.params.id;
+
+  Celebrity.findById(id)
+  .then(celebrity=>{
+      res.render('celebrities/edit', {celeb: celebrity})
+  })
+  .catch((err)=>{
+      next(err)
+  })
+})
+
+
+router.post('/update/:id', (req, res, next)=>{
+
+  let id=req.params.id;
+
+  Celebrity.findByIdAndUpdate(id, {
+      name: req.body.name,
+      occupation: req.body.occupation,
+      catchPhrase: req.body.catchPhrase
+  })
+  .then((result)=>{
+      res.redirect('/show/'+id)
+  })
+  .catch((err)=>{
+      next(err);
+  })
+
+})
+
+
 module.exports = router;
