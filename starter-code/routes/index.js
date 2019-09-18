cae214b2fbd6b4976f2ad802561ee5c12b9cde51
Jorge Luis <jorge.luis4212@gmail.com>
2019-09-18 01:07:21 -0400

    added celebrities route

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router  = express.Router();
+const Celebrity = require('../models/Celebrity')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+router.get('/celebrity', (req, res, next) => {
+  Celebrity.find({}).then(celebs =>{
+    const hbsObj = {
+      celebrity: celebs
+    }
+    res.render('celebrity', hbsObj);
+  })
 });
 
 module.exports = router;
