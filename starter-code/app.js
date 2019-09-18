e8283bb3b49e5d30d49a6f54b3fb716d0155f904
Bobby Wang <bobby.p.wang@gmail.com>
2019-09-18 16:46:09 -0400

    Added auto login after signinup.

--- starter-code/app.js
+++ starter-code/app.js
@@ -9,7 +9,12 @@ const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
 const session = require("express-session");
+const bcrypt = require('bcryptjs');
 const MongoStore = require("connect-mongo")(session);
+const flash = require("express-flash")
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require('./models/User.js')
 
 
 mongoose
@@ -66,8 +71,56 @@ app.use(session({
   })
 }));
 
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, user);
+  });
+});
+
+
+passport.use(new LocalStrategy({
+  passReqToCallback: true
+}, (req, username, password, next) => {
+  User.findOne({
+    username
+  }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, {
+        message: "Incorrect username"
+      });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, {
+        message: "Incorrect password"
+      });
+    }
+
+    return next(null, user);
+  });
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash())
 app.use((req, res, next) => {
-  res.locals.currentUser = req.session.currentUser;
+  console.log(res.locals.currentUser)
+  console.log('---------------------------------')
+  res.locals.currentUser = req.user;
+  console.log(res.locals.currentUser)
+  // passport uses req.user
+  // res.locals.currentUser = req.session.currentUser;
+  res.locals.errorMessage = req.flash('error');
   next();
 });
 
--- starter-code/routes/user.js
+++ starter-code/routes/user.js
@@ -2,74 +2,93 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User.js')
 const bcrypt = require('bcryptjs')
+const passport = require('passport')
 
-router.get('/signup', (req, res, next) => {
-    res.render('users/signup')
-})
+router.get("/signup", (req, res, next) => {
+    res.render("users/signup");
+});
 
 router.post('/signup', (req, res, next) => {
 
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(req.body.password, salt);
+
+    const salt = bcrypt.genSaltSync(10)
+    let password = bcrypt.hashSync(req.body.password, salt)
 
     User.create({
-            username: req.body.username,
-            password: hash
-        })
-        .then((user) => {
-            req.session.currentUser = user
-            
-            console.log(req.session.currentUser)
-            res.redirect('/')
-        })
-        .catch((e) => {
-            errorMessage = e
-        })
-})
+        username: req.body.username,
+        password: password
+    })
+        .then(user => {
+            req.login(user, function(err){
+                if(!err){
+                    res.redirect('/')
+                } else{
+                    next(err);
+                }
 
-router.get('/login', (req, res, next) => {
+            })
+            })
+        .catch(e => {
+            res.status(500).send(e)})
+});
+
+router.get("/login", (req, res, next) => {
     res.render('users/login')
-})
+});
 
-router.post('/login', (req, res, next) => {
+router.post("/login", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+    passReqToCallback: true
+}));
+//
+// manual login code not using passport
+//
+// router.post('/login', (req, res, next) => {
 
-    User.findOne({
-            username: req.body.username
-        })
-        .then((user) => {
-            if (!user) {
-                console.log('this is the logged in user', user)
-                res.redirect('/')
-            }
-            if (bcrypt.compareSync(req.body.password, user.password)) {
-                req.session.currentUser = user
-                res.redirect('/')
-            } else {
-                res.render('users/login', {
-                    errorMessage: "Incorrect password"
-                })
-            }
-        })
-        .catch((e) => {
-            errorMessage = e
-        })
-})
+// router.get('/login', (req, res, next) => {
+//     res.render('users/login')
+// })
+
+//     User.findOne({
+//             username: req.body.username
+//         })
+//         .then((user) => {
+//             if (!user) {
+//                 req.flash('error', `sorry that username doesn't exist`)
+//                 res.redirect('/')
+//             }
+//             if (bcrypt.compareSync(req.body.password, user.password)) {
+//                 req.user = user
+//                 res.redirect('/')
+//             } else {
+//                 res.render('users/login', {
+//                     errorMessage: "Incorrect password"
+//                 })
+//             }
+//         })
+//         .catch((e) => {
+//             errorMessage = e
+//         })
+// })
 
 router.get("/logout", (req, res, next) => {
-    req.session.destroy((err) => {
-      res.redirect("/login");
-    });
-  });
+    req.logout()
+    // password uses just req.logout()
+    // req.session.destroy((err) => {
+    res.redirect("/login")
+})
 
 router.get('/secret', (req, res, next) => {
-    
-    console.log(req.session.currentUser)
 
-    if (req.session.currentUser) {
+    console.log(req.user)
+
+    if (req.user) {
         res.render('users/secret')
     } else {
         res.redirect('/')
     }
 })
 
-module.exports = router;
+module.exports = router
