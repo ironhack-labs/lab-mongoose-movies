4b8bca4dfbd1d4374d9d29d6f387d537146e6d04
agloks <perdi9090@yahoo.com>
2019-09-27 17:35:55 -0300

    up

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router  = express.Router();
+const model = require("../models/celebrity")
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+
+router.get("/celebrities",(req,res,next) => {
+  model.find().then((e)=>
+  {
+    console.log(e)
+    res.render("./celebrities/index",{e})
+  }
+    ).catch()
 });
 
 module.exports = router;
