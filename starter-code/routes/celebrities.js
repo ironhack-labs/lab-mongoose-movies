7e6c1d515c5a04ea00be8a66a44788f306c2a147
bruno <brunno-dev@gmail.com>
2019-10-15 18:41:19 -0300

    working

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -40,6 +40,7 @@ router.post('/celebrities', (req, res) => {
 //rota com método para deletar
 router.post('/celebrities/:id/delete', (req, res) => {
   const thisId = req.params.id;
+  console.log("PARAMS >>>>>",thisId)
   Celebrity.findByIdAndRemove(thisId)
     .then(() => res.redirect('/celebrities'))
     .catch(error => {
@@ -66,7 +67,7 @@ router.get('/celebrities/:id/edit', (req, res) => {
 //rota com método para criar
 router.post('/celebrities/:id/edit', (req, res) => {
   const thisId = req.params.id;
-  // console.log(req.body)
+  console.log(req.params.id)
   Celebrity.findByIdAndUpdate(thisId, req.body)
   .then(() => res.redirect('/celebrities'))
   .catch(error => {throw new Error(error)})
