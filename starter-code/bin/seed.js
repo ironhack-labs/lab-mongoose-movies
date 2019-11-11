7d303054c09d0e8ea2c40ed9aabafa2282b90d93
Hanna Mohamed <hanna@MacBook-Pro-de-Hanna.local>
2019-11-11 10:36:06 +0100

    done

--- starter-code/bin/seed.js
+++ starter-code/bin/seed.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const Celebrity = require("../models/celebrity");
+// const Celebrity = require("../models/celebrity");
+const Movie = require("../models/movie");
 
 mongoose
   .connect("mongodb://localhost/starter-code", { useNewUrlParser: true })
@@ -12,16 +13,35 @@ mongoose
     console.error("Error connecting to mongo", err);
   });
 
-const celebrities = [
-  { name: "Tom Cruise", occupation: "actor", catchPhrase: "Hey I am an actor" },
-  { name: "Beyonce", occupation: "singer", catchPhrase: "Hey I am a singer" },
+// const celebrities = [
+//   { name: "Tom Cruise", occupation: "actor", catchPhrase: "Hey I am an actor" },
+//   { name: "Beyonce", occupation: "singer", catchPhrase: "Hey I am a singer" },
+//   {
+//     name: "Kim Kardashian",
+//     occupation: "unknown",
+//     catchPhrase: "Hey I am known"
+//   }
+// ];
+
+// Celebrity.insertMany(celebrities)
+//   .then(dbRes => console.log("All celebrities have been added"))
+//   .catch(dbErr => console.log(`Err - ${dbErr}`));
+
+const movies = [
   {
-    name: "Kim Kardashian",
-    occupation: "unknown",
-    catchPhrase: "Hey I am known"
+    title: "First movie",
+    genre: "comedy",
+    plot: "we'll have to define this"
+  },
+  {
+    title: "Second movie",
+    genre: "drama",
+    plot: "we'll have to define this too"
   }
 ];
 
-Celebrity.insertMany(celebrities)
-  .then(dbRes => console.log("All celebrities have been added"))
-  .catch(dbErr => console.log(`Err - ${dbErr}`));
+Movie.insertMany(movies)
+  .then(dbRes => {
+    console.log("All movies have been inserted");
+  })
+  .catch(dbErr => console.log(dbErr));
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const Celebrity = require("../models/celebrity");
+const Movie = require("../models/movie");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
@@ -82,6 +83,85 @@ router.post("/celebrities/:id/edit", (req, res) => {
   })
     .then(dbRes => {
       res.redirect("/celebrities");
+    })
+    .catch(dbErr => {
+      console.error(dbErr);
+    });
+});
+
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then(dbRes => {
+      res.render("movies", {
+        title: "Movies",
+        movies: dbRes
+      });
+    })
+    .catch(console.log("error"));
+});
+
+router.get("/movie/:id", (req, res) => {
+  Movie.findOne({ _id: req.params.id }).then(dbRes => {
+    res.render("show-movie", {
+      title: "Movie details",
+      movie: dbRes
+    });
+  });
+});
+
+router.get("/movies/new", (req, res) => {
+  res.render("new-movie", {
+    title: "Create a new movie"
+  });
+});
+
+router.post("/movies/new", (req, res) => {
+  const newMovie = {
+    title: req.body.title,
+    genre: req.body.genre,
+    plot: req.body.plot
+  };
+  Movie.create(newMovie)
+    .then(dbRes => {
+      console.log("Movie successfully created");
+      res.redirect("/movies");
+    })
+    .catch(dbErr => {
+      console.error(dbErr);
+    });
+});
+
+router.post("/movies/:id/delete", (req, res) => {
+  Movie.findByIdAndRemove(req.params.id)
+    .then(dbRes => {
+      res.redirect("/movies");
+    })
+    .catch(dbErr => {
+      console.error(dbErr);
+    });
+});
+
+router.get("/movies/:id/edit", (req, res) => {
+  Movie.findById(req.params.id)
+    .then(dbRes => {
+      res.render("edit-movie", {
+        title: "Edit an existing celebrity",
+        details: dbRes
+      });
+    })
+    .catch(dbErr => {
+      console.log(dbErr);
+    });
+});
+
+router.post("/movies/:id/edit", (req, res) => {
+  Movie.findByIdAndUpdate(req.params.id, {
+    title: req.body.title,
+    genre: req.body.genre,
+    plot: req.body.plot
+  })
+    .then(dbRes => {
+      res.redirect("/movies");
     })
     .catch(dbErr => {
       console.error(dbErr);
