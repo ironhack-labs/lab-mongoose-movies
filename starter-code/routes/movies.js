806b4b8c2961ea279760874d52d08f37ea452472
camille <camille.grison@gmail.com>
2019-11-11 23:48:39 +0100

    iteration 9

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const moviesModel = require('../models/Movies'); // create this variable to call our db
+const moviesModel = require('../models/Movies'); // create this variable to call our db collection
 
 //iteration 8: GET movies page
 router.get('/movies', (req, res, next) => { // url adress
@@ -16,6 +16,26 @@ router.get('/movies', (req, res, next) => { // url adress
       console.log("error while retrieving movies view", error);
     });
 });
+
+//iteration 9 : movies details
+router.get('/movies/:id', (req, res, next) => {
+    moviesModel
+      .findOne({
+        "_id": req.params.id // id = property of params which is property of req
+      })
+      .then(movie => {
+        res.render('movies/show', { // render the show.hbs file
+          movieShow: movie // movieShow renders an object and will let us to access its data in the show.hbs file
+        });
+        //console.log(movie); // movie is the object selected in the db when clicking
+      })
+      .catch(error => {
+        next();
+        console.log("error while retrieving show of a celebrity", error);
+      });
+  }
+
+);
 
 console.log("inside the movies.js file !");
 
