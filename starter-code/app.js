468203173ef4a3d397931ba6a8ed986a86a043a3
José Carneiro <josemcarneiro@gmail.com>
2019-11-14 18:33:39 +0000

    Removed dependency on body-parser, unecessary built files

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,4 +1,3 @@
-const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const favicon = require('serve-favicon');
@@ -25,8 +24,8 @@ app.locals.title = 'Lab Mongoose Movies';
 
 // Middleware Setup
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
