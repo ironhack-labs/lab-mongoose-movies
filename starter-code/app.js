6ed234d76840c3c72059933b60dd78a595e76f69
enriqueMontano <montanoenrique@outlook.com>
2019-11-16 00:44:58 +0100

    iteration 2 done

--- starter-code/app.js
+++ starter-code/app.js
@@ -54,4 +54,9 @@ app.locals.title = "Express - Generated with IronGenerator";
 const index = require("./routes/index");
 app.use("/", index);
 
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
+
 module.exports = app;
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Celebrities = require("../models/celebrity");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/celebrities", (req, res) => {
+  Celebrities.find().then(celebrity =>
+    res.render("Celebrities", { celebrity })
+  );
 });
 
 module.exports = router;
