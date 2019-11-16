1a1ef77c839bda8686aeec808f0f955a916b11c4
gabzafra <gab.zafra@gmail.com>
2019-11-16 18:48:58 +0100

    iteration 2

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const bodyParser = require('body-parser');
+const Celebrities = require("../models/celebrity");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get(`/celebrities`, (req, res, next) => {
+  Celebrities
+    .find()
+    .then(celebs => res.render(`celebrities/index`,{celebs}))
+    .catch(error => next(error));
 });
 
 module.exports = router;
