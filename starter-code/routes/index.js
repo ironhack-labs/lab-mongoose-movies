67cbe6236d37661762a7d4a57901a96b69f61b41
enriqueMontano <montanoenrique@outlook.com>
2019-11-16 01:51:07 +0100

    iteration 3 done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -8,9 +8,28 @@ router.get("/", (req, res, next) => {
 });
 
 router.get("/celebrities", (req, res) => {
-  Celebrities.find().then(celebrity =>
-    res.render("Celebrities", { celebrity })
-  );
+  Celebrities.find()
+    .then(celebrity => res.render("celebrities/index", { celebrity }))
+    .catch(err => {
+      console.log(err);
+      next();
+    });
+});
+
+router.get("/show/:id", (req, res) => {
+  let celebrity = req.params.id;
+
+  Celebrities.find({ _id: celebrity })
+
+    .then(celebrity => {
+      console.log(celebrity)
+
+      res.render("celebrities/show",  celebrity[0] );
+    })
+    .catch(err => {
+      console.log(err);
+      next();
+    });
 });
 
 module.exports = router;
