927af28d2bf61925a5acc8b98e32c036c95c28b5
Flor Urbinati <flor29600@hotmail.com>
2019-11-16 20:45:57 +0100

    iteration 3

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,39 +1,35 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 const Celebrities = require("../Models/Celebrity")
-const hbs = require('hbs');
-const path = require('path');
 
-
-// router.set('views', path.join(__dirname, 'views'));
-// router.set('view engine', 'hbs');
-// router.use(express.static(path.join(__dirname, 'public')));
-// router.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-// mongoose
-//   .connect('mongodb://localhost/RandomCollection', {
-//     useNewUrlParser: true
-//   })
-//   .then(x => {
-//     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-//   })
-//   .catch(err => {
-//     console.error('Error connecting to mongo', err)
-//   });
 
 /* GET home page */
-router.get('/celebrities', (req, res, next) => {
+router.get('/', (req, res, next) => {
   Celebrities.find()
     .then(celebrityInDB => {
       console.log(celebrityInDB)
       res.render('celebrities/index', {
         celebrityInDB
       });
+    })
+    .catch(error =>{
+      next();
+      console.log(error)
     });
-    // .catch(error =>{
-      
-    // } )
+});
+
+router.get('/celebrities/:id', (req, res, next) => {
+  Celebrities.findById(req.params.id)
+    .then(celebrity=> {
+      console.log(celebrity)
+      res.render('celebrities/show',
+        celebrity
+      );
+    })
+      .catch(error =>{
+        next();
+        console.log(error)
+      });
 });
 
 module.exports = router;
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -4,6 +4,6 @@ const router  = express.Router();
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
-});
+})
 
 module.exports = router;
