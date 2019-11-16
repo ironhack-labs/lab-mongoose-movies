85c40faba314ad0bac32ffe05f6326cea05370b7
enriqueMontano <montanoenrique@outlook.com>
2019-11-16 15:46:03 +0100

    iteration 4 done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -18,18 +18,36 @@ router.get("/celebrities", (req, res) => {
 
 router.get("/show/:id", (req, res) => {
   let celebrity = req.params.id;
-
   Celebrities.find({ _id: celebrity })
-
     .then(celebrity => {
-      console.log(celebrity)
-
-      res.render("celebrities/show",  celebrity[0] );
+      res.render("celebrities/show", celebrity[0]);
     })
     .catch(err => {
       console.log(err);
       next();
     });
+});
+
+router.get("/new/", (req, res) => {
+  res.render("celebrities/new");
+});
+
+router.post("/celebrities-create", (req, res) => {
+  Celebrities.create({
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase
+  }).then(celebrityCreated => {
+    celebrityCreated.save(err => {
+      if (err) {
+        res.render("celebrities/new", {
+          errorMessage: "Something went wrong. Try again later."
+        });
+        return;
+      }
+      res.redirect("celebrities");
+    });
+  });
 });
 
 module.exports = router;
