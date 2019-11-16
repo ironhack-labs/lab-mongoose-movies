6b9853cdcbc664a75b766fed360d1554ccf2e46a
gabzafra <gab.zafra@gmail.com>
2019-11-16 19:01:51 +0100

    iteration 3

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -8,11 +8,20 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get(`/celebrities/:id`, (req, res, next) => {
+  Celebrities
+    .findById(req.params.id)
+    .then(celeb => res.render(`celebrities/show`,celeb))
+    .catch(error => next(error));
+});
+
 router.get(`/celebrities`, (req, res, next) => {
   Celebrities
     .find()
     .then(celebs => res.render(`celebrities/index`,{celebs}))
     .catch(error => next(error));
 });
+
+
 
 module.exports = router;
