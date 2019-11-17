0f41dd69371882de09078ca8899fabbe97e8908e
adelcanto <a.delcanto@outlook.com>
2019-11-17 20:38:26 +0100

    Iteration 3 - DONE

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,16 +1,30 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const Celebrities = require('../models/celebrity')
 const mongoose = require('mongoose');
+
+
 
 router.get('/celebrities', (req, res, next) => {
   Celebrities.find()
     .then(celebritiesFound => {
-      res.render('celebrities/index', {celebritiesFound})
+      res.render('celebrities/index', {
+        celebritiesFound
+      })
     })
     .catch((err) => {
       console.log(err)
     })
 });
+
+router.get('/celebrities/:id', (req, res, next) => {
+  Celebrities.findById(req.params.id)
+    .then(celebrityById => {
+      res.render('celebrities/show', celebrityById)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
 
 module.exports = router;
