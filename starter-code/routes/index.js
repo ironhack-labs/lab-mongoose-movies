18285304c16ae4c37a07305e952f510c195e59f0
Laura <laura.vivas.gamboa@gmail.com>
2019-11-17 05:37:47 +0100

    Third commit

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -2,20 +2,69 @@ const express = require('express');
 const router  = express.Router();
 const Celebrities = require("../models/Celebrity");
 
+const { check, validationResult } = require('express-validator');
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
 
-app.get("/celebrities", (req, res, next) => {
-  Celebrities.find({})
+router.get("/celebrities", (req, res, next) => {
+  Celebrities.find()
   .then(celebrities => {
-    res.render("celebrities/index", {celebrities})
+    console.log("Founded celebrities = " + celebrities.length);
+    res.render("celebrities/index", {
+      celebrities
+    })
   })
   .catch(error => {
     console.log('Error while getting the celebrities', error);
     next(error);
+  })
+
+});
+
+router.get("/celebrities/new", (req, res) => {
+  res.render("celebrities/new")
+})
+
+router.get("/celebrities/:id?", (req, res, next) => {
+  let celebrityId = req.params.id;
+
+  console.log("given id=" + celebrityId);
+  Celebrities.findById(celebrityId)
+    .then((celebrity) => {
+      res.render('celebrities/show', {celebrity});
+    })
+    .catch((error) => {
+      next(error);
+    });
+    
+});
+
+
+router.post("/celebrities", [
+  check('name').isLength({ min: 1 }),
+  check('ocupation').isLength({ min: 1 }),
+  check('catchFrase').isLength({ min: 1 })
+], (req, res) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+  }
+  const celebrity = {
+    name : req.body.name,
+    ocupation : req.body.ocupation,
+    catchFrase : req.body.catchFrase
+  };
+  Celebrities.create(celebrity)
+  .then(celebrityCreated => {
+    res.redirect("/celebrities");
+  })
+  .catch(error => {
+    res.render("celebrities/new")
   })
 
 })
