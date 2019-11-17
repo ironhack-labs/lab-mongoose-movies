2228421f5362a783984d9e3e824016dac84cdc48
Flor Urbinati <flor29600@hotmail.com>
2019-11-17 18:25:24 +0100

    iteration 6 - Celebrities DONE ;)

--- starter-code/app.js
+++ starter-code/app.js
@@ -29,6 +29,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+mongoose.set('useFindAndModify', false)
 
 // Express View engine setup
 
--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Celebrities = require("../Models/Celebrity")
-
+const bodyParser = require('body-parser');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -32,33 +32,66 @@ router.get('/celebrities/:id', (req, res, next) => {
     });
 });
 
+router.post("/celebrities/:id", (req, res, next) => {
+  Celebrities.updateOne({
+      _id: req.params.id
+    }, {
+      name: req.body.name,
+      occupation: req.body.occupation,
+      catchPhrase: req.body.catchPhrase
+
+    })
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch(error => {
+      next();
+      console.log(error)
+    });
+});
+
 router.get('/celebrities/new', (req, res, next) => {
   res.render('celebrities/new');
 })
 
 router.post("/celebrities", (req, res) => {
   Celebrities.create({
-    name: req.body.name,
-    occupation: req.body.occupation,
-    catchPhrase: req.body.catchPhrase
-  }).then(() => {
-    res.redirect("/");
-  })
-  .catch(error => {
-    res.redirect("/celebrities/new");
-    console.log(error)
-  });
+      name: req.body.name,
+      occupation: req.body.occupation,
+      catchPhrase: req.body.catchPhrase
+    }).then(() => {
+      res.redirect("/");
+    })
+    .catch(error => {
+      res.redirect("/celebrities/new");
+      console.log(error)
+    });
 });
 
-router.post("/celebrities/:id/delete", (req, res,next) => {
+router.post("/celebrities/:id/delete", (req, res, next) => {
   Celebrities.findByIdAndDelete(req.params.id)
-  .then(() => {
-    res.redirect("/");
-  })
-  .catch(error => {
-    next();
-    console.log(error)
-  });
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch(error => {
+      next();
+      console.log(error)
+    });
 });
+
+router.get('/celebrities/:id/edit', (req, res, next) => {
+  Celebrities.findById(req.params.id)
+    .then(celebrity => {
+      console.log(celebrity)
+      res.render('celebrities/edit',
+        celebrity
+      );
+    })
+    .catch(error => {
+      next();
+      console.log(error)
+    });
+});
+
 
 module.exports = router;
