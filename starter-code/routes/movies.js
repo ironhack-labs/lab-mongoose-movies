87625c9fdd7e0b0aecc6f5be509d3d0ec78824f2
María Simó <maria@coponstudio.es>
2019-11-17 18:39:57 +0100

    It 9. Movie details route and view

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -6,14 +6,29 @@ const Movies = require("../models/Movie");
 
 router.get('/movies', (req, res, next) => {
     Movies
-      .find()
-      .then(allMovies => {
-        res.render('movies/index', { allMovies });
-      })
-      .catch(err => {
-        next();
-        console.log(`There was an error : \n ${err}`)
-      })
-  })
+        .find()
+        .then(allMovies => {
+            res.render('movies/index', { allMovies });
+        })
+        .catch(err => {
+            next();
+            console.log(`There was an error : \n ${err}`)
+        })
+})
+
+
+router.get('/movies/:id', (req, res, next) => {
+
+    Movies
+        .findById({ _id: req.params.id })
+        .then(foundMovie => {
+            res.render('movies/show', {foundMovie})
+        })
+        .catch(err => {
+            next();
+            console.log(`There was an error : \n ${err}`)
+        })
+})
+
 
 module.exports = router;
