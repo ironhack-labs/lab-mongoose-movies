31ea0a9b16875965cc49c8380a3672dc4c765b35
Flor Urbinati <flor29600@hotmail.com>
2019-11-17 19:31:50 +0100

    movies bonus completed

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const Celebrities = require("../Models/Celebrity")
+const Celebrities = require("../Models/Celebrity");
+const Movies = require("../Models/Movies");
 const bodyParser = require('body-parser');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+//celebrities routes
+
+router.get('/celebrities', (req, res, next) => {
   Celebrities.find()
     .then(celebrityInDB => {
       console.log(celebrityInDB)
@@ -93,5 +100,95 @@ router.get('/celebrities/:id/edit', (req, res, next) => {
     });
 });
 
+//movies routes
+
+router.get('/movies', (req, res, next) => {
+  Movies.find()
+    .then(movieInDB => {
+      console.log(movieInDB)
+      res.render('movies/index', {
+        movieInDB
+      });
+    })
+    .catch(error => {
+      next();
+      console.log(error)
+    });
+});
+
+router.get('/movies/:id', (req, res, next) => {
+  Movies.findById(req.params.id)
+    .then(movie => {
+      console.log(movie)
+      res.render('movies/show',
+        movie
+      );
+    })
+    .catch(error => {
+      next();
+      console.log(error)
+    });
+});
+
+router.post("/movies/:id", (req, res, next) => {
+  Movies.updateOne({
+      _id: req.params.id
+    }, {
+      title: req.body.title,
+      genre: req.body.genre,
+      plot: req.body.plot
+
+    })
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch(error => {
+      next();
+      console.log(error)
+    });
+});
+
+router.get('/movies/new', (req, res, next) => {
+  res.render('movies/new');
+})
+
+router.post("/movies", (req, res) => {
+  Movies.create({
+      title: req.body.title,
+      genre: req.body.genre,
+      plot: req.body.plot
+    }).then(() => {
+      res.redirect("/");
+    })
+    .catch(error => {
+      res.redirect("/movies/new");
+      console.log(error)
+    });
+});
+
+router.post("/movies/:id/delete", (req, res, next) => {
+  Movies.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch(error => {
+      next();
+      console.log(error)
+    });
+});
+
+router.get('/movies/:id/edit', (req, res, next) => {
+  Movies.findById(req.params.id)
+    .then(movie => {
+      console.log(movie)
+      res.render('movies/edit',
+        movie
+      );
+    })
+    .catch(error => {
+      next();
+      console.log(error)
+    });
+});
 
 module.exports = router;
