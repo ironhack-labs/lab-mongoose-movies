57dd046de86ea9d2f95996b51b748ed2036c1920
María Simó <maria@coponstudio.es>
2019-11-17 02:11:45 +0100

    Added iteration 3

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,23 +1,35 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const Celebrities = require("../models/Celebrity");
 
 
-router.get('/', (req,res,next) => {
+router.get('/', (req, res, next) => {
   res.render('index')
 })
 
 
-router.get('/celebrities', (req, res, next) => {
+router.get('/celebrities/:id?', (req, res, next) => {
   Celebrities
     .find()
     .then(allCelebs => {
-      res.render('celebrities/index', {allCelebs});
+      let id = req.params.id
+
+      if (id) {
+        Celebrities
+          .find({_id : id})
+          .then(oneCeleb => {
+            res.render('celebrities/show', { oneCeleb });
+          })
+      } else {
+        res.render('celebrities/index', { allCelebs, id });
+      }
+
     })
     .catch(err => {
       next();
       console.log(`There was an error : \n ${err}`)
     })
 });
+
 
 module.exports = router;
