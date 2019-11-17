6181c9b47afc8bc880bde1a9bc0660fde14430cb
Laura <laura.vivas.gamboa@gmail.com>
2019-11-17 04:23:38 +0100

    Second commit

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router  = express.Router();
+const Celebrities = require("../models/Celebrity");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
+app.get("/celebrities", (req, res, next) => {
+  Celebrities.find({})
+  .then(celebrities => {
+    res.render("celebrities/index", {celebrities})
+  })
+  .catch(error => {
+    console.log('Error while getting the celebrities', error);
+    next(error);
+  })
+
+})
+
+
 
 module.exports = router;
