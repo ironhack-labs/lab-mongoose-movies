bd4533f51d8f080c3e07b11a4fa7b71dcd46f1f9
Javier Agudo <javieragudo3d@gmail.com>
2019-11-17 17:31:40 +0100

    Iteration One DONE, plant a seed

--- starter-code/app.js
+++ starter-code/app.js
@@ -11,7 +11,7 @@ const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/celebritiesDB', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -47,7 +47,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'CelebritiesDB';
 
 
 
@@ -56,3 +56,7 @@ app.use('/', index);
 
 
 module.exports = app;
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
---
+++ starter-code/bin/seed.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Celebrity = require("../models/celebrity");
+
+mongoose.connect(`mongodb://localhost/Celebrity`);
+
+const celebrities = [
+  {
+    name: "Arnold Schwarzenegger",
+    occupation: "Actor",
+    catchPhrase: "Sayonara, baby"
+  },
+  {
+    name: "Romeo Santos",
+    occupation: "Bachata Singer",
+    catchPhrase: "El chico de las poesías"
+  },
+  {
+    name: "Belén Esteban",
+    occupation: "People's Princess",
+    catchPhrase: "Andrea, coño, cómete el pollo"
+  }
+];
+
+Celebrity.create(celebrities, err => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Created ${celebrities.length} celebrities`);
+  mongoose.connection.close();
+});
