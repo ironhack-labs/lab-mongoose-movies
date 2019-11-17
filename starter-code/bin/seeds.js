a0fe0dd361e97d7967665be1d2f40aace6253204
María Simó <maria@coponstudio.es>
2019-11-17 01:37:47 +0100

    Iteration 1

---
+++ starter-code/bin/seeds.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const Celebrity = require("../models/Celebrity");
+
+mongoose
+    .connect('mongodb://localhost/movies', { useNewUrlParser: true })
+    .then(x => {
+        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    })
+    .catch(err => {
+        console.error('Error connecting to mongo', err)
+    });
+
+
+const celebrities = [
+    {
+        name: "Mae West",
+        occupation: "Actress",
+        catchPhrase: "Good girls go to heaven, bad girls go everywhere"
+    },
+    {
+        name: "Wiston Churchill",
+        occupation: "Politician",
+        catchPhrase: "Success is walking from failure to failure with no loss of enthusiasm."
+    },
+    {
+        name: "Charles Darwin",
+        occupation: "Scientist",
+        catchPhrase: "It is not the strongest of the species that survive, nor the most intelligent, but the one most responsive to change"
+    },
+    {
+        name: "Oscar Wilde",
+        occupation: "Writer",
+        catchPhrase: "Be yourself; everyone else is already taken."
+    }
+]
+
+Celebrity
+    .deleteMany()
+    .then(() => {
+        Celebrity
+            .insertMany(celebrities)
+            .then(data => console.log(`Added to the database: \n ${data}`))
+            .catch(err => console.log(`There was an error: \n ${err}`))
+    })
