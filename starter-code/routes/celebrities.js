88b24866fbe44492f78686768623f0bc64c21dd2
adelcanto <a.delcanto@outlook.com>
2019-11-17 21:39:29 +0100

    Iteration 4 - DONE

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const Celebrities = require('../models/celebrity')
 const mongoose = require('mongoose');
+const bodyParser   = require('body-parser');
 
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
 
 
 router.get('/celebrities', (req, res, next) => {
@@ -17,6 +20,25 @@ router.get('/celebrities', (req, res, next) => {
     })
 });
 
+router.get('/celebrities/new', (req, res, next) => {
+  res.render('celebrities/new')
+})
+
+router.post("/celebrities/new", (req, res, next) => {
+  let newCelebrity = {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase
+  }
+  Celebrities.create(newCelebrity)
+  .then(()=>{
+    res.redirect('/celebrities')
+  })
+  .catch(()=>{
+    res.redirect('/celebrities/new')
+  })
+})
+
 router.get('/celebrities/:id', (req, res, next) => {
   Celebrities.findById(req.params.id)
     .then(celebrityById => {
@@ -26,5 +48,9 @@ router.get('/celebrities/:id', (req, res, next) => {
       console.log(err)
     })
 })
+
+
+
+
 
 module.exports = router;
