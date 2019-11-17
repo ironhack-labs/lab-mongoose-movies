b72bb6cd8d6f7cd2134911b220e1ddeca62a9fd4
María Simó <maria@coponstudio.es>
2019-11-17 18:08:50 +0100

    Iteration 7, movies model and seeds

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Celebrities = require("../models/Celebrity");
+const Movies = require("../models/Movie");
 
 mongoose
     .connect('mongodb://localhost/movies', { useNewUrlParser: true })
@@ -34,11 +35,45 @@ const celebrities = [
     }
 ]
 
+const movies = [
+    {
+        title : "The Open House",
+        genre : "Thriller",
+        plot : "Following a tragedy, a mother and her teen son move to a relative's vacant vacation home, where eerie and unexplained forces conspire against them."
+    },
+    {
+        title : "The King",
+        genre : "Drama",
+        plot : "Wayward Prince Hal must turn from carouser to warrior king as he faces hostilities from inside and outside the castle walls in the battle for England."
+    },
+    {
+        title : "Paradox",
+        genre : "Independent",
+        plot : "Neil Young and his band of outlaws sow seeds of strange mischief and musical wonder under Western skies in this dreamlike film by Daryl Hannah."
+    },
+    {
+        title : "The Little Switzerland",
+        genre : "Comedy",
+        plot : "The discovery of the tomb of William Tell’s son in a town in the Basque Country spurs the village's cantankerous citizens to lobby for Swiss annexation."
+    }
+
+]
+
 Celebrities
     .deleteMany()
     .then(() => {
         Celebrities
             .insertMany(celebrities)
+            .then(data => console.log(`Added to the database: \n ${data}`))
+            .catch(err => console.log(`There was an error: \n ${err}`))
+    })
+
+
+Movies
+    .deleteMany()
+    .then(() => {
+        Movies
+            .insertMany(movies)
             .then(data => console.log(`Added to the database: \n ${data}`))
             .catch(err => console.log(`There was an error: \n ${err}`))
     })
