93d26eb843bf06cb4af9696fa4b5981de8845e59
adelcanto <a.delcanto@outlook.com>
2019-11-17 23:12:25 +0100

    Iteration 5 & 6 - DONE

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -7,7 +7,6 @@ const bodyParser   = require('body-parser');
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
 
-
 router.get('/celebrities', (req, res, next) => {
   Celebrities.find()
     .then(celebritiesFound => {
@@ -39,6 +38,38 @@ router.post("/celebrities/new", (req, res, next) => {
   })
 })
 
+router.get("/celebrities/:id/delete", (req, res, next) => {
+  // res.json(req.params.id)
+  Celebrities.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.redirect('/celebrities')
+    })
+})
+
+router.post("/celebrities/:id/edit", (req, res, next) => {
+  let updatedCelebrity = {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase
+  }
+  
+  Celebrities.findByIdAndUpdate(req.params.id, updatedCelebrity)
+  .then(() => {
+    res.redirect(`/celebrities/${req.params.id}`)
+  })
+  .catch(error => next(error))
+})
+
+router.get("/celebrities/:id/edit", (req, res, next) => {
+  Celebrities.findByIdAndUpdate(req.params.id)
+  .then(celebById => {
+    res.render('celebrities/edit', celebById)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+})
+
 router.get('/celebrities/:id', (req, res, next) => {
   Celebrities.findById(req.params.id)
     .then(celebrityById => {
@@ -48,9 +79,5 @@ router.get('/celebrities/:id', (req, res, next) => {
       console.log(err)
     })
 })
-
-
-
-
 
 module.exports = router;
