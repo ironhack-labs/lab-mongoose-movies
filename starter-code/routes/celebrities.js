c03788851d3146587f252b57d0e051729d9547f0
Johann <johanson88@gmail.com>
2019-11-18 19:07:21 +0100

    Iteration 4 done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -5,6 +5,22 @@ const Celebrity = require('./../models/Celebrity');
 
 //Get celebrities by ID
 ///celebrities/{{this._id}}"
+router.get('/new', (req,res,next) => {
+  res.render('celebrities/new');
+});
+
+router.post('/new', (req,res,next) => {
+  const {name,occupation,catchPhrase} = req.body;
+  const celebrity = new Celebrity({name,occupation,catchPhrase});
+  celebrity.save()
+    .then((celebrity) => res.redirect('/celebrities')
+    .catch( (err) => {
+      console.log(err);
+      res.redirect('celebrities/new');
+    }))});
+
+
+
 router.get('/:celebrityId', (req,res,next) => {
   const {celebrityId} = req.params;
   console.log(celebrityId);
