99bf9276afc2c522475709f19045803c80b88ade
Johann <johanson88@gmail.com>
2019-11-18 21:57:34 +0100

    Iteration 9

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -3,6 +3,13 @@ const router = express.Router();
 
 const Movie = require('./../models/Movie');
 
+router.get('/:movieId', (req, res, next) => {
+  const {movieId} = req.params;
+  Movie.findById(movieId)
+    .then((oneMovie) => res.render('movies/show',{oneMovie}))
+    .catch((err) => console.error(err));
+})
+
 router.get('/', (req,res,next) => {
   Movie.find()
     .then( (allMoviesFromDb) => res.render('movies',{allMoviesFromDb}))
