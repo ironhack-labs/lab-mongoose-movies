2cafc6fe6a986313fd57b653bf4a7bd31437f3e3
caprosset <capucinerosset@gmail.com>
2019-11-18 18:40:40 +0100

    Iteration 5

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -5,6 +5,25 @@ const Celebrity = require('./../models/Celebrity');
 
 
 
+// POST /celebrities/:id/delete
+router.post('/:id/delete', (req, res, next) => {
+  console.log(req.params);
+  const celebrityId = req.params.id;
+
+  Celebrity.findByIdAndRemove(celebrityId)
+    .then( () => {
+      return Celebrity.find();
+    })
+    .then( (allCelebritiesFromDB) => { 
+        res.render('celebrities/index', { allCelebritiesFromDB }); 
+    })
+    .catch( (err) => {
+      console.log(err);
+      next();
+    });
+})
+
+
 // GET /celebrities/new - renders the form
 router.get('/new', (req, res, next) => {
   console.log('HELLO IM IN THE FORM');
@@ -12,8 +31,7 @@ router.get('/new', (req, res, next) => {
   res.render('celebrities/new');
 })
 
-
-// POST /celebrities - handles the inputing data
+// POST /celebrities - handles the inputing data to create new celebrity
 router.post('/',  (req, res, next) => {
   console.log(req.body); 
   const { name, occupation, catchPhrase } = req.body; // destructuring
@@ -53,6 +71,8 @@ router.get('/:id', (req, res, next) => {
     })
     .catch( (err) => console.log(err));
 });
+
+
 
 
 module.exports = router;
