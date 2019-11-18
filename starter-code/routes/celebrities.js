211450ec8af867c7a47235cc6f9e67b95f5c4607
caprosset <capucinerosset@gmail.com>
2019-11-18 18:22:40 +0100

    Iteration 4

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -3,6 +3,33 @@ const router = express.Router();
 
 const Celebrity = require('./../models/Celebrity');
 
+
+
+// GET /celebrities/new - renders the form
+router.get('/new', (req, res, next) => {
+  console.log('HELLO IM IN THE FORM');
+  
+  res.render('celebrities/new');
+})
+
+
+// POST /celebrities - handles the inputing data
+router.post('/',  (req, res, next) => {
+  console.log(req.body); 
+  const { name, occupation, catchPhrase } = req.body; // destructuring
+
+  Celebrity.create( { name, occupation, catchPhrase } )
+    .then( celebrity => { 
+      res.redirect('/celebrities'); 
+    })
+    .catch( (err) => {
+      console.log(err);
+      res.render('celebrities/new');
+    });
+})
+
+
+
 // GET /celebrities
 router.get('/', (req, res, next) => {
   Celebrity.find()
@@ -15,10 +42,13 @@ router.get('/', (req, res, next) => {
 
 // GET /celebrities/id
 router.get('/:id', (req, res, next) => {
-  const { celebrityId } = req.params;
+  console.log(req.params);
+  const celebrityId = req.params.id;
 
-  Celebrity.findOne(celebrityId)
+  Celebrity.findById(celebrityId)
     .then( (celebrityDetails) => {
+      console.log('CELEBRITY DETAILS', celebrityDetails);
+      
       res.render('celebrities/show', { celebrityDetails });
     })
     .catch( (err) => console.log(err));
