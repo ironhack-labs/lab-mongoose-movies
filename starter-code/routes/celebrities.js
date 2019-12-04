95a4fc3586efc0e8b23b51cb4f4e1bcaf98e3dcb
mendosaurus <masaki.endo@nex.nikkei.com>
2019-12-04 15:54:29 -0500

    iiteration 3

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -4,7 +4,11 @@ const Celebrity = require("../models/Celebrity");
 
 // Locate the /celebrities GET route in routes/celebrities.js.
 router.get("/", (req, res, next) => {
+  // In the route callback:
+  // Call the Celebrity model's find method to retrieve all the celebrities.
   Celebrity.find()
+    // If there isn't an error, render the celebrities/index view.
+    // Pass the array of celebrities into the view as a variable.
     .then(celebrities => {
       res.render("celebrities", { celebrities });
     })
@@ -13,9 +17,17 @@ router.get("/", (req, res, next) => {
     });
 });
 
-module.exports = router;
-// In the route callback:
-// Call the Celebrity model's find method to retrieve all the celebrities.
-// If there isn't an error, render the celebrities/index view.
-// Pass the array of celebrities into the view as a variable.
+router.get("/:id", (req, res, next) => {
+  // Call the Celebrity model's findOne or findById method to retrieve the details of a specific celebrity by its id.
+  Celebrity.findById(req.params.id)
+    .then(celebrity => {
+      console.log(celebrity);
+    //   res.render("the file",{data which is passed to the file})
+      res.render("celebrities/show", { celebrity });
+    })
+    .catch(error => {
+      next(error);
+    });
+});
 
+module.exports = router;
