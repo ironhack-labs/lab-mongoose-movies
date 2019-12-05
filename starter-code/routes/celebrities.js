5bcdcaaf95979eaaa52f46d2d2b54034116d38a0
Julian Phillips <julian.p@me.com>
2019-12-04 20:53:11 -0500

    feat: Iteration 3: Complete.

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -2,14 +2,30 @@ const express = require("express");
 const router = express.Router();
 const Celebrity = require("../Models/Celebrity");
 
-/* GET home page */
+// Base Celebrity route
 router.get("/", (req, res, next) => {
-  // Display all of the celebrities
+  // Search for all of the celebrities
   Celebrity.find({})
     .then(data => {
+      // With that data, render the index page
       res.render("celebrities/index", { celebs: data });
     })
     .catch(err => {
+      //  If an error is found, render the error page
+      next.render("error", { error: err });
+    });
+});
+
+// Celeb Details
+router.get("/:id", (req, res, next) => {
+  // Search for all of the celebrities
+  Celebrity.findById(req.params.id)
+    .then(data => {
+      // With that data, render the index page
+      res.render("celebrities/show", { celeb: data });
+    })
+    .catch(err => {
+      //  If an error is found, render the error page
       next.render("error", { error: err });
     });
 });
