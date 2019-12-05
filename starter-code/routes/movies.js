09e091aa161c3dd4d69521bda28920d586fce386
mendosaurus <masaki.endo@nex.nikkei.com>
2019-12-04 22:30:10 -0500

    make add new movie feature

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -13,6 +13,30 @@ router.get("/", (req, res, next) => {
     });
 });
 
+// render page with the form on it
+router.get("/new", (req, res, next) => {
+  res.render("movies/new");
+});
+
+// send the data to after the form is filled out.
+router.post("/", (req, res, next) => {
+  let newTitle = req.body.title;
+  let newGenre = req.body.genre;
+  let newPlot = req.body.plot;
+
+  Movie.create({
+    title: newTitle,
+    genre: newGenre,
+    plot: newPlot
+  })
+    .then(result => {
+      res.redirect("/movies");
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
 // detail page
 router.get("/:id", (req, res, next) => {
   // Call the Movie model's findOne or findById method to retrieve the details of a specific movie by its id.
