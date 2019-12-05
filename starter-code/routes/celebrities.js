d0ecf36c45c50e5b8f45e8474d9f31315897a56a
Julian Phillips <julian.p@me.com>
2019-12-04 22:30:33 -0500

    feat: Iteration 4 - Complete

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -16,6 +16,39 @@ router.get("/", (req, res, next) => {
     });
 });
 
+// Add a new Celeb
+router.get("/new", (req, res, next) => {
+  // Search for all of the celebrities
+  Celebrity.find({})
+    .then(data => {
+      // With that data, render the form for new celebrities
+      res.render("celebrities/new", { celebs: data });
+    })
+    .catch(err => {
+      //  If an error is found, render the error page
+      next.render("error", { error: err });
+    });
+});
+
+// Post to the database
+router.post("/", (req, res, next) => {
+  let name = req.body.name;
+  let occupation = req.body.occupation;
+  let catchPhrase = req.body.catchPhrase;
+
+  Celebrity.create({
+    name: name,
+    occupation: occupation,
+    catchPhrase: catchPhrase
+  })
+    .then(response => {
+      res.redirect("/celebrities");
+    })
+    .catch(err => {
+      next.render("error", { error: err });
+    });
+});
+
 // Celeb Details
 router.get("/:id", (req, res, next) => {
   // Search for all of the celebrities
