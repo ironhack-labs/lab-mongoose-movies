fdb26c5121aa0e769ac7a0a1060701f7173b9daf
Fernando Lima Borrelli <flborrelli@gmail.com>
2020-01-04 17:23:07 -0300

    4th interation done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -9,22 +9,42 @@ router.get('/', (req, res, next) => {
     res.render('celebrities/index', { celebrity })
   })
   .catch(error => {
-    next(error)
+    next()
+    console.log(error)
   })
 });
 
 //GET Celebrities ID
-router.get('/:Id', (req, res, next) => {
-  let { Id } = req.params;
-  Celebrity.findById(Id)
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+  Celebrity.findById(id)
   .then(celebrity => {
     res.render('celebrities/show', celebrity)
   })
   .catch(error => {
-    next(error)
+    next();
+    console.log(error)
   })
-
 })
+
+//GET New Celebrities
+router.get('/new', (req, res, next) => {
+  res.render('celebrities/new');
+});
+
+//POST New Celebrities - insert new data to MongoDB
+router.post('/', (req, res, next) => {
+  let {name, occupation, catchPhrase} = req.body;
+  Celebrity
+  .create({name, occupation, catchPhrase})  
+  .then(newCelebrities => {
+    res.redirect('/celebrities')
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
+
 
 module.exports = router;
 
