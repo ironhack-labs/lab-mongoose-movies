f0bcf43e62877ab8cfd25140e374f726f1e2f73e
Giu Roperto <giu.roperto@gmail.com>
2020-01-05 21:36:36 -0300

    iteration 12 done - FINISHED!

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -5,10 +5,12 @@ const Movie = require('../models/movie');
 
 router.get('/', (req, res, next) => {
   Movie.find()
-  .then(movies => {
-    res.render('../views/movies/index.hbs', { movies });
-  })
-  .catch(err => console.log(err));
+    .then(movies => {
+      res.render('../views/movies/index.hbs', {
+        movies
+      });
+    })
+    .catch(err => console.log(err));
 });
 
 router.get('/new', (req, res, next) => {
@@ -16,25 +18,58 @@ router.get('/new', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-  const { title, genre, plot } = req.body;
-  const newMovie = new Movie({ title, genre, plot });
+  const {
+    title,
+    genre,
+    plot
+  } = req.body;
+  const newMovie = new Movie({
+    title,
+    genre,
+    plot
+  });
   newMovie.save()
-  .then(_ => res.redirect('/movies'))
-  .catch(_ => res.redirect('/movies/new'));
+    .then(_ => res.redirect('/movies'))
+    .catch(_ => res.redirect('/movies/new'));
 });
 
 router.post('/:id/delete', (req, res, next) => {
   Movie.findByIdAndRemove(req.params.id)
+    .then(_ => res.redirect('/movies'))
+    .catch(err => console.log(err));
+});
+
+router.get('/:id/edit', (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then(movie => {
+      res.render('../views/movies/edit.hbs', {
+        movie
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      next();
+    });
+});
+
+router.post('/:id', (req, res, next) => {
+  const { title, genre, plot } = req.body;
+  Movie.findOneAndUpdate({ _id: req.params.id }, { title, genre, plot }, { new: true })
   .then(_ => res.redirect('/movies'))
-  .catch(err => console.log(err));
+  .catch(err => {
+    console.log(err);
+    next();
+  });
 });
 
 router.get('/:id', (req, res, next) => {
   Movie.findById(req.params.id)
-  .then(movie => {
-    res.render('../views/movies/show.hbs', { movie })
-  })
-  .catch(err => console.log(err));
+    .then(movie => {
+      res.render('../views/movies/show.hbs', {
+        movie
+      })
+    })
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
