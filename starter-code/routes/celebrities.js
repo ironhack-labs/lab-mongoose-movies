62e9fc7efa65a7da5a2a6346ccb47b0e3029dc5b
lpolon <lpolon@gmail.com>
2020-01-06 23:12:54 -0300

    iteration 5 ok

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -12,31 +12,41 @@ celebrities.get('/', async (req, res, next) => {
 });
 
 celebrities.get('/new', (req, res, next) => {
-  res.render('../views/celebrities/new.hbs')
-})
+  res.render('../views/celebrities/new.hbs');
+});
 
 celebrities.get('/:id', async (req, res, next) => {
   try {
     const {id} = req.params;
     const foundObjFromId = await Celebrity.findById(id);
-    res.render('../views/celebrities/show.hbs', foundObjFromId)
+    res.render('../views/celebrities/show.hbs', foundObjFromId);
   } catch (err) {
     next(err);
   }
 });
 
 celebrities.post('/', async (req, res, next) => {
-  const bodyObj = {...req.body}
-  const newCelebrityDocument = new Celebrity(bodyObj)
+  const bodyObj = {...req.body};
+  const newCelebrityDocument = new Celebrity(bodyObj);
   try {
     await newCelebrityDocument.save();
     // TODO: pergunta: O code academy sugere que é importante colocar os status de sucesso e etc... tem alguma outra coisa que está cuidando disso?
-    res.status(201).redirect('/')
+    res.status(201).redirect('/');
   } catch (error) {
-    console.log(error)
-    next(error)
+    console.log(error);
+    next(error);
   }
-  
-})
+});
+
+celebrities.post('/:id/delete', async (req, res, next) => {
+  const {id} = req.params;
+  try {
+    await Celebrity.findByIdAndRemove(id);
+    res.redirect('/celebrities');
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
 
 module.exports = celebrities;
