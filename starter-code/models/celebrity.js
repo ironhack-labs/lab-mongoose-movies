e1ed407614483dd96f9ce493a5f292b0356ffb11
J. A. Hernández <joseanher81@gmail.com>
2020-01-28 20:40:00 +0100

    Celebrity schema

---
+++ starter-code/models/celebrity.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const celebritySchema = new Schema({
+  name: String,
+  occupation: String,
+  catchPhrease: String
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Celebrity = mongoose.model("Celebrity", celebritySchema);
+module.exports = Celebrity;
+
