6611d5964eae99ef0ad876cc4f25ff5b95767fba
Miguel <alonsogalalrdomiguel@gmail.com>
2020-01-29 18:27:07 +0100

    iteration1 done

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,37 +1,23 @@
-require('dotenv').config()
-const Celebrity = require ("../models/Celebrity")
-const mongoose = require('mongoose');
-const dbUrl = process.env.DBURL;
-//const withDbConnection = require("../withDbConnection")
+const celebrity = require("../models/Celebrity");
+const withDbConnection = require("../withDbConnection");
 
-//const dbName = "starter-code";
-//mongoose.connect(`mongodb://localhost/${dbName}`,{useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connect(dbUrl);
-
-const celebrities = [
+withDbConnection(async () => {
+  //await celebrity.collection.drop();
+  await celebrity.create([
     {
-        name: "Dani Rovira", occupation: "Comedian", catchPhrase:"Er taperwé"
+      name: "Dani Rovira",
+      occupation: "Comedian",
+      catchPhrase: "Er taperwé"
     },
     {
-        name: "Heath Ledger", occupation: "Actor", catchPhrase:"Why so serious"
+      name: "Heath Ledger",
+      occupation: "Actor",
+      catchPhrase: "Why so serious"
     },
     {
-        name: "Chadwick Boseman", occupation: "Actor", catchPhrase:"Wakanda forever"
+      name: "Chadwick Boseman",
+      occupation: "Actor",
+      catchPhrase: "Wakanda forever"
     }
-]
-async function seedDb() {
-    try {
-      await Celebrity.collection.drop(); 
-      let celeb = await Celebrity.create(celebrities); 
-      console.log(`SUCCESS Adding celebs to DB! ${celeb}`);
-    } catch (error) {
-      console.log(`ERROR ${error}`);
-    }
-    mongoose.connection.close();
-};
-  
-seedDb();
-// withDbConnection(async() => {
-//     await Celebrity.collection.drop();
-//     await Celebrity.create(celebrities);
-// })
+  ]);
+});
