572033c7b3d7d0c55d42379d8a1fd4d67aae1eac
Ana Segarra <anasegarraber@gmail.com>
2020-01-30 00:39:54 +0100

    Fix deprecation warning

--- starter-code/app.js
+++ starter-code/app.js
@@ -10,7 +10,7 @@ const logger = require('morgan');
 const path = require('path');
 
 mongoose
-	.connect(process.env.DBURL, { useNewUrlParser: true })
+	.connect(process.env.DBURL, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(x => {
 		console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
 	})
