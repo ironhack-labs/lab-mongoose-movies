fd7739dda0001a2b0b442f8af1465509893019a4
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-01-30 23:39:31 +0100

    Create an add page for adding new celebrities and update the layout style

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -24,4 +24,17 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
+router.get("/new", async (req, res, next) => {
+  res.render("celebrities/new");
+});
+
+router.post("/new", async (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
+  const obj = await Celebrity.create({
+    name,
+    occupation,
+    catchPhrase
+  });
+  res.redirect("/celebrities/new");
+});
 module.exports = router;
