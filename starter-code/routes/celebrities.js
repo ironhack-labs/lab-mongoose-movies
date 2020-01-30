0111b344bdab3175917206922b79ff94407afc2d
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-01-30 13:01:59 +0100

    Create a single page for every celebrity

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -2,12 +2,24 @@ const express = require("express");
 const router = express.Router();
 const Celebrity = require("../models/Celebrity");
 
+// Retrieve -> Get a list of all celebrities
 router.get("/", async (req, res, next) => {
   try {
     const celebrities = await Celebrity.find();
     res.render("celebrities/index", { celebrities });
   } catch (e) {
     //check if this works
+    next();
+  }
+});
+
+// Retrieve -> Get a specific celebrity and show a separate page
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const celebrity = await Celebrity.findById(id);
+    res.render("celebrities/show", { celebrity });
+  } catch (e) {
     next();
   }
 });
