2205747a6f8c52c8b18ae3b375ebe6f6c47361fb
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-01-30 12:04:05 +0100

    Update error catch on celebrity route

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const Celebrity = require("../models/Celebrity");
 
-router.get("/", async (req, res) => {
+router.get("/", async (req, res, next) => {
   try {
     const celebrities = await Celebrity.find();
     res.render("celebrities/index", { celebrities });
   } catch (e) {
-    res.render("error", { e });
+    //check if this works
+    next();
   }
 });
 
