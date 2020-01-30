80eae4c0b0ce0953d1c87b44936f579ca310c787
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2020-01-30 21:43:14 +0100

    V2. Iteration 1 Completed

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,20 +1,13 @@
-const withDbConnection = require("../withDbConnection");
+
+const { withDbConnection, dropIfExists } = require("../withDbConnection");
 const Celebrity = require("../models/celebrity");
 
-// withDbConnection(async () => {
-//   await CelebrityFraseTa.collection.drop();
-//   await Celebrity.create([
-//     { name: "Tom Cruise", occupation: "Actor", catchPhrase: "Mission Impossible" },
-//     { name: "Johnny Cash", occupation: "Singer", catchPhrase: "I walk the line" },
 
-//     { ame: "Michael Phelps", occupation: "Swimmer", catchPhrase: "Just do it" }
-//   ]);
-// });
 
 
 withDbConnection(async () => {
   await dropIfExists(Celebrity);
-  await FraseTa.create([
+  await Celebrity.create([
     { name: "Tom Cruise", occupation: "Actor", catchPhrase: "Mission Impossible" },
     { name: "Johnny Cash", occupation: "Singer", catchPhrase: "I walk the line" },
     { name: "Michael Phelps", occupation: "Swimmer", catchPhrase: "Just do it" }
