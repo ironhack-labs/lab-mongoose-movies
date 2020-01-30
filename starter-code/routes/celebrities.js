835f1c7d07cc955afe0bfc4171c81fd72955b2e1
Román Méndez <mendezbeck@gmail.com>
2020-01-30 22:16:34 +0100

    iteration 2 done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -4,7 +4,10 @@ const Celebrity = require("../models/celebrity")
 
 router.get("/", async (req, res) => {
   const celebrities = await Celebrity.find();
+  console.log(celebrities)
   res.render("celebrities/index", {
     celebrities
   })
 })
+
+module.exports = router;
