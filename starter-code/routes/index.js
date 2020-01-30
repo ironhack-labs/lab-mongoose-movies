748ae809d789e71ab6828f94feb2004252c693f6
Fran <fmjerez@icloud.com>
2020-01-30 13:10:09 +0100

    Iteration 3 Done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,22 +1,12 @@
 const express = require('express');
 const router  = express.Router();
 
-const Celebrity = require('../models/celebrity')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-router.get('/celebrities', (req, res, next) => {
-  Celebrity.find()
-    .then(allTheCelebritiesFromDB => {
-      res.render('celebrities', { celebrities : allTheCelebritiesFromDB });
-    })
-    .catch(error => {
-      console.log('Error while getting the celebrities from de DB: ', error)
-    })
 
-});
 
 module.exports = router;
