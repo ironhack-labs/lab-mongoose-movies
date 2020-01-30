7abeae6ff2dc0f79b17fb6d6e09e05bff65dc45d
Miguel <alonsogalalrdomiguel@gmail.com>
2020-01-31 00:15:54 +0100

    feature4 complete

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -2,23 +2,44 @@ const Celebrity = require("../models/Celebrity");
 const express = require("express");
 const router = express.Router();
 
+//list of celebrities
 router.get("/", async (req, res, next) => {
   try {
     const seed = await Celebrity.find();
     res.render("celebrities/index", { seed });
   } catch (error) {
+    console.log(error);
     //check if this works
     next();
   }
 });
 
+// see one celebrity
 router.get("/:id", async (req, res, next) => {
   const { id } = req.params;
   try {
     const celebrity = await Celebrity.findById(id);
     res.render("celebrities/show", { celebrity });
   } catch (error) {
+    console.log(error);
     next();
+  }
+});
+
+//create a new celebrity
+router.get("/new", (req, res, next) => {
+  res.render("celebrities/new");
+});
+
+//create the celebrity and send to the database
+router.post("/new", async (req, res, next) => {
+  const { name, celebrity, phrase } = req.body;
+  try {
+    await Celebrity.save(name, celebrity, phrase);
+    res.redirect("celebrities/");
+  } catch (error) {
+    console.log(error);
+    res.render("celebrities/new");
   }
 });
 
