edc87034ea816cb8a3ca00b6e9989844082f4fb4
Ruben Vaquero <rvaquero@elpublicamaleon.com>
2020-01-30 22:16:39 +0100

    iteration 3 finish

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -2,11 +2,24 @@ const express = require("express");
 const router = express.Router();
 const Celebrity = require("../models/Celebrity");
 
-// CRUD -> (R) Retrieve
+// Show the list celebrity in celebrity/index
 router.get("/", async (req, res, next) => {
   try {
     const celebrity = await Celebrity.find();
     res.render("celebrities/index", { celebrity });
+  } catch (err) {
+    res.send(`error: ${err}`);
+    next();
+  }
+});
+
+// Show the list celebrity ID in celebrity/show
+router.get("/show/:id", async (req, res, next) => {
+  try {
+    //const id  = req.params.id;
+    const { id } = req.params;
+    const foundObjFromId = await Celebrity.findById(id);
+    res.render("celebrities/show", { foundObjFromId });
   } catch (err) {
     res.send(`error: ${err}`);
     next();
