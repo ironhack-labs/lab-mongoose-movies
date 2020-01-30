f8b55bb2b03c8d08945d585f2f45bc07de5fd9e7
Fran <fmjerez@icloud.com>
2020-01-30 20:25:50 +0100

    Iteration 4 Done

--- starter-code/models/celebrity.js
+++ starter-code/models/celebrity.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema
 const celebritySchema = new Schema(
   {
+    //_id: objectId,
     name: String,
     occupation: { type: String, default: 'unknown' },
     catchPhrase: String
