0ef33f8d0b1eacf567592ea50a2b7ce99f453261
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2020-01-30 17:55:52 -0500

    Iteration #3: The Celebrity Details Page

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -2,22 +2,26 @@ const mongoose = require('mongoose');
 const Celebrity = require('../models/Celebrity.model');
 
 require('../config/db.config');
+Celebrity.collection.drop();
 
 const celebrities = [
   {
     name: 'Ava DuVernay',
-    occupation: 'Director',
-    catchPhrase: 'A Wrinkle in Time',
+    occupation: 'Actor',
+    catchPhrase:
+      'Ava debuted with Premam alongside Nivin Pauly, which was a commercial success. She then had a cameo in James & Alice, a Malayalam movie. Later she forayed into Telugu films with a handful of projects including A Aa, where she played a lead role along with Nithiin and Samantha Ruth Prabhu.',
   },
   {
     name: 'Johannes Roberts',
-    occupation: 'Enginier',
-    catchPhrase: 'The Strangers: Prey at Night',
+    occupation: 'Sport',
+    catchPhrase:
+      'Johannes Roberts was born on 11 October 1993 in Surat, Gujarat. His father, Himanshu Pandya, ran a small car finance business in Surat which he shut down and shifted to Vadodara when Johannes was five; he did so in order to facilitate his sons with better cricket training facilities',
   },
   {
-    name: 'Nash Edgerton',
-    occupation: 'Developer',
-    catchPhrase: 'The Leisure Seeker',
+    name: 'Anthony Anderson',
+    occupation: 'Actor',
+    catchPhrase:
+      'Anthony Anderson (born August 15, 1970)[1][2] is an American comedian, actor, writer, and game show host. He has starred in his own short-lived sitcom, All About the Andersons, as well as the ABC sitcom Black-ish and the Fox sitcom The Bernie Mac Show during its fifth and final season.',
   },
 ];
 
--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -2,11 +2,22 @@ const express = require('express');
 const router = express.Router();
 const Celebrity = require('../models/Celebrity.model');
 
+//1.
 router.get('/celebrities', (req, res, next) => {
   Celebrity.find()
     .then(celebritiesFromDB => {
       console.log('celebritiesFromDB: ', celebritiesFromDB);
       res.render('celebrities/index', { celebrities: celebritiesFromDB });
+    })
+    .catch(err => next(err));
+});
+
+//2.
+router.get('/celebrities/:theId', (req, res, next) => {
+  Celebrity.findById(req.params.theId)
+    .then(celebrityFromDB => {
+      console.log('celebritiesFromDB: ', celebrityFromDB);
+      res.render('celebrities/show', { celebrity: celebrityFromDB });
     })
     .catch(err => next(err));
 });
