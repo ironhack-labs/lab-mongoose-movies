27545b0f16a5ab81366796a75b63c5eb2d02e5bb
pelinski <teresapelinski@gmail.com>
2020-01-31 16:52:51 +0100

    removed callback for proper use of promise

--- starter-code/bin/seed.js
+++ starter-code/bin/seed.js
@@ -4,36 +4,28 @@ const celebrity = require("../models/celebrity");
 
 withDbConnection(async () => {
   await dropIfExists(celebrity);
-  await celebrity.create(
-    [
-      {
-        name: "Bad Gyal",
-        occupation: "Singer",
-        catchPhrase: "El pussy k mana."
-      },
-      {
-        name: "La Zowi",
-        occupation: "Singer",
-        catchPhrase:
-          "Lo que hago es música punk y la música punk no se explica."
-      },
-      {
-        name: "DJ Florentino",
-        occupation: "Producer",
-        catchPhrase: "El más romántico de los románticos."
-      },
-      {
-        name: "Cecilio G.",
-        occupation: "Singer",
-        catchPhrase: "Yo soy de la calle, yo no soy Bad Gyal."
-      }
-    ],
-    err => {
-      console.log("Database seeded");
-      if (err != null) {
-        console.log(err);
-      }
-      mongoose.disconnect();
+  await celebrity.create([
+    {
+      name: "Bad Gyal",
+      occupation: "Singer",
+      catchPhrase: "El pussy k mana."
+    },
+    {
+      name: "La Zowi",
+      occupation: "Singer",
+      catchPhrase: "Lo que hago es música punk y la música punk no se explica."
+    },
+    {
+      name: "DJ Florentino",
+      occupation: "Producer",
+      catchPhrase: "El más romántico de los románticos."
+    },
+    {
+      name: "Cecilio G.",
+      occupation: "Singer",
+      catchPhrase: "Yo soy de la calle, yo no soy Bad Gyal."
     }
-  );
+  ]);
 });
+
+//it1 : check the database with mongo to confirm that your data was saved
