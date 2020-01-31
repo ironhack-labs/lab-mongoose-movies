1a739908e63e2583b7403f8a6d93f2b1f26cc69e
mario010203 <mariojose1490@hotmail.com>
2020-02-01 00:01:20 +0100

    half iteration 1 done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router  = express.Router();
+const celebrity = require("../models/celebrity");
+
+
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/celebrities", async (req, res) => {
+  const celeb = await celebrity.find();
+  res.render("../views/test.hbs");
 });
 
 module.exports = router;
