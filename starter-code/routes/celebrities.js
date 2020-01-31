a19c448e7a018fc56da2aace8909c3566f6f91a2
carballo <carballet@gmail.com>
2020-01-31 19:07:32 +0100

    Iteracion 4 Terminada

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -14,18 +14,40 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+/* Página de crear las Celebrities */
+
+router.get("/new", (req, res, next) => {
+    res.render("celebrities/new"); 
+});
+
+/* Envio del formulario de create a la bbdd */
+
+router.post("/new", async (req, res, next) => {
+  console.log(req.body);
+  const { name, occupation, catchPhrase } = req.body;
+  const obj = await Celebrity.create({
+    name,
+    occupation,
+    catchPhrase
+  });
+  console.log(obj);
+  return res.redirect("/celebrities");
+});
+
 /* Según la celebrity por ID que quiera ver devuelvo todos sus datos */
 
 router.get("/:id", async (req, res, next) => {
 const { id } = req.params;
 try {
   const celebridad = await Celebrity.findById(id);
-  res.render("celebrities/show", { celebridad }); // Esta línea es la que le pasa cosas al .hbs
+  res.render("celebrities/show", { celebridad }); // Esta línea es la que le pasa cosas al show.hbs
 } catch (err) {
   res.send(`Error al mostrar una celebrity: ${err}`);
   next();
 }
 });
+
+
 
 
 /* Exporto un objeto "router" con todas las celebrities */
