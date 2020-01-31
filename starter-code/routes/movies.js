ee7eb0c22490300c500ee79a80fd586906ebff3e
alberto gomez marin <albertogomezmarin@albertos-MacBook-Air.local>
2020-02-01 00:15:31 +0100

    iteration10 done

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -13,6 +13,26 @@ router.get("/", async (req, res, next) => {
   }
   next();
 });
+
+//GET ADD NEW
+router.get("/new", (req, res, next) => {
+  res.render("movies/new");
+});
+
+//post add new
+router.post("/new", async (req, res, next) => {
+  try {
+    const { title, genre, plot } = req.body;
+    const obj = await Movies.create({ title, genre, plot });
+    console.log(`Added new movie ${obj}`);
+    return res.redirect("/movies");
+  } catch (error) {
+    console.log(` Error adding new movie A ${error}`);
+    res.render("movies/new");
+  }
+});
+
+//get show view details
 router.get("/:id", async (req, res, next) => {
   try {
     const { id } = req.params; //request del parametro
