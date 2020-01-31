8980c3e0255428691f49ad58a696fd095e1c9aed
N10T <andy.tisba@gmail.com>
2020-01-31 18:09:24 +0100

    Iteration 3 \ done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -15,13 +15,34 @@ router.get("/all", (req, res) => {
     });
 });
 
-router.get("/:id", (req, res) => {
-  celebrityModel
-    .findById(req.params.id)
-    .then(celebrity => {
-      res.render("celebrities/show", { celebrity });
-    })
-    .catch(dbErr => console.error("OH no, db err :", dbErr));
-});
-
-module.exports = router;
+router.get("/new", (req, res) => {
+    res.render("celebrities/new");
+  });
+  
+  router.post("/new", (req, res) => {
+          const { name, occupation, catchPhrase } = req.body;
+          // if some tests must be performed, do so before database query
+          celebrityModel
+      .create({
+            name,
+        occupation,
+        catchPhrase,
+      })
+      .then(dbRes => res.redirect("/celebrities/all"))
+      .catch(dbErr => {
+            console.log(dbErr);
+            res.render("celebrities/new");
+          });
+      });
+  
+    router.get("/:id", (req, res) => {
+      celebrityModel
+        .findById(req.params.id)
+        .then(celebrity => {
+          res.render("celebrities/show", {celebrity});
+        })
+        .catch(dbErr => console.error("OH no, db err :", dbErr));
+    });
+    
+    
+    module.exports = router;
