5e039122102b96f18a404d254c988a68f4bca6b5
Alex Meyer <alexandre.meyer78@gmail.com>
2020-01-31 15:21:11 +0100

    Link a celebrity to a Movie

--- starter-code/models/Movie.js
+++ starter-code/models/Movie.js
@@ -3,9 +3,10 @@ const schema = new mongoose.Schema({
   title: String,
   director: String,
   stars: [String],
-  image: String,
+  image: { type : String, default : 'https://loremflickr.com/cache/resized/65535_48690418248_9d334f129b_182_268_nofilter.jpg' },
   description: String,
-  showtimes: [String]
+  showtimes: [String],
+  celebrities : [{ type: mongoose.Schema.Types.ObjectId, ref: 'Celebrity' }],
 });
 
 const Movie = mongoose.model("Movie", schema);
--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router  = express.Router();
 
 const Movie = require('./../models/Movie.js');
+const Celebrity = require('./../models/Celebrity.js');
 
 /* /movies/index page */
 router.get('/index', (req, res, next) => {
@@ -27,16 +28,35 @@ router.get('/:id', (req, res, next) => {
 
 /* /movies/new page */
 router.get('/new', (req, res, next) => {
-  res.render('movies/new');
+  Celebrity.find()
+    .then(celebrities => {
+      res.render('movies/new', {celebrities});
+    })
+    .catch(error => {
+      next();
+    })
+
 });
 
 /* /movies/new POST data from form */
 router.post('/new', async (req, res, next) => {
   try {
+    
     const newMovie = {
       title : req.body.title,
       director : req.body.director,
-      description : req.body.description,
+      description : req.body.description
+    }
+
+    // If there is a celebrity tied to the movie
+    if ( req.body.celebrity !== "") {
+      const celeb = await Celebrity.findOne({_id:req.body.celebrity})
+
+      // Add its id to the celebrities list
+      newMovie.celebrities = [celeb._id]
+
+      // Add its name to the stars
+      newMovie.stars = [celeb.name]
     }
 
     const newMovieRecord = new Movie(newMovie);
