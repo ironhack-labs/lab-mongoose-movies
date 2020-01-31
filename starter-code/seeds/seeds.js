25a640e17f9b3b90670f216303cc8a5940134f68
Ivan Esteban <ivan.estebanisabel@gmail.com>
2020-01-31 17:51:45 +0100

    develop

--- starter-code/seeds/seeds.js
+++ starter-code/seeds/seeds.js
@@ -4,8 +4,8 @@ const celebrity = require("../models/celebrity");
 withDbConnection(async () => {
   await celebrity.collection.drop();
   await celebrity.create([
-    { name: "asd", occupation: "asd", catchPhrase: "asd" },
-    { name: "asd", occupation: "asd", catchPhrase: "asd" },
-    { name: "asd", occupation: "asd", catchPhrase: "asd" },
+    { name: "Chiquito", occupation: "humorista", catchPhrase: "Fistro pecadorr" },
+    { name: "Iker Jimenez", occupation: "presentador", catchPhrase: "Bienvenido a la nave del misterio" },
+    { name: "Andres Montes", occupation: "narrador", catchPhrase: "Ratatatatata" },
   ]);
 });
