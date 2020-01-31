aae5977ea48b93b57c0f5bf7d54339620204ac95
mario010203 <mariojose1490@hotmail.com>
2020-01-31 23:29:19 +0100

    first iteration done

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,7 +1,15 @@
-const celebrity = require("../models/celebrity.js");
+const { withDbConnection, dropIfExists } = require("../withDbConnection");
+const celebrity = require("../models/celebrity");
 
-celebrity.create([
-    { name: "Tom Cruise", occupation: "actor", catchPhrase:"Show me the money" },
-    { name: "Beyonce", occupation: "singer", catchPhrase:"Single lady" },
-    { name: "Daffy Duck", occupation: "comedian", catchPhrase:"cuac cuac" },
+withDbConnection(async () => {
+  await dropIfExists(celebrity);
+  await celebrity.create([
+    {
+      name: "Tom Cruise",
+      occupation: "actor",
+      catchPhrase: "Show me the money"
+    },
+    { name: "Beyonce", occupation: "singer", catchPhrase: "Single lady" },
+    { name: "Daffy Duck", occupation: "comedian", catchPhrase: "cuac cuac" }
   ]);
+});
--- starter-code/models/celebrity.js
+++ starter-code/models/celebrity.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema(
-    {
-      name: String,
-      ocuppation: String,
-      catchPhrase: { type: Boolean, default: false }
-    },
-    {
-      timestamps: true
-    }
-  );
-  module.exports = mongoose.model("celebrity", schema);
-  
+  {
+    name: { type: String },
+    ocuppation: { type: String },
+    catchPhrase: { type: String }
+  },
+  {
+    timestamps: true
+  }
+);
+module.exports = mongoose.model("celebrity", schema);
