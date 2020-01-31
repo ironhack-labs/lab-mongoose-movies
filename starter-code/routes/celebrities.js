71a3b97d9b8c1fa2eb7accbb730c57c904fb76ed
Fran <fmjerez@icloud.com>
2020-01-31 09:56:14 +0100

    Iteration 6 done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -57,6 +57,47 @@ router.post("/", (req, res, next) => {
 
 })
 
+
+
+
+router.get("/:id/edit", (res, req, next) =>{
+  Celebrity.findById(req.params.id)
+  .then(theCelebrity => {
+    res.render('celebrities/edit',  theCelebrity)
+  })
+  .catch((error) => {
+    console.log(error)
+    next()
+  })
+})
+
+
+
+router.post('/:id/edit', (req, res, next) => {
+  Celebrity.findOne({ _id: req.body.id })
+  .then((celebrity) => {
+    res.render('celebrities/edit', celebrity)
+  })
+  .catch(() => {
+    next()
+  })
+});	
+
+router.post('/:id', (req, res, next) => {
+  Celebrity.updateOne(
+    {_id: req.body.id},
+    {
+        name: req.body.name,
+        occupation: req.body.occupation,
+        catchPhrase: req.body.catchPhrase
+      }
+    )
+    .then(()=>{
+      res.redirect('/celebrities')
+      console.log("actualizado")
+    })
+
+})
 router.post("/:id/delete", (req, res, next) => {
   console.log(req.body);
   Celebrity.findByIdAndDelete(req.body.id)
@@ -69,66 +110,6 @@ router.post("/:id/delete", (req, res, next) => {
     })
 
 })
-
-
-/*router.get("/:id/edit", (res, req, next) =>{
-  Celebrity.findById(req.params.id)
-  .then(theCelebrity => {
-    res.render('celebrities/edit',  theCelebrity)
-  })
-  .catch((error) => {
-    console.log(error)
-    next()
-  })
-})*/
-
-router.post('/:id/edit', (req, res, next) => {
-  Celebrity.findOne({ _id: req.body.id })
-    .then((celebrity) => {
-      res.render('celebrities/edit', celebrity)
-    })
-    .catch((error) => {
-      console.log(error)
-      next()
-    })
-  })
-
-router.post('/:id/edit', (req, res, next) => {
-  celebrities.updateOne(
-    {_id: req.body.id},
-    {
-        name: req.body.name,
-        occupation: req.body.occupation,
-        catchPhrase: req.body.catchPhrase
-      }
-    )
-    .then(()=>{
-      res.redirect('celebrities')
-    })
-    .catch((error) => {
-      console.log(error)
-      next()
-    })
-
-})
-
-/*router.post("/edit/", (req, res, next) => {
-  console.log(req.body);
-  Celebrity.updateOne({ _id: req.body._id }, 
-    { name : req.body.name,
-      occupation: req.body.occupation,
-      catchPhrase: req.body.catchPhrase,
-    })
-    .then((celebrity) => {
-      console.log(celebrity)
-      res.render('celebrities/new')
-    })
-    .catch((error) => {
-      console.log(error)
-      next()
-    })*/
-
-
 
 
 
