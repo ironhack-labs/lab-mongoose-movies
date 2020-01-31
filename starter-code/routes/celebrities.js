033b1d36f44453fc0a25b694b07f2b4f5df61352
Miguel <alonsogalalrdomiguel@gmail.com>
2020-01-31 20:17:50 +0100

    feature 6 Done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -14,18 +14,6 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-// see one celebrity
-router.get("/:id", async (req, res, next) => {
-  const { id } = req.params;
-  try {
-    const celebrity = await Celebrity.findById(id);
-    res.render("celebrities/show", { celebrity });
-  } catch (error) {
-    console.log(error);
-    next();
-  }
-});
-
 //create a new celebrity
 router.get("/new", async (req, res, next) => {
   res.render("celebrities/new");
@@ -43,11 +31,48 @@ router.post("/new", async (req, res, next) => {
 });
 
 //delete the celebrity
-
 router.get("/delete/:id", async (req, res, next) => {
   const { id } = req.params;
   const obj = await Celebrity.findByIdAndRemove(id);
   res.redirect("/celebrities");
+});
+
+//update the celebrity
+router.get("/edit/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const seed = await Celebrity.findById(id);
+    return res.render("celebrities/edit", { seed });
+  } catch (error) {
+    next();
+  }
+});
+
+router.post("/edit/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const { name, occupation, catchPhrase } = req.body;
+  try {
+    await Celebrity.findByIdAndUpdate(id, {
+      name,
+      occupation,
+      catchPhrase
+    });
+    res.redirect("/celebrities");
+  } catch (error) {
+    console.log(error);
+    next();
+  }
+});
+// see one celebrity
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const celebrity = await Celebrity.findById(id);
+    res.render("celebrities/show", { celebrity });
+  } catch (error) {
+    console.log(error);
+    next();
+  }
 });
 
 module.exports = router;
