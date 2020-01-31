8e688661f4c66583a0f333ab69b03435c3571f32
N10T <andy.tisba@gmail.com>
2020-01-31 17:13:55 +0100

    Iteration 3 - done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -3,16 +3,25 @@ const router = new express.Router();
 const celebrityModel = require("../models/Celebrity");
 
 router.get("/all", (req, res) => {
-    celebrityModel
+  celebrityModel
     .find()
     .then(celebrities => {
-      res.render("./celebrities/index", {
+      res.render("celebrities", {
         celebrities
       });
     })
     .catch(dbErr => {
       console.log("OH NO ! Database error", dbErr);
     });
+});
+
+router.get("/:id", (req, res) => {
+  celebrityModel
+    .findById(req.params.id)
+    .then(celebrity => {
+      res.render("celebrities/show", { celebrity });
+    })
+    .catch(dbErr => console.error("OH no, db err :", dbErr));
 });
 
 module.exports = router;
