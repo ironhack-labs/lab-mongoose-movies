4f76ae5307ee11d6319d224fb87a9c67dd66b96b
Miguel <alonsogalalrdomiguel@gmail.com>
2020-01-31 23:02:16 +0100

    layaout complete

--- starter-code/app.js
+++ starter-code/app.js
@@ -49,7 +49,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // default value for title local
-app.locals.title = "The best celebrity quotes";
+app.locals.title = "Web of celebrities";
 
 const index = require("./routes/index");
 app.use("/", index);
