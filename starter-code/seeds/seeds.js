2ff60d4799ac219de8e81c2188775e67d198d3e4
Grecia Patricia <grecia.busquets@gmail.com>
2020-02-01 10:00:07 +0100

    Changing DataBase data

--- starter-code/seeds/seeds.js
+++ starter-code/seeds/seeds.js
@@ -5,20 +5,20 @@ const Celebrity = require('../models/celebrity.js');
 withDbConnection (async () => {
   await Celebrity.deleteMany();
   await Celebrity.create ( [
-    { name: "Steve Jobs", occupation: "Developer", catchPhrase:"Connecting the dots" },
+    { name: "Steve Jobs", occupation: "Apple Founder", catchPhrase:"Connecting the dots" },
     { name: "Harry Potter", occupation: "Hogwart's Student", catchPhrase:"Expelliarmus" },
     { name: "Marc", occupation: "Teacher", catchPhrase:"Ya lo iréis pillando" }
   ])
 });
 
-const Movie = require('../models/movies.js');
+const Movie = require('../models/movie.js');
 
 withDbConnection (async () => {
   await Movie.deleteMany();
   await Movie.create ( [
-    { title: "Movie1", genre: "Drama", plot:"Connecting the dots" },
-    { title: "Movie2", genre: "Comedy", plot:"Connecting the dots" },
-    { title: "Movie3", genre: "SCiFi", plot:"Connecting the dots" }
+    { title: "Across The Universe", genre: "Drama", plot:"A story about Vietnam War" },
+    { title: "Knives Out", genre: "Comedy", plot:"A story around a murder" },
+    { title: "Harry Potter", genre: "SCiFi", plot:"A kid who discovers the world of magic" }
   ])
 });
 
