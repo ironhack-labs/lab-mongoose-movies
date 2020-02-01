d72e3eeecddac8c666631ee53c2e16741a1ee1ce
Carlos Dominguez <carlos.cfdr86@gmail.com>
2020-02-01 17:47:58 -0600

    Iteration #9: The Movie Details Page

--- starter-code/controllers/movies.controller.js
+++ starter-code/controllers/movies.controller.js
@@ -4,3 +4,9 @@ exports.moviesGet = async (req, res, next) => {
   const movies = await Movie.find().catch( err => next( err ))
   res.render('movies/', {movies})
 }
+
+exports.movieGet= async (req, res, next) => {
+  const movie = await Movie.findById( req.params.id)
+                                   .catch( err=>next( err ) )
+  res.render('movies/show', movie )
+}
--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router  = express.Router();
 const {
-  moviesGet
+  moviesGet,
+  movieGet
 } = require('../controllers/movies.controller')
 
 /* POST / GET movies ROUTES */
 router
   .get('/', moviesGet)
+  .get('/:id', movieGet)
 
 module.exports = router;
