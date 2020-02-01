a9d8260461f1df808191f08545ac411d7f9067e0
N10T <andy.tisba@gmail.com>
2020-02-01 12:13:48 +0100

    Iteration 6 - done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -56,8 +56,26 @@ router.post("/new", (req, res) => {
         });
     });  
 
+    router.get("/:id/edit", (req, res) => {
+        celebrityModel
+          .findById(req.params.id)
+          .then(celebrity => {
+            res.render("celebrities/edit", {celebrity});
+          })
+          .catch(dbErr => console.error("OH no, db err :", dbErr));
+      });
 
-
+    router.post("/:id", (req, res) => {
+        const { name, occupation, catchPhrase } = req.body;
+        // if some tests must be performed, do so before database query
+        celebrityModel
+    .findByIdAndUpdate(req.params.id,{ name, occupation, catchPhrase })
+    .then(dbRes => res.redirect("/celebrities/all"))
+    .catch(dbErr => {
+          console.log(dbErr);
+          res.render("celebrities/edit");
+        });
+    });  
 
     
     module.exports = router;
