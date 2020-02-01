b34a4367cea8ed6328e0db2e3e61b0d71cda9e48
David Garcia <dgarciabravo13@gmail.com>
2020-02-01 16:06:41 +0100

    create date in db movie2

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router  = express.Router();
+const celebrities = require("./celebrities");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
+router.use("/celebrities",celebrities);
 
 module.exports = router;
