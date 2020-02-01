04a17f4ae6a36cb0e0a3d2510719ec2f981d43c9
Román Méndez <mendezbeck@gmail.com>
2020-02-01 20:12:44 +0100

    schema and seeds created for Movie in iteration 7

--- starter-code/models/celebrity.js
+++ starter-code/models/celebrity.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const celebritySchema = new Schema({
-  name: String,
-  occupation: String,
-  catchPhrase: String
-}, {
-  timestamp: true
-});
+const celebritySchema = new Schema(
+  {
+    name: String,
+    occupation: String,
+    catchPhrase: String
+  },
+  {
+    timestamp: true
+  }
+);
 
 const Celebrity = mongoose.model("Celebrity", celebritySchema);
 
