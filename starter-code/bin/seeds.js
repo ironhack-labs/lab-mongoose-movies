4d7597def0fb90668974871f4278fc500526c2f7
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2020-02-01 18:59:22 +0100

    Iteration 2 Completed

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -7,7 +7,7 @@ const Celebrity = require("../models/celebrity");
 
 withDbConnection(async () => {
   await dropIfExists(Celebrity);
-  let celeb = await Celebrity.create([
+  await Celebrity.create([
     { name: "Tom Cruise", occupation: "Actor", catchPhrase: "Mission Impossible" },
     { name: "Johnny Cash", occupation: "Singer", catchPhrase: "I walk the line" },
     { name: "Michael Phelps", occupation: "Swimmer", catchPhrase: "Just do it" }
