25ee52f94e070ce5456c1e40942d353fa72e5161
Carlos Mateo <cmc2k5@gmail.com>
2020-02-01 02:08:36 +0100

    Iteration 4 - completed

--- starter-code/routes/celebritiesRoutes.js
+++ starter-code/routes/celebritiesRoutes.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 const celebrityModel = require('../model/celebrity');
 
 router.get('/', async (req, res, next) => {
     try {
         const celebrities = await celebrityModel.find();
-        res.render("celebrities/index", { celebrities });
+        res.render('celebrities/index', { celebrities });
     } catch (err) {
         console.error(err);
         next();
@@ -15,12 +15,33 @@ router.get('/', async (req, res, next) => {
 router.get('/:id', async (req, res, next) => {
     try {
         const celebrity = await celebrityModel.findById(req.params.id);
-        res.render("celebrities/show", { celebrity });
+        res.render('celebrities/show', { celebrity });
     } catch (err) {
         console.error(err);
         next();
     }
 });
 
+router.get('/new', async (req, res, next) => {
+    try {
+        res.render('celebrities/new');
+    } catch (err) {
+        console.error(err);
+        next();
+    }
+});
+
+router.post('/', async (req, res, next) => {
+    try {
+        const { name, ocuppation, catchPhrase } = req.body;
+        const celebrity = await celebrityModel.create({ name, ocuppation, catchPhrase });
+        celebrity.save();
+        res.redirect('/celebrities');
+    } catch (err) {
+        console.error(err);
+        res.redirect('celebrities/new');
+        next();
+    }
+});
 
 module.exports = router;
