fd9fb011ed9955b1f7e9cf1519c771fde7300f70
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2020-02-01 20:58:41 -0500

    add Iteration #4: Adding New Celebrities

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Celebrity = require('../models/Celebrity.model');
 
-//1.
+//1.Show all celebrities list
 router.get('/celebrities', (req, res, next) => {
   Celebrity.find()
     .then(celebritiesFromDB => {
@@ -12,7 +12,25 @@ router.get('/celebrities', (req, res, next) => {
     .catch(err => next(err));
 });
 
-//2.
+//3.Add new celebrities
+// |Needs to come before /celebrities/:theId, else will never get rendered
+router.get('/celebrities/add', (req, res, next) => {
+  //   console.log(req.body);
+  res.render('celebrities/new');
+});
+//4.Post added celebrity and redirect to /celebrities
+router.post('/celebrities/new', (req, res, next) => {
+  //   const { name, occupation, catchPhrase } = req.body;
+  //   Celebrity.create({ name, occupation, catchPhrase })
+  Celebrity.create(req.body)
+    .then(celebrityFromDB => {
+      console.log('celebritiesFromDB: ', celebrityFromDB);
+      res.redirect('/celebrities', { celebrity: celebrityFromDB });
+    })
+    .catch(err => next(err));
+});
+
+//2. Get celebrity details
 router.get('/celebrities/:theId', (req, res, next) => {
   Celebrity.findById(req.params.theId)
     .then(celebrityFromDB => {
