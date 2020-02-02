0c06f3db0fafe71eff279ab6d9bd59e5a37d62e3
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2020-02-02 12:14:09 +0100

    Doesnt display celebrity characteristics

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Celebrities = require("../models/celebrity");
 
 
-// CRUD -> (R) Retrieve . Iteration 2: Show Our Celebrities
+//Iteration 2: Show Our Celebrities
 router.get("/", async (req, res, next) => {
   try {
     const celebrities = await Celebrities.find();
@@ -13,6 +13,47 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-// CRUD -> (R) Retrieve . Iteration 3: The Celebrity Details Page
+//Iteration 3: The Celebrity Details Page
+
+router.get("/", async (req, res, next) => {
+  try {
+    //const { id } = req.params;
+    const celebrity = await Celebrity.findById(id);
+    res.render("celebrities/show", { celebrity });
+  } catch (error) {
+    console.log(`Error finding celebrity by id ${error}`);
+  }
+});
+
+
+
+// router.get('/celebrities/:celebritiesId', (req, res, next) => {
+//   Book.findById(req.params.bookId)
+//     .then(theCelebrity => {
+//       res.render('celebrities/show', { celebrities: theCelebrity });
+//     })
+//     .catch(error => {
+//       console.log('Error while retrieving celebrity details: ', error);
+//     })
+// });
+
+// router.get("/:id", async (req, res, next) => {
+//   try {
+//     const { id } = req.params;
+//     const celebrity = await Celebrity.findById(id);
+//     res.render("celebrities/show", { celebrity });
+//   } catch (error) {
+//     console.log(`Error finding celebrity by id ${error}`);
+//   }
+// });
+
+// Iteration 3
+
+// router.get('/celebrities/:celebritiesId', (req, res, next) => {
+//   console.log('The ID from the URL is: ', celebritiesId);
+//   res.render('celebrity-details');
+// });
+
+
 
 module.exports = router;
