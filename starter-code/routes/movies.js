01c807e328c1dfb0b49b0e2a312a36e1276d8f9d
Javier Lopez <jlopezro@gmail.com>
2020-02-02 22:04:38 +0100

    Iterations 10 and 11 done

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -24,4 +24,35 @@ router.get("/:id", (req, res, next) => {
     })
   });
 
+router.get("/new", (req, res, next) => {
+    res.render("movies/new"); 
+});
+
+router.post("/", (req, res, next) => {
+  const { title, genre, plot } = req.body;
+  const object = Movie.create ({
+    title,
+    genre,
+    plot,
+  })
+  .then(addMovie => {
+  res.redirect("/movies")
+  })
+  .catch(error => {
+    res.render("/movies/new")
+  })
+  object.save();
+  });
+
+  router.post("/:id/delete", (req, res, next) => {
+    Movie.findByIdAndRemove(req.params.id)
+    .then(moviesDel => {
+      res.redirect("/movies")
+    })
+    .catch(error => {
+      next();
+      console.log(error);
+    })
+  })
+
 module.exports = router;
