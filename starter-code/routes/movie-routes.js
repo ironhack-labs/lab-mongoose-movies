ca1ac0e4bc9dcad7b3f3bc2effb506c517611369
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2020-02-02 00:00:25 -0500

    Iteration #9: The Movie Details Page and Iteration #10: Adding New Movies

--- starter-code/routes/movie-routes.js
+++ starter-code/routes/movie-routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Movie = require('../models/Movie.model');
 
-//Get /movies route
+//1.Get /movies route
 router.get('/movies', (req, res) => {
   Movie.find()
     .then(moviesFromDB => {
@@ -10,6 +10,30 @@ router.get('/movies', (req, res) => {
       res.render('movie-views/movies', { moviesFromDB });
     })
     .catch(err => console.log(`Error while getting movies from DB: ${err}`));
+});
+
+//2.Get /movie-details route
+router.get('/movie-details', (req, res) => {
+  //   console.log(req.query);
+  Movie.findById(req.query.movies_id)
+    .then(gotTheMovie => {
+      res.render('movie-views/movie-details', { gotTheMovie });
+    })
+    .catch(err => console.log(`Error while looking for the req movie: ${err}`));
+});
+
+//3.Get the /add movie route - to add we need to render form and then request
+router.get('/add-movie', (req, res) => {
+  res.render('movie-views/add-movie-form');
+});
+//4.POST the movie - to mongoDB
+router.post('/add-movie', (req, res) => {
+  Movie.create(req.body)
+    .then(savedMovie => {
+      console.log('savedMovie: ', savedMovie);
+      res.redirect(`/movie-details?movies_id=${savedMovie._id}`);
+    })
+    .catch(err => console.log(`Error while trying to save your movie: ${err}`));
 });
 
 module.exports = router;
