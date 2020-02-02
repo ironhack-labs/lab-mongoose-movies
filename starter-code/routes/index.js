a327ad688ee786e2fe82daf0a0d06347166cce30
mario010203 <mariojose1490@hotmail.com>
2020-02-02 21:09:18 +0100

    2nd interation done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,8 +1,6 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 const celebrity = require("../models/celebrity");
-
-
 
 /* GET home page */
 router.get("/celebrities", async (req, res) => {
