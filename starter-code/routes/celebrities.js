b8a081533da6619ebec7b2453884baca303c4441
Víctor Peña Romero <vic.pero@icloud.com>
2020-02-02 20:49:47 -0600

    iteration 3 done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -2,12 +2,20 @@ const router = require('express').Router()
 
 const Celebrity = require('../models/Celebrity')
 
-router.get('/celebrities', async (req, res, next) => {
+router
+  .get('/celebrities', async (req, res, next) => {
 
-  const allCelebrities = await Celebrity.find() /* --- DUDA --- */
-  console.log(allCelebrities)
-  res.render('celebrities/index', {allCelebrities})
+    const allCelebrities = await Celebrity.find() /* --- DUDA --- */
+    // console.log(allCelebrities)
+    res.render('celebrities/index', {allCelebrities})
 
-})
+  })
+  .get('/celebrities/:id', async (req, res, next) => {
+    const {id} = req.params
+    const celebrity = await Celebrity.findById(id)
+    // console.log(celebrity)
+    res.render('celebrities/show', celebrity)
+
+  })
 
 module.exports = router
