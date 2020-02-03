709d32504c8976ff835454b3b035803ab221215f
mario010203 <mariojose1490@hotmail.com>
2020-02-03 17:22:24 +0100

    3rd iteration done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -2,14 +2,16 @@ const express = require('express');
 const router = express.Router();
 const Celebrity = require("../models/celebrity");
 /* Get the Celebrities */
-router.get("/", async (req, res, next) => {
-  try {
-    const celebrity = await Celebrity.find();
-    res.render("celebrities/", { celebrity });
-  } catch (e) {
-    res.send(`error: ${e}`);
-    next();
-  }
+router.get("/", async (req, res) => {
+  const celebrity = await Celebrity.find();
+  res.render("celebrities/index", { celebrity });
+});
+
+router.get("/show/:id", async (req, res) => {
+      const { id } = req.params;
+    const foundObject = await Celebrity.findById(id);
+   return res.render("celebrities/show", { foundObject }); 
+
 });
 
 module.exports = router;
