be9799956f1a4f5f0904790fd0c32218c5d86b17
esrfree <esrfree@gmail.com>
2020-02-03 21:08:15 -0500

    Iteration #4: Adding New Celebrities

--- starter-code/routes/celebrity.js
+++ starter-code/routes/celebrity.js
@@ -12,13 +12,29 @@ router.get('/celebrities', (req, res, next) => {
     .catch( err => console.log('Error while getting celebrities ', err ))
 });
 
+/* GET & POST new celebrities */
+router.get('/celebrities/new', (req, res, next) => {
+    res.render('./celebrities/new')
+})
+
+router.post('/celebrities', (req, res, next) => {
+    Celebrity.create( req.body )
+    .then( newCeleb => {
+        console.log(`New celebrity created: ${newCeleb.name}`)
+        res.redirect('/celebrities')
+    } )
+    .catch( err => console.log('Error while creating a new celebrity ', err ) )
+})
+
+/* GET celebrity detail page */
 router.get('/celebrities/:id', (req, res, next) => {
     Celebrity.findById( req.params.id)
     .then( foundCeleb => {
         //console.log(foundCeleb)
         res.render('./celebrities/show', {foundCeleb})
     })
-    .catch()
+    .catch( err => console.log('Error while getting celebrity details ', err ) )
 })
+
 
 module.exports = router;
