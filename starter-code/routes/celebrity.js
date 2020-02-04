ac4797632629dc842e0eb70ddc03b6076b4b32ab
esrfree <esrfree@gmail.com>
2020-02-03 19:58:26 -0500

    Iteration #3: The Celebrity Details Page

--- starter-code/routes/celebrity.js
+++ starter-code/routes/celebrity.js
@@ -6,10 +6,19 @@ const Celebrity = require('../models/celebrity_model')
 router.get('/celebrities', (req, res, next) => {
     Celebrity.find()
     .then( celebs => {
-        console.log(celebs);
+        //console.log(celebs);
         res.render('./celebrities/index', {celebs});
     })
     .catch( err => console.log('Error while getting celebrities ', err ))
 });
+
+router.get('/celebrities/:id', (req, res, next) => {
+    Celebrity.findById( req.params.id)
+    .then( foundCeleb => {
+        //console.log(foundCeleb)
+        res.render('./celebrities/show', {foundCeleb})
+    })
+    .catch()
+})
 
 module.exports = router;
