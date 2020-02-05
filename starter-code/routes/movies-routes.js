7fc84aa1b251fa1b443d15814d2d5ea9b6eaa222
Kevin Medina <kmedinakm@hotmail.com>
2020-02-04 22:59:30 -0500

    update css

--- starter-code/routes/movies-routes.js
+++ starter-code/routes/movies-routes.js
@@ -4,16 +4,6 @@ const router = express.Router();
 const Movie = require('../models/Movie.model');
 const Celebrity = require('../models/Celebrity.model');
 
-// Iteration 8 Delete a movie
-router.post('/movies/:id/delete', (req, res, next) => {
-    Movie.findByIdAndRemove(req.params.id)
-    .then(deletedMovie => {
-        console.log(`The deleted movie: ${deletedMovie}`);
-        res.redirect('/movies');
-    })
-    .catch(err => console.log(`An error has occured when deleting a movie: ${err}`));
-});
-
 // Iteration 5 Adding New Movies
 router.get('/movies/new', (req, res, next) => {
     res.render('movies/new-movie');
@@ -47,6 +37,16 @@ router.get('/movies/:id', (req, res, next) => {
         res.render('movies/movie-details', { movie: theMovie });
     })
     .catch(err => console.log(`An error has occured when getting the movie details page: ${err}`));
+});
+
+// Iteration 8 Delete a movie
+router.post('/movies/:id/delete', (req, res, next) => {
+    Movie.findByIdAndRemove(req.params.id)
+    .then(deletedMovie => {
+        console.log(`The deleted movie: ${deletedMovie}`);
+        res.redirect('/movies');
+    })
+    .catch(err => console.log(`An error has occured when deleting a movie: ${err}`));
 });
 
 
