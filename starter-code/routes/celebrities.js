b8fa36d9762f327da8579b20cd1df3f9f8ca470d
Pedro Castañeda <pcramos19@gmail.com>
2020-02-08 18:43:26 +0100

    done

---
+++ starter-code/routes/celebrities.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const router = express.Router();
+const Celebrities = require("../models/Celebrity");
+
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/celebrities", (req, res, next) => {
+  Celebrities.find()
+    .then(allCelebs => {
+      res.render("celebrities/index", { allCelebs });
+    })
+    .catch(() => {
+      next();
+    });
+});
+
+router.get("/celebrities/:id", (req, res, next) => {
+  Celebrities.find({ _id: req.params.id })
+    .then(celebFound => {
+      res.render("celebrities/show", { celebFound });
+    })
+    .catch(() => {
+      next();
+    });
+});
+
+router.get("/celebrities/new", (req, res, next) => {
+  res.render("celebrities/new");
+});
+
+router.post("/celebrities/new", (req, res, next) => {
+  Celebrities.create({
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase
+  }).then(() => {
+    res.render("/celebrities");
+  });
+});
+
+router.post("/celebrities/:id/delete", (req, res, next) => {
+  Celebrities.findByIdAndRemove({ _id: req.params.id })
+    .then(() => {
+      res.redirect("/celebrities");
+    })
+    .catch(() => {
+      next();
+    });
+});
+
+router.get("/celebrities/:id/edit", (req, res, next) => {
+  Celebrities.find({ _id: req.params.id })
+    .then(celebToEdit => {
+      res.render("celebrities/edit", { celebToEdit });
+    })
+    .catch(() => {
+      next();
+    });
+});
+
+router.post("/celebrities/:id/", (req, res, next) => {
+  Celebrities.findByIdAndUpdate({ _id: req.params.id }, req.body)
+    .then(() => {
+      res.redirect("/celebrities");
+    })
+    .catch(() => {
+      next();
+    });
+});
+
+module.exports = router;
---
+++ starter-code/routes/movies.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const router = express.Router();
+const Movies = require("../models/Movie");
+
+router.get("/movies", (req, res, next) => {
+  Movies.find()
+    .then(allMovies => {
+      res.render("movies/index", { allMovies });
+    })
+    .catch(() => {
+      next();
+    });
+});
+
+router.get("/movies/:id", (req, res, next) => {
+  Movies.findById({ _id: req.params.id })
+    .then(foundMovie => {
+      res.render("movies/show", { foundMovie });
+    })
+    .catch(() => {
+      next();
+    });
+});
+
+router.get("/movies/new", (req, res, next) => {
+  res.render("movies/new");
+});
+
+router.post("/movies/new", (req, res, next) => {
+  Movies.create({
+    name: req.body.name,
+    genre: req.body.genre,
+    plot: req.body.plot
+  }).then(() => {
+    res.render("/movies");
+  });
+});
+
+router.get("/movies/:id/edit", (req, res, next) => {
+  Movies.find({ _id: req.params.id })
+    .then(movieToEdit => {
+      res.render("movies/edit", { movieToEdit });
+    })
+    .catch(() => {
+      next();
+    });
+});
+
+router.post("/movies/:id", (req, res, next) => {
+  Movies.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => {
+      res.redirect("/movies");
+      })
+    .catch(() => {
+      next();
+    });
+});
+
+router.post("/movies/:id/delete", (req, res, next) => {
+  Movies.findByIdAndDelete({ _id: req.params.id })
+    .then(() => {
+      res.redirect("/movies");
+    })
+    .catch(() => {
+      next();
+    });
+});
+
+module.exports = router;
