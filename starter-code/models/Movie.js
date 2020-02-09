992c472061c598e32b375c459a743c5ab00704bc
cesarvalleiva <cesarvalleiva@gmail.com>
2020-02-09 19:43:42 +0100

    done

---
+++ starter-code/models/Movie.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const schemaName = new Schema(
+    {
+        title: String,
+        genre: String,
+        plot: String,
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Model = mongoose.model("Movie", schemaName);
+module.exports = Model;
