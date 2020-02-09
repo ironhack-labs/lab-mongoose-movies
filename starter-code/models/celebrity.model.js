1d5500ca34925739bd6c09f10ccbba609fa77dda
Sergio Collado <sergiocollado95@gmail.com>
2020-02-09 19:26:51 +0100

    Done with changes

--- starter-code/models/celebrity.model.js
+++ starter-code/models/celebrity.model.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const celebritySchema = {
+const celebritySchema = new Schema({
     name: String,
     occupation: String,
     catchPhrase: String
-}
+});
 
 const Celebrity = mongoose.model('Celebrity', celebritySchema)
 
