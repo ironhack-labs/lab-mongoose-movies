46928f9cb6e4ed69fcb5f52920fa6f9841f5e454
jphrubant <jphrubant@gmail.com>
2020-02-10 21:54:29 +0100

    Only done up to iteration 5

---
+++ starter-code/routes/celebrities.js
@@ -0,0 +1,59 @@
+var express = require("express");
+var celebritiesRouter = express.Router();
+
+const Celebrity = require("./../models/Celebrity");
+
+celebritiesRouter.post('/:id/delete', (req, res) => {
+    Celebrity.findByIdAndRemove(req.params.id)
+        .then(celebrity => {
+            res.redirect("/celebrities");
+        })
+        .catch( (err) => {
+            console.log(err)
+        });
+});
+
+celebritiesRouter.post('/new', (req, res) => {
+    const {name, occupation, catchphrase} = req.body;
+
+    Celebrity.create({name, occupation, catchphrase})
+        .then(celebrity => {
+            res.redirect("/celebrities");
+        })
+        .catch( (err) => {
+            console.log(err)
+        });
+});
+
+celebritiesRouter.get('/new', (req, res) => {
+    res.render("../views/celebrities/new");
+});
+
+celebritiesRouter.get("/:id", (req, res) => {
+    Celebrity.findById(req.params.id)
+        .then( (oneCeleb) => {
+            const data = {
+                celebrity: oneCeleb
+            }
+            res.render("celebrities/show", data)
+        })
+        .catch( (err) => {
+            console.log(err)
+        });
+})
+
+celebritiesRouter.get("/", (req, res) => {
+    Celebrity.find()
+    .then( (allCelebs) => {
+        const data = {
+            celebrities: allCelebs
+        };
+        res.render("celebrities/index2", data)
+    })
+    .catch( (err) => {
+        console.log(err)
+        next();
+    });
+});
+
+module.exports = celebritiesRouter;
