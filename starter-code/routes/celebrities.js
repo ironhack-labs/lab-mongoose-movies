b32f5e1e9ddd8f10e16ca93923a31bdc57ced086
armetegithub <armomomete@protonmail.com>
2020-02-10 01:22:49 +0100

    Hasta Movies

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,30 +1,66 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const Celebrity = require("../models/celebrity");
 
-
-
-
-
-router.get('/', (req, res, next) => {
+router.post("/", (req, res, next) => {
   Celebrity.find()
-    .then(celebrities => res.render('celebrities/index', {
-      celebrities: celebrities
-    }))
-    .catch(err => console.log(`error getting the data from db ${err}`))
+    .then(celebrities =>
+      res.render("celebrities/index", {
+        celebrities: celebrities
+      })
+    )
+    .catch(err => console.log(`error getting the data from db ${err}`));
 });
 
-
-router.get('/:id', (req, res, next) => {
+router.post("/:id", (req, res, next) => {
   Celebrity.findById(req.params.id)
     .then(celebrities => {
-      res.render('celebrities/show', celebrities)
+      res.render("celebrities/show", celebrities);
     })
-    .catch(res.redirect('/celebrities', {
-
-    }));
+    .catch(
+      res.redirect("/celebrities", {
+        error: `There was an error trying to access the star profile`
+      })
+    );
 });
 
+router.get("/new", (req, res, next) => {
+  res.render("celebrities/new");
+});
+
+router.post("/new", (req, res) => {
+  Celebrity.create({
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase
+  })
+    .then(res.redirect("/celebrities"))
+    .catch(
+      res.redirect("/celebrities/new", {
+        error: `There was an error trying to create ${req.body.name}`
+      })
+    );
+});
+
+router.post("/:id/delete", (req, res, next) => {
+  Celebrity.findByIdAndDelete(req.params.id)
+    .then(res.redirect("/celebrities"))
+    .catch(
+      res.redirect("/celebrities", {
+        error: `There was an error trying to delete the star`
+      })
+    );
+});
+
+router.post("/:id/edit", (req, res, next) => {
+  Celebrity.findByIdAndUpdate(req.body.id, {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase
+  })
+    .then(res.redirect("/celebrities"))
+    .catch(next);
+});
 
 // router.get('/', (req, res, next) => {
 //   Celebrity.find()
