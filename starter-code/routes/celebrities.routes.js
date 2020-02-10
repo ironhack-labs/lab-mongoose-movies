ccab345cbb90bfd7f6d07f82229dc641a5bf6b46
davidvrl <veral.david@gmail.com>
2020-02-10 09:24:56 +0100

    celebrities2

--- starter-code/routes/celebrities.routes.js
+++ starter-code/routes/celebrities.routes.js
@@ -39,5 +39,35 @@ router.post('/new', (req, res) => {
     .catch(err => console.log(err))
 })
 
+// Borrado de famosos
+router.post('/:id/delete', (req, res) => {
+
+  const celebrityId = req.params.id
+
+  Celebrity.findByIdAndDelete(celebrityId)
+    .then(() => res.redirect('/celebrities'))
+    .catch(err => console.log("Error borrando el famoso en la BBDD: ", err))
+
+})
+
+// Editar famoso
+router.get('/edit', (req, res) => {
+
+  const celebrityId = req.query.id
+
+  Celebrity.findById(celebrityId)
+    .then(theCelebrity => res.render('celebrities/celebrities-edit', theCelebrity))
+    .catch(err => console.log(err))
+})
+
+router.post('/edit/:id', (req, res) => {
+
+  const celebrityId = req.params.id
+
+  Celebrity.findByIdAndUpdate(celebrityId, req.body)
+    .then(x => res.redirect(`/celebrities/details/${celebrityId}`))
+    .catch(err => console.log(err))
+})
+
 
 module.exports = router
