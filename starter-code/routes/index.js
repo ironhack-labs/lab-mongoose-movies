bdab4a59b05d9ec5fefd60f617b5c5c77b6d0dcb
Jethet <henriette.hettinga@gmail.com>
2020-02-10 21:30:49 +0100

    Commit all files for first section: Celebrities

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router  = express.Router();
+const celebrityRouter = require('./celebrities')
+
+router.use('/celebrities', celebrityRouter)
 
 /* GET home page */
 router.get('/', (req, res, next) => {
