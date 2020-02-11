59775111cc7bdc31858174941b264500b1caf5af
doveriko <abelmorpe@gmail.com>
2020-02-11 07:53:17 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -9,9 +9,9 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
-
+const dbName = "celebrities";
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect(`mongodb://localhost/${dbName}`, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
