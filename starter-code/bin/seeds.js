05f85cb3347db93ecdc43ee6e55954f511826f97
javier Varela <javivarela@javiers-MacBook-Pro.local>
2020-03-04 22:33:59 +0100

    Iteration #7: The Movie Model

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,31 +1,31 @@
 const mongoose = require('mongoose');
-const Celebrity = require('../models/celebrity');
+const Movie = require('../models/movie');
 
 mongoose
   .connect('mongodb://localhost/celebrities-project', {useNewUrlParser: true});
 
-const celebrities = [
+const movies = [
     {
-      name: "Popeye",
-      occupation: "Sailor",
-      catchPhrase: "I yam what I yam an' tha's all I yam"
+      title: "The Shawshank Redemption",
+      genre: "Drama",
+      plot: "Chronicles the experiences of a formerly successful banker as a prisoner in the gloomy jailhouse of Shawshank after being found guilty of a crime he did not commit. The film portrays the man's unique way of dealing with his new, torturous life; along the way he befriends a number of fellow prisoners, most notably a wise long-term inmate named Red."
     },
     {
-      name: "Donald Trump",
-      occupation: "President of the USA",
-      catchPhrase: "Make America Great Again"
+      title: "The Godfather",
+      genre: "Drama",
+      plot: "The Godfather Don Vito Corleone is the head of the Corleone mafia family in New York. He is at the event of his daughter's wedding. Michael, Vito's youngest son and a decorated WW II Marine is also present at the wedding. Michael seems to be uninterested in being a part of the family business. Vito is a powerful man, and is kind to all those who give him respect but is ruthless against those who do not. But when a powerful and treacherous rival wants to sell drugs and needs the Don's influence for the same, Vito refuses to do it. What follows is a clash between Vito's fading old values and the new ways which may cause Michael to do the thing he was most reluctant in doing and wage a mob war against all the other mafia families which could tear the Corleone family apart."
     },
     {
-      name : "Bob Marley",
-      ocuppation: "Musician",
-      catchPhrase: "No Woman no cry"
+      title : "The Dark Knight",
+      genre: "Drama",
+      plot: "Set within a year after the events of Batman Begins (2005), Batman, Lieutenant James Gordon, and new District Attorney Harvey Dent successfully begin to round up the criminals that plague Gotham City, until a mysterious and sadistic criminal mastermind known only as The Joker appears in Gotham, creating a new wave of chaos. Batman's struggle against The Joker becomes deeply personal, forcing him to confront everything he believes and improve his technology to stop him. A love triangle develops between Bruce Wayne, Dent, and Rachel Dawes."
     }
   ];
 
-Celebrity.create(celebrities, err => {
+Movie.create(movies, err => {
   if (err) {
     throw err;
   }
-  console.log(`Created ${celebrities.length} celebrities`);
+  console.log(`Created ${movies.length} movies`);
   mongoose.connection.close();
 });
