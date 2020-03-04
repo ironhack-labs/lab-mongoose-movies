70208ad8ec7e8d99fa4a2a2045935c0267cbe90c
javier Varela <javivarela@javiers-MacBook-Pro.local>
2020-03-04 23:06:51 +0100

    Iteration #8: Listing Our Movies

--- starter-code/app.js
+++ starter-code/app.js
@@ -53,8 +53,10 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 const index = require('./routes/index');
 const celebrities = require('./routes/celebrities');
+const movies = require('./routes/movies');
 app.use('/', index);
 app.use('/celebrities', celebrities);
+app.use('/movies', movies);
 
 
 module.exports = app;
---
+++ starter-code/routes/movies.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router  = express.Router();
+const Movie = require('../models/movie');
+
+router.get('/', (req, res, next) => {
+  Movie.find()
+    .then(movies => {
+      res.render('movies/index', { movies });
+    })
+    .catch(next);
+});
+
+// router.get('/new', (req, res, next) => {
+// 	res.render('celebrities/new');
+// });
+
+// router.get('/:id', (req, res, next) => {
+//   Celebrity.findById(req.params.id)
+//     .then(celebrity => {
+//       res.render('celebrities/show', { celebrity });
+//     })
+//     .catch(next);
+// });
+
+// router.post('/', (req, res, next) => {
+// 	const { name, occupation, catchPhrase } = req.body;
+// 	Celebrity.create({
+// 		name,
+// 		occupation,
+// 		catchPhrase,
+// 	})
+// 		.then(() => {
+// 			res.redirect('/celebrities');
+// 		})
+// 		.catch(next);
+// });
+
+// router.post('/:id/delete', (req, res, next) => {
+// 	const { id } = req.params;
+
+// 	Celebrity.findByIdAndDelete(id)
+// 		.then(() => {
+// 			res.redirect('/celebrities');
+// 		})
+// 		.catch(next);
+// });
+
+// router.get('/:id/edit', (req, res, next) => {
+//   Celebrity.findById(req.params.id)
+//     .then(celebrity => {
+//       res.render('celebrities/edit', { celebrity });
+//     })
+//     .catch(next);
+// });
+
+// router.post('/:id', (req, res, next) => {
+//   const { name, occupation, catchPhrase } = req.body;
+//   const { id } = req.params;
+
+// 	Celebrity.update({ _id : id }, { $set: { name, occupation, catchPhrase }})
+// 		.then(() => {
+// 			res.redirect('/celebrities');
+// 		})
+// 		.catch(next);
+// });
+
+
+
+module.exports = router;
