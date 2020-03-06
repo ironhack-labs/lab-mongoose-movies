bda984c4a3c6b87599d5d58fa3fbe9f5abae1b28
Michael Mueller <michael@miniguide.co>
2020-03-06 23:16:44 +0100

    Iteration 2

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router  = express.Router();
+const Celebrity = require('../models/celebrity'); 
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/celebrities', (req, res, next) => {
+  Celebrity.find()
+  .then((celebrities) => {
+    res.render('celebrities/index', {
+      celebrities
+    });
+  })
+  .catch((error) => {
+    console.log('An error happened while finding celebrities: ', error);
+    next(error);
+  })
 });
 
 module.exports = router;
