b1b9f8d8a4d34e57fdc0df03833b64b133dfc8e6
“Marcela <marcelavieirafreire@gmail.com>
2020-03-31 20:02:11 -0300

    half done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router  = express.Router();
 const Celebrity = require('../models/Celebrity')
+const Movie = require('../models/Celebrity')
+
+
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+// -------- CELEBRITY -----------
 
 // lista celebridades na home
 router.get('/celebrities/index', (req, res) => {
@@ -113,6 +119,118 @@ router.post('/celebrities/edit', (req, res) => {
   })
   .catch(error => console.log(error));
 });
+
+
+
+// -------- MOVIES -----------
+
+// lista filmes na home
+router.get('/movies/index', (req, res) => {
+  Movie
+  .find()
+  .then(movies => {
+    // console.log(celebrity)
+    res.render('movies/index', {movies})})
+  .catch(error => console.log(error))
+});
+
+
+// / info sobre o filme
+router.get('/movies/show/:moviesId', (req, res) => {
+  const {
+    moviesId
+  } = req.params;
+
+  Movie
+    .findById(moviesId)
+    .then(movies=> {
+      res.render('movies/show', {
+        movies
+      });
+    })
+    .catch(error => console.log(error));
+});
+
+router.get('/movies/new', (req, res) => {
+  res.render('movies/new');
+})
+
+
+//add novo filme
+router.post('/movies/new', (req, res) => {
+  console.log('body: ', req.body);
+
+  const {
+    title,
+    genre,
+    plot
+  } = req.body;
+
+  Movie.create({
+    title,
+    genre,
+    plot
+    })
+    .then(response => {
+      // console.log(response);
+      res.redirect('/movies/index');
+    })
+    .catch(error => console.log(error));
+});
+
+
+
+// deletar filme
+router.post('/movies/:moviesId/delete', (req, res) => {
+  const {
+    moviesId
+  } = req.params;
+    console.log(moviesId);
+  Movie
+    .findByIdAndRemove(moviesId)
+    .then(response => {
+    res.redirect('/movies/index');
+    })
+    .catch(error => console.log(error));
+});
+
+
+// movie edit
+router.get('/movies/edit/:moviesId', (req, res) => {
+  const {
+    moviesId
+  } = req.params;
+  Movie
+    .findById(moviesId)
+    .then(movies => {
+      // console.log(book);
+      res.render('movies/edit', {
+        movies
+      });
+    })
+    .catch(error => console.log(error));
+});
+
+
+//POST edit
+router.post('/movies/edit', (req, res) => {
+  const {
+    moviesId, title, genre, plot
+  } = req.body
+
+  Movie
+  .findByIdAndUpdate(moviesId, {$set: {
+    title, genre, plot}
+  }, {
+    new: true
+  })
+  .then(response => {
+    // console.log(response);
+    res.redirect(`/movies/show/${moviesId}`);
+  })
+  .catch(error => console.log(error));
+});
+
 
 
 
