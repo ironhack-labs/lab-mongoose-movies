5911bdcca6b25e9d9ff2eb175e311d83ac10ae20
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-04-01 00:11:00 -0300

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -9,18 +9,17 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
-
-mongoose
-  .connect('mongodb://localhost/starter-code', {
-    useNewUrlParser: true ,
-    useUnifiedTopology: true
-  })
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+(async () => {
+  try {
+    const x = await mongoose.connect('mongodb://localhost/starter-code', {
+      useNewUrlParser: true ,
+      useUnifiedTopology: true
+    });
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+  } catch (error) {
+    console.log(error);
+  }
+})();
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router  = express.Router();
 
-// const Movie = require('../models/movie')
-
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
