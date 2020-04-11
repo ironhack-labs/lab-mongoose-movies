875d589343882640aa47a41375423785df1f13ed
doveriko <abelmorpe@gmail.com>
2020-04-11 11:01:23 +0200

    complete iteration 3

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,16 +1,27 @@
-const express = require('express')
-const celebsRouter = express.Router()
+const express = require("express");
+const celebsRouter = express.Router();
 
-const Celebrity = require("./../models/celebrity")
+const Celebrity = require("./../models/celebrity");
 
 celebsRouter.get("/", (req, res) => {
-    Celebrity.find({})
+  Celebrity.find({})
     .then((celebrities) => {
       res.render("celebrities/index", { celebrities });
     })
     .catch((error) => {
       console.log(error);
     });
-})
+});
 
-module.exports = celebsRouter
+celebsRouter.get("/:celebrityId", (req, res) => {
+  const celebrityId = req.params.celebrityId;
+
+  Celebrity.findById(celebrityId)
+  .then( (celebrity) => {
+      res.render("celebrities/show", {celebrity})
+  })
+  .catch( (err) => console.log(err));
+});
+
+
+module.exports = celebsRouter;
