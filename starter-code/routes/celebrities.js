4f479c3893eecb57c03fde1991b8dc8d8cd9fed1
Manuel <mbuergel@gmx.de>
2020-04-12 10:28:33 +0200

    hmmm

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -5,17 +5,38 @@ const Celeb = require("../models/celebrity");
 
 // show all celebrities
 
-router.get("/", (res, req, next) => {
-
+router.get("/", (req, res, next) => {
   Celeb.find().then((celebs) => {
-
-    console.log("All celebs: " + celebs);
-
     let obj = { allCelebs: celebs };
 
-    res.render("celebrities/index", obj)
-      // .catch((err) => console.log("Error while doing something", error));
+    res.render("celebrities/index", obj);
+    // .catch((err) => console.log("Error while doing something", error));
   });
 });
 
+router.get("/:identifier", (req, res, next) => {
+  Celeb.findById(req.params.identifier).then((celeb) => {
+    console.log("One celeb is " + celeb);
+
+    res.render("celebrities/show", { oneCeleb: celeb });
+  });
+});
+
+router.get("/new", (req, res) => {
+  res.render("celebrities/new")
+});
+
+
 module.exports = router;
+
+
+// router.post("/", (req, res, next) => {
+  
+//   let newCel = new Celeb({name: req.body.name, occupation: req.body.occupation, catchPhrase: req.body.catchPhrase})
+
+//   newCel.save().then(() => {
+//     res.redirect("celebrities/index")
+//   })
+
+// })
+
