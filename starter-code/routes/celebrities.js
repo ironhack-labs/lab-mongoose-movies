c4eba77f75461d85e519e9572fa669bb7882f515
PiaG <piagmdf@gmail.com>
2020-04-14 19:46:41 +0200

    iteration 6 done; 5 has to be done now

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 // neccessary connection to database-model
 const Celebrity = require('../models/celebrity')
@@ -7,13 +7,15 @@ const Celebrity = require('../models/celebrity')
 // GET /celebrities --> List of
 router.get('/', (req, res, next) => {
     Celebrity.find()
-    .then((celebrities) => {
-        //console.log('all our celebrities: ' + celebrities)
-        res.render('celebrities/index', { allCelebrities: celebrities });
-    })
-    .catch((error) => {
-        console.log(error);
-      })
+        .then((celebrities) => {
+            //console.log('all our celebrities: ' + celebrities)
+            res.render('celebrities/index', {
+                allCelebrities: celebrities
+            });
+        })
+        .catch((error) => {
+            console.log(error);
+        })
 });
 
 // GET /celebrities/new  --> shows form to users
@@ -25,22 +27,50 @@ router.get('/new', (req, res, next) => {
 // POST /celebrities/new --> User can add their Celebs
 router.post('/new', (req, res, next) => {
     // console.log("req.body", req.body)
-    let userCelebrity = new Celebrity ({ name: req.body.name, occupation: req.body.occupation, catchPhrase: req.body.catchphrase })
+    let userCelebrity = new Celebrity({
+        name: req.body.name,
+        occupation: req.body.occupation,
+        catchPhrase: req.body.catchphrase
+    })
     userCelebrity.save()
-    .then(() => {
-        res.redirect('/celebrities')
-    });
+        .then(() => {
+            res.redirect('/celebrities')
+        });
 });
+
+
+// GET /celebrities/:id/edit ---> show our form
+router.get('/:identifier/edit', (req, res, next) => {
+    Celebrity.findById(req.params.identifier).then((celeb) => {
+        res.render('celebrities/edit', celeb)
+    })
+})
+
+// POST /celebrities/:id/edit ---> show our form
+router.post('/:identifier/edit', (req, res, next) => {
+    Celebrity.findByIdAndUpdate(
+        req.params.identifier, {
+            name: req.body.name,
+            occupation: req.body.occupation,
+            catchPhrase: req.body.catchPhrase
+        }
+    ).then(() => { res.redirect('/celebrities')})
+})
+
+
+
 
 // GET /celebrities/:identifier --> Detail of
 router.get('/:identifier', (req, res, next) => {
     Celebrity.findById(req.params.identifier)
-    .then((celebrity) => {
-        res.render('celebrities/show', { showCelebrity: celebrity})
-    })
-    .catch((error) => {
-        console.log(error);
-      })
+        .then((celebrity) => {
+            res.render('celebrities/show', {
+                showCelebrity: celebrity
+            })
+        })
+        .catch((error) => {
+            console.log(error);
+        })
 });
 
 
