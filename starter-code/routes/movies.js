117f4df1b1a344cb365cddc1583fe3ecb9ebdfae
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-18 19:43:23 +0200

    movie documents can now be deleted

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -37,16 +37,30 @@ router.get('/:movieId', (req, res, next) => {
 
 router.get('/new', (req, res, next) => res.render('movies/new'))
 
-router.post("/new", (req, res, next) => {
-  const {title, genre, plot} = req.body
-  console.log("requirement made: ", req.body)
-  Movie.create({title, genre, plot})
-  .then(() => {
-    res.redirect("/movies")
-  }).catch((err) => {
-    console.log('An error ocurred when generating a movie: ', err)
-    res.redirect('/new')
-  });
+router.post('/new', (req, res, next) => {
+  const { title, genre, plot } = req.body
+  console.log('requirement made: ', req.body)
+  Movie.create({ title, genre, plot })
+    .then(() => {
+      res.redirect('/movies')
+    })
+    .catch((err) => {
+      console.log('An error ocurred when generating a movie: ', err)
+      res.redirect('/new')
+    })
+})
+
+//DELETE
+
+router.post('/:movieId/delete', (req, res, next) => {
+  Movie.findByIdAndDelete(req.params.movieId)
+    .then(() => {
+      res.redirect('/movies')
+    })
+    .catch((err) => {
+      console.log('An error ocurred when deleting a movie: ', err)
+      res.redirect('/new')
+    })
 })
 
 module.exports = router
