d24d0b681ff1744e0ffd635dd47ef2a7843f0b46
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-18 18:37:54 +0200

    movies/:id now shows the details of the specific movie fetched by id

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -7,15 +7,30 @@ const Movie = require('./../models/movie-model')
 
 router.get('/', (req, res, next) => {
   Movie.find()
-  .then((allMovies) => {
-    res.render("movies/index", {allMovies})
-  }).catch((err) => {
-    console.log(
-      'An error ocurred when fetching all Movie documents from the DB: ',
-      err
-    )
-    next()
-  });
+    .then((allMovies) => {
+      res.render('movies/index', { allMovies })
+    })
+    .catch((err) => {
+      console.log(
+        'An error ocurred when fetching all Movie documents from the DB: ',
+        err
+      )
+      next()
+    })
+})
+
+router.get('/:movieId', (req, res, next) => {
+  Movie.findById(req.params.movieId)
+    .then((movie) => {
+      res.render('movies/show', movie)
+    })
+    .catch((err) => {
+      console.log(
+        'An error ocurred when fetching an specific movie by ID: ',
+        err
+      )
+      next()
+    })
 })
 
 module.exports = router
