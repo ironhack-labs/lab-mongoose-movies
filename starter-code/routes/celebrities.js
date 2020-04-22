0dc5374ecd42ca778a6e7db5f5f5a4b5c861b1b1
Margarida Pascoal <amppascoal@gmail.com>
2020-04-22 19:20:43 +0100

    unfinished

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -6,9 +6,35 @@ const Celebrity = require('../models/celebrity.js');
 router.get('/celebrities', (req, res) => {
   Celebrity.find() 
     .then(allCelebs => {
-      console.log(allCelebs)
+      //console.log(allCelebs)
       res.render('celebrities/index', {celebs: allCelebs});  
     });
 });
+
+router.get('/celebrities/:id', (req, res) => {
+  const id = req.params.id;
+  Celebrity.findById(id) 
+    .then(celeb => {
+      console.log(celeb)
+      res.render('celebrities/show', celeb);  
+    })
+    .catch(error => {
+      console.error('Error ', error);
+    });
+});
+
+
+router.get('/celebrities/new', (req, res) => res.render('celebrities/new')); 
+
+// router.post('/celebrities/new', (req,res) => {
+//   const {name, occupation, catchPhrase} = req.body;
+
+//   const newCeleb = new Celebrity({name, occupation, catchPhrase});
+  
+//   newCeleb.save( ()=> {
+//     res.redirect('/celebrities');
+//   });
+
+// });
 
 module.exports = router;
