0dbd5518edd545af8b197f54848f28e802cfb9cf
Margarida Pascoal <amppascoal@gmail.com>
2020-05-01 10:14:54 +0100

    movie lab

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -18,6 +18,7 @@ router.get('/movies', (req, res, next) => {
 router.get('/movies/new', (req, res) => res.render('movies/new-movie')); 
 
 router.post('/movies/create', (req, res, next) => {
+
   const {title, genre, plot, cast} = req.body;
 
   const newMovie = new Movie({title, genre, plot, cast});
@@ -31,6 +32,59 @@ router.post('/movies/create', (req, res, next) => {
   });
 
 });
+
+// Show individual view of movie
+
+router.get('/movies/:id', (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then(movie => {
+      res.render('movies/show', {movie});
+    })
+    .catch(error => {
+      next(error);
+  });
+});
+
+
+router.get('/movies/:id/edit', (req, res, next) => {
+  Movie.findById(req.params.id)
+  .then(movie => {
+    res.render('movies/edit', {movie: movie})
+  })
+  .catch(error => {
+    next(error);
+  });
+});
+
+router.post('/movies/:id', (req, res, next) => {
+  const updatedMovie = {
+    title: req.body.title,
+    plot: req.body.plots,
+    genre: req.body.genre,
+  };
+
+  Movie.update({id: req.params.id}, updatedMovie)
+    .then(() => {
+      res.redirect('/movies');
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.post('/movies/:id/delete', (req, res, next) => {
+  Movie.findById(req.params.id)
+  .then(movie => {
+    movie.remove()
+      .then(() => {
+        res.redirect('/movies');
+      })
+      .catch(error => {
+        next(error);
+      });
+  });
+});
+
 
 
 module.exports = router;
