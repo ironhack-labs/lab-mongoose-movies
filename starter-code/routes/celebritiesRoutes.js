209a8ed7eb4fd2b5e4ee9a041dc543f2a065be80
David de Lamo <d.lamo.valverde@gmail.com>
2020-05-30 20:01:55 +0200

    iteration 3: list celebrities

--- starter-code/routes/celebritiesRoutes.js
+++ starter-code/routes/celebritiesRoutes.js
@@ -3,6 +3,15 @@ const router = express.Router();
 
 const Celebrity = require('../models/celebrity')
 
+router.get('/', async (req, res, next) => {
+    try {
+        const allCelebrities = await Celebrity.find({});
+        res.render('celebrities/celebrities', {celebrities: allCelebrities});
+    } catch (error) {
+        console.log('Error listing celebrities: ' + error);
+    }
+})
+
 router.get('/new', (req, res, next) => {
     res.render('celebrities/new-celebrity');
 })
@@ -13,8 +22,8 @@ router.post('/create', async (req, res, next) => {
         const newCelebrity = new Celebrity({name, occupation, catchPhrase});
         await Celebrity.create(newCelebrity)
         res.redirect('/')
-    } catch (err) {
-        console.log('Error creating celebrity: ' + err);
+    } catch (error) {
+        console.log('Error creating celebrity: ' + error);
         res.render('celebrities/new-celebrity');
     }
 })
