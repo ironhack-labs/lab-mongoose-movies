88e44c9cc6d95825e45475663980a860eb1feae7
irene villanueva <irens.vm@gmail.com>
2020-05-30 19:54:18 +0200

    it1.4

---
+++ starter-code/bin/seeds.js
@@ -0,0 +1,13 @@
+const celebrities = [{
+    name: "Lady Gaga",
+    occupation: "Singer",
+    catchPhrase: "Some women choose to follow men, and some women choose to follow their dreams. If you're wondering which way to go, remember that your career will never wake up and tell you that it doesn't love you anymore.",
+}, {
+    name: "Bad Gyal",
+    occupation: "Singer",
+    catchPhrase: "He vuelto zorras",
+}, {
+    name: "Adam Brody",
+    occupation: "Actor",
+    catchPhrase: "i'm a fake intellectual",
+}]
