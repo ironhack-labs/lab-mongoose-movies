596aa9c54247b0bf72735e61e12d087abaeb8a9d
Juli Ramon <juli@nexumlabs.com>
2020-05-31 19:50:15 +0200

    Iteration 3 - Done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -9,6 +9,12 @@ router.get('/', (req, res, next) => {
             res.render('celebrities/index', {celebrities: allCelebrities})
         })
         .catch(err => console.log('Error displaying the celebrities', err))
-  });
+    });
+
+router.get('/:id', (req, res, next) => {
+    Celebrity.findById(req.params.id)
+        .then(celebrity => res.render('celebrities/show', celebrity))
+        .catch(err => console.log('Error displyaing loading a celebrity page', err))
+    });
   
-  module.exports = router;
+module.exports = router;
