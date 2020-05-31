a3747cd7eb94e0e0614d3abc0f2873f49d739a47
Alba Gustems <albagustems@gmail.com>
2020-05-31 21:09:06 +0200

    Iteration 9: The Movie Details Page completed

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -14,4 +14,15 @@ router.get('/movies', (req, res, next) => {
         .catch(err => console.log(`An error has occurred while searching for movies: ${err}`))
 })
 
+router.get('/movies/:id', (req, res, next) => {
+    Movie.findById(req.params.id)
+        .then(detailsMovies => {
+            console.log(detailsMovies)
+            res.render('movies/show', {
+                movies: detailsMovies
+            })
+        })
+        .catch(err => console.log(`An error has occurred while searching for the movie: ${err}`))
+})
+
 module.exports = router;
