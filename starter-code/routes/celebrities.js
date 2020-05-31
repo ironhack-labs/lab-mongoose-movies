ad89227c79434ffde733ea4c200e94f1f3e6fdc7
Alba Gustems <albagustems@gmail.com>
2020-05-31 13:36:09 +0200

    Iteration 3: The Celebrity Details Page completed

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -5,13 +5,24 @@ const Celebrity = require('../models/celebrity')
 
 router.get('/celebrities', (req, res, next) => {
     Celebrity.find()
-        .then((allCelebrities) => {
+        .then(allCelebrities => {
             console.log(allCelebrities)
             res.render('celebrities/index', {
                 celebrities: allCelebrities
             })
         })
         .catch(err => console.log(`An error has occurred while searching for celebrities: ${err}`))
+})
+
+router.get('/celebrities/:id', (req, res, next) => {
+    Celebrity.findById(req.params.id)
+        .then(detailsCelebrity => {
+            console.log(detailsCelebrity)
+            res.render('celebrities/show', {
+                celebrities: detailsCelebrity
+            })
+        })
+        .catch(err => console.log(`An error has occurred while searching for the details: ${err}`))
 })
 
 module.exports = router;
