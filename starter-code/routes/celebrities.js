355a07019ea939166ac5021657e8ab11c7fafb54
Claudi Sanchez <claudi.sanchezserrano@gmail.com>
2020-05-31 17:45:09 +0200

    iteration 4

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,26 +1,60 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 const Celebrity = require('../models/celebrity.js');
 
 /* GET celebrities page */
 router.get('/', (req, res, next) => {
     Celebrity.find()
-    .then(allCelebrities => {
-        res.render('celebrities/index', {celebrities: allCelebrities});
-    }).catch(error => {
-        next(error);
+        .then(allCelebrities => {
+            res.render('celebrities/index', {
+                celebrities: allCelebrities
+            });
+        }).catch(error => {
+            next(error);
+        });
+});
+
+/*POST celebrity*/
+router.post('/', (req, res, next) => {
+    const {
+        name,
+        occupation,
+        catchPhrase
+    } = req.body;
+    const newCelebrity = new Celebrity({
+        name,
+        occupation,
+        catchPhrase
+    });
+    newCelebrity.save()
+    .then((celebrity) => {
+      res.redirect('/celebrities');
+    })
+    .catch((error) => {
+        res.redirect('/new');
     });
 });
 
-/*GET celebrities details page*/
-router.get('/:id', (req, res, next) =>{
-    Celebrity.findById(req.params.id)
-    .then(theCelebrity => {
-        res.render('celebrities/show', {theCelebrity});
-    }).catch(error => {
-        next(error);
-    });
+/*GET celebrities add details page*/
+router.get('/new', (req, res, next) => {
+    res.render('celebrities/new');
 });
+
+/*GET celebrities details page*/
+router.get('/:id', (req, res, next) => {
+    Celebrity.findById(req.params.id)
+        .then(theCelebrity => {
+            res.render('celebrities/show', {
+                theCelebrity
+            });
+        }).catch(error => {
+            next(error);
+        });
+});
+
+
+
+
 
 module.exports = router;
