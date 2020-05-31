474ac47fedbbe2847fafe122584762c499239a40
Claudi Sanchez <claudi.sanchezserrano@gmail.com>
2020-05-31 12:34:53 +0200

    iteration 1 - done

--- starter-code/app.js
+++ starter-code/app.js
@@ -13,10 +13,10 @@ const path         = require('path');
 mongoose
   .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
   .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error('Error connecting to mongo', err);
   });
 
 const app_name = require('./package.json').name;
