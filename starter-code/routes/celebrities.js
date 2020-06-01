49402ab9008e019264cc058854b4fd67fd0daa82
Marc De Mena <mdemena@gmail.com>
2020-06-01 10:58:34 +0200

    Finished Iteration 3

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -5,7 +5,12 @@ const route = express.Router();
 route.get('/', (req, res, next) => {
 	Celebrity.find()
 		.then((celebrities) => res.render('celebrities/index', { celebrities }))
-		.catch((err) => next);
+		.catch((err) => next());
+});
+route.get('/:id', (req, res, next) => {
+	Celebrity.findById(req.params.id)
+		.then((celebrity) => res.render('celebrities/show', celebrity))
+		.catch((err) => next());
 });
 
 module.exports = route;
