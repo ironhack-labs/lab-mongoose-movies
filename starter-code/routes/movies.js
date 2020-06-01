03e25ba3646ec4334b5c22bbde2396ffd0a3b7cd
Laura Ramirez <lau.ramirez.encinas@gmail.com>
2020-06-01 12:20:58 +0200

    it11-12

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -41,6 +41,27 @@ router.post('/movies', (req, res, next) => {
         .catch((error) => console.log(error))
 });
 
+router.post('/movies/:id/delete', (req,res,next)=>{
+    Movie.findByIdAndRemove(req.params.id)
+    .then(movie=>res.redirect('/movies'))
+    .catch(error => console.log(error))
+});
+
+router.get('/movies/:id/edit', (req, res, next) => {    
+    Movie.findById(req.params.id)
+    .then(movie=>{
+        res.render('movies/edit', {movie:movie})
+    })
+    .catch(error => console.log(error))
+    
+});
+
+router.post('/movies/:id', (req, res, next)=>{
+    const { title, genre, plot} = req.body;
+    Movie.update({_id:req.params.id},{$set:{title, genre, plot}},{ new: true })
+    .then(celebrity=>res.redirect('/movies'))
+    .catch(error => console.log(error))
+})
 
 module.exports = router
 
