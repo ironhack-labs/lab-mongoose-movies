6d09a8ac7d85fb022e01d2747f590323274d5229
irene villanueva <irens.vm@gmail.com>
2020-06-01 12:16:38 +0200

    it 3 done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -2,10 +2,24 @@ const express = require("express")
 const router = express.Router()
 const Celebrity = require("../models/celebrity-model")
 
-router.get('/celebrities/index', (req, res, next) => {
+router.get("/celebrities/index", (req, res, next) => {
     Celebrity.find()
-        .then(allCelebrities => res.render('celebrities/index', {
+        .then(allCelebrities => res.render("celebrities/index", {
             allCelebrities
         }))
         .catch(err => console.log("An error ocurred", err))
 })
+
+
+
+router.get("/celebrities/:id", (req, res, next) => {
+    Celebrity.findById().then(infoCelebrity => {
+            console.log(infoCelebrity)
+            res.render('celebrities/show', {
+                celebrities: infoCelebrity
+            })
+        })
+        .catch(err => console.log("An error has occurred with celebrities details", err))
+})
+
+module.exports = router;
