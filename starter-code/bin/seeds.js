30c6f1462bcd72472c89c99f8f62314bc4221758
Alberto Correia <alberto_cpv@hotmail.com>
2020-06-01 12:23:30 +0200

    iteracion 1

---
+++ starter-code/bin/seeds.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose")
+const Celebrity = require("../models/celebrity")
+const DB_NAME = 'mongoose-movies';
+
+mongoose.connect(`mongodb://localhost/${DB_NAME}`, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const celebrities = [{
+    name: "Marcos Mundstock",
+    occupation: "Musician",
+    catchPhrase: "If you cant convince them, confuse them"
+},
+{
+    name: "Homer Simpson",
+    occupation: "Philosopher",
+    catchPhrase: "In this house we obey the laws of thermodynamics!"
+},
+{
+    name: "Michael Jordan",
+    occupation: "Basketball player",
+    catchPhrase: "That´s all i needed"
+}]
+
+
+Celebrity.create(celebrities)
+.then (() => console.log("Celebrities created succesfully"))
+.catch (error => console.log(error))
