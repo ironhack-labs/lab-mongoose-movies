2ea0cfb429abb64d69c4578bfd0f72f41487ea4d
irene villanueva <irens.vm@gmail.com>
2020-06-01 13:24:45 +0200

    it4 done, fail en add404

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -10,12 +10,34 @@ router.get("/", (req, res, next) => {
         .catch(err => console.log("An error ocurred", err))
 })
 
+router.get("/new", (req, res, next) => {
+    res.render("celebrities/new")
+})
+
+router.post("/new", (req, res, next) => {
+    const {
+        name,
+        occupation,
+        catchPhrase
+    } = req.body;
+    const newCelebrity = new Celebrity({
+        name,
+        occupation,
+        catchPhrase
+    })
+    newCelebrity.save()
+        .then(() => res.render("celebrities"))
+        .catch(err => {
+            res.redirect("celebrities/new")
+        })
+})
+
 
 
 router.get("/:id", (req, res, next) => {
     Celebrity.findById(req.params.id).then(infoCelebrity => {
             console.log(infoCelebrity)
-            res.render('celebrities/show', {
+            res.render("celebrities/show", {
                 celebrities: infoCelebrity
             })
         })
