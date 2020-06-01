ba67e510662d042464af2238759648af9de96930
irene villanueva <irens.vm@gmail.com>
2020-06-01 16:46:26 +0200

    it11done

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -10,6 +10,42 @@ router.get("/", (req, res, next) => {
         .catch(err => console.log("An error ocurred", err))
 })
 
+
+
+router.get("/new", (req, res, next) => {
+    Movie.find()
+        .then(allMovies => res.render("movies/new", {
+            allMovies
+        }))
+})
+
+router.post("/new", (req, res, next) => {
+
+    const {
+        title,
+        genre,
+        plot
+    } = req.body;
+    const newMovie = new Movie({
+        title,
+        genre,
+        plot
+    })
+    newMovie.save()
+        .then(() => res.render("/"))
+        .catch(err => {
+            res.redirect("/new")
+        })
+})
+
+router.post("/:id/delete", (req, res, next) => {
+    console.log(req.params.id)
+    Movie.findByIdAndRemove(req.params.id)
+        .then(() => res.redirect("/movies"))
+        .catch(err => console.log("Error rying to delete a movie", err))
+})
+
+
 router.get("/:id", (req, res, next) => {
     Movie.findById(req.params.id).then(infoMovie => {
             console.log(infoMovie)
