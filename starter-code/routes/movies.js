0999de8422299230e1fd56b4e1045dc6378f874f
Alberto Correia <alberto_cpv@hotmail.com>
2020-06-02 00:48:52 +0200

    Lab done, missing bonus

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -14,5 +14,49 @@ const Movie = require("../models/movie")
             console.log(error)
         });
     })
+    //POST delete movie
+    router.post("/movies/:id/delete", (req, res) => {
+        Movie.findByIdAndDelete(req.params.id)
+        .then(() => {
+            res.redirect("/movies")
+        }).catch((error) => {
+            console.log(error)
+        });
+    })
+// GET to a new movie page
+ router.get("/movies/new", (req, res) => {
+    res.render("movies/new")
+})
+//POST new movie page
+ router.post("/movies/new", (req, res) => {
+     const {
+         title,
+         genre,
+         plot
+     } = req.body
+     const newMovie = new Movie ({
+        title,
+        genre,
+        plot
+     })
+     newMovie.save()
+     .then(() => {
+         res.redirect("/movies")
+     }).catch((error) => {
+         console.log(error)
+     });
+ })
+
+/* GET to an specific movie page */
+router.get('/movies/:id', (req, res) => {
+    Movie.findById(req.params.id)
+    .then((result) => {
+        console.log(result)
+        res.render("movies/show", {movies:result})
+    })
+    .catch((error) => {
+        console.log(error)
+    });
+})  
 
     module.exports = router;
