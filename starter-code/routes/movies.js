80609395108dfe51598bed2aa853e069826f38f9
Toni <sancheztoni.garcia@gmail.com>
2020-06-01 21:17:17 +0200

    iteration 9

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Movies = require("../models/movies");
 
+//Movies List
 router.get("/", (req, res, next) => {
   Movies.find()
     .then((movie) => {
@@ -9,6 +10,18 @@ router.get("/", (req, res, next) => {
     })
     .catch((err) => {
       console.log("Error when get Movies list", err);
+      next();
+    });
+});
+
+//GET movie info
+router.get("/:id", (req, res, next) => {
+  Movies.findById(req.params.id)
+    .then((movie) => {
+      res.render("movies/show", movie);
+    })
+    .catch((err) => {
+      console.log("An error has occurred while charge the movie info", err);
       next();
     });
 });
