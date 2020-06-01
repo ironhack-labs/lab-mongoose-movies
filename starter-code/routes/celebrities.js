e4e96a55e64e623f80b3d07e80331c40d114c8ba
Alberto Correia <alberto_cpv@hotmail.com>
2020-06-01 22:34:14 +0200

    iteration 4

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -15,6 +15,30 @@ const Celebrity = require("../models/celebrity")
             console.log(error)
         });
     })
+// GET to a new celebrity page
+ router.get("/celebrities/new", (req, res) => {
+    res.render("celebrities/new")
+})
+//POST new celebrity page
+ router.post("/celebrities/new", (req, res) => {
+     const {
+         name,
+         occupation,
+         catchPhrase
+     } = req.body
+     const newCelebrity = new Celebrity ({
+        name,
+        occupation,
+        catchPhrase
+     })
+     newCelebrity.save()
+     .then(() => {
+         res.redirect("/celebrities")
+     }).catch((error) => {
+         console.log(error)
+     });
+ })
+
 /* GET to an specific celebrity page */
 router.get('/celebrities/:id', (req, res) => {
     Celebrity.findById(req.params.id)
@@ -25,7 +49,8 @@ router.get('/celebrities/:id', (req, res) => {
     .catch((error) => {
         console.log(error)
     });
-})
+})  
+
     
 
 module.exports = router
