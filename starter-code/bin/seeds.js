3c3b20df7c73c74400e983346eb8ef1bc82e5fe8
irene villanueva <irens.vm@gmail.com>
2020-06-01 16:26:20 +0200

    it7done

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-const Celebrities = require("../models/celebrity")
+const Movies = require("../models/movie.js")
 
 
 
@@ -16,26 +16,50 @@ mongoose.connect(`mongodb://localhost/${DB_NAME}`, {
 
 
 
-const celebrities = [{
-    name: "Lady Gaga",
-    occupation: "Singer",
-    catchPhrase: "Some women choose to follow men, and some women choose to follow their dreams. If you're wondering which way to go, remember that your career will never wake up and tell you that it doesn't love you anymore.",
+//const celebrities = [{
+//    name: "Lady Gaga",
+//    occupation: "Singer",
+//    catchPhrase: "Some women choose to follow men, and some women choose to follow their dreams. If you're wondering which way to go, remember that your career will never wake up and tell you that it doesn't love you anymore.",
+//}, {
+//    name: "Bad Gyal",
+//    occupation: "Singer",
+//    catchPhrase: "He vuelto zorras",
+//}, {
+//    name: "Adam Brody",
+//    occupation: "Actor",
+//    catchPhrase: "I'm a fake intellectual",
+//}]
+//
+
+
+
+//Celebrities.create(celebrities)
+//    .then(allCelebrities => {
+//        console.log(`${celebrities.length} celebrities created on Compass`)
+//        mongoose.connection.close()
+//    })
+//    .catch(err => console.log(`error creating celebrities: ${err}`))
+
+
+const movies = [{
+    title: "film one",
+    genre: "comedy",
+    plot: "Some women choose to follow men, and some women choose to follow their dreams. If you're wondering which way to go, remember that your career will never wake up and tell you that it doesn't love you anymore.",
 }, {
-    name: "Bad Gyal",
-    occupation: "Singer",
-    catchPhrase: "He vuelto zorras",
+    title: "film two",
+    genre: "drama",
+    plot: "Some women choose to follow men, and some women choose to follow their dreams. If you're wondering which way to go, remember that your career will never wake up and tell you that it doesn't love you anymore.",
+
 }, {
-    name: "Adam Brody",
-    occupation: "Actor",
-    catchPhrase: "I'm a fake intellectual",
+    title: "Lady film",
+    genre: "thriller",
+    plot: "Some women choose to follow men, and some women choose to follow their dreams. If you're wondering which way to go, remember that your career will never wake up and tell you that it doesn't love you anymore.",
+
 }]
 
-
-
-
-Celebrities.create(celebrities)
-    .then(allCelebrities => {
-        console.log(`${celebrities.length} celebrities created on Compass`)
+Movies.create(movies)
+    .then(movies => {
+        console.log(`${movies.length} movies created on Compass`)
         mongoose.connection.close()
     })
-    .catch(err => console.log(`error creating celebrities: ${err}`))
+    .catch(err => console.log(`error creating movies: ${err}`))
