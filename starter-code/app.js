89f5ed5fe7a69cdc65395dd6d0557c17130c6bf0
Antoine Rolland <antoine-rolland@hotmail.fr>
2020-06-01 18:13:52 +0200

    Iteration 2

--- starter-code/app.js
+++ starter-code/app.js
@@ -11,7 +11,7 @@ const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/celebrity-db', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -52,7 +52,8 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 
 const index = require('./routes/index');
+const celebrities = require('./routes/celebrities');
 app.use('/', index);
-
+app.use('/celebrities', celebrities);
 
 module.exports = app;
