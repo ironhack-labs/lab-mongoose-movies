2d391e66e9900e9772b83e0836c6157ce90d499b
Toni <sancheztoni.garcia@gmail.com>
2020-06-01 21:36:26 +0200

    iteration 11 & 12. Lab finished

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -13,6 +13,36 @@ router.get("/", (req, res, next) => {
       next();
     });
 });
+//DELETE Movie
+router.post("/:id/delete", (req, res, next) => {
+  Movies.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.redirect("/movies");
+    })
+    .catch((err) => {
+      console.log("An error has ocurred while delete movie", err);
+      next();
+    });
+});
+
+//EDIT Movie (GET & POST)
+router.get("/:id/edit", (req, res, next) => {
+  Movies.findById(req.params.id)
+    .then((movie) => res.render("movies/edit", movie))
+    .catch((err) => {
+      console.log("An error has occurred while charge the edit route", err);
+      next();
+    });
+});
+router.post("/:id", (req, res, next) => {
+  const { title, genre, plot } = req.body;
+  Movies.update({ _id: req.params.id }, { $set: { title, genre, plot } })
+    .then(() => res.redirect("/movies"))
+    .catch((err) => {
+      console.log("Error has occurred while save the new data", err);
+      next();
+    });
+});
 
 //GET movie info
 router.get("/:id", (req, res, next) => {
