de039a3569dcf706e57841d20293f58b9f00f1ac
Alberto Correia <alberto_cpv@hotmail.com>
2020-06-02 00:00:11 +0200

    iteration movies 1

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose")
+/*const mongoose = require("mongoose")
 const Celebrity = require("../models/celebrity")
 const DB_NAME = 'mongoose-movies'
 
@@ -27,4 +27,35 @@ const celebrities = [{
 
 Celebrity.create(celebrities)
 .then (() => console.log("Celebrities created succesfully"))
+.catch (error => console.log(error))*/
+
+const mongoose = require("mongoose")
+const Movie = require("../models/movies")
+const DB_NAME = 'mongoose-movies'
+
+mongoose.connect(`mongodb://localhost/${DB_NAME}`, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const movies = [{
+    title: "Torrente 16",
+    genre: "Comedia",
+    plot: "The same over and over again"
+},
+{
+    title: "Space Jam 3",
+    genre: "Sports",
+    plot: "Looney tunes have to win a basketball game to make Donal Trump out of the white house"
+},
+{
+    title: "Pub fiction",
+    genre: "Drama",
+    plot: "Drama arrives at the moment of pay the bill"
+}]
+
+
+Movie.create(movies)
+.then (() => console.log("Movies created succesfully"))
 .catch (error => console.log(error))
