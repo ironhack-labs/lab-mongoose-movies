234a4417a19c86dfdc14226a1879610c27b0c6c2
Natalia López <natalialopezyeste@gmail.com>
2020-06-02 15:37:25 +0200

    Iteration 4

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -15,4 +15,29 @@ router.get('/', (req, res, next) => {
     });
 });
 
+
+router.get("/new", (req, res, next) => {
+  res.render('celebrities/new');
+});
+
+router.post("/new", (req, res, next) => {
+  const {name, occupation, catchPhrase} = req.body;
+  const newCelebrity = new Celebrity({name, occupation, catchPhrase});
+  newCelebrity.save().then(celebrity => res.redirect("/celebrities")).catch(err => console.log(err));
+});
+
+
+router.get('/:id', (req, res, next) => {
+  Celebrity.findById(req.params.id)
+    .then(celebrity => {
+        console.log(celebrity);
+      res.render('celebrities/show', celebrity);
+    })
+    .catch(error => {
+      console.log('Error while getting the celebrities from the DB: ', error);
+    });
+});
+
+
+
 module.exports = router;
