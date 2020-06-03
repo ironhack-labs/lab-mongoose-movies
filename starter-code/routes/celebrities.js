43cef0aa007e108a7cca574101b603046e18aa39
Alex <alex@Alexs-MacBook-Pro.local>
2020-06-03 19:41:48 +0200

    iteration 5,7 - done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -11,10 +11,27 @@ router.get('/', (req, res, next) => {
         .catch(err => next(err));
 });
 
+router.get('/:celeb_id/delete', (req, res, next) => {
+    Celebs.findByIdAndRemove(req.params.celeb_id)
+        .then(() => res.redirect('/celebrities'))
+        .catch(err => next(err));
+});
+
 router.get('/create-new', (req, res, next) => {
     res.render('celebrities/create-new');
 });
 
+router.post('/create-new', (req, res, next) => {
+    const {name, occupation, catchPhrase} = req.body;
+    const newCelebrity = new Celebs ({ name, occupation, catchPhrase });
+
+    newCelebrity.save()
+    .then(() => res.redirect('/celebrities'))
+    .catch(err => {
+        console.log(`An error has occurred while adding a new celebrity: ${err}`)
+        res.redirect('celebrities/new');
+    });
+});
 
 router.get('/:celeb_id', (req, res, next) => {
     Celebs.findById(req.params.celeb_id)
@@ -25,18 +42,11 @@ router.get('/:celeb_id', (req, res, next) => {
         })
         .catch(err => console.log(`An error has occurred while searching for the details: ${err}`))
 });
+// router.get('/celeb_id/delete', (req, res, next) => {
+//     res.redirect('/celebrities/index');
+// });
 
 
-router.post('/create-new', (req, res, next) => {
-    const {name, occupation, catchPhrase} = req.body;
-    const newCelebrity = new Celebs ({ name, occupation, catchPhrase });
 
-    newCelebrity.save()
-    .then(() => res.redirect('/celebrities'))
-    .catch(err => {
-        console.log(`An error has occurred while adding a new celebrity: ${err}`)
-        res.redirect('celebrities/new')
-    });
-});
 
 module.exports = router;
