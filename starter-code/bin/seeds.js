649360cce68c21a3d37b70c3841bcb0d9b60b2f3
JesusSantana <jesus@esmarketingdigital.com>
2020-06-03 17:05:49 +0200

    Iteration 7 - The Movie Model

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,14 +1,14 @@
+const mongoose = require("mongoose");
 
-const mongoose = require('mongoose')
+const Celebrity = require("../models/celebrity");
+const Movie = require("../models/movie.js");
 
-const Celebrity = require('../models/celebrity')
-
-const DB_TITLE = 'celebrity-db';
+const DB_TITLE = "celebrity-db";
 
 mongoose.connect(`mongodb://localhost/${DB_TITLE}`, {
   useCreateIndex: true,
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 Celebrity.collection.drop();
@@ -17,20 +17,43 @@ const celebs = [
   {
     name: "Dave Gahan",
     occupation: "Singer",
-    catchPhrase: "it's no good"
+    catchPhrase: "it's no good",
   },
   {
     name: "Clinton «Clint» Eastwood Jr",
     occupation: "Actor",
-    catchPhrase: "Man becomes more creative during war!"
+    catchPhrase: "Man becomes more creative during war!",
   },
   {
     name: "Steven Patrick Morrissey",
     occupation: "Singer",
-    catchPhrase: "There Is a Light That Never Goes Out!"
-  }
-]
+    catchPhrase: "There Is a Light That Never Goes Out!",
+  },
+];
 
 Celebrity.create(celebs)
-.then(celeb => console.log("Celebrity database created !"))
-.catch(err => `An error occurred : ${err}`)
+  .then((celeb) => console.log("Celebrity database created !"))
+  .catch((err) => `An error occurred : ${err}`);
+
+const Movies = [
+  {
+    title: "La La Land",
+    genre: "Musical",
+    plot: `While navigating their careers in Los Angeles, a pianist and an actress fall in love`,
+  },
+
+  {
+    title: "Master and Commander",
+    genre: "Adventure",
+    plot: `During the Napoleonic Wars, a brash British captain pushes his ship and crew to their limits.`,
+  },
+  {
+    title: "Mulan",
+    genre: "Action",
+    plot: `A young Chinese maiden disguises herself'`,
+  }
+];
+
+Movie.create(Movies)
+  .then((items) => console.log(`${items.length} have been created`))
+  .catch((err) => console.log(`Error occured creating celebrities: ${err}`));
