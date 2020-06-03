be781998b691fbab592303c042d1ffaced95ee6c
JesusSantana <jesus@esmarketingdigital.com>
2020-06-03 17:46:21 +0200

    Refactor 2

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,7 +12,7 @@ const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/celebrity-db', {useNewUrlParser: true})
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err))
 
