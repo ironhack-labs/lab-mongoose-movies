f12cf99f1212e5ab95e316708d2f81729b44d47c
Robert Ledezma <Robert@MacBook-Pro-de-Robert-2.local>
2020-06-07 00:09:09 +0200

    Iteración 9 done

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -6,13 +6,28 @@ const Movie = require('../models/movie.js')
 router.get('/', (req, res, next) => {
   Movie.find()
     .then(allMovies => {
-      console.log(allMovies)
+      //console.log(allMovies)
       res.render('movies/index', {
         movies: allMovies
       })
     })
     .catch(err => console.log(`No se imprimen las películas: ${err}`))
 })
+
+// Render de detalles de la movie
+
+router.get('/:id', (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then(movieDetails => {
+      res.render('movies/show', {
+        movie: movieDetails
+      })
+    })
+    .catch(error => {
+      console.log('Error al acceder a los detalles de la movie', error);
+    })
+});
+
 
 module.exports = router;
 
