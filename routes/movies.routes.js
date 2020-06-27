e24616f9d2787af34cfea1b73dab2113d78993f7
Elena Sanchez Leon <elena.farma94@gmail.com>
2020-06-27 16:51:21 +0200

    Finalizado

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const Movie = require('./../models/movie.model')
 
 
-
+// Iteración 8 - list
 router.get('/', (req, res)=> {
     Movie
         .find()
@@ -13,7 +13,56 @@ router.get('/', (req, res)=> {
     }  
 )
 
+// Iteración 10 - add
 
+router.get('/create', (req, res)=> res.render('movies.views/createMovie'))
+
+router.post('/create', (req, res) => {
+
+    const {title, genre, plot} = req.body
+    console.log('los req.body son: ',req.body)
+
+    Movie
+        .create({title, genre, plot})
+        .then(()=> res.redirect('/movies'))
+        .catch(error => console.log('el error es: ', error))
+
+})
+
+// BONUS Iteración 12 - editing
+
+router.get('/:id/edit', (req, res) =>{
+
+    Movie
+        .findById(req.params.id)
+        .then(movie => res.render('movies.views/edit', movie))
+        .catch(error => console.log('el error es: ', error))
+})
+
+
+router.post('/:id', (req, res)=>{
+
+    const {title, genre, plot} = req.body
+
+    Movie
+        .findByIdAndUpdate(req.params.id, {title, genre, plot})
+        .then(()=> res.redirect(`/movies/${req.params.id}`))
+        .catch(error => console.log('el error es: ', error))
+})
+
+// Iteración 11 - remove
+
+router.post('/:id/remove', (req,res) =>{
+
+    Movie
+        .findByIdAndRemove(req.params.id)
+        .then(() => res.redirect('/movies'))
+        .catch(error => console.log('el error es: ', error))
+
+})
+
+
+// Iteración 9 - details
 
 router.get('/:movieID', (req, res)=>{
 
@@ -23,4 +72,10 @@ router.get('/:movieID', (req, res)=>{
         .catch(error => console.log('el error es: ', error))
     }
 )
+
+
+
+
 module.exports = router
+
+
