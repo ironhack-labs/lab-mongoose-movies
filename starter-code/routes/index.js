58aa27af478fb2d7caade200cd44cc079bf0af03
cristianvl8 <cristianvinualeslobera@gmail.com>
2020-06-27 12:35:27 +0200

    a medias

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,78 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+
+const Celebrity = require('./../models/celebrity.model.js')
 
 /* GET home page */
+
+
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+//CELEBRITIES//
+
+
+router.get('/celebrities', (req, res) => {
+
+  Celebrity
+    .find()
+    .then(allcelebrities => res.render('celebrities', { allcelebrities }))
+    .catch(err => console.log("Error en la BBDD", err))
+
+})
+
+//CELEBRITY DETAILS//
+
+router.get("/details/:celebrityId", (req, res) => {
+
+  Celebrity
+    .findById(req.params.celebrityId)
+    .then(theCelebritie => res.render('celebrities-details', theCelebritie))
+    .catch(err => console.log("Error en la BBDD", err))
+})
+
+
+//CREATE CELEBRITY//
+
+
+router.get('/new', (req, res) => {
+
+  res.render('new-form')
+  
+})
+
+router.post('/new', (req, res) => {
+
+  const {name, occupation, catchPhrase} = req.body
+  
+
+  Celebrity
+  
+    .create({name,occupation,catchPhrase})
+    .then(() => res.redirect('celebrities'))
+    .catch(err => console.log("Error en la BBDD", err))
+  
+})
+
+//DELETE CELEBRITY//
+ 
+router.get("/delete/:celebrityId", (req, res) => {
+
+  Celebrity
+    
+    .findByIdAndRemove(req.params.celebrityId)
+    .then(() => res.redirect('/celebrities'))
+    .catch(err => console.log("Error en la BBDD", err))
+})
+
+//EDIT CELEBRITY//
+
+
+
+
+
+
 module.exports = router;
+
+
