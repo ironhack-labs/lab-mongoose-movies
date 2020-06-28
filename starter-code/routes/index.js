fb735bf44959d2c28ccd31fd96fedc5848222416
joseironhack2017 <jos.gomez1984@gmail.com>
2020-06-28 07:46:41 +0200

    done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,14 @@
+
 const express = require('express');
 const router  = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.render('index');
 });
 
-module.exports = router;
+module.exports = app => {
+
+    // Base URLS
+    app.use('/', require('./base.routes.js'))
+}
