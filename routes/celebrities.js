953d0742f5e413541ce2972feb02ae85680dd32e
Míriam Sánchez Alcón <msanchezalcon.msa@gmail.com>
2020-06-28 16:18:10 +0200

    iter 3

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -5,14 +5,19 @@ const Celebrity = require('./../models/celebrity')
 
 // router.get('/', (req, res) => res.render('celebrities/index'))
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
 
-    Celebrity
-        .find()
+    Celebrity.find()
         .then(allCelebrities => res.render('celebrities/index', { allCelebrities }))
         .catch(err => console.log("Error en la BBDD", err))
 })
 
+
+router.get('/celebrities/:id', (req, res, next) => {
+    Celebrity.findById(req.params.id)
+        .then(oneCelebrity => res.render('celebrities/show', oneCelebrity)
+            .catch(err => console.log('Error while getting celebrities:', err)))
+})
 
 
 module.exports = router
