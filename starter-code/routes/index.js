63225d1ce7efbb449398810859d889317a155271
Andrea Astorga <andrea.astorga95@gmail.com>
2020-07-05 17:59:45 +0200

    Iteration 2

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const Celebrity = require('../models/Celebrity');
 const router  = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/celebrities', (req, res, next) => {
+  Celebrity.find({})
+  .then(celebritiesFromDB => res.render('celebrities/index', { celebritiesFromDB }))
+  .catch(error => console.log('Error found', error))
 });
 
 module.exports = router;
