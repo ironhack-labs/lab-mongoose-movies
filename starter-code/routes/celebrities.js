dbfa6e96e8f7628cd678a2772891ad6b0ba0a9e6
Izquiano <izquiano@hotmail.com>
2020-07-05 22:30:43 +0200

    Iteration 3

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -9,8 +9,13 @@ router.get('/celebrities', (req, res, next) => {
   Celebrity.find()
   .then((celebritiesFromDb)=> res.render('celebrities/index', {celebritiesFromDb}))
   .catch((e)=> console.log(e))
-
   
+})
+/* GET celebrities page */
+router.get('/celebrities/:id', (req, res, next) => {
+  Celebrity.findById(req.params.id)
+  .then((celebrityFromDb)=> res.render('celebrities/show', {celebrityFromDb}))
+  .catch((e)=> console.log(e))
 })
 
 
