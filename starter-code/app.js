8385a1be8e360dc20712d6c316a3c97daf0915f9
Rodrigo Poblacion <rodrigopoblacion@gmail.com>
2020-07-17 21:18:39 +0200

    Sin empezar

--- starter-code/app.js
+++ starter-code/app.js
@@ -50,7 +50,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.locals.title = 'Express - Generated with IronGenerator';
 
 
-
+ 
 const index = require('./routes/index');
 app.use('/', index);
 
