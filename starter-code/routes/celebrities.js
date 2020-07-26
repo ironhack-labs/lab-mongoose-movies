3219b58bf67c45de35d1eb95ded9bf780e0b93bd
Marina Urbano <murbanobouterin@gmail.com>
2020-07-26 22:09:47 +0200

    modificaciones, I6 no funciona con el post :id

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Celebrity = require("../models/Celebrity");
+const { findById } = require("../models/Celebrity");
 const router = express.Router();
 
 /* 
@@ -9,17 +10,17 @@ llavors no cal ficar un altre cop el nom de la ruta
 */
 
 router.get("/", (req, res, next) => {
-    Celebrity.find()
-        .then((showAllCelebrities) => {
-            // console.log('Retrieved celebrities from DB:', showAllCelebrities);
-            res.render("celebrities/index", {
-                celebrities: showAllCelebrities,
-            });
-        })
-        .catch((error) => {
-            console.log("Error while getting the celebrities from the DB: ", error);
-            next(error);
-        });
+  Celebrity.find()
+    .then((showAllCelebrities) => {
+      // console.log('Retrieved celebrities from DB:', showAllCelebrities);
+      res.render("celebrities/index", {
+        celebrities: showAllCelebrities,
+      });
+    })
+    .catch((error) => {
+      console.log("Error while getting the celebrities from the DB: ", error);
+      next(error);
+    });
 });
 
 /*1.get per renderitzar la pàgina on està el formulari
@@ -27,91 +28,78 @@ router.get("/", (req, res, next) => {
 */
 
 router.get("/new", (req, res, next) => {
-    res.render("celebrities/new");
+  res.render("celebrities/new");
 });
 
 router.post("/new", (req, res, next) => {
-    const {
-        name,
-        occupation,
-        catchPhrase
-    } = req.body;
-    const newCelebrity = new Celebrity({
-        name,
-        occupation,
-        catchPhrase,
+  const { name, occupation, catchPhrase } = req.body;
+  const newCelebrity = new Celebrity({
+    name,
+    occupation,
+    catchPhrase,
+  });
+
+  newCelebrity
+    .save()
+    .then((celebrity) => {
+      res.redirect("/celebrities");
+    })
+    .catch((error) => {
+      res.render("celebrities/new");
     });
-
-    newCelebrity
-        .save()
-        .then((celebrity) => {
-            res.redirect("/celebrities");
-        })
-        .catch((error) => {
-            res.render("celebrities/new");
-        });
 });
 
-router.get("/edit", (req, res, next) => {
-    let ce;
-    Celebrity.findOne({
-            _id: req.query.celebrity_id,
-        })
-        .then((celebrity) => {
-            res.render("celebrities/edit", {
-                celebrity,
-            });
-        })
-        .catch((error) => {
-            next(error);
-        });
-});
-
-router.post("/edit", (req, res, next) => {
-    const {
-        name,
-        occupation,
-        catchPhrase
-    } = req.body;
-    Celebrity.update({
-            _id: req.query.celebrity_id,
-        }, {
-            $set: {
-                name,
-                occupation,
-                catchPhrase,
-            },
-        }, {
-            new: true,
-        })
-        .then((celebrity) => {
-            res.redirect("/celebrities");
-        })
-        .catch((error) => next(error));
-});
-
+//I5- delete
 router.post("/:id/delete", (req, res, next) => {
-    Celebrity.findByIdAndRemove(req.params.id)
-        .then((celebrity) => {
-            res.redirect("/celebrities");
-        })
-        .catch((error) => next(error));
+  Celebrity.findByIdAndRemove(req.params.id)
+    .then((celebrity) => {
+      res.redirect("/celebrities");
+    })
+    .catch((error) => next(error));
+});
+
+//I6- edit
+//GET
+router.get("/:id/edit", (req, res, next) => {
+  Celebrity.findById(req.params.id)
+    .then((editCelebrity) => {
+      res.render("celebrities/edit", { editCelebrity });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+//POST
+router.post("/:id/edit", (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
+  Celebrity.update(
+    { _id: req.params.id },
+    { $set: { name, occupation, catchPhrase } },
+    { new: true }
+  )
+    .then((editCelebrity) => {
+      res.redirect("/celebrities");
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 router.get("/:id", (req, res, next) => {
-    let celebrityId = req.params.id;
-    Celebrity.findById({
-            _id: celebrityId,
-        })
-        .then((theCelebrity) => {
-            res.render("celebrities/show", {
-                celebrity: theCelebrity,
-            });
-        })
-        .catch((error) => {
-            console.log("Error while retrieving celebrity details: ", error);
-            next(error);
-        });
+  let celebrityId = req.params.id;
+  Celebrity.findById({
+    _id: celebrityId,
+  })
+    .then((theCelebrity) => {
+      res.render("celebrities/show", {
+        celebrity: theCelebrity,
+      });
+    })
+    .catch((error) => {
+      console.log("Error while retrieving celebrity details: ", error);
+      next(error);
+    });
 });
 
 module.exports = router;
