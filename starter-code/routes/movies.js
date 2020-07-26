2dbc5eef710014fcf66a366ca4f2de44c4530aa2
Noiret <sergio.ko@icloud.com>
2020-07-26 18:19:17 +0200

    Iteration 10 done.

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -17,6 +17,35 @@ router.get('/', (req, res, next) => {
             next(err)
         })
 });
+//IT 10 Get que renderiza las nuevas movies
+router.get('/new', (req, res, next) => {
+    res.render('movies/new')
+})
+//IT 10 Post que guarda la info en la DB y redirige a /movies
+router.post('/new', (req, res, next) => {
+    //Cojo la info del form con req.body y la guardo en 3 constantes.
+    const {
+        title,
+        genre,
+        plot
+    } = req.body;
+    //Paso esas 3 constantes como argumentos al modelo de Celebrity y creo una nueva.
+    //La guardo en una variable.
+    const newMovie = new Movie({
+        title,
+        genre,
+        plot
+    })
+    newMovie
+        .save()
+        .then((newMov) => {
+            res.redirect('/movies');
+        })
+        .catch((err) => {
+            console.log('Error while creating a new movie', err)
+            res.render('movies/new')
+        })
+})
 
 //IT 9 - more info
 router.get('/:id', (req, res, next) => {
