7ce3e2c14c8100188e0130573a1b9b82bfc8254b
Leandro <leandro.a.loiacono@gmail.com>
2020-07-26 20:46:08 +0200

    iteration 11

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,29 +1,57 @@
 const mongoose = require('mongoose');
 const Celebrity = require('../models/Celebrity');
+const Movie = require('../models/Movie');
 
 const dbName = 'celebrities-project';
 mongoose.connect(`mongodb://localhost/${dbName}`, { useNewUrlParser: true, useUnifiedTopology: true } );
 
-const arrCeleb = [
+// const arrCeleb = [
+//   {
+//     name: "The Soup Nazi",
+//     occupation: "Chef",
+//     catchPhrase: "No soup for you!"
+//   },
+//   {
+//     name: "Fox Mulder",
+//     occupation: "Detective",
+//     catchPhrase: "The truth is out there"
+//   },
+//   {
+//     name: "Elaine Benes",
+//     occupation: "Editor",
+//     catchPhrase: "Yada, yada, yada"
+//   }
+// ];
+
+// Celebrity.create(arrCeleb, (err) => {
+//   if (err) {throw(err) }
+//   console.log(`There are ${arrCeleb.length} new celebrities in town!`)
+//   mongoose.connection.close();
+// })
+
+const arrMovies = [
   {
-    name: "The Soup Nazi",
-    occupation: "Chef",
-    catchPhrase: "No soup for you!"
+    title: 'The bad song',
+    genre: 'Drama',
+    plot: 'A guy that only recommends bad music came across with the worst song ever.'
   },
   {
-    name: "Fox Mulder",
-    occupation: "Detective",
-    catchPhrase: "The truth is out there"
+    title: 'Fake Movies',
+    genre: 'Documentary',
+    plot: 'A company that delivers fake trailers for movies that never will be made recieves an offer impossible to refuse.'
   },
   {
-    name: "Elaine Benes",
-    occupation: "Editor",
-    catchPhrase: "Yada, yada, yada"
-  }
+    title: 'The bootcamp',
+    genre: 'mockumentary',
+    plot: 'Mockumentary based on the daily events of a bootcamp campus and his students.' 
+  },
+
 ];
 
-Celebrity.create(arrCeleb, (err) => {
-  if (err) {throw(err) }
-  console.log(`There are ${arrCeleb.length} new celebrities in town!`)
+Movie.create(arrMovies, (err) => {
+  if (err) {throw(err)}
+  console.log(`There are ${arrMovies.length} new movies to watch!`)
   mongoose.connection.close();
 })
+
+
--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 const Celebrity = require("../models/Celebrity");
-const { route } = require(".");
 
 /* GET celebrities page */
 router.get("/", (req, res, next) => {
