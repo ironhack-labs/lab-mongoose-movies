85ee1ec14cf034ddf9b8a32355c53677fca8f0e4
Mariana Mazzariello <mfmazzariello@gmail.com>
2020-07-27 20:01:46 +0200

    Iteration 3 is done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,23 +1,28 @@
-const express = require('express');
-const router  = express.Router();
-const Celebrity = require('./../models/celebrity');
+const express = require("express");
+const router = express.Router();
+const Celebrity = require("./../models/celebrity");
 
-
-// router.get('/celebrities', (req, res, next) => {
-//   res.render('celebrities');
-// });
-
-
-router.get('/celebrities', (req, res, next) => {
+router.get("/celebrities", (req, res, next) => {
   Celebrity.find()
-    .then(allTheCelebritiesFromDB => {
-      console.log('Retrieved celebrities from DB:', allTheCelebritiesFromDB);
-      res.render('celebrities/index', { celebrity1: allTheCelebritiesFromDB });
+    .then((allTheCelebritiesFromDB) => {
+      console.log("Retrieved celebrities from DB:", allTheCelebritiesFromDB);
+      res.render("celebrities/index", { celebrity1: allTheCelebritiesFromDB });
     })
-    .catch(error => {
-      console.log('Error while getting the books from the DB: ', error);
-    })
+    .catch((error) => {
+      console.log("Error while getting the books from the DB: ", error);
+    });
 });
 
+router.get("/celebrities/:id", (req, res, next) => {
+  Celebrity.findById(req.params.id)
+
+    .then(celebrity => {
+            
+      res.render("celebrities/show", { celebrity });
+    })
+    .catch(error => {
+      console.log('error', error2)
+    });
+});
 
 module.exports = router;
