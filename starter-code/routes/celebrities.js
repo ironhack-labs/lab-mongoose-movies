3295c8e435ae685cb8bb69446ae68d9c7a73b8b2
Mariana Mazzariello <mfmazzariello@gmail.com>
2020-07-28 21:39:48 +0200

    Iteration 6 is done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+
 const Celebrity = require("./../models/celebrity");
 
 router.get("/celebrities", (req, res, next) => {
@@ -13,38 +14,69 @@ router.get("/celebrities", (req, res, next) => {
     });
 });
 
+router.get("/celebrities/new", (req, res, next) => {
+  res.render("celebrities/new");
+});
+
+router.post("/celebrities/new", (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
+  const newCelebrity = new Celebrity({ name, occupation, catchPhrase });
+  newCelebrity
+    .save()
+    .then((celebrities) => {
+      res.redirect("/celebrities");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+router.post("/celebrities/:id/delete", (req, res, next) => {
+  Celebrity.findByIdAndRemove(req.params.id)
+    .then((celebrities) => {
+      res.redirect("/celebrities");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+router.get("/celebrities/:id/edit", (req, res, next) => {
+  Celebrity.findById(req.params.id)
+    .then((celebrities) => {
+      res.render("celebrities/edit", { celebrities });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+router.post("/celebrities/:id", (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
+  Celebrity.update(
+    { name: body.name },
+    { occupation: body.occupation}, 
+    { catchPhrase: body.catchPhrase})
+
+    .then((celebrities) => {
+      res.redirect("/celebrities");
+    })
+    .catch((error) => {
+      console.log(error);
+      next(error)
+    });
+});
+
+
 router.get("/celebrities/:id", (req, res, next) => {
   Celebrity.findById(req.params.id)
 
-    .then(celebrity => {
-            
+    .then((celebrity) => {
       res.render("celebrities/show", { celebrity });
     })
-    .catch(error => {
-      console.log('error', error2)
+    .catch((error) => {
+      console.log("Error while retrieving celebrities details: ", error);
     });
 });
-
-router.get("/celebrities/new", (req, res, next) => {
-  Celebrity.findById(req.params.id)
-
-    .then(celebrity => {
-            
-      res.render("celebrities/new", { celebrity });
-    })
-    .catch(error => {
-      console.log('error', error2)
-    });
-});
-
-
-
-
-
-
-
-
-
-
 
 module.exports = router;
