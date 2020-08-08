8797974b2587b89b6fe228ceec2bb64823b6bb46
Hanna Murai <annmuray75@gmail.com>
2020-08-07 21:08:06 -0500

    changes

---
+++ starter-code/bin/seeds.js
@@ -0,0 +1,15 @@
+// import { LOADIPHLPAPI } from "dns";
+// import { Signer } from "crypto";
+// import { builtinModules } from "module";
+
+const celebs = [
+  { name: "Dua Lipa", occupation: "singer", catchPhrase: "Let`s get physical" },
+  { name: "Sia", occupation: "singer", catchPhrase: "Chandelier" },
+  {
+    name: "Calvin Harris",
+    occupation: "singer",
+    catchPhrase: "How deep is your love?"
+  }
+];
+
+module.exports = celebs;
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,19 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Celebrity = require("../Models/Celebrity");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
+router.get("/celebrities", (req, res, next) => {
+  Celebrity.find()
+    .then(celeb => {
+      console.log(celeb);
+    })
+    .next(error => {
+      console.log("Error!");
+    });
+});
 module.exports = router;
