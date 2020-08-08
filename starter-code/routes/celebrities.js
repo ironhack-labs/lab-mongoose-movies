06923a35147f178a3cbb9bbdd84dd83459aa5ee1
Arianna Valle <ariannamarbella@gmail.com>
2020-08-07 22:09:52 -0400

    Complete iteration 1 & 2

---
+++ starter-code/routes/celebrities.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const router = express.Router();
+const Celebrity = require("../models/celebrity");
+
+router.get("/", (req,res,next)=>{
+	Celebrity.find()
+	.then(celebritiesFromDB => {
+		console.log(`List of celebrities: ${celebritiesFromDB}`)
+		res.render("celebrities/index", {celebrities: celebritiesFromDB})
+	})
+	.catch(err => console.log(`Error retrieving celebrities: ${err}`))
+})
+
+router.get("/create", (req, res, next) => {
+	res.render("celebrity-views/createCelebrity");
+});
+
+router.post("/create", (req, res, next) => {
+	// console.log({ body: req.body });
+	Celebrity.create(req.body)
+		.then((createdCelebrity) => {
+			res.render("celebrity-views/celebrityDetails", { celebrity: createdCelebrity });
+		})
+		.catch((err) => console.log(`Error creating celebrity: ${err}`));
+});
+
+router.get("/update/:id", (req, res, next) => {
+		const { id } = req.params;
+		Celebrity.findById(id)
+		  .then(celebrityToEdit => {
+			res.render("celebrity-views/editCelebrity", celebrityToEdit);
+		  })
+		  .catch(error => console.log(`Error while getting a single celebrity for edit: ${error}`));
+	  });
+
+router.post("/update/:id", (req, res, next) => {
+    Celebrity.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then((updatedCelebrity) => {
+			console.log({ updatedCelebrity });
+			res.redirect(`/celebrities/${req.params.id}`)
+        })
+        .catch((err) => console.log(`Error updating celebrity: ${err}`));
+});
+
+router.post("/delete/:id", (req, res, next) => {
+    Celebrity.findByIdAndDelete(req.params.id)
+	.then(() => res.redirect('/celebrities/browse'))
+    .catch(error => console.log(`Error while deleting a celebrity: ${error}`));
+});
+
+router.get('/:id', (req, res, next)=>{
+	Celebrity.findById(req.params.id)
+	.then(celebrityFromDB => {
+		res.render("celebrity-views/celebrityDetails", {celebrity : celebrityFromDB})
+	})
+	.catch(err => console.log(`Error retrieving celebrity details: ${err}`))
+	
+})
+
+module.exports = router;
