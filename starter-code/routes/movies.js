eec259bf8291e956c90b3a0fc748eda1f2e5006d
Matthew Howard <mattalanhoward@gmail.com>
2020-08-09 17:07:01 +0200

    Iteration 11

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
-//require celebrities model 
+//require movies model 
 const router = express.Router();
 const MovieModel = require('../models/movie');
 
 
-//display celebrities and list on localhost:3000/celebrities
+//display movies and list on localhost:3000/movies
 router.get('/movies', (req,res, next) => {
     MovieModel.find()
     .then(allTheMoviesFromDB => {
@@ -16,5 +16,45 @@ router.get('/movies', (req,res, next) => {
         console.log(`Err While getting the Movies from the DB: ${err}`)
       })
 })
+
+//display movie details on /movies/:id
+router.get('/movies/:id', (req,res,next) => {
+    const { id } = req.params;
+    console.log(id)
+    MovieModel.findById(id)
+    .then(allMovieDetails => {
+        console.log(allMovieDetails)
+        res.render('movies/show', {movieDetails: allMovieDetails})
+    })
+    .catch(err=> {
+        console.log(`Err While getting the Movie Details from the DB: ${err}`)
+    })
+})
+
+//new movieform
+router.get('/newMovie', (req,res) =>{
+    res.render('movies/new')
+})
+
+router.post('/newMovie', (req, res, next) => {
+    const { title, genre, plot} = req.body;
+  
+    MovieModel.create({title, genre, plot})
+    .then(() => 
+    res.redirect('/movies'))
+    .catch(error => 
+      res.redirect('/newMovie') 
+      `Error while creating a new movie: ${error}`);
+  });
+
+
+//delete Movie
+  router.post('/movies/:id/delete', (req, res, next) => {
+    const { id } = req.params;
+  
+    MovieModel.findByIdAndDelete(id)
+    .then(() => res.redirect('/movies'))
+    .catch(error => console.log(`Error while deleting a Movie: ${error}`));
+  });
 
 module.exports = router;
