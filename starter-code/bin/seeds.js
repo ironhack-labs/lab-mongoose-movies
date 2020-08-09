68c3d63146d6fbaa4e9d7b26f42bcc795b9d2ccf
Michel Ribbens <michelribbens@gmail.com>
2020-08-09 14:37:49 +0100

    iteration 7&8 done

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,34 +1,64 @@
 const mongoose = require('mongoose');
 
-const Celebrity = require('../models/Celebrity.model');
+// const Celebrity = require('../models/Celebrity.model');
+const Movie = require('../models/Movie.model');
 
 require('../configs/db.config');
 
-// User.collection.drop();
+// Celebs
+// const footballCelebs = [
+//   {
+//     name: 'Messi',
+//     occupation: 'Attacker',
+//     catchPhrase: 'The best decisions aren’t made with your mind, but with your instinct.'
+//   },
+//   {
+//     name: 'Virgil van Dijk',
+//     occupation: 'Defender',
+//     catchPhrase: 'There are always things we can still learn to perfect.'
+//   },
+//   {
+//     name: 'Cristiano Ronaldo',
+//     occupation: 'Attacker',
+//     catchPhrase: 'Sou melhor que Messi'
+//   }
+// ];
 
-const footballCelebs = [
+// Celebrity.create(footballCelebs)
+//   .then(dbCelebs => {
+//     console.log(`Created ${dbCelebs.length} celebrities`);
+//     mongoose.connection.close();
+//   })
+//   .catch(err =>
+//     console.log(`An error occurred while creating football celebs in the DB: ${err}`)
+//   );
+
+
+// Movies
+
+const fakeMovies = [
   {
-    name: 'Messi',
-    occupation: 'Attacker',
-    catchPhrase: 'The best decisions aren’t made with your mind, but with your instinct.'
+    title: 'The Messias and his teammates',
+    genre: 'Drama',
+    plot: 'The best player in the world has to deal with some bad teammates.'
   },
   {
-    name: 'Virgil van Dijk',
-    occupation: 'Defender',
-    catchPhrase: 'There are always things we can still learn to perfect.'
+    title: 'All Good in Liverpool',
+    genre: 'Comedy',
+    plot: 'After many years of failure, the new team gets ahead of its rival teams.'
   },
   {
-    name: 'Cristiano Ronaldo',
-    occupation: 'Attacker',
-    catchPhrase: 'Sou melhor que Messi'
+    title: 'There always someone better than me...',
+    genre: 'Drama',
+    plot: 'Even when he is trying everything to be the best in the world, Christiano Ronaldo will always be the second best.'
   }
 ];
 
-Celebrity.create(footballCelebs)
-  .then(dbCelebs => {
-    console.log(`Created ${dbCelebs.length} celebrities`);
+Movie.create(fakeMovies)
+  .then(dbMovies => {
+    console.log(`Created ${dbMovies.length} celebrities`);
     mongoose.connection.close();
   })
   .catch(err =>
-    console.log(`An error occurred while creating fake users in the DB: ${err}`)
+    console.log(`An error occurred while creating fake movies in the DB: ${err}`)
   );
---
+++ starter-code/routes/movies.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const router = express.Router();
+
+const Movie = require('../models/Movie.model');
+
+
+// Iteration 2: Get all Celebs
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then(moviesFromDB => {
+      console.log(`List of movies: ${moviesFromDB}`)
+      res.render('./movies/index', { movies : moviesFromDB })
+    })
+    .catch((err) => {
+      console.log(`Error while getting movies from the DB: ${err}`);
+      next();
+    });
+});
+
+
+// // Iteration 3: Show Celeb Details
+// router.get('/celebrities/:id', (req, res, next) => {
+//   const { id } = req.params;
+//   Celebrity.findById(id)
+//     .then(celeb => {
+//       console.log(`Details of football celeb: ${celeb}`)
+//       res.render('./celebrities/show', celeb)
+//     })
+//     .catch((err) => {
+//       console.log(`Error while getting celebrities from the DB: ${err}`);
+//       next();
+//     });
+// });
+
+
+// // Iteration 4: Create a new Celeb
+// router.get('/celebrities/new', (req, res) => res.render('./celebrities/new'));
+
+// router.post('/celebrities/new', (req, res) => {
+//   const { name, occupation, catchPhrase } = req.body;
+//   Celebrity.findOne({ name })
+//     .then(celebDocFromDB => {
+//       if (!celebDocFromDB) {
+//         Celebrity.create({ name, occupation, catchPhrase })
+//         .then(() => res.redirect('/celebrities'));
+//       } else {
+//         res.render('./celebrities/new', { message: 'Celebrity already exist.' });
+//         return;
+//       }
+//     })
+//     .catch((err) => {
+//       console.log(`Error while creating a new celebrity: ${err}`);
+//       next();
+//     });
+// });
+
+
+// // Iteration 5: Delete a Celeb
+// router.post('/celebrities/:id/delete', (req, res, next) => {
+//   const { id } = req.params;
+//   Celebrity.findByIdAndDelete(id)
+//     .then(() => res.redirect('/celebrities'))
+//     .catch((err) => {
+//       console.log(`Error while deleting a celebrity: ${err}`);
+//       next();
+//     });
+// });
+
+
+// // Iteration 6: Edit a Celeb
+// router.get('/celebrities/:id/edit', (req, res, next) => {
+//   const { id } = req.params;
+//   Celebrity.findById(id)
+//     .then(celebToEdit => {
+//       res.render('./celebrities/edit', celebToEdit);
+//     })
+//     .catch((err) => {
+//       console.log(`Error while editing a celebrity: ${err}`);
+//       next();
+//     });
+// });
+
+// router.post('/celebrities/:id/edit', (req, res, next) => {
+//   const { id } = req.params;
+//   const { name, occupation, catchPhrase } = req.body;
+
+//   Celebrity.findByIdAndUpdate(
+//     id,
+//     { name, occupation, catchPhrase },
+//     { new: true }
+//   )
+//   .then(() => res.redirect('/celebrities'))
+//   .catch((err) => {
+//     console.log(`Error while editing a celebrity: ${err}`);
+//     next();
+//   });
+// });
+
+
+
+module.exports = router;
