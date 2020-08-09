24026b4d738465b3f837e59744ee6e3121cfe3c1
Michel Ribbens <michelribbens@gmail.com>
2020-08-09 22:12:47 +0100

    all iterations done

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const Movie = require('../models/Movie.model');
 
 
-// Iteration 2: Get all Celebs
+// Iteration 8: Get all Movies
 router.get('/movies', (req, res, next) => {
   Movie.find()
     .then(moviesFromDB => {
@@ -18,84 +18,83 @@ router.get('/movies', (req, res, next) => {
 });
 
 
-// // Iteration 3: Show Celeb Details
-// router.get('/celebrities/:id', (req, res, next) => {
-//   const { id } = req.params;
-//   Celebrity.findById(id)
-//     .then(celeb => {
-//       console.log(`Details of football celeb: ${celeb}`)
-//       res.render('./celebrities/show', celeb)
-//     })
-//     .catch((err) => {
-//       console.log(`Error while getting celebrities from the DB: ${err}`);
-//       next();
-//     });
-// });
+// Iteration 9: Show Movie Details
+router.get('/movies/:id', (req, res, next) => {
+  const { id } = req.params;
+  Movie.findById(id)
+    .then(movie => {
+      console.log(`Details of football movie: ${movie}`)
+      res.render('./movies/show', movie)
+    })
+    .catch((err) => {
+      console.log(`Error while getting movies from the DB: ${err}`);
+      next();
+    });
+});
 
 
-// // Iteration 4: Create a new Celeb
-// router.get('/celebrities/new', (req, res) => res.render('./celebrities/new'));
+// Iteration 10: Create a new Movie
+router.get('/movies/new', (req, res) => res.render('./movies/new'));
 
-// router.post('/celebrities/new', (req, res) => {
-//   const { name, occupation, catchPhrase } = req.body;
-//   Celebrity.findOne({ name })
-//     .then(celebDocFromDB => {
-//       if (!celebDocFromDB) {
-//         Celebrity.create({ name, occupation, catchPhrase })
-//         .then(() => res.redirect('/celebrities'));
-//       } else {
-//         res.render('./celebrities/new', { message: 'Celebrity already exist.' });
-//         return;
-//       }
-//     })
-//     .catch((err) => {
-//       console.log(`Error while creating a new celebrity: ${err}`);
-//       next();
-//     });
-// });
-
-
-// // Iteration 5: Delete a Celeb
-// router.post('/celebrities/:id/delete', (req, res, next) => {
-//   const { id } = req.params;
-//   Celebrity.findByIdAndDelete(id)
-//     .then(() => res.redirect('/celebrities'))
-//     .catch((err) => {
-//       console.log(`Error while deleting a celebrity: ${err}`);
-//       next();
-//     });
-// });
+router.post('/movies/new', (req, res) => {
+  const { title, genre, plot } = req.body;
+  Movie.findOne({ title })
+    .then(movieDocFromDB => {
+      if (!movieDocFromDB) {
+        Movie.create({ title, genre, plot })
+        .then(() => res.redirect('/movies'));
+      } else {
+        res.render('./movies/new', { message: 'Movie already exist.' });
+        return;
+      }
+    })
+    .catch((err) => {
+      console.log(`Error while creating a new movie: ${err}`);
+      next();
+    });
+});
 
 
-// // Iteration 6: Edit a Celeb
-// router.get('/celebrities/:id/edit', (req, res, next) => {
-//   const { id } = req.params;
-//   Celebrity.findById(id)
-//     .then(celebToEdit => {
-//       res.render('./celebrities/edit', celebToEdit);
-//     })
-//     .catch((err) => {
-//       console.log(`Error while editing a celebrity: ${err}`);
-//       next();
-//     });
-// });
+// Iteration 11: Delete a Celeb
+router.post('/movies/:id/delete', (req, res, next) => {
+  const { id } = req.params;
+  Movie.findByIdAndDelete(id)
+    .then(() => res.redirect('/movies'))
+    .catch((err) => {
+      console.log(`Error while deleting a movie: ${err}`);
+      next();
+    });
+});
 
-// router.post('/celebrities/:id/edit', (req, res, next) => {
-//   const { id } = req.params;
-//   const { name, occupation, catchPhrase } = req.body;
 
-//   Celebrity.findByIdAndUpdate(
-//     id,
-//     { name, occupation, catchPhrase },
-//     { new: true }
-//   )
-//   .then(() => res.redirect('/celebrities'))
-//   .catch((err) => {
-//     console.log(`Error while editing a celebrity: ${err}`);
-//     next();
-//   });
-// });
+// Iteration 12: Edit a Movie
+router.get('/movies/:id/edit', (req, res, next) => {
+  const { id } = req.params;
+  Movie.findById(id)
+    .then(movieToEdit => {
+      res.render('./movies/edit', movieToEdit);
+    })
+    .catch((err) => {
+      console.log(`Error while editing a movie: ${err}`);
+      next();
+    });
+});
 
+router.post('/movies/:id/edit', (req, res, next) => {
+  const { id } = req.params;
+  const { title, genre, plot } = req.body;
+
+  Movie.findByIdAndUpdate(
+    id,
+    { title, genre, plot },
+    { new: true }
+  )
+  .then(() => res.redirect('/movies'))
+  .catch((err) => {
+    console.log(`Error while editing a movie: ${err}`);
+    next();
+  });
+});
 
 
 module.exports = router;
