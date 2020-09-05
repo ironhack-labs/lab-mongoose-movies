526d780eb511040d8c183aa7739cfd2b4607f8bf
anna dodo <anna.dorenskaya@gmail.com>
2020-09-05 16:50:26 +0200

    4th iteration

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,27 +1,42 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const Celebrity = require('../models/Celebrity')
+const Celebrity = require("../models/Celebrity");
 
-router.get('/', (req, res, next) => {
-    Celebrity.find()
-        .then(data => {
-            
-            res.render('celebrities/index', {data})
-        })
-        .catch(err => {
-            next(err)
-        })
-})
-router.get('/view/:id', (req, res, next) => {
-    Celebrity.findById(req.params.id)
-        .then(data => {
-            console.log(data)
-            res.render('celebrities/show', data)
-        })
-        .catch(err => {
-            next(err)
-        })
-})
-
+router.get("/", (req, res, next) => {
+  Celebrity.find()
+    .then((data) => {
+      res.render("celebrities/index", { data });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+router.get("/view/:id", (req, res, next) => {
+  Celebrity.findById(req.params.id)
+    .then((data) => {
+      res.render("celebrities/show", data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+router.get("/new", (req, res, next) => {
+  res.render("celebrities/new");
+});
+router.post("/", (req, res, next) => {
+  console.log(req.body);
+  const { name, occupation, catchPhrase } = req.body;
+  Celebrity.create({
+    name,
+    occupation,
+    catchPhrase,
+  })
+    .then(() => {
+      res.redirect("/celebrities");
+    })
+    .catch(() => {
+      res.render("celebrities/new");
+    });
+});
 
 module.exports = router;
