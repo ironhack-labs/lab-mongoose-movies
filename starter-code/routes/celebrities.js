9cb26a0b5a600ae18aaa85a3fec316553a2f8154
anna dodo <anna.dorenskaya@gmail.com>
2020-09-05 16:10:08 +0200

    3rd iteration

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -5,12 +5,23 @@ const Celebrity = require('../models/Celebrity')
 router.get('/', (req, res, next) => {
     Celebrity.find()
         .then(data => {
-            console.log(data)
+            
             res.render('celebrities/index', {data})
         })
         .catch(err => {
             next(err)
         })
 })
+router.get('/view/:id', (req, res, next) => {
+    Celebrity.findById(req.params.id)
+        .then(data => {
+            console.log(data)
+            res.render('celebrities/show', data)
+        })
+        .catch(err => {
+            next(err)
+        })
+})
+
 
 module.exports = router;
