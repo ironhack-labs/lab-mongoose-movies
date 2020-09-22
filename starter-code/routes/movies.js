98a76f7925dc022f6bbcba4919ac93245eeaeee9
Laura d'Aboville <lauradaboville@hotmail.com>
2020-09-22 17:49:14 +0200

    iteration 8 done movies

---
+++ starter-code/routes/movies.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router  = express.Router();
+const moviesModel = require("../models/movies.model");
+
+router.get('/movies', (req, res, next) => {
+    moviesModel.find({}) 
+    .then((dbResult) => {
+      res.render("movies/index", { movies: dbResult });
+    })
+    .catch((error) => {
+      next(error);
+    });
+  });
+  
+  module.exports = router;
+  
