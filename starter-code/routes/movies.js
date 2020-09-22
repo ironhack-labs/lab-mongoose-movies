32bc9781428b29efd8aa1313ea90c2c04267babe
Lisa Poncet <poncet.lisa@gmail.com>
2020-09-22 17:04:43 +0200

    iteration 9 done

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -12,4 +12,13 @@ router.get("/movies", async function (req, res, next) {
   }
 });
 
+router.get("/movies/:id", async function (req, res, next) {
+  try {
+    const movieDocument = await Movie.findById(req.params.id);
+    res.render("movies/details", { movie: movieDocument });
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
