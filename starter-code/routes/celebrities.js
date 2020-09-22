fb45bedccbb9711e3d51fe7bd477a649f370eab7
Bouziane <bey.bouziane@live.fr>
2020-09-22 17:59:31 +0200

    Iteration 3-4 done

---
+++ starter-code/routes/celebrities.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const Celebrity = require("../models/celebrity");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const celebrity = await Celebrity.find();
+    res.render("celebrities", { celebrity });
+  } catch (error) {
+    next(error);
+    return error;
+  }
+});
+
+router.get("/news", async (req, res) => {
+  res.render("celebrities/news");
+});
+
+router.post("/", (req, res) => {
+  console.log(req.body);
+  Celebrity.create(req.body)
+    .then((result) => {
+      console.log(result);
+      res.redirect("/celebrities");
+    })
+    .catch((error) => console.error(error));
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const celebrityID = req.params.id;
+    const dbResult = await Celebrity.findById(celebrityID);
+    res.render("celebrities/show", { celebrity: dbResult });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+module.exports = router;
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -7,14 +7,4 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-router.get("/celebrities", async (req, res, next) => {
-  try {
-    const celebrity = await Celebrity.find();
-    res.render("celebrities", { celebrity });
-  } catch (error) {
-    next(error);
-    return error;
-  }
-});
-
 module.exports = router;
