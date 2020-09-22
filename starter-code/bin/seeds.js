65567e4feec4ddb3c3397bb5012b3097e161f2b6
Marcha <marchaf@gmail.com>
2020-09-22 16:38:06 +0200

    add seed movies

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,41 @@
-toto
+require("dotenv").config();
+const Movies = require("../models/movie");
+const Celebrities = require("../models/celebrity");
+const mongoose = require("mongoose");
+
+const movie = [
+  {
+    title: "The Shawshank Redemption",
+    genre: "Drama",
+    plot: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+  },
+  {
+    title: "The Godfather",
+    genre: "Crime",
+    plot: "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+  },
+  {
+    title: "The Dark Knight",
+    genre: "Action",
+    plot: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+  },
+];
+
+mongoose
+  .connect('mongodb://localhost/starter-code', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    Movies.create(labels)
+    .then((dbResult) => {
+      console.log(dbResult);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
