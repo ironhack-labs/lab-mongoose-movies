004d42d189003f9f9a62d5ef91422dfa7203c820
Raphaelle Olivo <rapholivo@hotmail.fr>
2020-09-22 20:42:44 +0200

    iteration 4 done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -13,13 +13,31 @@ router.get("/celebrities", async (req, res) => {
   }
 });
 
-router.get("/celebrities/:id", async (req, res) => {
-  celebritiesModel
-    .findById(req.params.id)
-    .then((dbRes) => {
-      res.render("celebrities/show", { celebrities: dbRes });
-    })
-    .catch(next);
+router.get("/celebrities/:id", async function (req, res, next) {
+  try {
+    console.log("test");
+    const celebrity = await celebritiesModel.findById(req.params.id);
+    res.render("celebrities/show", { celebrity });
+  } catch (error) {
+    next();
+    console.error(error);
+  }
+});
+
+router.get("/celebrities/new", (req, res, next) => {
+  res.render("celebrities/new");
+});
+
+router.post("/celebrities/new", async function (req, res, next) {
+  try {
+    console.log("before");
+    const celebrity = await celebritiesModel.create(req.body);
+    res.redirect("/celebrities");
+    console.log("after");
+  } catch (error) {
+    next();
+    console.error(error);
+  }
 });
 
 module.exports = router;
--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -1,24 +1,16 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 const moviesModel = require("../models/movies.model");
 
-router.get('/movies', (req, res, next) => {
-    moviesModel.find({}) 
+router.get("/movies", (req, res, next) => {
+  moviesModel
+    .find({})
     .then((dbResult) => {
       res.render("movies/index", { movies: dbResult });
     })
     .catch((error) => {
       next(error);
     });
-  });
-  
-  module.exports = router;
-  
-
-  router.get("/movies/:id", async (req, res) => {
-    moviesModel.findById(req.params.id)
-    .then((dbRes) => {
-        res.render("movies/show", { movies: dbRes });
-      })
-      .catch(next);
 });
+
+module.exports = router;
