2122cff26da85b952646fa2ac2c4667ce6aa91bb
AyloSrd <aylo.srd@gmail.com>
2020-09-22 17:14:24 +0200

    iteration-3 done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -5,7 +5,17 @@ const CelebrityModel = require('../models/Celebrity')
 router.get('/', async function (req, res, next) {
 	try {
 		const celebrities = await CelebrityModel.find()
-		res.render('celebrities/index', {celebrities})
+		res.render('celebrities/index', { celebrities })
+	} catch (error) {
+		next()
+		console.error(error)
+	}
+})
+
+router.get('/:id', async function (req, res, next) {
+	try {
+		const celebrity = await CelebrityModel.findById(req.params.id)
+		res.render('celebrities/show', { celebrity })
 	} catch (error) {
 		next()
 		console.error(error)
