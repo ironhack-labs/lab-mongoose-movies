dba01640966a5b362ba570f1b2b1d2474a72a93f
vincentpro <vincent_pro@hotmail.fr>
2020-09-22 17:24:37 +0200

    ite 11 done

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Movie = require('../models/movies');
 
-router.get('/', (req, res, next) => {
+router.get('/', async(req, res, next) => {
     try {
         const movies = await Movie.find();
         res.render('movies/index', { movies: movies });
@@ -11,5 +11,46 @@ router.get('/', (req, res, next) => {
         console.log(err);
     }
 });
+
+router.get('/:id', async(req, res, next) => {
+    try {
+        const movie = await Movie.findById(req.params.id);
+        res.render('movies/show', { movie: movie });
+    } catch (err) {
+        next();
+        console.log(err);
+    }
+});
+
+router.get('/new', async(req, res, next) => {
+    try {
+        res.render('movies/new');
+    } catch (err) {
+        next();
+        console.log(err);
+    }
+});
+
+router.post('/', async(req, res, next) => {
+    try {
+        await Movie.create(req.body);
+        res.redirect("/movies");
+    } catch (err) {
+        res.redirect("/movies/new");
+        console.log(err);
+    }
+});
+
+router.post('/:id/delete', async(req, res, next) => {
+    try {
+        await Movie.findByIdAndRemove(req.params.id);
+        res.redirect("/movies");
+    } catch (err) {
+        next();
+        console.log(err);
+    }
+});
+
+
 
 module.exports = router;
