935f92b69f1e1cdc16d0c0209363218d0445f58f
JorellLangmar <jorell.langmar@gmail.com>
2020-09-22 17:36:43 +0200

    Iteration 5 done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -5,22 +5,20 @@ const Celebrity = require("../models/celebrity");
 // Get them all
 // prefixed with /albums in app.js (app.use("/albums", albumsRouter)) so "/albums" here
 router.get("/", async (req, res, next) => {
-    try {
-        const allCelebrities = await Celebrity.find()
-        res.render("celebrities/index.hbs", { celebrities: allCelebrities });
-    } catch (error) {
-        next(error)
-    }
+  try {
+    const allCelebrities = await Celebrity.find();
+    res.render("celebrities/index.hbs", { celebrities: allCelebrities });
+  } catch (error) {
+    next(error);
+  }
 });
-
-
 
 // Get one
 // prefixed with /albums in app.js (app.use("/albums", albumsRouter)) so /albums/page/:id here
 router.get("/:id", (req, res, next) => {
-    Celebrity.findById(req.params.id)
+  Celebrity.findById(req.params.id)
     .then((dbRes) => {
-        res.render("celebrities/show.hbs", { celebrity: dbRes });
+      res.render("celebrities/show.hbs", { celebrity: dbRes });
     })
     .catch(next);
 });
@@ -36,19 +34,30 @@ router.post("/:id/edit", (req, res) => {});
 // Get the create form
 // prefixed with /albums in app.js (app.use("/albums", albumsRouter))  /albums/create
 router.get("/new", (req, res) => {
-    res.render("celebrities/new.hbs");
+  res.render("celebrities/new.hbs");
 });
 
 // Listen to the post of the create form
 // prefixed with /albums in app.js (app.use("/albums", albumsRouter)) so /albums/create here
 router.post("/new", async (req, res, next) => {
-    try {
-        const newCelebrity = req.body;
-        const createdCelebrity = await Celebrity.create(newCelebrity);
-        res.redirect("/celebrities");
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const newCelebrity = req.body;
+    const createdCelebrity = await Celebrity.create(newCelebrity);
+    res.redirect("/celebrities");
+  } catch (error) {
+    next(error);
+  }
+});
+
+//Delete part
+router.post("/:id/delete", async (req, res, next) => {
+  try {
+    const removeCelebrity = req.params.id;
+    const deletedCelebrity = await Celebrity.findByIdAndDelete(removeCelebrity);
+    res.redirect("/celebrities");
+  } catch (error) {
+    next(error);
+  }
 });
 
 // Listen to the delete
