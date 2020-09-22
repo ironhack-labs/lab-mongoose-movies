bb7e0c542624797644dc2ed0d55f8922da66b25d
sechs <philippe.buteau@skema.edu>
2020-09-22 16:26:18 +0200

    starter code for tati

---
+++ starter-code/models/celebrityModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const celebritySchema = new Schema({
+  name: String,
+  isBand: Boolean,
+  description: String,
+});
+
+const Celebrity = mongoose.model("Celebrity", celebritySchema);
+
+module.exports = Celebrity;
