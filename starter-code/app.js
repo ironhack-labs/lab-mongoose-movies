9fa43f31e7e3c4dd104a0b4f09dab91629a19a3f
pheno <nicolas.enault@gmail.com>
2020-09-22 18:09:17 +0200

    add nav partial

--- starter-code/app.js
+++ starter-code/app.js
@@ -43,6 +43,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+hbs.registerPartials(__dirname + "/views/partials");
 
 
 
