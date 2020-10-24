e4ca0697c75e688118e380055b314372aa0b9deb
Eric Blair <erbmart7@gmail.com>
2020-10-24 18:26:57 +0200

    mandatory iterations done

---
+++ starter-code/bin/seeds.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Celebrity = require('../models/celebrity');
+
+mongoose.connect(`mongodb://localhost/starter-code`, { useNewUrlParser: true, useUnifiedTopology: true } );
+const celebrities = [
+    {
+        name: "Bad Bunny",
+        occupation: "Singer",
+        catchPhrase: "Bad bunny baby ba ba ba"
+    },
+    {
+        name: "Anuel",
+        occupation: "Singer",
+        catchPhrase: "Real hasta la muerte, BEBESITA"
+    },
+    {
+        name: "Ozuna",
+        occupation: "Singer",
+        catchPhrase: "El negrito de ojos claros"
+    }
+]
+Celebrity.create(celebrities, (err) => {
+    if (err) { throw(err) }
+    console.log(`Created ${celebrities.length} celebrities`)
+    mongoose.connection.close();
+  });
---
+++ starter-code/models/celebrity.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const schema  = new mongoose.Schema ({
+    name: String,
+    occupation: String,
+    catchPhrase: { type: String, required: true },
+});
+const Celebrity = mongoose.model("celebrity", schema);
+module.exports = Celebrity;
