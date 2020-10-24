af2416882af5540e0070e88bc7d5ffb30d8b2cf6
Lexirem <me.vallribera@gmail.com>
2020-10-24 20:13:20 +0200

    iteration 5

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -13,6 +13,33 @@ router.get('/', async (req, res, next) => {
     }
   }); 
 
+  router.get('/new', async (req, res, next) => {
+    res.render('celebrities/new');
+  });
+
+  router.post('/new', (req, res, next) => {
+    const {name, occupation, catchPhrase} = req.body;
+    const newCeleb = new Celebrity({name, occupation, catchPhrase});
+    newCeleb.save()
+    .then( () => {
+      res.redirect('/celebrities')  
+    }) 
+    .catch(error =>{
+      res.render('celebrities/new')
+    }) 
+  });
+
+  router.post('/:id/delete', (req, res, next) => {
+    Celebrity.findByIdAndRemove(req.params.id)
+    .then( () => {
+      res.redirect('/celebrities')  
+    }) 
+    .catch(error =>{
+      next()
+    }) 
+  });
+
+
   router.get('/:id', async (req, res, next) => {
     try{
       let respuesta2 = await Celebrity.findById(req.params.id)
@@ -23,14 +50,6 @@ router.get('/', async (req, res, next) => {
     }
   }); 
 
-  router.get('/new', async (req, res, next) => {
-    try{
-      let respuesta2 = await Celebrity.findById(req.params.id)
-      console.log(respuesta2)
-        res.render('celebrities/new', {respuesta2});
-    }catch(err){
-        console.log('Error while getting the celebrities from the DB: ', err);
-    }
-  }); 
+
 
   module.exports = router;
