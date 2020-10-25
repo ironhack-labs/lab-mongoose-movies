26304b279300450c3a494f54c127b5ef9c04cc84
Carla Navia <carlanavia@MacBook-Pro-de-Carla.local>
2020-10-25 10:50:48 +0100

    iteración 4, 5

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -11,7 +11,7 @@ router.get("/", (req, res, next) => {
   Celebrity.find().then(
     //Almacenamos todo en allCeleb
     function (allCeleb) {
-      console.log("Celebrities", allCeleb);
+      //console.log("Celebrities", allCeleb);
       //renderizamos celebrities/index con los datos de allCeleb
       res.render("celebrities/index", { allCeleb });
     },
@@ -32,6 +32,48 @@ router.get("/", (req, res, next) => {
 //   }
 // });
 
+//Iteration #4: Adding New Celebrities
+//GET: Show a form to create a celebrity
+router.get("/new", (req, res, next) => {
+  res.render("celebrities/new");
+});
+
+//POST: Send the data from the form to this route to create the celebrity and save to the database
+router.post("/", (req, res, next) => {
+  //obtener los datos del formulario y almacenarlos en nuevas variables (desestructurar del **req.body**).
+  const { name, occupation, catchPhrase } = req.body;
+  console.log(req.body);
+  //crear un nuevo Celebrity usando el modelo que importamos.
+  const newCeleb = new Celebrity({ name, occupation, catchPhrase });
+  //almacenar el nuevo celebrity en la base de datos, utilizando el método save()
+  newCeleb.save().then(
+    function (data) {
+      res.redirect("/celebrities");
+    },
+    function (err) {
+      console.log("Something went wrong!", err);
+      res.redirect("celebrities/new");
+    }
+  );
+});
+
+//Iteration #5: Deleting Celebrities
+router.post("/:id/delete", (req, res, next) => {
+  // traemos el id a través de params, y lo metemos en una variable
+  let celebrityId = req.params.id;
+  console.log(celebrityId);
+  Celebrity.findByIdAndRemove(celebrityId).then(
+    //Almacenamos todo en data
+    function (data) {
+      res.redirect("/celebrities");
+    },
+    function (error) {
+      next(error);
+      console.log("Error while getting the books from the DB: ", error);
+    }
+  );
+});
+
 //Iteration #3: The Celebrity Details Page
 router.get("/:id", (req, res, next) => {
   // traemos el id a través de params, y lo metemos en una variable
@@ -46,28 +88,5 @@ router.get("/:id", (req, res, next) => {
     }
   );
 });
-
-//Iteration #4: Adding New Celebrities
-//GET: Show a form to create a celebrity
-router.get("/new", (req, res, next) => {
-  res.render("celebrities/new");
-});
-// //POST: Send the data from the form to this route to create the celebrity and save to the database
-// router.post("/new", (req, res, next) => {
-//   //obtener los datos del formulario y almacenarlos en nuevas variables (desestructurar del **req.body**).
-//   const { name, occupation, catchPhrase } = req.body;
-//   //crear un nuevo Celebrity usando el modelo que importamos.
-//   const newCeleb = new Celebrity({ name, occupation, catchPhrase });
-//   //almacenar el nuevo celebrity en la base de datos, utilizando el método save()
-//   newCeleb.save().then(
-//     function (data) {
-//       res.redirect("/");
-//     },
-//     function (err) {
-//       console.log("Something went wrong!", err);
-//       res.redirect("/new");
-//     }
-//   );
-// });
 
 module.exports = router;
