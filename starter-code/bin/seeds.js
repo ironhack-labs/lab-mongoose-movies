f96dfc444eeaede15622047f918be0c5cc44f6cd
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-11-06 12:27:10 -0600

    3 actors created

---
+++ starter-code/bin/seeds.js
@@ -0,0 +1,23 @@
+
+
+const Celebrity = require('../models/Celebrity.model')
+
+const celebrities = [
+  {
+    name: 'brad pitt',
+    ocupation: 'actor',
+    catchPhrase: "A family is a risky venture, because the greater the love, the greater the loss... That's the trade-off. But I'll take it all"
+  },
+  {
+    name: 'julia roberts',
+    ocupation: 'actor',
+    catchPhrase: "You know it’s love when all you want is that person to be happy, even if you’re not part of their happiness."
+
+  },
+  {
+    name: 'meryl streep',
+    ocupation: 'actor',
+    catchPhrase: "Integrate what you believe in every single area of your life."
+
+  },
+];
