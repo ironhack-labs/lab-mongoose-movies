b7f632e45d38491065a8b73abbc56c24df28af81
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-11-06 16:28:44 -0600

    edited seeds 2

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -3,21 +3,21 @@ const app = express();
 
 const mongoose     = require('mongoose');
 
-mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+// mongoose
+//   .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+//   .then(x => {
+//     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+//
+//   })
+//   .catch(err => {
+//     console.error('Error connecting to mongo', err)
+//   });
 //falta cerrar conexxión
 
 
 const Celebrity = require('../model/Celebrity.js')
 
-var celebrities = [
+const  celebrities = [
   {
     name: 'brad pitt',
     ocupation: 'actor',
