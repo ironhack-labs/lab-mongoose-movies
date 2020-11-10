b1f05300801dde6c5c4dc099f8df72ae535b7bfc
mahavinay <mahavinay28@gmail.com>
2020-11-10 21:28:26 +0100

    iteration 4 completed

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -10,12 +10,27 @@ router.get('/celebrities', (req, res, next) => {
     .catch((err) => next(err))
   });
 
+  router.get('/celebrities/new', (req,res) => {
+    res.render("./celebrities/new")
+    })
+
+    router.post('/celebrities/new', (req,res) => {
+        const {name, occupation, catchPhrase} = req.body
+        const newCel = new Celebrity({name, occupation, catchPhrase})
+        newCel.save()
+        .then(() => res.redirect("/celebrities"))
+        .catch((err) => res.render("./celebrites/new"))
+        })
+
   router.get('/celebrities/:id', (req, res, next) => {
     const { id } = req.params;
     Celebrity.findById(id)
     .then ((celebFromDb) => res.render("./celebrities/show", celebFromDb))
     .catch((err) => next(err))    
   });
+
+
+
 
 
 module.exports = router;
