ae791a783c75bdf01d9f66ba5e4e324bc8d85597
Nina van den Braak <ninavandenbraak@gmail.com>
2020-11-10 17:02:48 +0100

    Lab is finished

--- starter-code/app.js
+++ starter-code/app.js
@@ -56,7 +56,6 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.locals.title = 'Express - Generated with IronGenerator';
 
 
-
 const index = require('./routes/index');
 app.use('/', index);
 
