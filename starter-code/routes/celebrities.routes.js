dd51148a476a39b3998799e5c3e78316d319890c
Héctor Carramiñana <hector.crm.hcca@gmail.com>
2020-11-14 13:59:35 +0100

    actualized

--- starter-code/routes/celebrities.routes.js
+++ starter-code/routes/celebrities.routes.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 const express = require('express')
 const Celebrity = require('../models/celebrity.model')
 const router = express.Router()
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 module.exports = app => {
 
     // Base URLS
