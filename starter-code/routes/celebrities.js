1a6f70778dfe1bc46158d94157df836c28205f04
David luque Alba <dvdlq99@gmail.com>
2020-11-16 09:01:58 +0100

    update

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,6 +1,7 @@
+const { Router } = require('express');
 const express = require('express');
-const router  = express.Router();
 const Celebrity = require('../models/Celebrity');
+const router = require('./index.js')
 
 
 router.get('/celebrities', (req, res, next) => {
@@ -26,12 +27,61 @@ router.get('/celebrities/:id', (req, res, next)=>{
         })
 })
 
+router.get('/celebrities/new', (req, res, next) => {
+    res.render('/celebrities/new')
+})
 
+router.post('/celebrities', (req, res, next) => {
 
+    const newCelebrity = req.body
 
+    Celebrity.create(newCelebrity)
+        .then(() => {
+            res.redirect('/celebrities')
+        })
+        .catch(err => {
+            console.log (`Error while creating the new instance`)
+            res.render('/celebrities/new')
+        })
 
+})
 
+router.post('/celebrities/:id/delete', (req, res, next) => {
+    const celebrityId = req.params.id
+    Celebrity.findByIdAndDelete(celebrityId)
+     .then(() => {
+         res.redirect('/celebrities')
+     })
+     .catch(err => {
+         next.call
+         return err
+     })
+})
 
+router.get('/celebrities/:id/edit', (req, res, next) => {
+    const celebrityId = req.params.id
 
+    Celebrity.findById(celebrityId)
+        .then(celebrity => {
+            res.render('edit', celebrity)
+        })
+        .catch(err => {
+            next.call
+            return err
+        })
 
-module.exports = router;
+})
+
+router.post('/celebrities/:id', (req, res, next) => {
+    const celebrityId = req.body.id
+    const editedCelebrity = req.body
+
+    Celebrity.findByIdAndUpdate(celebrityId, editedCelebrity)
+        .then((result) => {
+            res.redirect(`/celebrities/${celebrityId}`)
+        })
+        .catch(err => {
+            next.call
+            return err
+        })
+})
