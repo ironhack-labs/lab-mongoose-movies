d779b80176fef94d36423692e786fc2b1448eb24
Ashley Vermaas <ashleyvermaas1@gmail.com>
2020-11-21 16:01:20 +0100

    complete iteration 3

--- starter-code/routes/celebrities-routes.js
+++ starter-code/routes/celebrities-routes.js
@@ -5,11 +5,19 @@ const Celebrity = require('../models/celebrity.model');
 router.get('/celebrities', (req, res, next) => {
     Celebrity.find()
     .then((celebritiesFromDB) => {
-        console.log(celebritiesFromDB);
         res.render('celebrities/index', {celebritiesFromDB});
        
     })
     .catch((error) => console.log(`An error occurred: ${error}`));
 });
+
+router.get('/celebrities/:id', (req, res, next) => {
+    Celebrity.findById(req.params.id)
+    .then((celebrityFromDB) => {
+        res.render('celebrities/show', {celebrityFromDB});
+    })
+    .catch((error) => console.log(`An error occurred: ${error}`));
+});
+
 
 module.exports = router;
