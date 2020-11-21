5f0aecf70f6e9e4a2e1f817d6ba241701a09b5e6
Dirk Kelderman <dirkkelderman@gmail.com>
2020-11-21 15:59:21 +0100

    iteration 3

--- starter-code/routes/celebrities.routes.js
+++ starter-code/routes/celebrities.routes.js
@@ -6,8 +6,15 @@ const Celebrity = require('../models/celebrity');
 router.get('/celebrities', (req, res, next) =>{
     Celebrity.find()
     .then((celebrities) => {
-        console.log(' celebs ====>', celebrities)
-        res.render('celebrities/index', {celebrities})
+        res.render('celebrities/index', celebrities)
+    })
+    .catch(err => { console.log(`Error: ${err}`)});
+});
+
+router.get('/celebrities/:id', (req, res, next) =>{
+    Celebrity.findById(req.params.id)
+    .then((celebrities) => {
+        res.render('celebrities/show', {celebrities})
     })
     .catch(err => { console.log(`Error: ${err}`)});
 });
