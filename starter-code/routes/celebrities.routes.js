c3e05767e5af13653f50b5787b9ff981f942d672
Dirk Kelderman <dirkkelderman@gmail.com>
2020-11-23 13:03:13 +0100

    iteration 5

--- starter-code/routes/celebrities.routes.js
+++ starter-code/routes/celebrities.routes.js
@@ -12,20 +12,35 @@ router.get('/celebrities', (req, res, next) =>{
 });
 
 router.get('/celebrities/new', (req, res, next) => {
-    res.render('celebrities/new')
+    res.render('celebrities/new');
 });
 
 router.post('/celebrities/new', (req, res, next) => {
     const {name, occupation, catchPhrase} = req.body;
     Celebrity.create({name, occupation, catchPhrase})
-    .then(res.redirect("/celebrities"))
-    .catch(res.render('celebrities/new'));
+    .then(() => {
+        console.log(`Created new celebrity`);
+        res.redirect('/celebrities');
+    })
+    .catch((err)=> {console.log(err)});
 });
 
 router.get('/celebrities/:id', (req, res, next) =>{
-    Celebrity.findById(req.params.id)
+    const {id} = req.params;
+    Celebrity.findById(id)
     .then((celebrities) => {
         res.render('celebrities/show', {celebrities})
+    })
+    .catch(err => { console.log(`Error: ${err}`)});
+});
+
+
+
+router.post('/celebrities/:id/delete', (req, res, next) => {
+    const {id} = req.params;
+    Celebrity.findByIdAndRemove(id)
+    .then(() => {
+        res.redirect('/celebrities')
     })
     .catch(err => { console.log(`Error: ${err}`)});
 });
