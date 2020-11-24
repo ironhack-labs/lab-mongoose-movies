b00b5514cf2fcb2240c7c16e59bcfbcd2fb9c82b
Ashley Vermaas <ashleyvermaas1@gmail.com>
2020-11-24 13:01:02 +0100

    complete bonus iteration 6

--- starter-code/routes/celebrities-routes.js
+++ starter-code/routes/celebrities-routes.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const Celebrity = require('../models/celebrity.model');
 
 router.get('/celebrities', (req, res, next) => {
     Celebrity.find()
-    .then((celebritiesFromDB) => {
-        res.render('celebrities/index', {celebritiesFromDB});
-       
-    })
-    .catch((error) => console.log(`Could not find the celebrities due to an error: ${error}`));
+        .then((celebritiesFromDB) => {
+            res.render('celebrities/index', {
+                celebritiesFromDB
+            });
+
+        })
+        .catch((error) => console.log(`Could not find the celebrities due to an error: ${error}`));
 });
 
 router.get('/celebrities/new', (req, res, next) => {
@@ -17,31 +19,51 @@ router.get('/celebrities/new', (req, res, next) => {
 
 router.post('/celebrities/new', (req, res, next) => {
     Celebrity.create(req.body)
-    .then((celebrityFromDB) => {
-        res.redirect('/celebrities');
-    })
-    .catch((error) => {
-        res.redirect('/celebrities/new');
-        console.log(`Could not add the celebrity due to an error: ${error}`);
-    });
+        .then((celebrityFromDB) => {
+            res.redirect('/celebrities');
+        })
+        .catch((error) => {
+            res.redirect('/celebrities/new');
+            console.log(`Could not add the celebrity due to an error: ${error}`);
+        });
 });
 
 router.get('/celebrities/:id', (req, res, next) => {
     Celebrity.findById(req.params.id)
-    .then((celebrityFromDB) => {
-        res.render('celebrities/show', {celebrityFromDB});
-    })
-    .catch((error) => console.log(`Could not find the celebrity due to an error: ${error}`));
+        .then((celebrityFromDB) => {
+            res.render('celebrities/show', {
+                celebrityFromDB
+            });
+        })
+        .catch((error) => console.log(`Could not find the celebrity due to an error: ${error}`));
 });
 
 router.post('/celebrities/:id/delete', (req, res, next) => {
     Celebrity.findByIdAndDelete(req.params.id)
-    .then(() => {
-        res.redirect('/celebrities');
-    })
-    .catch((error) => {
-        console.log(`Could not delete the celebrity due to an error: ${error}`);
-    });
+        .then(() => {
+            res.redirect('/celebrities');
+        })
+        .catch((error) => {
+            console.log(`Could not delete the celebrity due to an error: ${error}`);
+        });
+});
+
+router.get('/celebrities/:id/edit', (req, res, next) => {
+    Celebrity.findById(req.params.id)
+        .then((celebrityFromDB) => {
+            res.render('celebrities/edit', celebrityFromDB);
+        })
+        .catch((error) => console.log(`Could not find the celebrity due to an error: ${error}`));
+});
+
+router.post('/celebrities/:id/edit', (req, res, next) => {
+    Celebrity.findByIdAndUpdate(req.params.id, req.body, {
+            new: true
+        })
+        .then(() => {
+            res.redirect('/celebrities');
+        })
+        .catch((error) => console.log(`The celebrity could not be updated due to an error: ${error}`));
 });
 
 module.exports = router;
