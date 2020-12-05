bf15901fc5ef97ae3d9bbe36979d6e97fffd388d
carmen valencia <carmenpilar1990@gmail.com>
2020-12-05 14:27:26 +0100

    1

---
+++ starter-code/bin/seeds.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const Celebrity = require("../models/celebrity.js");
+require("dotenv").config()
+
+const dbOptions = {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+
+const celebValue = [
+    {
+name: "Beyonce",
+occupation: "Singer",
+catchPrase: "Who run the world?",
+    },
+
+    {
+        name: "Mick Jagger",
+        occupation: "Singer",
+        catchPrase: "I can't get no satisfation",
+            },
+
+            {
+                name: "Maradona",
+                occupation: "Ex-footballer",
+                catchPrase: "La pelota no se mancha",
+                    },
+
+];
+//Llamamos el modelo celebrity y creamos/cerramos
+async function seedDb(){
+    try {
+        await mongoose.connect(process.env.MONGODB_URL, dbOptions)
+        const celebrity = await Celebrity.create(celebValue)
+        console.log(celebrity)
+        mongoose.connection.close()
+    }catch(err){
+        console.error(err)
+    }
+}
+seedDb()
+
+//5.Run the seed file with node to seed your database.node bin/seeds.js
+
---
+++ starter-code/models/celebrity.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const celebritySchema = new mongoose.Schema({
+    name: String,
+    occupation: String,
+    catchPhrase: String,
+});
+
+const Celebrity = mongoose.model("Celebrity", celebritySchema)
+module.exports = Celebrity;
