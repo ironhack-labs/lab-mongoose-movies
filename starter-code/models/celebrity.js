97d0919d0d89c11c492034534b839f1328bb5ca3
Mathias Valverde <mathiasvalverde@hotmail.fr>
2020-12-08 20:04:54 +0100

    iteration 1-5

---
+++ starter-code/models/celebrity.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const celebritySchema = new Schema(
+  {
+    name: String,
+    occupation: String,
+    catchPhrase: String,
+  },
+  { timestamps: true }
+);
+const CelebrityModel = mongoose.model("celebrity", celebritySchema);
+
+module.exports = CelebrityModel;
---
+++ starter-code/routes/celebrities.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = new express.Router();
+const CelebrityModel = require("./../models/celebrity");
+
+router.get("/celebrities", async (req, res, next) => {
+  try {
+    res.render("celebrities", { celebrities: await CelebrityModel.find() });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/celebrities/new", async (req, res, next) => {
+  res.render("./celebrities/new");
+});
+
+router.post("/celebrities/new", async (req, res, next) => {
+  try {
+    await CelebrityModel.create(req.body);
+    res.redirect("/celebrities");
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/celebrities/:id", (req, res, next) => {
+  CelebrityModel.findById(req.params.id)
+    .then((celebrityInfo) => {
+      res.render("celebrities/show", celebrityInfo);
+    })
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
+});
+
+router.post("/celebrities/:id/delete", (req, res, next) => {
+  CelebrityModel.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.redirect("/celebrities");
+    })
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
+});
+
+module.exports = router;
