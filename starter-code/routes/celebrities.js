822d777dfedee0509da99574d52ba80122daac5a
Mina Samwail <minasamwail@MacBook-Air-de-Mina.local>
2020-12-09 23:07:04 +0100

    celebrities done Try to do the bonue but can't update

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = require("../app");
+const { update } = require("./../models/Celebrity");
 const router = new express.Router();
 const CelebrityModel = require("./../models/Celebrity");
 
+// get all
 router.get("/celebrities", async (req, res, next) => {
   try {
     const allCelebrities = await CelebrityModel.find();
@@ -17,6 +19,7 @@ router.get("/celebrities/new", async (req, res, next) => {
   await res.render("./celebrities/new");
 });
 
+//show details of one artist
 router.get("/celebrities/:id", async (req, res, next) => {
   try {
     const oneCelebritie = await CelebrityModel.findById(req.params.id);
@@ -26,6 +29,30 @@ router.get("/celebrities/:id", async (req, res, next) => {
   }
 });
 
+//udate one
+router.get("/celebrities/edit/:id", async (req, res, next) => {
+  try {
+    const editCelebrities = await CelebrityModel.findById(req.params.id);
+    res.render("./celebrities/edit", editCelebrities);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/celebrities/edit/:id", async (req, res, next) => {
+  try {
+    const updatedCelebrities = await CelebrityModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.redirect("/celebrities");
+  } catch (err) {
+    next(err);
+  }
+});
+
+// delete one
 router.post("/celebrities/delete/:id", async (req, res, next) => {
   try {
     await CelebrityModel.findByIdAndRemove(req.params.id);
@@ -35,6 +62,7 @@ router.post("/celebrities/delete/:id", async (req, res, next) => {
   }
 });
 
+//create one
 router.post("/celebrities/new", async (req, res, next) => {
   try {
     await CelebrityModel.create(req.body);
