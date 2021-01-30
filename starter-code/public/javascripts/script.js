218b0bfcbff94a3acdf7d553a1d616b418c63c73
Javier Repi <xavirepi@hotmail.com>
2021-01-30 16:55:50 +0100

    complete b(bonus) iteration 6

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -3,3 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+function clicked() {
+  return confirm('Are you sure you want to remove the drone?');
+}
