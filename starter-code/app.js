d5d025a125475bba480b7186880df8cf2f739792
Javier Repi <xavirepi@hotmail.com>
2021-01-30 11:32:51 +0100

    complete iteration 1

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,23 +1,33 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
-
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const process = require('process');
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/starter-code', {
+    useNewUrlParser: true
+  })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
   .catch(err => {
     console.error('Error connecting to mongo', err)
   });
+
+process.on("SIGINT", () => {
+  mongoose.connection
+    .close()
+    .then(() => console.log("Successfully disconnected from the DB"))
+    .catch((e) => console.error("Errro disconnecting from the DB", e))
+    .finally(() => process.exit());
+});
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -27,17 +37,19 @@ const app = express();
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
+  src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -48,7 +60,6 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
-
 
 
 const index = require('./routes/index');
---
+++ starter-code/bin/seeds.js
@@ -0,0 +1,29 @@
+const celebrities = [
+    {
+        name: "Leonardo DiCaprio",
+        ocupation: "Actor",
+        catchPhrase: "My true love was Rose"
+    },
+    {
+        name: "Leo Messio",
+        ocupation: "Athlete",
+        catchPhrase: "I'm the GOAT"
+    },
+    {
+        name: "Lebron James",
+        ocupation: "Athlete",
+        catchPhrase: "Call me King James. And I'm the GOAT too, by the way."
+    }
+]
+
+require('../app')
+const mongoose = require('mongoose')
+const Celebrity = require('../models/Celebrity.model')
+
+Celebrity.create(celebrities)
+    .then(createdCelebs => {
+        console.log(`${createdCelebs.length} celebrities created!`)
+        // mongoose.connection.close()
+    })
+    .catch(error => console.log(`Error while creating a new celebrity: ${error}`))
+    .finally()
