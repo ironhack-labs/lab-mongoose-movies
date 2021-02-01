88c669069fdf0c68e30402fbc51371e32bd44dfd
Discordante <diegocerratos@gmail.com>
2021-02-01 14:29:45 +0100

    Iteration-0 and mongoose conection done

--- starter-code/app.js
+++ starter-code/app.js
@@ -9,20 +9,13 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
-
-mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
-
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
+
+// require database configuration
+require('./configs/db.config');
 
 // Middleware Setup
 app.use(logger('dev'));
