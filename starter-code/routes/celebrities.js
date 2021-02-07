8c93117ed67a582e05b4a89b100de9bdd17fd962
Eduardo García <edugarciasb16@gmail.com>
2021-02-07 01:08:48 +0100

    Iteration 8 done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -22,5 +22,50 @@ router.get('/celebrity/new', (req, res, next) => {
     res.render('celebrities/new')
 })
 
+router.post('/celebrity/new', (req, res, next) => {
+    console.log(req.body)
+    const { name, occupation, catchPhrase, image} = req.body
+
+    Celebrity.create({ name, occupation, catchPhrase, image})
+        .then(celebrityFromDB => console.log(`New celebrity created: ${celebrityFromDB.name}.`))
+        .then(() => res.redirect('/celebrities'))
+        .catch((e) => next(e));
+})
+
+router.post('/celebrity/:id/delete', (req, res, next) => {
+    // Iteration #5: Delete the drone
+    Celebrity.findByIdAndDelete(req.params.id)
+        .then((d) => {
+            console.log(`Celebrity ${d.name} has been eliminated`)
+            res.redirect('/celebrities')
+        })
+        .catch(
+            error =>
+                console.log(`Error while deleting a single celebrity: ${error}`)
+        )
+});
+
+router.get('/celebrity/:id/edit', (req, res, next) => {
+    Celebrity.findById(req.params.id)
+        .then(celebritiesToEdit => {
+            //console.log(droneToEdit
+            res.render('celebrities/edit', celebritiesToEdit)
+        })
+        .catch(e => console.error(`Error while getting a single celebrity for edit: `, e))
+});
+
+router.post('/celebrity/:id/edit', (req, res, next) => {
+    const { id } = req.params;
+    const { name, occupation, catchPhrase, image } = req.body
+    console.log()
+    Celebrity.findByIdAndUpdate(id, { name, occupation, catchPhrase, image }, { new: true })
+        .then(() =>{
+            res.redirect('/celebrities')
+        })
+        .catch(error =>
+            console.log(`Error while updating a single celebrity: ${error}`)
+        );
+});
+
 
 module.exports = router
