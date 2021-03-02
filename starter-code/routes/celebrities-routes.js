b40048c593af44ed699f71a0efd52c2c5dae545f
Magali <fort_magali@hotmail.fr>
2021-03-02 21:23:49 +0100

    add bonus (celebrity details-update-delete) andstandardize display

--- starter-code/routes/celebrities-routes.js
+++ starter-code/routes/celebrities-routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const CelebrityModel = require("./../model/celebrity-model");
 
 
-// add new celebrities:
+// Add new celebrities:
 
 router.get("/new", (req, res) => {
     res.render("celebrities/new-celebrity")
@@ -21,7 +21,7 @@ router.post("/create", (req, res) => {
     })
 })
 
-//Show all celebrities:
+// Show all celebrities:
 
 router.get("/", (req, res) => {
     CelebrityModel.find()
@@ -29,6 +29,36 @@ router.get("/", (req, res) => {
         res.render("celebrities/celebrities", {allCeleb})
     })
     .catch((err) => console.log(err))
+})
+
+// See the details of a specific celebrity
+
+router.get("/:id", (req, res, next) => {
+    CelebrityModel.findById(req.params.id)
+    .then((celeb) => res.render("celebrities/celebrity-details", {celeb}))
+    .catch(next)
+})
+
+// Update existing celebrities
+
+router.get("/:id/edit", (req, res, next) => {
+    CelebrityModel.findByIdAndUpdate(req.params.id)
+    .then((celeb) => res.render("celebrities/edit-celebrities", {celeb}))
+    .catch(next)
+})
+
+router.post("/:id/edit", (req, res, next) => {
+    CelebrityModel.findByIdAndUpdate(req.params.id, req.body, {new:true})
+    .then((celeb) => res.redirect("/celebrities"))
+    .catch(next)
+})
+
+//Delete celebrities
+
+router.post("/:id/delete", (req, res, next) => {
+    CelebrityModel.findByIdAndDelete(req.params.id)
+    .then(() => res.redirect("/celebrities"))
+    .catch(next)
 })
 
 module.exports = router;
