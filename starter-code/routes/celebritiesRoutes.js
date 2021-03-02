cb1de1f6fa86d7b53fab6360ccdb7a889318b357
Anna Lefour <anna.lefour@laposte.net>
2021-03-02 23:27:47 +0100

    It 3.2 done

--- starter-code/routes/celebritiesRoutes.js
+++ starter-code/routes/celebritiesRoutes.js
@@ -9,25 +9,35 @@ const CelebrityModel = require("../models/Celebrity");
 //Celeb get
 
 router.get("/new", (req, res) => {
-    console.log('yo');
-    res.render("celebrities/new-celebrity.hbs");
+  console.log("yo");
+  res.render("celebrities/new-celebrity.hbs");
 });
 
 router.post("/create", async (req, res, next) => {
-    const { name, occupation, catchPhrase } = req.body;
-    console.log('------------- entrée dans router post create yayyyy ---------------');
-    try {
-        console.log('------------- entrée dans TRYYYYYY ---------------');
-        await CelebrityModel.create({
-            name,
-            occupation,
-            catchPhrase
-        })
-        console.log('------------- entrée dans MODEL.CREATE ---------------');
-        res.redirect('/');
-    } catch (err) {
-        next(err);
-    }
+  const { name, occupation, catchPhrase } = req.body;
+  console.log(req.body)
+  try {
+    await CelebrityModel.create({
+      name,
+      occupation,
+      catchPhrase
+    });
+    res.redirect("/");
+  } catch (err) {
+    next(err);
+  }
+});
+
+//It3
+
+router.get("/", (req, res) => {
+  CelebrityModel.find()
+    .then((dbRes) => {
+      console.log(dbRes);
+      res.render("celebrities/celebrities.hbs", { myCelebrities: dbRes });
+      // console.log(myCelebrities);
+    })
+    .catch((err) => res.send(err));
 });
 
 module.exports = router;
