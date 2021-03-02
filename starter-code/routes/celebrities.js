a313e5b1941ab38e594d65e94310596dea42c3d5
Florian Jam <jam.florian@gmail.com>
2021-03-02 22:55:41 +0100

    iteration 6 done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -13,7 +13,7 @@ router.get('/', (req, res, next) => {
 
 
 
-router.get('/:id([a-z0-9]{24})', (req, res, next) => {
+router.get('/:id([a-z0-9]{24})/show', (req, res, next) => {
   CelebrityModel.findById(req.params.id)
   .then((celebrity) => res.render("../views/celebrities/show.hbs", {celebrity}))
   .catch((err) => next(err));
@@ -33,11 +33,24 @@ router.post('/new', (req, res, next) => {
   .catch(() => res.redirect("/celebrities/new"));
 });
 
-router.post('/:id/delete', (req, res, next) => {
+router.post('/:id([a-z0-9]{24})/delete', (req, res, next) => {
   console.log(req.params.id)
   CelebrityModel.findByIdAndRemove(req.params.id)
   .then(() => res.redirect("/celebrities"))
   .catch((err) => next(err))
 });
+
+router.get('/:id([a-z0-9]{24})/edit', (req, res, next) => {
+  CelebrityModel.findById(req.params.id)
+  .then((celebrity) => res.render('../views/celebrities/edit.hbs', {celebrity}))
+  .catch((err) => next(err))
+});
+
+router.post('/:id([a-z0-9]{24})/edited', (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
+  CelebrityModel.findByIdAndUpdate(req.params.id, {name, occupation, catchPhrase})
+  .then(() => res.redirect('/celebrities'))
+  .catch(err => next(err))
+})
 
 module.exports = router;
