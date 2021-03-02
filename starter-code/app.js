0817804aa9eb32d7cfd8e18b4554a64527d4267e
Mackenzie Minch <madameminch@gmail.com>
2021-03-02 22:49:13 +0100

    first draft

--- starter-code/app.js
+++ starter-code/app.js
@@ -32,11 +32,11 @@ app.use(cookieParser());
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
+// app.use(require('node-sass-middleware')({
+//   src:  path.join(__dirname, 'public'),
+//   dest: path.join(__dirname, 'public'),
+//   sourceMap: true
+// }));
       
 
 app.set('views', path.join(__dirname, 'views'));
@@ -51,8 +51,11 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 
 
-const index = require('./routes/index');
-app.use('/', index);
+const indexRouter = require('./routes/index');
+const celebritiesRouter = require('./routes/celebrities');
+
+app.use('/', indexRouter);
+app.use('/', celebritiesRouter);
 
 
 module.exports = app;
