a977ec8a6de2cf4450334c18280c2067071007ac
Thais Hauguel <hauguelthais@gmail.com>
2021-03-02 16:41:49 +0100

    iteration 4, 5, 6

--- starter-code/routes/movies-routes.js
+++ starter-code/routes/movies-routes.js
@@ -4,13 +4,25 @@ const MovieModel = require('../model/movie-model')
 const CelebrityModel = require('../model/celebrity-model')
 
 
-router.get("/create", (req, res, next)=>{
-    CelebrityModel.create
-    res.render('new-movie')
+router.get("/", (req, res, next)=>{
+    MovieModel.find().populate("celebrities")
+    .then((movies)=>res.render('movies/movies', movies ))
+    .catch(next) 
+})
+
+
+router.get("/new", (req, res, next)=>{
+    CelebrityModel.find()
+    .then((celebrities)=>res.render('new-movie', celebrities ))
+    .catch(next) 
 })
 
 
 router.post("/create", (req,res,next)=>{
-    const {title, genre, plot}= req.body
-    MovieModel
+    const {title, genre, plot, cast}= req.body
+    MovieModel.create({title, genre, plot, cast})
+    .then(()=>res.redirect("/movies"))
+    .catch(next)
 })
+
+module.exports = router
