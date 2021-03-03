8e058e00241c334d19d7670ecb3f43da4ada088f
ronronscelestes <julie.plantey@gmail.com>
2021-03-03 08:34:40 +0100

    done bonus

--- starter-code/routes/celebrities-routes.js
+++ starter-code/routes/celebrities-routes.js
@@ -27,5 +27,46 @@ router.post('/create', (req, res, next) => {
     });
 });
 
+//CELEBRITY DETAIL GET
+router.get('/celebrity/:id', async (req, res, next) => {
+    try {
+        const celebrity = await CelebrityModel.findById(req.params.id);
+        res.render('celebrities/celebrity-details', {celebrity});
+    } catch(err) {
+        next(err)
+    }
+})
+
+//CELEBRITY DELETE GET
+router.get('/celebrity/:id/delete', async (req, res, next) => {
+    try {
+        const deleteById = await CelebrityModel.findByIdAndDelete(req.params.id);
+        res.redirect('/celebrities')
+    } catch(err) {
+        next(err)
+    }
+})
+
+//CELEBRITY UPDATE GET
+router.get('/celebrity/:id/edit', async (req, res, next) => {
+    try {
+        const celebrity = await CelebrityModel.findById(req.params.id);
+        res.render("celebrities/edit-celebrity", {celebrity})
+    } catch(err) {
+        next(err)
+    }
+
+})
+
+//CELEBRITY UPDATE POST
+router.post('/celebrity/:id/update', async (req, res, next) => {
+    try {
+        const updateCelebrity = await CelebrityModel.findByIdAndUpdate(req.params.id, req.body);
+        res.redirect('/celebrities')
+    } catch(err) {
+        next(err)
+    }
+
+})
 
 module.exports = router;
