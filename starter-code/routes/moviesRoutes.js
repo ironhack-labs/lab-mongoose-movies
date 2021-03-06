b39ae7720e2036907984bb40d6bb94e16c73859b
Anna Lefour <anna.lefour@laposte.net>
2021-03-06 15:52:55 +0100

    Add a movie select form working

--- starter-code/routes/moviesRoutes.js
+++ starter-code/routes/moviesRoutes.js
@@ -6,14 +6,18 @@ const CelebrityModel = require("../models/Celebrity");
 
 
 router.get('/new', (req, res, next) => {
-    res.render('movies/new-movies');
+    CelebrityModel.find()
+    .then((dbRes) => {
+        res.render('movies/new-movies', {myCelebrities : dbRes})
+    })
+    .catch(err => next(err))
 })
 
 router.post('/create', (req, res, next) => {
     const {title, genre, plot, cast} = req.body
     CelebrityModel.create(req.body)
-    .then(() => {
-        res.redirect('/movies/')
+    .then((dbRes) => {
+        res.redirect('/movies/', {newMovie: dbRes})
     })
     .catch(err => next(err))
 })
