5895e192f247aa5a6954f14ec360f20c68e98890
michaloumen <michelle.lmendonca@gmail.com>
2021-03-23 17:14:54 -0300

    adicionei filmes e deletei

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,68 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Celebrity = require("../models/Celebrity");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/celebrities", async (req, res, next) => {
+  try {
+    const celebrities = await Celebrity.find();
+
+    res.render("celebrities", { celebrities });
+  } catch (error) {
+    next(error);
+    return error;
+  }
+});
+
+router.get("/celebrities/new", (req, res, next) => {
+  res.render("create-celebrity");
+});
+
+router.get("/celebrities/:celebId", async (req, res, next) => {
+  try {
+    const { celebId } = req.params;
+    const celebrity = await Celebrity.findById(celebId);
+
+    res.render("celebrities-detail", celebrity);
+  } catch (error) {
+    next(error);
+    return error;
+  }
+});
+
+router.post("/celebrities", async (req, res, next) => {
+  try {
+    const { name, occupation, catchPhrase } = req.body;
+
+    const newCeleb = new Celebrity({
+      name,
+      occupation,
+      catchPhrase,
+    });
+
+    await newCeleb.save();
+
+    res.redirect("/celebrities");
+  } catch (error) {
+    res.render("create-celebrity", {
+      errorMessage: "Ocorreu algum erro, tente novamente",
+    });
+  }
+});
+
+router.post("/celebrities/:celebId/delete", async (req, res, next) => {
+  try {
+    const { celebId } = req.params;
+    await Celebrity.findByIdAndDelete(celebId);
+    res.redirect("/celebrities");
+  } catch (error) {
+    next(error);
+    return error;
+  }
 });
 
 module.exports = router;
