494b29efbdaa9370b411753b1f6c5ffeee22f771
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2021-03-24 10:42:41 -0300

    add iterations 4 to 6

--- starter-code/models/Celebrity.model.js
+++ starter-code/models/Celebrity.model.js
@@ -5,7 +5,7 @@ const { model, Schema } = mongoose;
 const Celebrity = model('Celebrity', new Schema({
     name: String,
     ocupation: String,
-    catchPhrase: String
+    catchPhrase: {type: String, default: 'None. Makes for the quite type.'}
 }))
 
 module.exports = Celebrity;
--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,20 +1,62 @@
-const router = require('express').Router()
+const router = require('express').Router();
 
-const mongoose = require('mongoose');
 const Celebrity = require('../models/Celebrity.model')
 
-router.get('/celebrities', (req, res, next) => {
+router.get('/', (req, res, next) => {
     Celebrity.find()
     .then(celebrities => res.render('./celebrities/index', { celebrities }))
     .catch(error => next(error))
 })
 
-router.get('/celebrities/:id', (req, res, next) => {
+router.get('/new', (req, res) => res.render('./celebrities/new'))
+
+router.post('/new', (req, res) => {
+    const updatedCelebrity = {
+        name: req.body.name,
+        ocupation: req.body.ocupation
+    }
+
+    if (req.body.catchPhrase) updatedCelebrity.catchPhrase = req.body.catchPhrase;
+
+    Celebrity.create(updatedCelebrity)
+    .then(() => res.redirect('/celebrities'))
+    .catch(() => res.redirect('/new'))
+})
+
+router.get('/:id', (req, res, next) => {
     const { id } = req.params;
 
     Celebrity.findById(id)
-    .then(celebrity => res.render('celebrities/show', { celebrity }))
+    .then(celebrity => res.render('./celebrities/show', { celebrity }))
     .catch(error => next(error))
 })
+
+router.post('/:id', (req, res, next) => {
+    const { id } = req.params;
+
+    Celebrity.findByIdAndUpdate(id, req.body)
+    .then(() => res.redirect('/celebrities'))
+    .catch(error => next(error))
+})
+
+router.get('/:id/edit', (req, res, next) => {
+    const { id } = req.params;
+
+    Celebrity.findById(id)
+    .then(celebrity => res.render('./celebrities/edit', { celebrity }))
+    .catch(err => next(err))
+})
+
+router.post('/:id/delete', (req, res, next) => {
+    const { id } = req.params;
+
+    Celebrity.findByIdAndRemove(id, err => {
+        if (err) return next(err);
+
+        res.redirect('/celebrities');
+    })
+})
+
+
 
 module.exports = router;
