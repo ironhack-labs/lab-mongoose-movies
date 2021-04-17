7003083c82de4330a0be16d6071ad0084011d11b
Silinde87 <paurodriguezmolina@gmail.com>
2021-04-17 20:08:59 +0200

    Iteration 3 done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -2,12 +2,24 @@ const express = require("express");
 const Celebrity = require("../models/Celebrity.model");
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/", (req, res, next) => {
 	Celebrity.find({})
 		.then((celebrities) => {
 			res.render("celebrities/index", { celebrities });
 		})
 		.catch((err) => {
+			next("error"); //Ask about it.
+			console.error(err);
+		});
+});
+
+router.get("/:id", (req, res, next) => {
+	Celebrity.findById(req.params.id)
+		.then((celebrity) => {
+			res.render("celebrities/show", celebrity);
+		})
+		.catch((err) => {
+			next("error"); //Ask about it.
 			console.error(err);
 		});
 });
