cb8c2a6a00b1d0a05ca19f2b4044f24acdbd802a
Bea Avis <bea.avis.p@gmail.com>
2021-04-17 19:41:35 +0200

    Iteration 4 done

--- starter-code/model/Celebrity.model.js
+++ starter-code/model/Celebrity.model.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const celebritySchema = new Schema({
-  name: { type: String, require: true, unique: true },
-  occupation: { type: String, require: true },
-  catchPhrase: { type: String, require: true },
+  name: { type: String, required: true, unique: true },
+  occupation: { type: String, required: true },
+  catchPhrase: { type: String, required: true },
 });
 
 const Celebrity = mongoose.model("Celebrity", celebritySchema);
--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -10,6 +10,18 @@ router.get('/', (req, res, next) => {
     .catch(error => next(error));
 });
 
+router.get('/new', (req, res, next) => {
+    res.render('celebrities/new');
+});
+
+router.post('/', (req, res, next) => {
+    console.log(req.body);
+    const { name, occupation, catchPhrase } = req.body;
+    Celebrity.create({ name, occupation, catchPhrase })
+    .then( () => res.redirect('/celebrities'))
+    .catch(error => res.render('celebrities/new', { error }));
+});
+
 router.get('/:id', (req, res, next) => {
     const { id } = req.params;
     Celebrity.findById(id).then( celebrity => {
