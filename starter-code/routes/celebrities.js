1ba965fdac38c05bb10756441918f0ae9ef09c0b
Silinde87 <paurodriguezmolina@gmail.com>
2021-04-17 21:10:31 +0200

    Iteration 4 done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { render } = require("../app");
 const Celebrity = require("../models/Celebrity.model");
 const router = express.Router();
 
@@ -9,6 +10,23 @@ router.get("/", (req, res, next) => {
 		})
 		.catch((err) => {
 			next("error"); //Ask about it.
+			console.error(err);
+		});
+});
+
+router.get("/new", (req, res, next) => {
+	res.render("celebrities/new");
+});
+
+router.post("/new", (req, res, next) => {
+	const { name, occupation, catchPhrase } = req.body;
+	Celebrity.create({ name, occupation, catchPhrase })
+		.then(() => {
+			res.redirect("/celebrities");
+		})
+		.catch((err) => {
+      //todo: fix error show at hbs
+			res.render("celebrities/new", { err });
 			console.error(err);
 		});
 });
