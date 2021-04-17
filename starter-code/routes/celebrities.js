b88d9dae62823aac5c6348a636cb63ba6357e789
Bea Avis <bea.avis.p@gmail.com>
2021-04-17 19:43:11 +0200

    Format code

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,34 +1,36 @@
-const express = require('express');
-const Celebrity = require('../model/Celebrity.model.js');
+const express = require("express");
+const Celebrity = require("../model/Celebrity.model.js");
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    Celebrity.find({}).then( celebrities => {
-        console.log(celebrities)
-        res.render( 'celebrities/index', { celebrities } );
+router.get("/", (req, res, next) => {
+  Celebrity.find({})
+    .then((celebrities) => {
+      console.log(celebrities);
+      res.render("celebrities/index", { celebrities });
     })
-    .catch(error => next(error));
+    .catch((error) => next(error));
 });
 
-router.get('/new', (req, res, next) => {
-    res.render('celebrities/new');
+router.get("/new", (req, res, next) => {
+  res.render("celebrities/new");
 });
 
-router.post('/', (req, res, next) => {
-    console.log(req.body);
-    const { name, occupation, catchPhrase } = req.body;
-    Celebrity.create({ name, occupation, catchPhrase })
-    .then( () => res.redirect('/celebrities'))
-    .catch(error => res.render('celebrities/new', { error }));
+router.post("/", (req, res, next) => {
+  console.log(req.body);
+  const { name, occupation, catchPhrase } = req.body;
+  Celebrity.create({ name, occupation, catchPhrase })
+    .then(() => res.redirect("/celebrities"))
+    .catch((error) => res.render("celebrities/new", { error }));
 });
 
-router.get('/:id', (req, res, next) => {
-    const { id } = req.params;
-    Celebrity.findById(id).then( celebrity => {
-        console.log(celebrity)
-        res.render( 'celebrities/show', celebrity );
+router.get("/:id", (req, res, next) => {
+  const { id } = req.params;
+  Celebrity.findById(id)
+    .then((celebrity) => {
+      console.log(celebrity);
+      res.render("celebrities/show", celebrity);
     })
-    .catch(error => next(error));
+    .catch((error) => next(error));
 });
 
 module.exports = router;
