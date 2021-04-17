afe4143cd0148bf2b1c7b7e942825e3891f587b5
Bea Avis <bea.avis.p@gmail.com>
2021-04-17 19:50:06 +0200

    Iteration 5 done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -4,10 +4,7 @@ const router = express.Router();
 
 router.get("/", (req, res, next) => {
   Celebrity.find({})
-    .then((celebrities) => {
-      console.log(celebrities);
-      res.render("celebrities/index", { celebrities });
-    })
+    .then((celebrities) => res.render("celebrities/index", { celebrities }))
     .catch((error) => next(error));
 });
 
@@ -16,20 +13,23 @@ router.get("/new", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
-  console.log(req.body);
   const { name, occupation, catchPhrase } = req.body;
   Celebrity.create({ name, occupation, catchPhrase })
     .then(() => res.redirect("/celebrities"))
     .catch((error) => res.render("celebrities/new", { error }));
 });
 
+router.post("/:id/delete", (req, res, next) => {
+  const { id } = req.params;
+  Celebrity.findByIdAndRemove(id)
+    .then(() => res.redirect("/celebrities"))
+    .catch((error) => next(error));
+});
+
 router.get("/:id", (req, res, next) => {
   const { id } = req.params;
   Celebrity.findById(id)
-    .then((celebrity) => {
-      console.log(celebrity);
-      res.render("celebrities/show", celebrity);
-    })
+    .then((celebrity) => res.render("celebrities/show", celebrity))
     .catch((error) => next(error));
 });
 
