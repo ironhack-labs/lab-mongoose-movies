1403718b64ad2832bb7e52fc58af78617ca07658
Bea Avis <bea.avis.p@gmail.com>
2021-04-18 00:18:12 +0200

    Iteration 7 done

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,17 +1,38 @@
-const initialCelebrities = require("../data");
+const initialCelebrities = require("../dataCelebrities");
+const initialMovies = require("../dataMovies");
 const Celebrity = require("../model/Celebrity.model");
+const Movie = require("../model/Movie.model");
 const mongoose = require("mongoose");
 
+// Create initial Celebrities
+// mongoose
+//   .connect("mongodb://localhost/iron-celebrities", { useNewUrlParser: true })
+//   .then((x) => {
+//     console.log(
+//       `Connected to Mongo! Database name: "${x.connections[0].name}" to create the firsts celebs.`
+//     );
+//     return Celebrity.insertMany(initialCelebrities);
+//   })
+//   .then((celebrities) => {
+//     console.log(celebrities.length, "inserted.");
+//     return mongoose.connection.close();
+//   })
+//   .then(() => console.log("Connection closed"))
+//   .catch((err) => {
+//     console.error("Error connecting to mongo", err);
+//   });
+
+// Create initial Movies
 mongoose
   .connect("mongodb://localhost/iron-celebrities", { useNewUrlParser: true })
   .then((x) => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}" to create the firsts celebs.`
     );
-    return Celebrity.insertMany(initialCelebrities);
+    return Movie.insertMany(initialMovies);
   })
-  .then((celebrities) => {
-    console.log(celebrities.length, "inserted.");
+  .then((movies) => {
+    console.log(movies.length, "inserted.");
     return mongoose.connection.close();
   })
   .then(() => console.log("Connection closed"))
