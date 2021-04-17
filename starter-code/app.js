ae3cbb7cbd729c07af31c86ec4c044f7f459a964
Pablo del Barrio <pablodbb@gmail.com>
2021-04-17 13:58:05 +0200

    Test commit 3

--- starter-code/app.js
+++ starter-code/app.js
@@ -51,7 +51,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Express - Generated with IronGenerator 💪🏼';
 
 
 
