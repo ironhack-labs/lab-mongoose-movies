6e6c7d3c9240a1b5f2484113b49a1eb8442b7285
Silinde87 <paurodriguezmolina@gmail.com>
2021-04-18 20:46:09 +0200

    Iteration 10 done

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const Movie = require("../models/Movie.model");
 const router = express.Router();
-const occupations = ["Action", "Adventure", "Comedy", "Fantasy"];
+const genres = ["Action", "Adventure", "Comedy", "Fantasy"];
 
 //Rendering the "movies list" view
 router.get("/", (req, res, next) => {
@@ -15,7 +15,25 @@ router.get("/", (req, res, next) => {
 		});
 });
 
-//Rendering the "Celebrity" view
+//Rendering the "new movie" view
+router.get("/new", (req, res, next) => {
+	res.render("movies/new", { genres });
+});
+
+//Getting the "new movie" values
+router.post("/new", (req, res, next) => {
+	const { title, genre, plot } = req.body;
+	Movie.create({ title, genre, plot })
+		.then(() => {			
+			res.redirect("/movies");
+		})
+		.catch((err) => {
+			res.render("movies/new", { err, genres });
+			console.error(err);
+		});
+});
+
+//Rendering the "Movie" view
 router.get("/:id", (req, res, next) => {
 	Movie.findById(req.params.id)
 		.then((movie) => {
