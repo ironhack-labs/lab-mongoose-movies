8956b60abae4b15d6dc968a1572ed8a64043c562
Tigran Harutyunyan <tigrandeboris@outlook.com>
2021-04-18 21:07:27 +0200

    ADD Iteration 4

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -10,6 +10,22 @@ router.get('/', (req, res) => {
     .catch(error => console.error(error));
 })
 
+router.post('/',(req, res) => {
+    const name = req.body.name;
+    const occupation = req.body.occupation;
+    const catchPhrase = req.body.catchPhrase;
+
+    Celebrity.create({name:name, occupation: occupation, catchPhrase: catchPhrase})
+        .then(() => {
+            res.redirect('/celebrities');
+    })
+    .catch(error => console.error(error));
+})
+
+router.get('/new', (req, res) => {
+    res.render('celebrities/new');
+})
+
 router.get('/:id', (req, res) => {
     const id = req.params.id;
     Celebrity.findById(id)
