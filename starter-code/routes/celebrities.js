a4aaaebb8bf6518fae996ba4ba275d802e2bab7a
Pablo del Barrio <pablodbb@gmail.com>
2021-04-18 16:39:22 +0200

    Iteration 3 done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -11,7 +11,16 @@ router.get('/', (req, res, next) => {
   .catch(error => next(error))
 })
 
-
+router.get('/:id', (req, res, next) => {
+  // const id = req.params.id;
+  const { id } = req.params;
+  // this CONTROLLER is...
+  Celebrity.findOne({ _id: id }) // ... asking for data from the Celebrity MODEL and ...
+    .then(celebrity => {
+      res.render('celebrities/show', { celebrity }); // ... sending a VIEW to the client
+    })
+    .catch(error => next(error))
+})
 
 
 module.exports = router;
