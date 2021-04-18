6622de3b4df45af2b9ab3b9b9be9243e02aab77c
Silinde87 <paurodriguezmolina@gmail.com>
2021-04-18 20:52:18 +0200

    Iteration 12 done

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -45,6 +45,32 @@ router.post("/:id/delete", (req, res, next) => {
 		});
 });
 
+//Rendering the "edit movie" view
+router.get("/:id/edit", (req, res, next) => {
+	Movie.findById(req.params.id)
+		.then((movie) =>{
+			res.render("movies/edit", { movie, genres })
+		})
+		.catch((err) => {
+			next("error"); //Ask about it.
+			console.error(err);
+		});
+});
+
+//Updating 'Movie' and returning to movies list
+router.post("/:id", (req, res, next) => {
+	const { title, genre, plot } = req.body;
+  const { id } = req.params;
+	Movie.findByIdAndUpdate(id, { title, genre, plot })
+		.then(()=>{
+			res.redirect(`/movies/${id}`);
+		})
+		.catch((err) =>{
+			next("error"); //Ask about it.
+			console.error(err);			
+		})
+})
+
 //Rendering the "Movie" view
 router.get("/:id", (req, res, next) => {
 	Movie.findById(req.params.id)
