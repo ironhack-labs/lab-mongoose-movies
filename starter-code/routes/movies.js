a3c4e32a0572468910a27d859cb13502b1f16bbf
QuimRobres <quim.robres@gmail.com>
2021-04-19 08:22:37 +0200

    iteration 9 done

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Celebrity = require("../models/Celebrity.model.js");
 const Movie = require ("../models/Movie.model.js");
 const router = express.Router();
 
@@ -9,6 +10,18 @@ router.get("/", (req, res, next) => {
     res.render("movies/index", { movies })
   })
   .catch((error) => console.error(error));
+})
+
+
+
+//SHOW MOVIE DETAILS
+router.get("/:id", (req, res, next) => {
+  const {id} = req.params;
+  Movie.findById(id)
+  .then((movie) => {
+    res.render("movies/show", movie);
+  })
+  .catch((error) => console.error(error))
 })
 
 module.exports = router;
