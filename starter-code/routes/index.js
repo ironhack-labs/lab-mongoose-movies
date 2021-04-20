e8b78740874b13a5f536c57fb5134a45ae959369
silvia rodriguez <silrodcar@gmail.com>
2021-04-20 14:36:48 +0200

    fixed errors iteration 3

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 
-const Celebrity = require(`../models/celebrity.model.js`)
+const Celebrity = require(`../models/celebrity.model`)
 
 
 /* GET home page */
@@ -9,14 +9,23 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-router.get('/celebrities/index', (req, res, next) => {
+router.get('/celebrities', (req, res, next) => {
   Celebrity.find({})
   .then((result) => {
-      res.render(`celebrities`, {celebrities:result})
+      res.status(200).render(`celebrities/index`, {celebrities:result})
   
   }).catch((err) => {
       console.log(err); 
   }); 
+  })
+
+  router.get("/celebrities/:id", (req, res) => {
+    const {id} = req.params;
+    Celebrity.findById(id)
+    .then((celebrity) => {
+      res.status(200).render("celebrities/show", {celebrity})
+    })
+    .catch((error) => console.error(error))
   })
 
 module.exports = router;
