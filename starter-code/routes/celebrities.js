f6e138c1a76c7c6a94ec7b28cd68e28a45e7d3d3
hisamparker <hisamparker@gmail.com>
2021-05-12 13:55:04 +0200

    #3

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -2,12 +2,22 @@ const express = require('express');
 const router  = express.Router();
 const Celebrity = require("../models/Celebrity.model");
 
-router.get("/celebrities", (req, res, next) => {
+router.get('/celebrities', (req, res, next) => {
   Celebrity.find()
     .then((dbCelebrity) => {
-      res.render("celebrities/index", {celebrities: dbCelebrity });
+      res.render('celebrities/index', {celebrities: dbCelebrity });
     })
     .catch((err) => next(err));
 });
+
+router.get('/celebrities/:id', (req, res, next) => {
+    const {id} = req.params;
+    Celebrity.findById(id)
+    .then(foundCeleb => {
+      console.log(foundCeleb);
+      res.render('celebrities/show', { celeb: foundCeleb });
+    })
+    .catch(e => next(e));
+}) 
 
 module.exports = router;
