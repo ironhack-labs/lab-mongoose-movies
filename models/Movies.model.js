0db70747bd8805c019ef33cfcb7b372232d3dd51
fereber1294 <freyesber@gmail.com>
2021-05-12 23:28:32 -0500

    lab done

---
+++ models/Movies.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const {
+    Schema,
+    model
+} = mongoose;
+
+const celebritySchema = new Schema({
+    title: String,
+    genre: String,
+    plot: String,
+
+});
+
+module.exports = model('Movies', celebritySchema);
