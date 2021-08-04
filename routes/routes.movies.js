38a71cce2f7e6eb176a58dd4edc98f8afb0a78bc
Hannes Polte <hannes.polte@gmx.de>
2021-08-04 09:55:46 +0200

    added the details view and delete button

--- routes/routes.movies.js
+++ routes/routes.movies.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose');
 const Movie = require('../models/movie.model');
 const Celebrity = require('../models/celebrity.model');
 
-
-
 router.get('/movies/list', (req, res) => {
   Celebrity.find().then((allCelebs) => {
     const celebs = allCelebs;
@@ -15,10 +13,28 @@ router.get('/movies/list', (req, res) => {
   });
 });
 
-
-
 router.post('/movies/new', (req, res) => {
   const { title, genre, plot, cast } = req.body;
-  Movie.create({title, genre, plot, cast}).then(()=>{res.redirect('/')});
+  Movie.create({ title, genre, plot, cast }).then(() => {
+    res.redirect('/movies/list');
+  });
 });
+
+router.get('/movies/:id', (req, res) => {
+  const id = req.params.id;
+  Movie.findById(id)
+    .populate('cast')
+    .then((singleMovie) => {
+      res.render('movies/movie-detail', { movie: singleMovie });
+    });
+});
+
+
+router.post('/movies/delete/:id',(req,res)=>{
+  const id= req.params.id;
+  Movie.findByIdAndDelete(id)
+  .then(()=>{
+    res.redirect('/movies/list')
+  })
+})
 module.exports = router;
