871e8862131afdb8909dd964cdb0e9c074c4e52d
franziska pendzialek <franziska.pendzialek@outlook.com>
2021-08-06 08:14:45 +0200

    finish Iteration #4

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -22,4 +22,21 @@ router.get("/celebrities/:id", (req, res, next) => {
       res.redirect("/celebrities");
     });
 });
+
+router.get("celebrities/new", (req, res, next) => {
+  res.render("celebrities/new");
+});
+
+router.post("/celebrities", (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
+  Celebrity.create({ name, occupation, catchPhrase })
+    .then(() => {
+      Celebrity.save().then(res.redirect("/celebrities"));
+    })
+    .catch((err) => {
+      console.log("Error occured while creating the celebrity", err);
+      res.redirect("/celebrities/new");
+    });
+});
+
 module.exports = router;
