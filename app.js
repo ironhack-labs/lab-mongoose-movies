21bbeb0e20f21de4d4d44b8468262399bedd9f91
franziska pendzialek <franziska.pendzialek@outlook.com>
2021-08-07 11:29:25 +0200

    add routes

--- app.js
+++ app.js
@@ -47,4 +47,10 @@ app.locals.title = "Express - Generated with IronGenerator";
 const index = require("./routes/index");
 app.use("/", index);
 
+const celebrityRoutes = require("./routes/celebrities");
+app.use("/", celebrityRoutes);
+
+const movieRoutes = require("./routes/movies");
+app.use("/", movieRoutes);
+
 module.exports = app;
