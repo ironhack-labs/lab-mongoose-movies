96cc854bd901f558b39fa4c77d62746b80868719
Antoine BERNIER <antoine.bernier@gmail.com>
2022-07-04 21:07:42 +0200

    fix: remove node-sass

--- app.js
+++ app.js
@@ -31,12 +31,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
       
 
 app.set('views', path.join(__dirname, 'views'));
